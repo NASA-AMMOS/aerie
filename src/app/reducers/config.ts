3fcd014b0f1ee319712c16b1d210d60b42d7e6ad
Chris Camargo <Christopher.A.Camargo@jpl.nasa.gov>
2018-01-24 10:48:55 -0800

    relative path notation (i thought i changed this everywhere but guess not)

--- src/app/reducers/config.ts
+++ src/app/reducers/config.ts
@@ -8,9 +8,9 @@
  */
 
 import { createSelector, createFeatureSelector } from '@ngrx/store';
-import { ConfigAction } from '../actions/Config';
+import { ConfigAction } from './../actions/Config';
 
-import { environment } from '../../environments/environment';
+import { environment } from './../../environments/environment';
 
 // Config Interface.
 export interface ConfigState {
