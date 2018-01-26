ce6b7408e8ea333b44f69f3fa9ed83da42ba1418
Chris Camargo <Christopher.A.Camargo@jpl.nasa.gov>
2018-01-25 16:48:57 -0800

    naming

---
+++ src/app/effects/source-explorer.effect.ts
@@ -0,0 +1,85 @@
+/**
+ * Copyright 2018, by the California Institute of Technology. ALL RIGHTS RESERVED. United States Government Sponsorship acknowledged.
+ * Any commercial use must be negotiated with the Office of Technology Transfer at the California Institute of Technology.
+ * This software may be subject to U.S. export control laws and regulations.
+ * By accepting this document, the user agrees to comply with all applicable U.S. export laws and regulations.
+ * User has the responsibility to obtain export licenses, or other export authority as may be required
+ * before exporting such information to foreign countries or providing access to foreign persons
+ */
+
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/mergeMap';
+import 'rxjs/add/operator/toArray';
+import 'rxjs/add/operator/withLatestFrom';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Action, Store } from '@ngrx/store';
+import { Effect, Actions } from '@ngrx/effects';
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
+
+import { AppState } from './../../app/store';
+
+import { SourceExplorerActionTypes } from './../actions/source-explorer';
+
+import * as sourceExplorerActions from './../actions/source-explorer';
+
+import {
+  toRavenSources,
+} from './../util/source';
+
+import {
+  toRavenBandData,
+} from './../util/bands';
+
+import {
+  MpsServerGraphData,
+  MpsServerSource,
+  RavenBandData,
+  RavenSource,
+} from './../models';
+
+@Injectable()
+export class SourceExplorerEffects {
+  @Effect()
+  fetchGraphData$: Observable<Action> = this.actions$
+    .ofType<sourceExplorerActions.FetchGraphData>(SourceExplorerActionTypes.FetchGraphData)
+    .withLatestFrom(this.store$)
+    .map(([action, state]) => ({ action, state }))
+    .switchMap(({ state, action }) =>
+      this.http.get<MpsServerGraphData>(action.source.url)
+        .map((graphData: MpsServerGraphData) => toRavenBandData(action.source.id, graphData, state.timeline.bands))
+        .map((bandData: RavenBandData) => new sourceExplorerActions.FetchGraphDataSuccess(action.source, bandData.bands, bandData.bandIdsToPoints))
+        .catch(() => of(new sourceExplorerActions.FetchGraphDataFailure())),
+    );
+
+  @Effect()
+  fetchInitialSources$: Observable<Action> = this.actions$
+    .ofType<sourceExplorerActions.FetchInitialSources>(SourceExplorerActionTypes.FetchInitialSources)
+    .withLatestFrom(this.store$)
+    .map(([action, state]) => state)
+    .switchMap((state: AppState) =>
+      this.http.get<MpsServerSource[]>(`${state.config.baseUrl}/${state.config.baseSourcesUrl}`)
+        .map((mpsServerSources: MpsServerSource[]) => toRavenSources('0', true, mpsServerSources))
+        .map((sources: RavenSource[]) =>  new sourceExplorerActions.FetchInitialSourcesSuccess(sources))
+        .catch(() => of(new sourceExplorerActions.FetchInitialSourcesFailure())),
+    );
+
+  @Effect()
+  fetchSources$: Observable<Action> = this.actions$
+    .ofType<sourceExplorerActions.FetchSources>(SourceExplorerActionTypes.FetchSources)
+    .switchMap(action =>
+      this.http.get<MpsServerSource[]>(action.source.url)
+        .map((mpsServerSources: MpsServerSource[]) => toRavenSources(action.source.id, false, mpsServerSources))
+        .map((sources: RavenSource[]) => new sourceExplorerActions.FetchSourcesSuccess(action.source, sources))
+        .catch(() => of(new sourceExplorerActions.FetchSourcesFailure())),
+    );
+
+  constructor(
+    private http: HttpClient,
+    private actions$: Actions,
+    private store$: Store<AppState>,
+  ) {}
+}
