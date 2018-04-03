611993c1f32fbb6b06151c5ff6c938e9cb3e2df2
Chris Camargo <Christopher.A.Camargo@jpl.nasa.gov>
2018-04-03 11:34:11 -0700

    pipe linting

--- src/app/pipes/map-data-item-key.pipe.ts
+++ src/app/pipes/map-data-item-key.pipe.ts
@@ -1,3 +1,12 @@
+/**
+ * Copyright 2018, by the California Institute of Technology. ALL RIGHTS RESERVED. United States Government Sponsorship acknowledged.
+ * Any commercial use must be negotiated with the Office of Technology Transfer at the California Institute of Technology.
+ * This software may be subject to U.S. export control laws and regulations.
+ * By accepting this document, the user agrees to comply with all applicable U.S. export laws and regulations.
+ * User has the responsibility to obtain export licenses, or other export authority as may be required
+ * before exporting such information to foreign countries or providing access to foreign persons
+ */
+
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
@@ -8,9 +17,9 @@ export class MapKeyPipe implements PipeTransform {
     const keyMap = {
       activityName: 'Activity Name',
       activityType: 'Activity Type',
-      startTime: 'Start Time',
-      endTime: 'End Time',
       duration: 'Duration',
+      endTime: 'End Time',
+      startTime: 'Start Time',
       value: 'Value',
     };
     return keyMap[value] || value;
