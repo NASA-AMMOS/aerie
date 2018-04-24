500a6b46aae46193009161590c02379b6b4f9479
burgos <roxana.gonzalez.burgos@jpl.nasa.gov>
2018-04-24 16:46:56 -0700

    add save state form validation.

--- src/app/components/raven-state-save-dialog/raven-state-save-dialog.component.ts
+++ src/app/components/raven-state-save-dialog/raven-state-save-dialog.component.ts
@@ -17,24 +17,37 @@ import {
   MatDialogRef,
 } from '@angular/material';
 
+import {
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+
 @Component({
   selector: 'raven-state-save-dialog',
   styleUrls: ['./raven-state-save-dialog.component.css'],
   templateUrl: './raven-state-save-dialog.component.html',
 })
 export class RavenStateSaveDialogComponent {
-  name: string;
+  saveStateForm: FormGroup;
 
   constructor(
     public dialogRef: MatDialogRef<RavenStateSaveDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any,
-  ) {}
+  ) {
+    this.saveStateForm = new FormGroup({
+      name: new FormControl('', [
+        Validators.required,
+        Validators.pattern('^([(a-zA-Z0-9\-\_\s)]*){1,30}$'),
+      ]),
+    });
+  }
 
   onCancel() {
     this.dialogRef.close({ save: false });
   }
 
   onSave() {
-    this.dialogRef.close({ name: this.name, save: true });
+    this.dialogRef.close({ name: this.saveStateForm.controls.name.value, save: true });
   }
 }
