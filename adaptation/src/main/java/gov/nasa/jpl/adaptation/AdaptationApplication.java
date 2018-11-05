6c1dbfa0d76205982a22f0abf57304cdbb15d27b
Hector Acosta <hector.r.acosta@jpl.nasa.gov>
2018-11-05 09:41:44 -0800

    adding adaptation project

---
+++ adaptation/src/main/java/gov/nasa/jpl/adaptation/AdaptationApplication.java
@@ -0,0 +1,12 @@
+package gov.nasa.jpl.adaptation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdaptationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdaptationApplication.class, args);
+    }
+}
---
+++ adaptation/src/test/java/gov/nasa/jpl/adaptation/AdaptationApplicationTests.java
@@ -0,0 +1,16 @@
+package gov.nasa.jpl.adaptation;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AdaptationApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
