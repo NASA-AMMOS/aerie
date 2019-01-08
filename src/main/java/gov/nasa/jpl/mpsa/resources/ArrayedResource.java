9c550ba12c48b8d69523e0fd9bd10444e81f3cd3
Zac McLaughlin <zacmclaughlin5@gmail.com>
2019-01-08 15:40:43 -0600

    implemented a builder for arrayed resources

--- src/main/java/gov/nasa/jpl/mpsa/resources/ArrayedResource.java
+++ src/main/java/gov/nasa/jpl/mpsa/resources/ArrayedResource.java
@@ -1,51 +1,41 @@
 package gov.nasa.jpl.mpsa.resources;
-import java.lang.reflect.ParameterizedType;
-import java.lang.reflect.Type;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
-public class ArrayedResource {
+import java.util.*;
+
+public class ArrayedResource<V extends Comparable> {
     // composition relationship with Resource
 
     private HashMap<String, Resource> individualResources;
     private String name;
+    private String[] entries;
 
-    public ArrayedResource(String name, String subsystem, String units, String interpolation, String[] entries) {
-        this.name = name;
+    private ArrayedResource(Builder builder){
+        this.name = builder.name;
+        this.entries = builder.entries;
         this.individualResources = new HashMap<>();
 
-        if(entries == null){
+        if(this.entries == null){
             throw new RuntimeException("Tried to create ArrayedResource with a null object for the list of entries. " +
                     "Make sure that your array gets initialized above the ArrayedResource in its class definition, " +
                     "or from a class that extends ParameterDeclaration");
         }
 
-        for (int i = 0; i < entries.length; i++) {
+        for (int i = 0; i < this.entries.length; i++) {
 
-            Resource resourceInstance = new Resource.Builder(name + "_" + entries[i])
-                    .forSubsystem(subsystem)
-                    .withUnits(units)
-                    .withInterpolation(interpolation)
+            Resource resourceInstance = new Resource.Builder(this.name + "_" + this.entries[i])
+                    .forSubsystem(builder.subsystem)
+                    .withUnits(builder.units)
+                    .withInterpolation(builder.interpolation)
+                    .withMin((V) builder.minimum)
+                    .withMax((V) builder.maximum)
+                    .withAllowedValues(builder.allowedValues)
+                    .isFrozen(builder.frozen)
                     .build();
 
-            individualResources.put(entries[i], resourceInstance);
+            individualResources.put(this.entries[i], resourceInstance);
 
         }
 
-    }
-
-    public ArrayedResource(String name, String subsystem, String units, String[] entries) {
-        this(name, subsystem, units, "constant", entries);
-    }
-
-    public ArrayedResource(String name, String subsystem, String[] entries) {
-        this(name, subsystem, "", entries);
-    }
-
-    public ArrayedResource(String name, String[] entries) {
-        this(name, "", entries);
     }
 
     public Resource get(String index) {
@@ -69,5 +59,69 @@ public class ArrayedResource {
         for (Map.Entry<String, Resource> singleRes : individualResources.entrySet()) {
             resources.addResource(singleRes.getValue());
         }
+    }
+
+    // Sticking with a builder design pattern for a streamlined interface
+    public static class Builder<V extends Comparable> {
+
+        private String name;
+        private String subsystem;
+        private String units;
+        private String interpolation;
+        private Set allowedValues;
+        private V minimum;
+        private V maximum;
+        private String[] entries;
+        private boolean frozen;
+
+        public Builder(String name) {
+            this.name = name;
+            this.entries = new String[]{"0"};
+        }
+
+        public Builder withEntries(String[] entries) {
+            this.entries = entries;
+            return this;
+        }
+
+        public Builder forSubsystem(String subsystem) {
+            this.subsystem = subsystem;
+            return this;
+        }
+
+        public Builder withUnits(String units) {
+            this.units = units;
+            return this;
+        }
+
+        public Builder withInterpolation(String interpolation) {
+            this.interpolation = interpolation;
+            return this;
+        }
+
+        public Builder withAllowedValues(Set allowedValues) {
+            this.allowedValues = allowedValues;
+            return this;
+        }
+
+        public Builder withMin(V minimum) {
+            this.minimum = minimum;
+            return this;
+        }
+
+        public Builder withMax(V maximum) {
+            this.maximum = maximum;
+            return this;
+        }
+
+        public Builder isFrozen(boolean frozen) {
+            this.frozen = frozen;
+            return this;
+        }
+
+        public ArrayedResource build(){
+            return new ArrayedResource(this);
+        }
+
     }
 }
