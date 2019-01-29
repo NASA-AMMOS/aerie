876aa3e7c2b8ebf946cb002a05c21c2552193c2b
taifun <taifun.oreilly@jpl.nasa.gov>
2019-01-29 07:58:29 -0800

    fixed activity layout after activity font size change

--- libs/ctl/ctl.js
+++ libs/ctl/ctl.js
@@ -95,6 +95,7 @@ ActivityBand.prototype.computeNumRowsWaterfallLayout = function() {
 
 ActivityBand.prototype.computeNumRowsCompactLayout = function() {
   var ctx = this.canvas.getContext('2d');
+  ctx.font = this.painter.font;
   var painter = this.painter;
   var start = this.timeAxis.start;
   var end = this.timeAxis.end;
@@ -131,9 +132,9 @@ ActivityBand.prototype.computeNumRowsCompactLayout = function() {
           if(this.painter.autoFit && !this.painter.trimLabel && interval.label !== null) {
             // since we using the viewtime axis, tracks may be computed off the canvas so
             // we need to use the no clamping function to compute the x value
-            var labelX1 = viewTimeAxis.getXFromTimeNoClamping(interval.start) + painter.labelPadding;
+            var labelX1 = viewTimeAxis.getXFromTimeNoClamping(interval.start) + this.painter.labelPadding;
             var labelWidth = ctx.measureText(interval.label).width;
-            prevDrawEnd = Math.max(prevDrawEnd, viewTimeAxis.getTimeFromX(labelX1+labelWidth));
+            prevDrawEnd = Math.max(prevDrawEnd, viewTimeAxis.getTimeFromX(labelX1+labelWidth+this.painter.labelPadding));
           }
         }
       }
