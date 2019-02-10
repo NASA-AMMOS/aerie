a4511b26fa45d0736dde9e91921b92d268549f73
Jonathan Castello <Jonathan.Castello@jpl.caltech.edu>
2019-02-09 17:53:07 -0800

    Add generic <raven-tree-node> component

---
+++ src/app/shared/components/raven-tree-node/raven-tree-node.module.ts
@@ -0,0 +1,34 @@
+/**
+ * Copyright 2018, by the California Institute of Technology. ALL RIGHTS RESERVED. United States Government Sponsorship acknowledged.
+ * Any commercial use must be negotiated with the Office of Technology Transfer at the California Institute of Technology.
+ * This software may be subject to U.S. export control laws and regulations.
+ * By accepting this document, the user agrees to comply with all applicable U.S. export laws and regulations.
+ * User has the responsibility to obtain export licenses, or other export authority as may be required
+ * before exporting such information to foreign countries or providing access to foreign persons
+ */
+
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import {
+  MatButtonModule,
+  MatIconModule,
+  MatMenuModule,
+  MatTooltipModule,
+} from '@angular/material';
+
+import { HierarchicalMenuModule } from '../hierarchical-menu/hierarchical-menu.module';
+import { RavenTreeNodeComponent } from './raven-tree-node.component';
+
+@NgModule({
+  declarations: [RavenTreeNodeComponent],
+  exports: [RavenTreeNodeComponent],
+  imports: [
+    CommonModule,
+    HierarchicalMenuModule,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
+    MatTooltipModule,
+  ],
+})
+export class RavenTreeNodeModule {}
