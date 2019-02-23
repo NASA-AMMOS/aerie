32445a00ce2650f618d29eb5b127e0563ff7b941
Hector Acosta <hector.r.acosta@jpl.nasa.gov>
2019-02-22 16:33:31 -0800

    Fixing the error of a command that should be implemented yet.

--- merlin-cli/src/main/java/gov/nasa/jpl/mpsa/cli/commands/impl/RemoveActivityCommand.java
+++ merlin-cli/src/main/java/gov/nasa/jpl/mpsa/cli/commands/impl/RemoveActivityCommand.java
@@ -1,31 +1,23 @@
 package gov.nasa.jpl.mpsa.cli.commands.impl;
 
-import gov.nasa.jpl.mpsa.activities.ActivityType;
 import gov.nasa.jpl.mpsa.cli.commands.Command;
 import gov.nasa.jpl.mpsa.cli.commands.UndoableCommand;
-import gov.nasa.jpl.mpsa.plan.Plan;
 
 import java.util.UUID;
 
 // this command removes an activity
 public class RemoveActivityCommand implements Command, UndoableCommand {
 
-    private ActivityType activityType;
-    private Plan plan;
 
     public RemoveActivityCommand() {
-        this.plan = Plan.getInstance();
-    }
 
-    public RemoveActivityCommand(ActivityType activityType) {
-        this.activityType = activityType;
+        // TODO: Get an instance of the plan
     }
 
 
     @Override
     public void execute() {
-        // Get the plan
-        removeActivityFromPlan(plan.getId(), activityType.getId());
+        // Get the right instance by ID and remove it from the plan
     }
 
     private void removeActivityFromPlan(String planId, UUID activityId) {
