a9ddfd5fab3b51698b85191260e3fb935aa88810
John Pham <johnphammail@gmail.com>
2019-05-02 10:12:04 -0700

    Add hover tooltips for commands and arguments
    Prevent autocomplete from completing with 1 option
    
    Add on click tooltip
    
    Refactor editor ngAfterViewInit, add onClick tooltip with helper info
    
    Add on hover tooltip for commands
    
    Add theme switch for tooltips
    
    Add tooltip toggle to toolbar
    
    Disable info tooltip from showing on an error
    
    Fix animation for tooltips
    
    Add overflow scroll to tooltip content
    
    Refactor show/hide tooltip logic
    
    Add tooltips for arguments
    
    Add jsdoc for parameter template helper
    
    Update style for hover tooltips to material
    
    Add feedback
    
    Add comment on using setTimeout for tooltip
    
    Add helper function for parameter tooltip
    
    remove type comment

--- nest/src/app/sequencing/components/seq-editor/seq-editor.component.ts
+++ nest/src/app/sequencing/components/seq-editor/seq-editor.component.ts
@@ -25,6 +25,10 @@ import {
   buildMpsLint,
   buildMpsMode,
 } from '../../code-mirror-languages/mps';
+import {
+  getCommandParameterDescriptionTemplate,
+  getCommandParameterHelpTemplate,
+} from '../../code-mirror-languages/mps/helpers';
 import { SeqEditorService } from '../../services/seq-editor.service';
 
 @Component({
@@ -78,6 +82,8 @@ export class SeqEditorComponent implements AfterViewInit, OnChanges {
   autocomplete = false;
   fullscreen = false;
   userTheme = 'dark';
+  tooltip: HTMLElement;
+  showTooltips = false;
 
   constructor(private seqEditorService: SeqEditorService) {}
 
@@ -104,42 +110,10 @@ export class SeqEditorComponent implements AfterViewInit, OnChanges {
   }
 
   ngAfterViewInit() {
-    this.seqEditorService.setEditor(this.editorMount, {
-      autofocus: this.autofocus,
-      extraKeys: {
-        ...this.extraKeys,
-        'Ctrl-R': this.redo.bind(this),
-        'Ctrl-Z': this.undo.bind(this),
-        Esc: this.toggleFullscreen.bind(this),
-      },
-      gutters: this.gutters,
-      lineNumbers: this.lineNumbers,
-      lineWrapping: this.lineWrapping,
-      lint: true,
-      mode: this.mode,
-      theme: this.theme,
-      value: this.value,
-    });
-    if (this.seqEditorService.editor) {
-      // Event is casted to any because CodeMirror type defs are not up to date/do not have a type for it
-      this.seqEditorService.editor.on(
-        'keyup',
-        (cm: CodeMirror.Editor, event: any) => {
-          // Don't open autocomplete menu again if the menu is open
-          // and if user is moving up/down options
-          if (
-            this.autocomplete &&
-            this.seqEditorService.editor &&
-            !cm.state.completeActive &&
-            event.keyCode !== 13 &&
-            event.keyCode !== 40 &&
-            event.keyCode !== 38
-          ) {
-            this.seqEditorService.editor.execCommand('autocomplete');
-          }
-        },
-      );
-    }
+    this.setupEditor();
+    this.setupAutocomplete();
+    this.setupTooltip();
+    this.setupTooltipHandler();
   }
 
   /**
@@ -209,6 +183,198 @@ export class SeqEditorComponent implements AfterViewInit, OnChanges {
           this.seqEditorService.editor.setOption('theme', 'monokai');
       }
       this.seqEditorService.editor.focus();
+    }
+  }
+
+  toggleTooltips() {
+    if (this.seqEditorService.editor) {
+      this.showTooltips = !this.showTooltips;
+      this.seqEditorService.editor.focus();
+    }
+  }
+
+  setupEditor() {
+    this.seqEditorService.setEditor(this.editorMount, {
+      autofocus: this.autofocus,
+      extraKeys: {
+        ...this.extraKeys,
+        'Ctrl-R': this.redo.bind(this),
+        'Ctrl-Z': this.undo.bind(this),
+        Esc: this.toggleFullscreen.bind(this),
+      },
+      gutters: this.gutters,
+      lineNumbers: this.lineNumbers,
+      lineWrapping: this.lineWrapping,
+      lint: true,
+      mode: this.mode,
+      theme: this.theme,
+      value: this.value,
+    });
+  }
+
+  setupAutocomplete() {
+    if (this.seqEditorService.editor) {
+      // Disables the autocomplete from automatically autocompleting when there is only 1 option
+      this.seqEditorService.editor.setOption('hintOptions', {
+        completeSingle: false,
+      });
+
+      this.seqEditorService.editor.on(
+        'keyup',
+        (cm: CodeMirror.Editor, event: KeyboardEvent) => {
+          // Don't open autocomplete menu again if the menu is open
+          // and if user is moving up/down options
+          if (
+            this.autocomplete &&
+            this.seqEditorService.editor &&
+            !cm.state.completeActive &&
+            event.key !== 'Enter' &&
+            event.key !== 'ArrowDown' &&
+            event.key !== 'ArrowUp'
+          ) {
+            this.seqEditorService.editor.execCommand('autocomplete');
+          }
+        },
+      );
+    }
+  }
+
+  setupTooltip() {
+    this.tooltip = document.createElement('div');
+    this.tooltip.className = 'CodeMirror-tooltip';
+    document.body.appendChild(this.tooltip);
+    this.tooltip.addEventListener('mouseleave', () => {
+      this.showTooltip(false);
+    });
+  }
+
+  setupTooltipHandler() {
+    if (this.seqEditorService.editor) {
+      this.seqEditorService.editor
+        .getWrapperElement()
+        .addEventListener('mousemove', (e: MouseEvent) => {
+          if (
+            this.commandsByName &&
+            this.showTooltips &&
+            this.seqEditorService.editor
+          ) {
+            // Used to give a tolerance for the mouse to be nearby the text, mouse doesn't have to be exactly on the text
+            const nearby = [0, 0, 0, 5];
+            // tslint:disable-next-line: deprecation
+            const node = (e.target || e.srcElement) as HTMLElement;
+            const text = node.innerText || node.textContent;
+            const cm = this.seqEditorService.editor;
+            let target;
+
+            // Check nearby positions to see if text is nearby mouse
+            for (let i = 0; i < nearby.length; i += 2) {
+              const pos = cm.coordsChar({
+                left: e.clientX + nearby[i],
+                top: e.clientY + nearby[i + 1],
+              });
+
+              // CodeMirror token that contains the context
+              const token = cm.getTokenAt(pos);
+
+              if (token && token.string === text) {
+                target = {
+                  pos,
+                  token,
+                };
+
+                node.addEventListener(
+                  'mouseleave',
+                  (mouseLeaveEvent: MouseEvent) => {
+                    const hoverTarget = mouseLeaveEvent.relatedTarget as HTMLElement;
+
+                    if (hoverTarget.className !== 'CodeMirror-tooltip') {
+                      this.showTooltip(false);
+                      node.removeEventListener('mouseleave', () => {});
+                    }
+                  },
+                );
+                break;
+              }
+            }
+
+            if (target && target.token.string !== '') {
+              const commandName = target.token.string;
+              const commandObject = this.commandsByName[commandName];
+
+              if (commandObject) {
+                // If the mouse is on a command
+                const tooltipMessage = getCommandParameterHelpTemplate(
+                  commandName,
+                  this.commandsByName,
+                );
+                this.tooltip.innerHTML = tooltipMessage;
+                this.positionTooltip(node);
+
+                this.showTooltip(true);
+              } else if (target.token.string !== '') {
+                // If the mouse is on an argument
+                // CodeMirror indexes line numbers starting at 0, need to offset
+                const hoveredParameter = target.token.string;
+
+                const currentLine = target.pos.line + 1;
+                const lineTokens = cm.getLineTokens(currentLine);
+
+                const newTooltipContent = getCommandParameterDescriptionTemplate(
+                  lineTokens[0].string,
+                  hoveredParameter,
+                  lineTokens,
+                  this.commandsByName,
+                );
+
+                // Only update the content if it's different than the current content
+                // Handles the case where the user is moving their mouse over the same token
+                if (this.tooltip.innerHTML !== newTooltipContent) {
+                  this.tooltip.innerHTML = newTooltipContent;
+                  this.positionTooltip(node);
+                  this.showTooltip(true);
+                }
+              } else {
+                // If the mouse is on blank space
+                this.showTooltip(false);
+              }
+            }
+          }
+        });
+    }
+  }
+
+  /**
+   * Used to position the hover tooltips near the mouse
+   */
+  positionTooltip(node: HTMLElement) {
+    const target_rect = node.getBoundingClientRect();
+    const tooltip_rect = this.tooltip.getBoundingClientRect();
+
+    if (tooltip_rect.height <= target_rect.top) {
+      this.tooltip.style.top =
+        target_rect.top - tooltip_rect.height + 20 + 'px';
+    } else {
+      this.tooltip.style.top = target_rect.bottom + 'px';
+    }
+    this.tooltip.style.left = target_rect.left + 'px';
+  }
+
+  /**
+   * Used to show/hide the hover tooltips
+   * We are using setTimeout to remove the tooltip from the DOM so
+   * it doesn't block MouseEvents for displaying tooltips for other tokens
+   */
+  private showTooltip(state: boolean) {
+    if (state) {
+      this.tooltip.style.opacity = '1';
+      setTimeout(() => {
+        this.tooltip.style.display = 'block';
+      }, 250);
+    } else {
+      this.tooltip.style.opacity = '0';
+      setTimeout(() => {
+        this.tooltip.style.display = 'none';
+      }, 250);
     }
   }
 }
