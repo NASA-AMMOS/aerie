c6da2f895c1e4c2894243b3a44f9e818c805de88
John Pham <johnphammail@gmail.com>
2019-06-13 14:50:18 -0700

    Update paths

--- command_dictionary_normalizer/src/index.ts
+++ command_dictionary_normalizer/src/index.ts
@@ -3,4 +3,5 @@ import Server from "./common/server";
 import routes from "./routes";
 
 const port = parseInt(process.env.PORT || "3000", 10);
+
 export default new Server().router(routes).listen(port);
---
+++ command_dictionary_normalizer/test/europa.controller.ts
@@ -0,0 +1,6 @@
+import { expect } from "chai";
+import "mocha";
+import request from "supertest";
+import Server from "../src";
+
+describe("Europa", () => {});
