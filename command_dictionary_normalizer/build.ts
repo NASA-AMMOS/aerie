f5529cc8562f8054eff882d35c95bc7f3827dae1
John Pham <johnphammail@gmail.com>
2019-06-19 13:38:49 -0700

    Remove unneeded dependencies

--- command_dictionary_normalizer/build.ts
+++
@@ -1,9 +0,0 @@
-import s from 'shelljs';
-const config = require('./tsconfig.json');
-const outDir = config.compilerOptions.outDir;
-
-s.rm('-rf', outDir);
-s.mkdir(outDir);
-s.cp('.env', `${outDir}/.env`);
-s.mkdir('-p', `${outDir}/common/swagger`);
-s.cp('src/common/api.yml', `${outDir}/common/api.yml`);
