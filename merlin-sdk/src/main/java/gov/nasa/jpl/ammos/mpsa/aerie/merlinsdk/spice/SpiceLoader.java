830af4c32a23b5afd5b0ad1e80dafe1c030b018d
Jonathan Castello <castello@jpl.caltech.edu>
2019-07-16 12:43:59 -0700

    [refactor] SpiceLoader cleanups (#233)
    * Clean up SpiceLoader to use some modern JDK 9 amenities
    
    * Move SPICE libraries into a better-scoped resource location

--- merlin-sdk/src/main/java/gov/nasa/jpl/ammos/mpsa/aerie/merlinsdk/spice/SpiceLoader.java
+++ merlin-sdk/src/main/java/gov/nasa/jpl/ammos/mpsa/aerie/merlinsdk/spice/SpiceLoader.java
@@ -1,82 +1,62 @@
 package gov.nasa.jpl.ammos.mpsa.aerie.merlinsdk.spice;
 
-import org.apache.commons.math3.complex.Quaternion;
-import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
-import spice.basic.CSPICE;
-import spice.basic.SpiceErrorException;
-
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.util.ArrayList;
-import java.util.List;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 
 // Inspired by:
 // https://stackoverflow.com/questions/12036607/bundle-native-dependencies-in-runnable-jar-with-maven
-
-public class SpiceLoader {
+public final class SpiceLoader {
+    private SpiceLoader() {}
 
     public static void loadSpice() {
-        String library = "JNISpice";
-        try {
-            System.load(saveLibrary(library));
-        } catch (IOException e) {
-            System.loadLibrary(library);
+        final String library = "JNISpice";
+
+        // Attempt to load our copy of JNISpice via `System.load`.
+        try (final InputStream in = getResourceByOS().openStream()) {
+            // Copy the JNISpice library to a temporary location on-disk, then load it into the JVM.
+            final Path file = Files.createTempFile(library + "-", ".tmp");
+            deleteFileOnExit(file);
+
+            Files.copy(in, file, StandardCopyOption.REPLACE_EXISTING);
+
+            System.load(file.toString());
+            return;
+        } catch (final IOException ex) {
+            // Swallow the error; fall through and try a different approach.
         }
+
+        // Attempt to load a host-installed copy of JNISpice via `System.loadLibrary`.
+        System.loadLibrary(library);
     }
 
-    public static String getLibraryNameByOS() {
-        String osName = System.getProperty("os.name").toLowerCase();
-        if (osName.startsWith("win")) {
-            return "JNISpice.dll";
-        } else if (osName.startsWith("linux")) {
-            return "libJNISpice.so";
-        } else if (osName.startsWith("mac")) {
-            return "libJNISpice.jnilib";
-        } else {
-            throw new UnsupportedOperationException("Platform " + osName + " is not supported by JNI Spice.");
+    private static URL getResourceByOS() {
+        final String resourcePath;
+        {
+            final String osName = System.getProperty("os.name").toLowerCase();
+            if (osName.startsWith("win")) {
+                resourcePath = "/gov/nasa/jpl/ammos/mpsa/aerie/merlinsdk/spice/JNISpice.dll";
+            } else if (osName.startsWith("linux")) {
+                resourcePath = "/gov/nasa/jpl/ammos/mpsa/aerie/merlinsdk/spice/libJNISpice.so";
+            } else if (osName.startsWith("mac")) {
+                resourcePath = "/gov/nasa/jpl/ammos/mpsa/aerie/merlinsdk/spice/libJNISpice.jnilib";
+            } else {
+                throw new UnsupportedOperationException("Platform " + osName + " is not supported by JNISpice.");
+            }
         }
+
+        return SpiceLoader.class.getResource(resourcePath);
     }
 
-    private static String saveLibrary(String library) throws IOException {
-        InputStream in = null;
-        OutputStream out = null;
-
-        try {
-            String libraryName = getLibraryNameByOS();
-            in = SpiceLoader.class.getResourceAsStream("/" + libraryName);
-            String tmpDirName = System.getProperty("java.io.tmpdir");
-            File tmpDir = new File(tmpDirName);
-            if (!tmpDir.exists()) {
-                tmpDir.mkdir();
+    private static void deleteFileOnExit(final Path path) {
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            try {
+                Files.delete(path);
+            } catch (final IOException ex) {
             }
-            File file = File.createTempFile(library + "-", ".tmp", tmpDir);
-            // Clean up the file when exiting
-            file.deleteOnExit();
-            out = new FileOutputStream(file);
-
-            int cnt;
-            byte buf[] = new byte[16 * 1024];
-            // copy until done.
-            while ((cnt = in.read(buf)) >= 1) {
-                out.write(buf, 0, cnt);
-            }
-            return file.getAbsolutePath();
-        } finally {
-            if (in != null) {
-                try {
-                    in.close();
-                } catch (IOException ignore) {
-                }
-            }
-            if (out != null) {
-                try {
-                    out.close();
-                } catch (IOException ignore) {
-                }
-            }
-        }
+        }));
     }
 }
