fed1a5053449e4528000f8e4f888b8c621823cff
Gavin Martin <gavin.c.martin@jpl.nasa.gov>
2019-08-21 20:20:50 -0700

    more javadocs

--- merlin-sdk/src/main/java/gov/nasa/jpl/ammos/mpsa/aerie/merlinsdk/engine/PendingEventQueue.java
+++ merlin-sdk/src/main/java/gov/nasa/jpl/ammos/mpsa/aerie/merlinsdk/engine/PendingEventQueue.java
@@ -6,11 +6,21 @@ import gov.nasa.jpl.ammos.mpsa.aerie.merlinsdk.activities.ActivityThread;
 import gov.nasa.jpl.ammos.mpsa.aerie.merlinsdk.states.StateContainer;
 import gov.nasa.jpl.ammos.mpsa.aerie.merlinsdk.time.Time;
 
+/**
+ * A priority queue of activity threads organized by event time
+ * 
+ * @param <T> the type of the adapter-provided state index structure
+ */
 public class PendingEventQueue<T extends StateContainer> extends PriorityQueue<ActivityThread<T>> {
 
     // FIXME: what is this??
     private static final long serialVersionUID = 1L;
     
+    /**
+     * Returns the event time of the first element in the queue
+     *
+     * @return the event time of the first element in the queue
+     */
     public Time getNextEventTime() {
         return this.peek().getEventTime();
     }
--- merlin-sdk/src/main/java/gov/nasa/jpl/ammos/mpsa/aerie/merlinsdk/states/StateContainer.java
+++ merlin-sdk/src/main/java/gov/nasa/jpl/ammos/mpsa/aerie/merlinsdk/states/StateContainer.java
@@ -2,6 +2,15 @@ package gov.nasa.jpl.ammos.mpsa.aerie.merlinsdk.states;
 
 import java.util.List;
 
+/**
+ * An adaptation-specific state index structure
+ */
 public interface StateContainer {
+
+    /**
+     * Returns a list of all of the state objects in the index structure
+     * 
+     * @return a list of all states within the structure
+     */
     public List<SettableState<?>> getStateList();
 }
