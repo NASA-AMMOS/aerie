3d9ab439ab219c6bcf5ed4daea81bd66fb379627
taifun <taifun.oreilly@jpl.nasa.gov>
2019-08-23 12:37:07 -0700

    format

--- nest/src/app/raven/components/raven-table/raven-table.module.ts
+++ nest/src/app/raven/components/raven-table/raven-table.module.ts
@@ -9,7 +9,7 @@
 
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {
   MatButtonModule,
   MatCardModule,
