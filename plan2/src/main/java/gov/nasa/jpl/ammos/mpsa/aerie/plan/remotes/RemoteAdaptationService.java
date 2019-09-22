c2d05cdbda152b47d4ad306a4efaef7366adcb18
Jonathan Castello <Jonathan.Castello@jpl.caltech.edu>
2019-09-22 10:21:20 -0700

    Remove Adaptation class

--- plan2/src/main/java/gov/nasa/jpl/ammos/mpsa/aerie/plan/remotes/RemoteAdaptationService.java
+++ plan2/src/main/java/gov/nasa/jpl/ammos/mpsa/aerie/plan/remotes/RemoteAdaptationService.java
@@ -1,12 +1,13 @@
 package gov.nasa.jpl.ammos.mpsa.aerie.plan.remotes;
 
+import gov.nasa.jpl.ammos.mpsa.aerie.plan.models.ActivityType;
 import org.apache.commons.lang3.NotImplementedException;
 
-import java.util.Optional;
+import java.util.Map;
 
 public final class RemoteAdaptationService implements AdaptationService {
   @Override
-  public Optional<Adaptation> getAdaptationById(String adaptationId) {
+  public Map<String, ActivityType> getActivityTypes(final String adaptationId) {
     throw new NotImplementedException("TODO: communicate with remote service");
   }
 }
