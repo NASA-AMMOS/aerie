561a9689f8072f7ab201684822a919e9641b2375
Chris Camargo <Christopher.A.Camargo@jpl.nasa.gov>
2019-11-04 17:13:34 -0800

    dev/e2e (#352)
    1. Add simple e2e verification tests for the adaptations page

--- merlin-ui/e2e/src/app.e2e-spec.ts
+++ merlin-ui/e2e/src/app.e2e-spec.ts
@@ -1,16 +1,74 @@
 import { browser, logging } from 'protractor';
-import { AppPage } from './app.po';
+import { AppPage, testAdaptation } from './app.po';
+import { Elements } from './elements.po';
 
-describe('workspace-project App', () => {
+/**
+ * @note These tests assume a clean/empty database.
+ */
+describe('merlin-ui App', () => {
   let page: AppPage;
 
   beforeEach(() => {
     page = new AppPage();
   });
 
-  it('should pass', () => {
-    page.navigateTo();
-    expect(true).toEqual(true);
+  describe('adaptations page', () => {
+    it(`should navigate to the adaptations page`, () => {
+      page.navigateTo('adaptations');
+    });
+
+    it(`the no adaptations message should be present, and the adaptation table should NOT be present`, () => {
+      expect(Elements.noAdaptationsMessage.isPresent()).toBe(true);
+      expect(Elements.adaptationsTable.isPresent()).toBe(false);
+    });
+
+    it(`create adaptation form 'Create' button should be disabled`, () => {
+      expect(Elements.createAdaptationFormCreateButton.isEnabled()).toBe(false);
+    });
+
+    it(`filling all the create adaptation form fields should enable the 'Create' button`, () => {
+      Elements.createAdaptationFormName.sendKeys(testAdaptation.name);
+      Elements.createAdaptationFormVersion.sendKeys(testAdaptation.version);
+      Elements.createAdaptationFormMission.sendKeys(testAdaptation.mission);
+      Elements.createAdaptationFormOwner.sendKeys(testAdaptation.owner);
+      Elements.createAdaptationFormFile.sendKeys(testAdaptation.filePath);
+      expect(Elements.createAdaptationFormCreateButton.isEnabled()).toBe(true);
+    });
+
+    it(`should click the 'Create' button on the create adaptation form`, () => {
+      Elements.createAdaptationFormCreateButton.click();
+    });
+
+    it('the no adaptations message should NOT be present, and the adaptation table should be present', () => {
+      expect(Elements.noAdaptationsMessage.isPresent()).toBe(false);
+      expect(Elements.adaptationsTable.isPresent()).toBe(true);
+    });
+
+    it('trying to delete an adaptation should show a confirm dialog', () => {
+      Elements.adaptationMenu.click();
+      Elements.adaptationMenuDelete.click();
+      expect(Elements.confirmDialogConfirmButton.isPresent()).toBe(true);
+      expect(Elements.confirmDialogCancelButton.isPresent()).toBe(true);
+    });
+
+    it(`after clicking 'No' on the confirm dialog the adaptation table should still be present`, () => {
+      Elements.confirmDialogCancelButton.click();
+      expect(Elements.adaptationsTable.isPresent()).toBe(true);
+    });
+
+    it(`after clicking 'Yes' on the confirm dialog the no adaptations message should be present`, () => {
+      Elements.adaptationMenu.click();
+      browser.sleep(1000); // Make sure there are no overlays (snack-bar or dialogs).
+      Elements.adaptationMenuDelete.click();
+      Elements.confirmDialogConfirmButton.click();
+      expect(Elements.noAdaptationsMessage.isPresent()).toBe(true);
+    });
+  });
+
+  describe('plans page', () => {
+    it(`should navigate to the plans page`, () => {
+      page.navigateTo('plans');
+    });
   });
 
   afterEach(async () => {
--- merlin-ui/e2e/src/app.po.ts
+++ merlin-ui/e2e/src/app.po.ts
@@ -1,7 +1,30 @@
-import { browser } from 'protractor';
+import { browser, ElementFinder, ExpectedConditions } from 'protractor';
+
+export const testAdaptation = {
+  name: 'test_banananation',
+  version: 'test_1.0.0',
+  mission: 'test_clipper',
+  owner: 'test_ccamargo',
+  filePath:
+    '/Users/ccamargo/Projects/aerie/banananation/target/banananation-1.0-SNAPSHOT.jar',
+};
 
 export class AppPage {
-  navigateTo() {
-    return browser.get(browser.baseUrl) as Promise<any>;
+  click(el: ElementFinder): void {
+    const isClickable = ExpectedConditions.elementToBeClickable(el);
+    browser.wait(isClickable, 5000);
+    browser
+      .actions()
+      .click(el)
+      .perform();
+  }
+
+  navigateTo(path: string): Promise<any> {
+    return browser.get(`${browser.baseUrl}/#/${path}`) as Promise<any>;
+  }
+
+  waitTilClickable(el: ElementFinder, timeout: number = 5000): void {
+    const isClickable = ExpectedConditions.elementToBeClickable(el);
+    browser.wait(isClickable, timeout);
   }
 }
