90da1e548ae6b9fc1480db7d5f1f073c12764e52
Jonathan Castello <Jonathan.Castello@jpl.caltech.edu>
2019-11-05 09:32:30 -0800

    Implement a ParameterMapper for each primitive type in Java

---
+++ merlin-sdk/src/main/java/gov/nasa/jpl/ammos/mpsa/aerie/merlinsdk/typemappers/DoubleParameterMapper.java
@@ -0,0 +1,25 @@
+package gov.nasa.jpl.ammos.mpsa.aerie.merlinsdk.typemappers;
+
+import gov.nasa.jpl.ammos.mpsa.aerie.merlinsdk.activities.representation.ParameterSchema;
+import gov.nasa.jpl.ammos.mpsa.aerie.merlinsdk.activities.representation.SerializedParameter;
+import gov.nasa.jpl.ammos.mpsa.aerie.merlinsdk.utilities.Result;
+
+public final class DoubleParameterMapper implements ParameterMapper<Double> {
+  @Override
+  public ParameterSchema getParameterSchema() {
+    return ParameterSchema.DOUBLE;
+  }
+
+  @Override
+  public Result<Double, String> deserializeParameter(final SerializedParameter serializedParameter) {
+    return serializedParameter
+        .asReal()
+        .map(Result::<Double, String>success)
+        .orElseGet(() -> Result.failure("Expected real number, got " + serializedParameter.toString()));
+  }
+
+  @Override
+  public SerializedParameter serializeParameter(final Double parameter) {
+    return SerializedParameter.of(parameter);
+  }
+}
