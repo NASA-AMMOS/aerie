10f39cfc0d9ff0eea40cc0d18cf029be377793fa
Meghana Kumar <meghana.n.kumar@jpl.nasa.gov>
2019-11-06 16:52:37 -0800

    GNCStates returns a list

--- merlin-multimission-models/src/main/java/gov/nasa/jpl/ammos/mpsa/aerie/merlinmultimissionmodels/gnc/GNCStates.java
+++ merlin-multimission-models/src/main/java/gov/nasa/jpl/ammos/mpsa/aerie/merlinmultimissionmodels/gnc/GNCStates.java
@@ -5,7 +5,6 @@ import gov.nasa.jpl.ammos.mpsa.aerie.merlinsdk.states.StateContainer;
 import gov.nasa.jpl.ammos.mpsa.aerie.merlinsdk.states.interfaces.State;
 import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -15,12 +14,9 @@ public class GNCStates implements StateContainer {
     public static final String scanAxisName = "roll axis vector";
     public static BasicState<Vector3D> scanAxis = new BasicState<>(scanAxisName, new Vector3D(1,0,0));
 
-    private static List<State<?>> stateList = new ArrayList<>();
     private static Map<String, BasicState<Vector3D>> vectorStateMap = new HashMap<>();
 
-
     public GNCStates(){
-        stateList.add(scanAxis);
         vectorStateMap.put(scanAxisName, this.scanAxis);
     }
 
