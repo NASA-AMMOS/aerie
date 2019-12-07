58c669c9e5f292b71848115ecfeb1ba972413702
Jonathan Castello <Jonathan.Castello@jpl.caltech.edu>
2019-12-07 14:27:24 -0800

    Remove unused loadActivities method

--- services/src/main/java/gov/nasa/jpl/ammos/mpsa/aerie/adaptation/utilities/AdaptationLoader.java
+++ services/src/main/java/gov/nasa/jpl/ammos/mpsa/aerie/adaptation/utilities/AdaptationLoader.java
@@ -1,40 +1,16 @@
 package gov.nasa.jpl.ammos.mpsa.aerie.adaptation.utilities;
 
-import gov.nasa.jpl.ammos.mpsa.aerie.adaptation.models.ActivityType;
 import gov.nasa.jpl.ammos.mpsa.aerie.adaptation.exceptions.AdaptationContractException;
 import gov.nasa.jpl.ammos.mpsa.aerie.merlinsdk.MerlinAdaptation;
-import gov.nasa.jpl.ammos.mpsa.aerie.merlinsdk.activities.ActivityMapper;
-import gov.nasa.jpl.ammos.mpsa.aerie.merlinsdk.activities.representation.ParameterSchema;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.nio.file.Path;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Objects;
-import java.util.Optional;
 import java.util.ServiceLoader;
-import java.util.stream.Collectors;
 
 public final class AdaptationLoader {
-    public static Map<String, ActivityType> loadActivities(final Path path) throws AdaptationContractException {
-        final MerlinAdaptation adaptation = loadAdaptation(path);
-
-        final Map<String, Map<String, ParameterSchema>> activitySchemas = Optional
-            .of(adaptation)
-            .map(MerlinAdaptation::getActivityMapper)
-            .map(ActivityMapper::getActivitySchemas)
-            .orElseGet(HashMap::new);
-
-        return activitySchemas
-            .entrySet()
-            .stream()
-            .collect(Collectors.toMap(
-                p -> p.getKey(),
-                p -> new ActivityType(p.getKey(), p.getValue())));
-    }
-
     public static MerlinAdaptation<?> loadAdaptation(final Path adaptationPath) throws AdaptationContractException {
       Objects.requireNonNull(adaptationPath);
 
