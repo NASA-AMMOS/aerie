bf15fbfb63fdc9f157e454d209f43fe212eaeef2
Jonathan Castello <Jonathan.Castello@jpl.caltech.edu>
2019-12-07 18:45:20 -0800

    [AERIE-375] Include default parameters in HTTP response

--- services/src/main/java/gov/nasa/jpl/ammos/mpsa/aerie/adaptation/http/ResponseSerializers.java
+++ services/src/main/java/gov/nasa/jpl/ammos/mpsa/aerie/adaptation/http/ResponseSerializers.java
@@ -3,10 +3,13 @@ package gov.nasa.jpl.ammos.mpsa.aerie.adaptation.http;
 import gov.nasa.jpl.ammos.mpsa.aerie.adaptation.models.ActivityType;
 import gov.nasa.jpl.ammos.mpsa.aerie.adaptation.models.Adaptation;
 import gov.nasa.jpl.ammos.mpsa.aerie.merlinsdk.activities.representation.ParameterSchema;
+import gov.nasa.jpl.ammos.mpsa.aerie.merlinsdk.activities.representation.SerializedParameter;
 
 import javax.json.Json;
+import javax.json.JsonArrayBuilder;
 import javax.json.JsonObjectBuilder;
 import javax.json.JsonValue;
+import java.util.List;
 import java.util.Map;
 
 public class ResponseSerializers {
@@ -23,6 +26,24 @@ public class ResponseSerializers {
     for (final var entry : schemas.entrySet()) {
       builder.add(entry.getKey(), serializeParameterSchema(entry.getValue()));
     }
+
+    return builder.build();
+  }
+
+  public static JsonValue serializeActivityParameter(final SerializedParameter parameter) {
+    if (parameter == null) return JsonValue.NULL;
+
+    return parameter.match(new ParameterSerializer());
+  }
+
+  public static JsonValue serializeActivityParameters(final Map<String, SerializedParameter> parameters) {
+    if (parameters == null) return JsonValue.NULL;
+
+    final JsonObjectBuilder builder = Json.createObjectBuilder();
+    for (final var entry : parameters.entrySet()) {
+      builder.add(entry.getKey(), serializeActivityParameter(entry.getValue()));
+    }
+
     return builder.build();
   }
 
@@ -30,6 +51,7 @@ public class ResponseSerializers {
     return Json
         .createObjectBuilder()
         .add("parameters", serializeParameterSchemas(activityType.parameters))
+        .add("defaults", serializeActivityParameters(activityType.defaults))
         .build();
   }
 
@@ -40,6 +62,7 @@ public class ResponseSerializers {
     for (final var entry : activityTypes.entrySet()) {
       builder.add(entry.getKey(), serializeActivityType(entry.getValue()));
     }
+
     return builder.build();
   }
 
@@ -123,6 +146,51 @@ public class ResponseSerializers {
           .add("type", "map")
           .add("items", builder)
           .build();
+    }
+  }
+
+  private static final class ParameterSerializer implements SerializedParameter.Visitor<JsonValue> {
+    @Override
+    public JsonValue onNull() {
+      return JsonValue.NULL;
+    }
+
+    @Override
+    public JsonValue onReal(final double value) {
+      return Json.createValue(value);
+    }
+
+    @Override
+    public JsonValue onInt(final long value) {
+      return Json.createValue(value);
+    }
+
+    @Override
+    public JsonValue onBoolean(final boolean value) {
+      return (value) ? JsonValue.TRUE : JsonValue.FALSE;
+    }
+
+    @Override
+    public JsonValue onString(final String value) {
+      return Json.createValue(value);
+    }
+
+    @Override
+    public JsonValue onList(final List<SerializedParameter> elements) {
+      final JsonArrayBuilder builder = Json.createArrayBuilder();
+      for (final var element : elements) {
+        builder.add(element.match(this));
+      }
+      return builder.build();
+    }
+
+    @Override
+    public JsonValue onMap(final Map<String, SerializedParameter> fields) {
+      final JsonObjectBuilder builder = Json.createObjectBuilder();
+      for (final var entry : fields.entrySet()) {
+        builder.add(entry.getKey(), entry.getValue().match(this));
+      }
+      return builder.build();
     }
   }
 }
