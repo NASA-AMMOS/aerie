96c87578291daf20bf299a9405e85deac1c663f8
Jonathan Castello <Jonathan.Castello@jpl.caltech.edu>
2019-12-18 14:45:55 -0800

    Consolidate configuration loading into driver class

--- services/src/main/java/gov/nasa/jpl/ammos/mpsa/aerie/adaptation/AerieAppDriver.java
+++ services/src/main/java/gov/nasa/jpl/ammos/mpsa/aerie/adaptation/AerieAppDriver.java
@@ -7,40 +7,55 @@ import gov.nasa.jpl.ammos.mpsa.aerie.adaptation.remotes.AdaptationRepository;
 import gov.nasa.jpl.ammos.mpsa.aerie.adaptation.remotes.RemoteAdaptationRepository;
 import io.javalin.Javalin;
 
+import javax.json.Json;
+import javax.json.JsonObject;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
 
 public final class AerieAppDriver {
     public static void main(final String[] args) {
-        // Load the properties
-        AppConfiguration configuration;
-        if (args.length > 0) {
-            try {
-                configuration = AppConfiguration.loadProperties(Path.of(args[0]));
-            } catch (IOException e) {
-                System.err.println(String.format("Configuration file \"%s\" could not be loaded.", args[0]));
-                configuration = null;
-            }
-
-        } else {
-            configuration = AppConfiguration.loadProperties();
-        }
-
-        if (configuration == null) {
-            System.err.println("Not all properties loaded. Exiting.");
-            System.exit(1);
-        }
+        // Fetch application configuration properties.
+        final AppConfiguration configuration = loadConfiguration(args);
 
         // Assemble the core non-web object graph.
-        final AdaptationRepository adaptationRepository = new RemoteAdaptationRepository(configuration.MONGO_URI, configuration.MONGO_DATABASE, configuration.MONGO_ADAPTATION_COLLECTION);
+        final AdaptationRepository adaptationRepository = new RemoteAdaptationRepository(
+            configuration.MONGO_URI,
+            configuration.MONGO_DATABASE,
+            configuration.MONGO_ADAPTATION_COLLECTION);
         final App app = new LocalApp(adaptationRepository);
         final AdaptationBindings bindings = new AdaptationBindings(app);
+
         // Initiate an HTTP server.
         final Javalin javalin = Javalin.create(config -> {
             config.showJavalinBanner = false;
             config.enableCorsForAllOrigins();
         });
         bindings.registerRoutes(javalin);
+
+        // Start the HTTP server.
         javalin.start(configuration.HTTP_PORT);
+    }
+
+    private static AppConfiguration loadConfiguration(final String[] args) {
+        // Determine where we're getting our configuration from.
+        final InputStream configStream;
+        if (args.length > 0) {
+            try {
+                configStream = Files.newInputStream(Path.of(args[0]));
+            } catch (final IOException ex) {
+                System.err.printf("Configuration file \"%s\" could not be loaded: %s\n", args[0], ex.getMessage());
+                System.exit(1);
+                throw new Error(ex);
+            }
+        } else {
+            configStream = Thread.currentThread().getContextClassLoader()
+                .getResourceAsStream("gov/nasa/jpl/ammos/mpsa/aerie/adaptation/config.json");
+        }
+
+        // Read and process the configuration source.
+        final JsonObject config = (JsonObject)(Json.createReader(configStream).readValue());
+        return AppConfiguration.parseProperties(config);
     }
 }
