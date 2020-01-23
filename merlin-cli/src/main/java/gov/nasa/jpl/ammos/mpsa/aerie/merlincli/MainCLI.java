8b18da346e83e07866fbacb833896303fcf5816c
roffo <Kenneth.R.Roffo@jpl.nasa.gov>
2020-01-23 15:19:15 -0800

    Added REPL mode to CLI

--- merlin-cli/src/main/java/gov/nasa/jpl/ammos/mpsa/aerie/merlincli/MainCLI.java
+++ merlin-cli/src/main/java/gov/nasa/jpl/ammos/mpsa/aerie/merlincli/MainCLI.java
@@ -7,14 +7,38 @@ import gov.nasa.jpl.ammos.mpsa.aerie.merlincli.models.RemoteAdaptationRepository
 import gov.nasa.jpl.ammos.mpsa.aerie.merlincli.models.RemotePlanRepository;
 import org.apache.http.impl.client.HttpClients;
 
+import java.util.Scanner;
+
 public class MainCLI {
 
     public static void main(String[] args) {
         final PlanRepository planRepository = new RemotePlanRepository(new HttpClientHandler(HttpClients.createDefault()));
         final AdaptationRepository adaptationRepository = new RemoteAdaptationRepository(new HttpClientHandler(HttpClients.createDefault()));
         final AerieCommandReceiver commandReceiver = new AerieCommandReceiver(planRepository, adaptationRepository);
-
         final CommandOptions commandOptions = new CommandOptions();
+
+        if (args.length > 0) {
+            runOne(commandOptions, commandReceiver, args);
+        } else {
+            runRepl(commandOptions, commandReceiver);
+        }
+    }
+
+    private static void runOne(CommandOptions commandOptions, AerieCommandReceiver commandReceiver, String[] args) {
         commandOptions.parse(commandReceiver, args);
+    }
+
+    private static void runRepl(CommandOptions commandOptions, AerieCommandReceiver commandReceiver) {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.print("\nEnter command: ");
+            String line = sc.nextLine();
+            commandOptions.parse(commandReceiver, parseArguments(line));
+        }
+    }
+
+    private static String[] parseArguments(String line) {
+        // TODO: Handle strings properly in arguments (may contain spaces)
+        return line.split("\\s+");
     }
 }
