3f23c2f4dc07ef2087394b3f03a6fb8a59be2c25
roffo <Kenneth.R.Roffo@jpl.nasa.gov>
2020-01-30 09:53:38 -0800

    Implement createPlan for LocalCommandReceiver

--- merlin-cli/src/main/java/gov/nasa/jpl/ammos/mpsa/aerie/merlincli/local/ScheduledActivity.java
+++ merlin-cli/src/main/java/gov/nasa/jpl/ammos/mpsa/aerie/merlincli/local/ScheduledActivity.java
@@ -1,17 +1,40 @@
 package gov.nasa.jpl.ammos.mpsa.aerie.merlincli.local;
 
+import gov.nasa.jpl.ammos.mpsa.aerie.merlincli.models.ActivityInstance;
+import gov.nasa.jpl.ammos.mpsa.aerie.merlincli.models.ActivityInstanceParameter;
 import gov.nasa.jpl.ammos.mpsa.aerie.merlinsdk.activities.representation.SerializedActivity;
+import gov.nasa.jpl.ammos.mpsa.aerie.merlinsdk.activities.representation.SerializedParameter;
+import gov.nasa.jpl.ammos.mpsa.aerie.merlinsdk.engine.SimulationInstant;
 import gov.nasa.jpl.ammos.mpsa.aerie.merlinsdk.time.Instant;
+import gov.nasa.jpl.ammos.mpsa.aerie.merlinsdk.time.TimeUnit;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
 public final class ScheduledActivity {
   public final Instant startTime;
   public final SerializedActivity activity;
 
+  public final static SimpleDateFormat timestampFormat = new SimpleDateFormat("yyyy-DDD'T'HH:mm:ss");
+
   public ScheduledActivity(final Instant startTime, final SerializedActivity activity) {
     this.startTime = startTime;
     this.activity = activity;
+  }
+
+  public ScheduledActivity(final ActivityInstance instance) throws ParseException {
+    final Map<String, SerializedParameter> serializedParameters = new HashMap<>();
+    final List<ActivityInstanceParameter> instanceParameters = instance.getParameters();
+    for (ActivityInstanceParameter parameter : instanceParameters) {
+      serializedParameters.put(parameter.getName(), SerializedParameter.of(parameter.getValue()));
+    }
+
+    this.startTime = SimulationInstant.fromQuantity(timestampFormat.parse(instance.getStartTimestamp()).getTime(), TimeUnit.MILLISECONDS);
+    this.activity = new SerializedActivity(instance.getActivityType(), serializedParameters);
   }
 
   @Override
