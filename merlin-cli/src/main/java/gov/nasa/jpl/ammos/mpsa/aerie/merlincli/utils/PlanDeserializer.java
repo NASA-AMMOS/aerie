79fab7025535803ae1486d0ba089ea2a088a1283
roffo <Kenneth.R.Roffo@jpl.nasa.gov>
2020-02-13 14:56:44 -0800

    Remove unused methods from PlanDeserializer

--- merlin-cli/src/main/java/gov/nasa/jpl/ammos/mpsa/aerie/merlincli/utils/PlanDeserializer.java
+++ merlin-cli/src/main/java/gov/nasa/jpl/ammos/mpsa/aerie/merlincli/utils/PlanDeserializer.java
@@ -115,46 +115,6 @@ public final class PlanDeserializer {
         return activityInstance;
     }
 
-    public static ActivityInstance deserializeActivityInstancePatch(final JsonValue jsonValue) throws InvalidEntityException {
-        if (!(jsonValue instanceof JsonObject)) throw new InvalidEntityException();
-        final JsonObject activityInstanceJson = (JsonObject)jsonValue;
-
-        Optional<String> type = Optional.empty();
-        Optional<String> startTimestamp = Optional.empty();
-        Optional<Map<String, SerializedParameter>> parameters = Optional.empty();
-
-        for (final var entry : activityInstanceJson.entrySet()) {
-            final JsonValue entryValue = entry.getValue();
-
-            switch (entry.getKey()) {
-                case "type":
-                    if (entryValue == JsonValue.NULL) throw new InvalidEntityException();
-                    type = Optional.of(deserializeString(entryValue));
-                    break;
-
-                case "startTimestamp":
-                    if (entryValue == JsonValue.NULL) throw new InvalidEntityException();
-                    startTimestamp = Optional.of(deserializeString(entryValue));
-                    break;
-
-                case "parameters":
-                    if (entryValue == JsonValue.NULL) throw new InvalidEntityException();
-                    parameters = Optional.of(deserializeActivityParameterMap(entryValue));
-                    break;
-
-                default:
-                    throw new InvalidEntityException();
-            }
-        }
-
-        final ActivityInstance activityInstance = new ActivityInstance();
-        activityInstance.setActivityType(type.orElse(null));
-        activityInstance.setStartTimestamp(startTimestamp.orElse(null));
-        activityInstance.setParameters(parameters.orElse(null));
-
-        return activityInstance;
-    }
-
     public static List<ActivityInstance> deserializeActivityInstanceList(final JsonValue jsonValue) throws InvalidEntityException {
         if (!(jsonValue instanceof JsonArray)) throw new InvalidEntityException();
         final JsonArray activityInstanceListJson = (JsonArray)jsonValue;
@@ -166,81 +126,6 @@ public final class PlanDeserializer {
         }
 
         return activityInstances;
-    }
-
-    public static Map<String, ActivityInstance> deserializeActivityInstanceMap(final JsonValue jsonValue) throws InvalidEntityException {
-        if (!(jsonValue instanceof JsonObject)) throw new InvalidEntityException();
-        final JsonObject activityInstanceMapJson = (JsonObject)jsonValue;
-
-        final Map<String, ActivityInstance> activityInstances = new HashMap<>();
-        for (final var entry : activityInstanceMapJson.entrySet()) {
-            activityInstances.put(entry.getKey(), deserializeActivityInstance(entry.getValue()));
-        }
-
-        return activityInstances;
-    }
-
-    public static List<ActivityInstance> deserializeActivityInstanceMapPatch(final JsonValue jsonValue) throws InvalidEntityException {
-        if (!(jsonValue instanceof JsonObject)) throw new InvalidEntityException();
-        final JsonObject activityInstanceMapJson = (JsonObject)jsonValue;
-
-        final List<ActivityInstance> activityInstances = new ArrayList<>();
-        for (final var entry : activityInstanceMapJson.entrySet()) {
-            if (entry.getValue() == JsonValue.NULL) {
-                activityInstances.add(null);
-            } else {
-                activityInstances.add(deserializeActivityInstance(entry.getValue()));
-            }
-        }
-
-        return activityInstances;
-    }
-
-    public static PlanDetail deserializePlanPatch(final JsonValue jsonValue) throws InvalidEntityException {
-        if (!(jsonValue instanceof JsonObject)) throw new InvalidEntityException();
-        final JsonObject newPlanJson = (JsonObject)jsonValue;
-
-        Optional<String> name = Optional.empty();
-        Optional<String> startTimestamp = Optional.empty();
-        Optional<String> endTimestamp = Optional.empty();
-        Optional<List<ActivityInstance>> activityInstances = Optional.empty();
-
-        for (final var entry : newPlanJson.entrySet()) {
-            final JsonValue entryValue = entry.getValue();
-
-            switch (entry.getKey()) {
-                case "name":
-                    if (entryValue == JsonValue.NULL) throw new InvalidEntityException();
-                    name = Optional.of(deserializeString(entryValue));
-                    break;
-
-                case "startTimestamp":
-                    if (entryValue == JsonValue.NULL) throw new InvalidEntityException();
-                    startTimestamp = Optional.of(deserializeString(entryValue));
-                    break;
-
-                case "endTimestamp":
-                    if (entryValue == JsonValue.NULL) throw new InvalidEntityException();
-                    endTimestamp = Optional.of(deserializeString(entryValue));
-                    break;
-
-                case "activityInstances":
-                    if (entryValue == JsonValue.NULL) throw new InvalidEntityException();
-                    activityInstances = Optional.of(deserializeActivityInstanceMapPatch(entryValue));
-                    break;
-
-                default:
-                    throw new InvalidEntityException();
-            }
-        }
-
-        final PlanDetail plan = new PlanDetail();
-        plan.setName(name.orElse(null));
-        plan.setStartTimestamp(startTimestamp.orElse(null));
-        plan.setEndTimestamp(endTimestamp.orElse(null));
-        plan.setActivityInstances(activityInstances.orElse(null));
-
-        return plan;
     }
 
     public static PlanDetail deserializePlan(final JsonValue jsonValue) throws InvalidEntityException {
