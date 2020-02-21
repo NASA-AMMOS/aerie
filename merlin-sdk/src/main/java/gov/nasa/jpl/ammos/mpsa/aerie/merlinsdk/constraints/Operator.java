afde8872c5e8eae61b7db6592856ccd8b73cf749
Jonathan Castello <Jonathan.Castello@jpl.caltech.edu>
2020-02-21 00:58:31 -0800

    Refactor window intersection/union computations

--- merlin-sdk/src/main/java/gov/nasa/jpl/ammos/mpsa/aerie/merlinsdk/constraints/Operator.java
+++ merlin-sdk/src/main/java/gov/nasa/jpl/ammos/mpsa/aerie/merlinsdk/constraints/Operator.java
@@ -1,103 +1,60 @@
 package gov.nasa.jpl.ammos.mpsa.aerie.merlinsdk.constraints;
 
-import gov.nasa.jpl.ammos.mpsa.aerie.merlinsdk.time.Instant;
 import gov.nasa.jpl.ammos.mpsa.aerie.merlinsdk.time.Window;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
-public class Operator implements Comparator<Window> {
-    /*
-    w1 < w2: -1
-    w1 == w2: 0
-    w1 > w2: 1
-     */
-    @Override
-    public int compare(Window w1, Window w2) {
-        if (w1.start.isBefore(w2.start)){
-            return -1;
-        }
-        else if (w1.start.isAfter(w2.start)){
-            return 1;
-        }
-        return 0;
-    }
+public final class Operator {
+    private Operator() {}
 
     /*
     Assumption: in both methods intersection and union,
     a and b are composed of in-order, disjoint intervals
     */
 
-    public static List<Window> intersection(List<Window> a, List<Window> b){
+    public static List<Window> intersection(final List<Window> a, final List<Window> b) {
+        final var intersection = new ArrayList<Window>();
 
-        List<Window> intersection = new ArrayList<>();
-        int i = 0, j = 0;
+        int i = 0;
+        int j = 0;
+        while (i < a.size() && j < b.size()) {
+            final var x = a.get(i);
+            final var y = b.get(j);
 
-        while (i < a.size() && j < b.size()){
-
-            Instant aStart = a.get(i).start;
-            Instant bStart = b.get(j).start;
-            Instant aEnd = a.get(i).end;
-            Instant bEnd = b.get(j).end;
-
-            //get the last start and the first end for the two windows
-            Instant maxStart = (aStart.isAfter(bStart) ? aStart : bStart);
-            Instant minEnd = (aEnd.isBefore(bEnd) ? aEnd : bEnd);
-
-            //see if there is an overlap
-            //also should isBefore check == in addition to <?
-            if (maxStart.isBefore(minEnd)){
-                intersection.add(Window.between(maxStart, minEnd));
+            if (x.overlaps(y)) {
+                intersection.add(Window.greatestLowerBound(x, y));
             }
 
-            if (aEnd.isBefore(bEnd)){
-                i++;
-            }
-            else {
-                j++;
+            if (x.end.isBefore(y.end)) {
+                i += 1;
+            } else {
+                j += 1;
             }
         }
 
         return intersection;
     }
 
-    public static List<Window> union(List<Window> a, List<Window> b){
-
-        List<Window> temp = new ArrayList<>();
-
+    public static List<Window> union(final List<Window> a, final List<Window> b) {
+        final var temp = new ArrayList<Window>();
         temp.addAll(a);
         temp.addAll(b);
+        temp.sort(Comparator.comparing(w -> w.start));
 
-        List<Window> union = new ArrayList<>();
+        final var union = new ArrayList<Window>();
 
-        Collections.sort(temp, new Operator());
-
-        for (Window window : temp){
-
-            //empty list, add first window
-            if (union.isEmpty()){
+        for (final var window : temp) {
+            if (union.isEmpty()) {
+                //empty list, add first window
                 union.add(window);
-                continue;
-            }
-
-            Window lastAddedWindow = union.get(union.size()-1);
-            //no overlap b/w last element in list and next element in temp list
-            if (lastAddedWindow.end.isBefore(window.start)){
+            } else if (!union.get(union.size() - 1).overlaps(window)) {
+                //no overlap b/w last element in list and next element in temp list
                 union.add(window);
-            }
-
-            //there is an overlap
-            else {
-               Instant end = (
-                        lastAddedWindow.end.isAfter(window.end) ?
-                                lastAddedWindow.end : window.end);
-
-               Window merged = Window.between(lastAddedWindow.start, end);
-
-               union.remove(union.size()-1);
-               union.add(merged);
+            } else {
+                //there is an overlap
+                union.set(union.size() - 1, Window.leastUpperBound(union.get(union.size() - 1), window));
             }
         }
 
@@ -105,23 +62,11 @@ public class Operator implements Comparator<Window> {
     }
 
 
-    public static Constraint And(Constraint a, Constraint b){
-        Constraint abAnd;
-
-        abAnd = () -> {
-            return intersection(a.getWindows(), b.getWindows());
-        };
-
-        return abAnd;
+    public static Constraint And(Constraint a, Constraint b) {
+        return () -> intersection(a.getWindows(), b.getWindows());
     }
 
-    public static Constraint Or(Constraint a, Constraint b){
-        Constraint abOr;
-
-        abOr = () -> {
-            return union(a.getWindows(), b.getWindows());
-        };
-
-        return abOr;
+    public static Constraint Or(Constraint a, Constraint b) {
+        return () -> union(a.getWindows(), b.getWindows());
     }
 }
--- merlin-sdk/src/main/java/gov/nasa/jpl/ammos/mpsa/aerie/merlinsdk/time/Window.java
+++ merlin-sdk/src/main/java/gov/nasa/jpl/ammos/mpsa/aerie/merlinsdk/time/Window.java
@@ -3,6 +3,9 @@ package gov.nasa.jpl.ammos.mpsa.aerie.merlinsdk.time;
 import java.util.Objects;
 
 public final class Window {
+  // TODO: Find a good way to represent windows with infinite bounds.
+  //   For instance, the window representing all time should be representable.
+  // TODO: Find a good way to represent the empty window.
   public final Instant start;
   public final Instant end;
 
@@ -13,6 +16,29 @@ public final class Window {
 
   public static Window between(final Instant start, final Instant end) {
     return new Window(start, end);
+  }
+
+  public boolean overlaps(final Window other) {
+    return this.end.compareTo(other.start) >= 0 && other.end.compareTo(this.start) >= 0;
+  }
+
+  /**
+   * Returns the largest window contained by both `x` and `y`.
+   */
+  public static Window greatestLowerBound(final Window x, final Window y) {
+    if (!x.overlaps(y)) {
+      // TODO: Find a good way to represent an empty interval.
+      return null;
+    } else {
+      return Window.between(Instant.max(x.start, y.start), Instant.min(x.end, y.end));
+    }
+  }
+
+  /**
+   * Returns the smallest window containing both `x` and `y`.
+   */
+  public static Window leastUpperBound(final Window x, final Window y) {
+    return Window.between(Instant.min(x.start, y.start), Instant.max(x.end, y.end));
   }
 
   @Override
