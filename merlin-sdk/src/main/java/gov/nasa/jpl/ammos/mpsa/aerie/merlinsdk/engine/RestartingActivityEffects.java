20e81f2438aecbe9e50ce440ddec379bc3d5c7e1
Jonathan Castello <Jonathan.Castello@jpl.caltech.edu>
2020-02-24 18:50:41 -0800

    Remove unnecessary level of nesting

--- merlin-sdk/src/main/java/gov/nasa/jpl/ammos/mpsa/aerie/merlinsdk/engine/RestartingActivityEffects.java
+++ merlin-sdk/src/main/java/gov/nasa/jpl/ammos/mpsa/aerie/merlinsdk/engine/RestartingActivityEffects.java
@@ -11,47 +11,103 @@ import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.Set;
 
-public final class RestartingActivityEffects {
+public final class RestartingActivityEffects implements ActivityEffects.Provider {
     private final Queue<Task> queue = new PriorityQueue<>(Comparator.comparing(t -> t.resumeTime));
 
     private Instant currentTime;
     private Task currentTask = null;
 
-    private RestartingActivityEffects(final Instant initialInstant) {
+    private RestartingActivityEffects(final Instant initialInstant, final Runnable rootScope) {
         this.currentTime = initialInstant;
+        this.queue.add(new Task(initialInstant, initialInstant, rootScope, null));
     }
 
     public static Instant execute(final Instant initialInstant, final Runnable scope) {
-        final var effects = new RestartingActivityEffects(initialInstant);
-        final var provider = effects.new Provider();
-
-        effects.queue.add(new Task(effects.currentTime, effects.currentTime, scope, null));
-
-        ActivityEffects.enter(provider, () -> {
-            while (!effects.queue.isEmpty()) {
-                final var task = effects.queue.remove();
-
-                effects.currentTime = task.startTime;
-                effects.currentTask = task;
-
-                try {
-                    task.scope.run();
-                    provider.waitForChildren();
-                    if (task.parent != null) {
-                        task.parent.runningChildren.remove(task);
-                        if (task.parent.waitingForChildren && task.parent.runningChildren.isEmpty()) {
-                            task.parent.waitingForChildren = false;
-                            task.parent.resumeTime = effects.currentTime;
-                            task.parent.childrenResumptions.add(effects.currentTime);
-                            effects.queue.add(task.parent);
-                        }
-                    }
-                } catch (final Rescheduled ignored) {
-                }
-            }
-        });
-
+        final var effects = new RestartingActivityEffects(initialInstant, scope);
+        ActivityEffects.enter(effects, effects::run);
         return effects.currentTime;
+    }
+
+    private void run() {
+        while (!this.queue.isEmpty()) {
+            final var task = this.queue.remove();
+
+            this.currentTime = task.startTime;
+            this.currentTask = task;
+
+            try {
+                task.scope.run();
+                this.waitForChildren();
+
+                if (task.parent != null) {
+                    task.parent.runningChildren.remove(task);
+                    if (task.parent.waitingForChildren && task.parent.runningChildren.isEmpty()) {
+                        task.parent.waitingForChildren = false;
+                        task.parent.resumeTime = this.currentTime;
+                        task.parent.childrenResumptions.add(this.currentTime);
+                        this.queue.add(task.parent);
+                    }
+                }
+            } catch (final Rescheduled ignored) {
+            }
+        }
+    }
+
+    @Override
+    public void delay(final Duration duration) {
+        final var resumeTime = this.currentTime.plus(duration);
+
+        if (this.replaying()) {
+            // Replaying; skip effects.
+            this.currentTime = resumeTime;
+            return;
+        }
+
+        final var task = this.currentTask;
+        this.queue.add(task);
+
+        task.resumeTime = resumeTime;
+        task.nextChildrenResumption = 0;
+        throw Rescheduled;
+    }
+
+    @Override
+    public void spawn(final Duration duration, final Runnable activity) {
+        if (this.replaying()) return;
+
+        final var resumeTime = this.currentTime.plus(duration);
+        final var newTask = new Task(resumeTime, resumeTime, activity, this.currentTask);
+
+        this.currentTask.runningChildren.add(newTask);
+        this.queue.add(newTask);
+    }
+
+    @Override
+    public void waitForChildren() {
+        if (this.replaying()) {
+            this.currentTime = this.currentTask.childrenResumptions.get(this.currentTask.nextChildrenResumption);
+            this.currentTask.nextChildrenResumption += 1;
+            return;
+        }
+
+        if (this.currentTask.runningChildren.isEmpty()) {
+            this.currentTask.childrenResumptions.add(this.currentTime);
+            return;
+        }
+
+        this.currentTask.waitingForChildren = true;
+        this.currentTask.nextChildrenResumption = 0;
+        throw Rescheduled;
+    }
+
+    @Override
+    public Instant now() {
+        return this.currentTime;
+    }
+
+    @Override
+    public boolean replaying() {
+        return this.currentTask.resumeTime.isAfter(this.currentTime);
     }
 
     private static final class Task {
@@ -75,71 +131,4 @@ public final class RestartingActivityEffects {
 
     private static class Rescheduled extends RuntimeException {}
     private static final Rescheduled Rescheduled = new Rescheduled();
-
-    private final class Provider implements ActivityEffects.Provider {
-        @Override
-        public void delay(final Duration duration) {
-            final var resumeTime = RestartingActivityEffects.this.currentTime.plus(duration);
-
-            if (this.replaying()) {
-                // Replaying; skip effects.
-                RestartingActivityEffects.this.currentTime = resumeTime;
-                return;
-            }
-
-            final var task = RestartingActivityEffects.this.currentTask;
-            RestartingActivityEffects.this.queue.add(task);
-
-            task.resumeTime = resumeTime;
-            task.nextChildrenResumption = 0;
-            throw Rescheduled;
-        }
-
-        @Override
-        public void spawn(final Duration duration, final Runnable activity) {
-            if (this.replaying()) return;
-
-            final var task = RestartingActivityEffects.this.currentTask;
-            final var currentTime = RestartingActivityEffects.this.currentTime;
-
-            final var resumeTime = currentTime.plus(duration);
-            final var newTask = new Task(resumeTime, resumeTime, activity, task);
-
-            task.runningChildren.add(newTask);
-            RestartingActivityEffects.this.queue.add(newTask);
-        }
-
-        @Override
-        public void waitForChildren() {
-            final var task = RestartingActivityEffects.this.currentTask;
-
-            if (this.replaying()) {
-                RestartingActivityEffects.this.currentTime = task.childrenResumptions.get(task.nextChildrenResumption);
-                task.nextChildrenResumption += 1;
-                return;
-            }
-
-            if (task.runningChildren.isEmpty()) {
-                task.childrenResumptions.add(RestartingActivityEffects.this.currentTime);
-                return;
-            }
-
-            task.waitingForChildren = true;
-            task.nextChildrenResumption = 0;
-            throw Rescheduled;
-        }
-
-        @Override
-        public Instant now() {
-            return RestartingActivityEffects.this.currentTime;
-        }
-
-        @Override
-        public boolean replaying() {
-            final var task = RestartingActivityEffects.this.currentTask;
-            final var currentTime = RestartingActivityEffects.this.currentTime;
-
-            return task.resumeTime.isAfter(currentTime);
-        }
-    }
 }
