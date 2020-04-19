522564e57a748dec04874b4fac6da77a3b073d62
Jonathan Castello <Jonathan.Castello@jpl.caltech.edu>
2020-04-19 13:14:13 -0700

    Extract example simulation out of SimulationEngineTests

--- merlin-sdk/src/test/java/gov/nasa/jpl/ammos/mpsa/aerie/merlinsdk/engine/SimulationEngineTests.java
+++ merlin-sdk/src/test/java/gov/nasa/jpl/ammos/mpsa/aerie/merlinsdk/engine/SimulationEngineTests.java
@@ -10,41 +10,7 @@ import gov.nasa.jpl.ammos.mpsa.aerie.merlinsdk.time.Duration;
 
 import org.junit.Test;
 
-public class SimulationEngineTests {
-    /// Trace a simulation with 100 tasks, each spawning 1 sub-task.
-    public static void main(final String[] args) {
-        final var simEngine = new SimulationEngine();
-
-        final var trace = new ArrayList<>();
-        simEngine.scheduleJobAfter(Duration.ZERO, (root) -> {
-            for (int i = 0; i < 100; i++) {
-                final var index = i;
-
-                root.defer(Duration.ZERO, (parent) -> {
-                    root.delay(Duration.of(100 * (index + 1), MICROSECONDS));
-                    trace.add(parent.now() + "\t" + "parent[" + index + "]-1");
-
-                    parent.delay(Duration.of(10, MICROSECONDS));
-                    trace.add(parent.now() + "\t" + "parent[" + index + "]-2");
-
-                    parent.defer(Duration.ZERO, (child) -> {
-                        trace.add(child.now() + "\t" + "child[" + index + "]-1");
-
-                        child.delay(Duration.of(1, MICROSECONDS));
-                        trace.add(child.now() + "\t" + "child[" + index + "]-2");
-                    });
-
-                    parent.delay(Duration.of(10, MICROSECONDS));
-                    trace.add(parent.now() + "\t" + "parent[" + index + "]-3");
-                });
-            }
-        });
-
-        simEngine.runToCompletion();
-
-        trace.forEach(System.out::println);
-    }
-
+public final class SimulationEngineTests {
     @Test
     public void testChildrenOrdering() {
         final var simEngine = new SimulationEngine();
