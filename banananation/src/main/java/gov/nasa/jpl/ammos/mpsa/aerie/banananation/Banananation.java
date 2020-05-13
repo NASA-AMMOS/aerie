9189b7f79200aeb12ee565c7d3db7ad5407f4a5f
roffo <kenneth.r.roffo@jpl.nasa.gov>
2020-05-13 14:58:38 -0700

    Use ActivityMapperLoader in Banananation

--- banananation/src/main/java/gov/nasa/jpl/ammos/mpsa/aerie/banananation/Banananation.java
+++ banananation/src/main/java/gov/nasa/jpl/ammos/mpsa/aerie/banananation/Banananation.java
@@ -1,13 +1,10 @@
 package gov.nasa.jpl.ammos.mpsa.aerie.banananation;
 
-import gov.nasa.jpl.ammos.mpsa.aerie.banananation.activities.BiteBananaActivity$$ActivityMapper;
-import gov.nasa.jpl.ammos.mpsa.aerie.banananation.activities.ParameterTestActivity$$ActivityMapper;
-import gov.nasa.jpl.ammos.mpsa.aerie.banananation.activities.mappers.PeelBananaActivityMapper;
 import gov.nasa.jpl.ammos.mpsa.aerie.banananation.state.BananaStates;
 import gov.nasa.jpl.ammos.mpsa.aerie.merlinsdk.MerlinAdaptation;
 import gov.nasa.jpl.ammos.mpsa.aerie.merlinsdk.SimulationState;
 import gov.nasa.jpl.ammos.mpsa.aerie.merlinsdk.activities.ActivityMapper;
-import gov.nasa.jpl.ammos.mpsa.aerie.merlinsdk.activities.CompositeActivityMapper;
+import gov.nasa.jpl.ammos.mpsa.aerie.merlinsdk.activities.ActivityMapperLoader;
 import gov.nasa.jpl.ammos.mpsa.aerie.merlinsdk.annotations.Adaptation;
 import gov.nasa.jpl.ammos.mpsa.aerie.merlinsdk.spice.SpiceLoader;
 import gov.nasa.jpl.ammos.mpsa.aerie.merlinsdk.states.interfaces.State;
@@ -19,15 +16,17 @@ import java.util.stream.Collectors;
 
 @Adaptation(name="Banananation", version="0.0.1")
 public class Banananation implements MerlinAdaptation {
-  private final ActivityMapper activityMapper = new CompositeActivityMapper(Map.of(
-      "BiteBanana", new BiteBananaActivity$$ActivityMapper(),
-      "PeelBanana", new PeelBananaActivityMapper(),
-      "ParameterTest", new ParameterTestActivity$$ActivityMapper()
-  ));
 
   @Override
   public ActivityMapper getActivityMapper() {
-    return activityMapper;
+    try {
+      return ActivityMapperLoader.loadActivityMapper(Banananation.class);
+    } catch (ActivityMapperLoader.ActivityMapperLoadException e) {
+      // TODO: We should add an exception to merlin-sdk that adaptations can
+      //       throw to signify that loading the activity mapper failed
+      e.printStackTrace();
+      return null;
+    }
   }
 
   @Override
