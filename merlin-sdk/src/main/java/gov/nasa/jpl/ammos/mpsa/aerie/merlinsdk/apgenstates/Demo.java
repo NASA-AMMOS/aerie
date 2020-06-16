bf674e8062ee54d359a98ab8988d3feaa412aeb5
Meghana Kumar <meghana.n.kumar@jpl.nasa.gov>
2020-06-16 13:22:15 -0700

    Add constraints demo

--- merlin-sdk/src/main/java/gov/nasa/jpl/ammos/mpsa/aerie/merlinsdk/apgenstates/Demo.java
+++ merlin-sdk/src/main/java/gov/nasa/jpl/ammos/mpsa/aerie/merlinsdk/apgenstates/Demo.java
@@ -3,6 +3,8 @@ package gov.nasa.jpl.ammos.mpsa.aerie.merlinsdk.apgenstates;
 import gov.nasa.jpl.ammos.mpsa.aerie.merlinsdk.apgenstates.states.APGenStateFactory;
 import gov.nasa.jpl.ammos.mpsa.aerie.merlinsdk.apgenstates.states.ConsumableState;
 import gov.nasa.jpl.ammos.mpsa.aerie.merlinsdk.apgenstates.states.SettableState;
+import gov.nasa.jpl.ammos.mpsa.aerie.merlinsdk.time.Duration;
+import gov.nasa.jpl.ammos.mpsa.aerie.merlinsdk.time.TimeUnit;
 
 public class Demo {
 
@@ -39,6 +41,13 @@ public class Demo {
 
     }
 
+    public static void print(String value)
+    {
+        System.out.println(value);
+    }
+
+
+
     public static void consumableStateDemo(){
 
         //Equivalent to setup
@@ -68,6 +77,60 @@ public class Demo {
         System.out.println(stateOfCharge + " : " + socState.get());
         System.out.println(powerDraw + " : " + powerDrawState.get());
         System.out.println();
+
+
+        socState.when((x) -> (x < 3.3));
+    }
+
+    public static void constraintsDemo(){
+        //Equivalent to setup
+        final var factory = new APGenStateFactory();
+        String stateOfCharge = "soc";
+        String dataRate = "dataRate";
+
+        //Example use case by mission modeler
+        ConsumableState socState = factory.createConsumableState(stateOfCharge, 10);
+        SettableState dataRateState = factory.createSettableState(dataRate, 200);
+
+        //Equivalent to events produced by Activities during a simulation
+        //Print statements for demonstration purposes
+        socState.add(5);
+        dataRateState.set(55.5);
+
+        factory.step(Duration.of(100, TimeUnit.MICROSECONDS));
+        socState.add(40);
+        dataRateState.set(5.5);
+
+        factory.step(Duration.of(100, TimeUnit.MICROSECONDS));
+        socState.add(-40);
+        dataRateState.set(55.5);
+
+        factory.step(Duration.of(100, TimeUnit.MICROSECONDS));
+        socState.add(80);
+        dataRateState.set(5.5);
+
+        factory.step(Duration.of(100, TimeUnit.MICROSECONDS));
+
+        //time      soc   datarate
+        /*
+        [0, 100]    15    55.5
+        [100, 200]  55    5.5
+        [200, 300]  15    55.5
+        [300, 400]  95    5.5
+         */
+
+        var socConstraint = socState.when(x -> x > 22);
+        var dataConstraint = dataRateState.when(x -> x > 10);
+
+        //todo: address question - do we want to say Windows [100, 100], [200, 200], and [300, 300] count
+        var andConstraint = socConstraint.and(dataConstraint);
+        var orConstraint = socConstraint.or(dataConstraint);
+
+        System.out.println(socConstraint.getWindows());
+        System.out.println(dataConstraint.getWindows());
+        System.out.println(andConstraint.getWindows());
+        System.out.println(orConstraint.getWindows());
+
     }
 
     public static void main(final String[] args) {
@@ -76,6 +139,9 @@ public class Demo {
 
         System.out.println("\nConsumable State Demo");
         if (true) consumableStateDemo();
+
+        System.out.println("\nConstraints Demo");
+        if (true) constraintsDemo();
     }
 
 
