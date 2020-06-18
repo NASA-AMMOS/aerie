c4be6a4feb4ad6530628b13f1e304dba3d21bbad
Jonathan Castello <Jonathan.Castello@jpl.caltech.edu>
2020-06-17 18:24:22 -0700

    Add a cleanup TODO

--- merlin-sdk/src/main/java/gov/nasa/jpl/ammos/mpsa/aerie/merlinsdk/effects/activities/ReactionContext.java
+++ merlin-sdk/src/main/java/gov/nasa/jpl/ammos/mpsa/aerie/merlinsdk/effects/activities/ReactionContext.java
@@ -8,6 +8,8 @@ public interface ReactionContext<T, Activity, Event> {
 
   void emit(Event event);
   void delay(Duration duration);
+  // TODO: parametrize `ReactionContext` over an opaque `ActivityHandle` type so that ID types are not leaked,
+  //   and so that we can remove `waitForActivity` (by moving it onto an interface implemented by the handle type).
   String spawn(Activity activity);
   String spawnAfter(Duration delay, Activity activity);
   void waitForActivity(String activityId);
