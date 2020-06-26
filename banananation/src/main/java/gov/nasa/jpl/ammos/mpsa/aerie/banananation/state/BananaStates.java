f244a96766634688d8476461f99c60a8140587e0
roffo <kenneth.r.roffo@jpl.nasa.gov>
2020-06-26 15:00:06 -0400

    Add constraints to banananation

--- banananation/src/main/java/gov/nasa/jpl/ammos/mpsa/aerie/banananation/state/BananaStates.java
+++ banananation/src/main/java/gov/nasa/jpl/ammos/mpsa/aerie/banananation/state/BananaStates.java
@@ -3,6 +3,9 @@ package gov.nasa.jpl.ammos.mpsa.aerie.banananation.state;
 import gov.nasa.jpl.ammos.mpsa.aerie.banananation.events.BananaEvent;
 import gov.nasa.jpl.ammos.mpsa.aerie.merlinsdk.independentstates.states.IndependentStateFactory;
 import gov.nasa.jpl.ammos.mpsa.aerie.merlinsdk.independentstates.states.ConsumableState;
+import gov.nasa.jpl.ammos.mpsa.aerie.merlinsdk.constraints.ViolableConstraint;
+
+import java.util.List;
 
 import static gov.nasa.jpl.ammos.mpsa.aerie.banananation.state.BananaQuerier.ctx;
 import static gov.nasa.jpl.ammos.mpsa.aerie.banananation.state.BananaQuerier.query;
@@ -12,4 +15,27 @@ public final class BananaStates {
 
   public static final ConsumableState fruit = factory.createConsumableState("fruit", 4.0);
   public static final ConsumableState peel = factory.createConsumableState("peel", 4.0);
+
+  public static final List<ViolableConstraint> violableConstraints = List.of(
+          new ViolableConstraint(fruit.when(x -> x < 2))
+                  .withId("minFruit")
+                  .withName("Minimum Fruit")
+                  .withMessage("Running dangerously low on fruit")
+                  .withCategory("warning"),
+          new ViolableConstraint(fruit.when(x -> x > 10))
+                  .withId("maxFruit")
+                  .withName("Maximum Fruit")
+                  .withMessage("Cannot hold more than 10 fruit")
+                  .withCategory("severe"),
+          new ViolableConstraint(fruit.when(x -> x > 5).and(peel.when(y -> y > 5)))
+                  .withId("fruitsAndPeels")
+                  .withName("Fruit Peels")
+                  .withMessage("Should throw away peels before getting more fruit")
+                  .withCategory("warning"),
+          new ViolableConstraint(peel.when(y -> y > 10))
+                  .withId("mexPeels")
+                  .withName("Maximum Peels")
+                  .withMessage("Too many peels is gross. Clean some up")
+                  .withCategory("severe")
+  );
 }
