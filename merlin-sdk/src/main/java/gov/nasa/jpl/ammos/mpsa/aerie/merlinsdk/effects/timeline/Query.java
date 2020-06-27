c454532bb0cd338a6fcef6be0a59a448ca18f361
Jonathan Castello <Jonathan.Castello@jpl.caltech.edu>
2020-06-26 19:46:57 -0700

    Implement model caching

--- merlin-sdk/src/main/java/gov/nasa/jpl/ammos/mpsa/aerie/merlinsdk/effects/timeline/Query.java
+++ merlin-sdk/src/main/java/gov/nasa/jpl/ammos/mpsa/aerie/merlinsdk/effects/timeline/Query.java
@@ -2,6 +2,9 @@ package gov.nasa.jpl.ammos.mpsa.aerie.merlinsdk.effects.timeline;
 
 import gov.nasa.jpl.ammos.mpsa.aerie.merlinsdk.effects.Projection;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static gov.nasa.jpl.ammos.mpsa.aerie.merlinsdk.effects.timeline.SimulationTimeline.START_INDEX;
 
 /**
@@ -44,6 +47,8 @@ public final class Query<Scope, Event, Model> {
     private final Projection<Event, Effect> projection;
     private final Applicator<Effect, Model> applicator;
 
+    private final Map<Integer, Model> cache = new HashMap<>();
+
     private InnerQuery(
         final SimulationTimeline<Scope, Event> database,
         final Projection<Event, Effect> projection,
@@ -55,15 +60,47 @@ public final class Query<Scope, Event, Model> {
     }
 
     public Model getAt(final Time<Scope, Event> time) {
-      final var model = this.applicator.initial();
+      // If we already have a model cached for this time point, we can just bail now.
+      if (time.getIndex() == START_INDEX) {
+        return this.applicator.initial();
+      } else if (this.cache.containsKey(time.getIndex())) {
+        return this.cache.get(time.getIndex());
+      }
 
-      final var effects = this.database.evaluate(this.projection, this.projection::atom, START_INDEX, time.getIndex());
+      // Look for a cached model anytime back to our most recent branch point, if any.
+      final var baseIndex = (time.getLastBranchBase() != null) ? time.getLastBranchBase().getIndex() : START_INDEX;
+      var previousIndex = time.getIndex();
+      while (previousIndex != baseIndex && !this.cache.containsKey(previousIndex)) {
+        previousIndex = this.database.get(previousIndex).getPrevious();
+      }
+
+      final Model model;
+      if (previousIndex == baseIndex) {
+        // We didn't find anything. We'll have to query our previous segment for its model.
+        if (time.getLastBranchBase() != null) {
+          model = this.applicator.duplicate(this.getAt(time.getLastBranchBase()));
+        } else {
+          // Well, we don't have a previous segment. Start with the initial model.
+          model = this.applicator.initial();
+        }
+      } else {
+        // We found something! Since we have exclusive ownership over our branch segment, we'll remove it from the cache,
+        // update it directly, and put it back in the cache at our current time point.
+        model = this.cache.remove(previousIndex);
+      }
+
+      // Compute the effects that have occurred since our last update on this branch.
+      final var effects = this.database.evaluate(this.projection, this.projection::atom, previousIndex, time.getIndex());
+
+      // Step this model up to the current point in time.
       for (final var effect : effects) {
         this.applicator.step(model, effect.getKey());
         this.applicator.apply(model, effect.getValue());
       }
 
-      // TODO: Stash one fork of the model in a cache.
+      // Cache this model for future queries.
+      this.cache.put(time.getIndex(), model);
+
       return model;
     }
   }
