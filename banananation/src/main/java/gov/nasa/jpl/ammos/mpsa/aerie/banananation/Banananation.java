475dac750a95722f9e4e689ae8a86220222102a3
Jonathan Castello <Jonathan.Castello@jpl.caltech.edu>
2020-07-12 21:53:15 -0700

    Add AbstractMerlinAdaptation base class

--- banananation/src/main/java/gov/nasa/jpl/ammos/mpsa/aerie/banananation/Banananation.java
+++ banananation/src/main/java/gov/nasa/jpl/ammos/mpsa/aerie/banananation/Banananation.java
@@ -2,26 +2,14 @@ package gov.nasa.jpl.ammos.mpsa.aerie.banananation;
 
 import gov.nasa.jpl.ammos.mpsa.aerie.banananation.events.BananaEvent;
 import gov.nasa.jpl.ammos.mpsa.aerie.banananation.state.BananaQuerier;
-import gov.nasa.jpl.ammos.mpsa.aerie.banananation.state.BananaStates;
-import gov.nasa.jpl.ammos.mpsa.aerie.merlinsdk.MerlinAdaptation;
-import gov.nasa.jpl.ammos.mpsa.aerie.merlinsdk.activities.ActivityMapper;
-import gov.nasa.jpl.ammos.mpsa.aerie.merlinsdk.activities.ActivityMapperLoader;
+import gov.nasa.jpl.ammos.mpsa.aerie.merlinsdk.AbstractMerlinAdaptation;
 import gov.nasa.jpl.ammos.mpsa.aerie.merlinsdk.annotations.Adaptation;
 import gov.nasa.jpl.ammos.mpsa.aerie.merlinsdk.effects.timeline.SimulationTimeline;
 import gov.nasa.jpl.ammos.mpsa.aerie.merlinsdk.spice.SpiceLoader;
 
 @Adaptation(name="Banananation", version="0.0.1")
-public class Banananation implements MerlinAdaptation<BananaEvent> {
+public class Banananation extends AbstractMerlinAdaptation<BananaEvent> {
   static { SpiceLoader.loadSpice(); }
-
-  @Override
-  public ActivityMapper getActivityMapper() {
-    try {
-      return ActivityMapperLoader.loadActivityMapper(Banananation.class);
-    } catch (final ActivityMapperLoader.ActivityMapperLoadException ex) {
-      throw new Error(ex);
-    }
-  }
 
   @Override
   public <T> Querier<T, BananaEvent> makeQuerier(final SimulationTimeline<T, BananaEvent> database) {
--- sample-adaptation/src/main/java/gov/nasa/jpl/ammos/mpsa/aerie/sampleadaptation/SampleAdaptation.java
+++ sample-adaptation/src/main/java/gov/nasa/jpl/ammos/mpsa/aerie/sampleadaptation/SampleAdaptation.java
@@ -1,24 +1,13 @@
 package gov.nasa.jpl.ammos.mpsa.aerie.sampleadaptation;
 
-import gov.nasa.jpl.ammos.mpsa.aerie.merlinsdk.MerlinAdaptation;
-import gov.nasa.jpl.ammos.mpsa.aerie.merlinsdk.activities.ActivityMapper;
-import gov.nasa.jpl.ammos.mpsa.aerie.merlinsdk.activities.ActivityMapperLoader;
+import gov.nasa.jpl.ammos.mpsa.aerie.merlinsdk.AbstractMerlinAdaptation;
 import gov.nasa.jpl.ammos.mpsa.aerie.merlinsdk.annotations.Adaptation;
 import gov.nasa.jpl.ammos.mpsa.aerie.merlinsdk.effects.timeline.SimulationTimeline;
 import gov.nasa.jpl.ammos.mpsa.aerie.sampleadaptation.events.SampleEvent;
 import gov.nasa.jpl.ammos.mpsa.aerie.sampleadaptation.states.SampleQuerier;
 
 @Adaptation(name="sample-adaptation", version="0.1")
-public class SampleAdaptation implements MerlinAdaptation<SampleEvent> {
-    @Override
-    public ActivityMapper getActivityMapper() {
-        try {
-            return ActivityMapperLoader.loadActivityMapper(SampleAdaptation.class);
-        } catch (ActivityMapperLoader.ActivityMapperLoadException e) {
-            throw new Error(e);
-        }
-    }
-
+public class SampleAdaptation extends AbstractMerlinAdaptation<SampleEvent> {
     @Override
     public <T> Querier<T, SampleEvent> makeQuerier(final SimulationTimeline<T, SampleEvent> database) {
         return new SampleQuerier<>(this.getActivityMapper(), database);
