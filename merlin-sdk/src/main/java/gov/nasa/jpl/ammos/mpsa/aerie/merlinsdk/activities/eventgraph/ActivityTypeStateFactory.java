f1788a47e380217c10af08b6d6af740ab012ecba
Jonathan Castello <Jonathan.Castello@jpl.caltech.edu>
2020-07-15 18:16:48 -0700

    Add helper .ofType(Class<Activity>)

--- merlin-sdk/src/main/java/gov/nasa/jpl/ammos/mpsa/aerie/merlinsdk/activities/eventgraph/ActivityTypeStateFactory.java
+++ merlin-sdk/src/main/java/gov/nasa/jpl/ammos/mpsa/aerie/merlinsdk/activities/eventgraph/ActivityTypeStateFactory.java
@@ -1,5 +1,8 @@
 package gov.nasa.jpl.ammos.mpsa.aerie.merlinsdk.activities.eventgraph;
 
+import gov.nasa.jpl.ammos.mpsa.aerie.merlinsdk.activities.Activity;
+import gov.nasa.jpl.ammos.mpsa.aerie.merlinsdk.activities.annotations.ActivityType;
+
 public final class ActivityTypeStateFactory {
   private final ActivityModelQuerier querier;
 
@@ -9,5 +12,9 @@ public final class ActivityTypeStateFactory {
 
   public ActivityTypeState ofType(final String activityType) {
     return new ActivityTypeState(activityType, this.querier);
+  }
+
+  public ActivityTypeState ofType(final Class<? extends Activity> activityType) {
+    return this.ofType(activityType.getAnnotation(ActivityType.class).name());
   }
 }
