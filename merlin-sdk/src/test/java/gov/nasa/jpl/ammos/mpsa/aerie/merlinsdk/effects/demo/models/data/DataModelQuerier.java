47b6ee8ce2e8d2b949b70bbad8cae95023f02d31
Jonathan Castello <Jonathan.Castello@jpl.caltech.edu>
2020-07-18 16:10:49 -0700

    Remove event graph demo adaptation

--- merlin-sdk/src/test/java/gov/nasa/jpl/ammos/mpsa/aerie/merlinsdk/effects/demo/models/data/DataModelQuerier.java
+++
@@ -1,5 +0,0 @@
-package gov.nasa.jpl.ammos.mpsa.aerie.merlinsdk.effects.demo.models.data;
-
-public interface DataModelQuerier {
-  DataBinQuerier getBin(final String binName);
-}
--- merlin-sdk/src/test/java/gov/nasa/jpl/ammos/mpsa/aerie/merlinsdk/effects/demo/states/GettableResource.java
+++
@@ -1,5 +0,0 @@
-package gov.nasa.jpl.ammos.mpsa.aerie.merlinsdk.effects.demo.states;
-
-public interface GettableResource<T> {
-  T get();
-}
