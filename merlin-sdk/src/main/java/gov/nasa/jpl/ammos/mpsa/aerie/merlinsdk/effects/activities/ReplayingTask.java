2e0db24190c2ced736c9f14799b83b7edc1c34a3
Jonathan Castello <Jonathan.Castello@jpl.caltech.edu>
2020-07-19 04:43:08 -0700

    Rename `reactor` parameter to `factory`

--- merlin-sdk/src/main/java/gov/nasa/jpl/ammos/mpsa/aerie/merlinsdk/effects/activities/ReplayingTask.java
+++ merlin-sdk/src/main/java/gov/nasa/jpl/ammos/mpsa/aerie/merlinsdk/effects/activities/ReplayingTask.java
@@ -8,39 +8,39 @@ import java.util.Objects;
 import java.util.function.Consumer;
 
 public final class ReplayingTask<T, Event, Activity> implements SimulationTask<T, Event> {
-  private final TaskFactory<T, Event, Activity> reactor;
+  private final TaskFactory<T, Event, Activity> factory;
   private final String activityId;
   private final Activity activity;
   private final PVector<ActivityBreadcrumb<T, Event>> breadcrumbs;
 
   public ReplayingTask(
-      final TaskFactory<T, Event, Activity> reactor,
+      final TaskFactory<T, Event, Activity> factory,
       final String activityId,
       final Activity activity,
       final PVector<ActivityBreadcrumb<T, Event>> breadcrumbs)
   {
-    this.reactor = Objects.requireNonNull(reactor);
+    this.factory = Objects.requireNonNull(factory);
     this.activityId = Objects.requireNonNull(activityId);
     this.activity = Objects.requireNonNull(activity);
     this.breadcrumbs = Objects.requireNonNull(breadcrumbs);
   }
 
   public ReplayingTask(
-      final TaskFactory<T, Event, Activity> reactor,
+      final TaskFactory<T, Event, Activity> factory,
       final String activityId,
       final Activity activity)
   {
-    this(reactor, activityId, activity, TreePVector.empty());
+    this(factory, activityId, activity, TreePVector.empty());
   }
 
   public ReplayingTask<T, Event, Activity> spawned(final String childId) {
     final var breadcrumbs = this.breadcrumbs.plus(new ActivityBreadcrumb.Spawn<>(childId));
-    return new ReplayingTask<>(this.reactor, this.activityId, this.activity, breadcrumbs);
+    return new ReplayingTask<>(this.factory, this.activityId, this.activity, breadcrumbs);
   }
 
   public ReplayingTask<T, Event, Activity> advancedTo(final History<T, Event> timePoint) {
     final var breadcrumbs = this.breadcrumbs.plus(new ActivityBreadcrumb.Advance<>(timePoint));
-    return new ReplayingTask<>(this.reactor, this.activityId, this.activity, breadcrumbs);
+    return new ReplayingTask<>(this.factory, this.activityId, this.activity, breadcrumbs);
   }
 
   public ActivityBreadcrumb<T, Event> getBreadcrumb(int index) {
@@ -61,11 +61,11 @@ public final class ReplayingTask<T, Event, Activity> implements SimulationTask<T
       final History<T, Event> history,
       final Consumer<ScheduleItem<T, Event>> scheduler)
   {
-    final var context = new ReplayingReactionContext<>(this.reactor, scheduler, this.advancedTo(history));
+    final var context = new ReplayingReactionContext<>(this.factory, scheduler, this.advancedTo(history));
 
     // TODO: avoid using exceptions for control flow by wrapping the executor in a Thread
     try {
-      this.reactor.execute(context, this.activityId, this.activity);
+      this.factory.execute(context, this.activityId, this.activity);
       scheduler.accept(new ScheduleItem.Complete<>(this.activityId));
     } catch (final ReplayingReactionContext.Defer request) {
       scheduler.accept(new ScheduleItem.Defer<>(request.duration, context.getContinuation()));
