2d975e98327872c8aa848c7fc71255c7ba2dfe21
Jonathan Castello <Jonathan.Castello@jpl.caltech.edu>
2020-07-21 16:19:31 -0700

    Extract activity processor into separate module

--- merlin-sdk/src/test/java/gov/nasa/jpl/ammos/mpsa/aerie/merlinsdk/events/SampleEventHandler.java
+++
@@ -1,8 +0,0 @@
-package gov.nasa.jpl.ammos.mpsa.aerie.merlinsdk.events;
-
-import gov.nasa.jpl.ammos.mpsa.aerie.merlinsdk.annotations.MerlinEvent;
-
-@MerlinEvent
-public interface SampleEventHandler<Result> {
-  Result log(String message);
-}
