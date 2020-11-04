fc105fe95d365ceceafabe7ef1964de59c8e23b5
Jonathan Castello <Jonathan.Castello@jpl.caltech.edu>
2020-11-04 05:05:24 -0800

    Remove unused import

--- merlin-sdk/src/main/java/gov/nasa/jpl/ammos/mpsa/aerie/merlinsdk/effects/timeline/SimulationTimeline.java
+++ merlin-sdk/src/main/java/gov/nasa/jpl/ammos/mpsa/aerie/merlinsdk/effects/timeline/SimulationTimeline.java
@@ -10,7 +10,6 @@ import org.apache.commons.lang3.tuple.Pair;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
