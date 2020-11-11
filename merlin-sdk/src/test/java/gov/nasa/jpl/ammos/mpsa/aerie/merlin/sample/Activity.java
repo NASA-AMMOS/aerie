0b24d59c8a9e5ef50f471888dec6d8ee99cbb72c
Jonathan Castello <Jonathan.Castello@jpl.caltech.edu>
2020-11-11 10:03:02 -0800

    Make Context into an interface

--- merlin-sdk/src/test/java/gov/nasa/jpl/ammos/mpsa/aerie/merlin/sample/Activity.java
+++ merlin-sdk/src/test/java/gov/nasa/jpl/ammos/mpsa/aerie/merlin/sample/Activity.java
@@ -1,7 +1,9 @@
 package gov.nasa.jpl.ammos.mpsa.aerie.merlin.sample;
 
+import gov.nasa.jpl.ammos.mpsa.aerie.merlin.framework.Context;
+
 // TODO: Automatically generate at compile time.
 public abstract class Activity<$Schema>
     implements gov.nasa.jpl.ammos.mpsa.aerie.merlin.framework.Activity
-    <Context<? extends $Schema>, FooResources<$Schema>>
+    <Context<? extends $Schema, FooEvent, FooActivityInstance<$Schema>>, FooResources<$Schema>>
 {}
