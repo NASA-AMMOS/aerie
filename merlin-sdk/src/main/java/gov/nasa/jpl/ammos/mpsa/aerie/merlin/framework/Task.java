f12dacf52c15c279da287a80b942d2965e1fac51
Jonathan Castello <Jonathan.Castello@jpl.caltech.edu>
2020-11-20 22:02:53 -0800

    Remove framework-provided Task type

--- merlin-sdk/src/main/java/gov/nasa/jpl/ammos/mpsa/aerie/merlin/framework/Task.java
+++
@@ -1,5 +0,0 @@
-package gov.nasa.jpl.ammos.mpsa.aerie.merlin.framework;
-
-public abstract class Task<$Schema, Resources> extends Module<$Schema> {
-  public abstract void run(Resources resources);
-}
--- merlin-sdk/src/test/java/gov/nasa/jpl/ammos/mpsa/aerie/merlin/sample/generated/Task.java
+++ merlin-sdk/src/test/java/gov/nasa/jpl/ammos/mpsa/aerie/merlin/sample/generated/Task.java
@@ -3,6 +3,6 @@ package gov.nasa.jpl.ammos.mpsa.aerie.merlin.sample.generated;
 import gov.nasa.jpl.ammos.mpsa.aerie.merlin.sample.FooResources;
 
 // TODO: Automatically generate at compile time.
-public abstract class Task<$Schema>
-    extends gov.nasa.jpl.ammos.mpsa.aerie.merlin.framework.Task<$Schema, FooResources<$Schema>>
-{}
+public abstract class Task<$Schema> extends Module<$Schema> {
+  public abstract void run(FooResources<$Schema> resources);
+}
