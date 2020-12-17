d05e2c65e4c97995bdf01965ffc2d39132225b52
Jonathan Castello <Jonathan.Castello@jpl.caltech.edu>
2020-12-16 20:42:56 -0800

    Pare down todo list comments

--- foo-adaptation/src/main/java/gov/nasa/jpl/ammos/mpsa/aerie/fooadaptation/FooResources.java
+++ foo-adaptation/src/main/java/gov/nasa/jpl/ammos/mpsa/aerie/fooadaptation/FooResources.java
@@ -7,18 +7,10 @@ import gov.nasa.jpl.ammos.mpsa.aerie.merlin.framework.states.LinearIntegrationMo
 import gov.nasa.jpl.ammos.mpsa.aerie.merlin.framework.states.RegisterModule;
 
 public final class FooResources<$Schema> extends Module<$Schema> {
-  // Need a clear story for how to logically group resource questions and event emissions together.
-  // Need a way to produce a condition for a resource.
-  // Need a way to assemble conditions into an overall constraint.
-  // Need a way to extract constraints from an adaptation.
   // Need a way to pose constraints against activities, and generally modeling activity behavior with resources.
-  // Need to support waitUntil(condition) (which ought to subsume waitFor(activity)).
-  // Need to port the breadcrumb-based replaying tasks to this framework, so that activities can be written
-  //   in direct style (rather than continuation-passing or state-machine style).
   // Need a clear story for external models.
   // Need to collect profiles from published resources as simulation proceeds.
   // Need to generalize RealDynamics to nonlinear polynomials.
-  // Need to use a more representative event type for the sample.
   // Need to implement compile-time code generation for various aspects of the Framework.
 
   public final RegisterModule<$Schema, Double> foo;
