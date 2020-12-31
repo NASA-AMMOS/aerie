314efbb434aeeea828c3da7280e2572d429553b0
Jonathan Castello <Jonathan.Castello@jpl.caltech.edu>
2020-12-30 20:16:03 -0800

    Remove 0.5-based sample adaptation

--- sample-adaptation/src/main/java/gov/nasa/jpl/ammos/mpsa/aerie/sampleadaptation/events/SampleEventHandler.java
+++
@@ -1,9 +0,0 @@
-package gov.nasa.jpl.ammos.mpsa.aerie.sampleadaptation.events;
-
-import gov.nasa.jpl.ammos.mpsa.aerie.merlinsdk.models.activities.ActivityEvent;
-import gov.nasa.jpl.ammos.mpsa.aerie.contrib.models.independent.events.IndependentStateEvent;
-
-public interface SampleEventHandler<Result> {
-    Result independent(IndependentStateEvent event);
-    Result activity(ActivityEvent event);
-}
