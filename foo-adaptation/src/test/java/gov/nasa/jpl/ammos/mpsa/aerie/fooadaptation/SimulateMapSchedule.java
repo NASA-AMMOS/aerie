48c4026d801ce496ef02751e9d79333d76bf3c4f
Jonathan Castello <Jonathan.Castello@jpl.caltech.edu>
2021-01-02 12:02:05 -0800

    Use JSON encoding in foo-adaptation test

--- foo-adaptation/src/test/java/gov/nasa/jpl/ammos/mpsa/aerie/fooadaptation/SimulateMapSchedule.java
+++ foo-adaptation/src/test/java/gov/nasa/jpl/ammos/mpsa/aerie/fooadaptation/SimulateMapSchedule.java
@@ -3,14 +3,18 @@ package gov.nasa.jpl.ammos.mpsa.aerie.fooadaptation;
 import gov.nasa.jpl.ammos.mpsa.aerie.fooadaptation.generated.GeneratedAdaptationFactory;
 import gov.nasa.jpl.ammos.mpsa.aerie.merlin.driver.SerializedActivity;
 import gov.nasa.jpl.ammos.mpsa.aerie.merlin.driver.SimulationDriver;
+import gov.nasa.jpl.ammos.mpsa.aerie.merlin.driver.json.JsonEncoding;
 import gov.nasa.jpl.ammos.mpsa.aerie.merlin.protocol.SerializedValue;
+import gov.nasa.jpl.ammos.mpsa.aerie.time.Duration;
 import org.apache.commons.lang3.tuple.Pair;
 
+import javax.json.Json;
 import java.time.Instant;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
-import static gov.nasa.jpl.ammos.mpsa.aerie.time.Duration.MILLISECONDS;
+import static gov.nasa.jpl.ammos.mpsa.aerie.time.Duration.MICROSECONDS;
 import static gov.nasa.jpl.ammos.mpsa.aerie.time.Duration.SECOND;
 import static gov.nasa.jpl.ammos.mpsa.aerie.time.Duration.SECONDS;
 import static gov.nasa.jpl.ammos.mpsa.aerie.time.Duration.duration;
@@ -28,50 +32,41 @@ public class SimulateMapSchedule {
   void simulateWithMapSchedule()
   throws SimulationDriver.TaskSpecInstantiationException
   {
-    final var adaptation = new GeneratedAdaptationFactory().instantiate();
-    final var schedule = Map.of(
-        UUID.randomUUID().toString(), Pair.of(
-            duration(0, MILLISECONDS),
-            new SerializedActivity(
-                "foo",
-                Map.of("x", SerializedValue.of(1),
-                       "y", SerializedValue.of("test_1")))),
-        UUID.randomUUID().toString(), Pair.of(
-            duration(50, MILLISECONDS),
-            new SerializedActivity(
-                "foo",
-                Map.of("x", SerializedValue.of(2),
-                       "y", SerializedValue.of("spawn")))),
-        UUID.randomUUID().toString(), Pair.of(
-            duration(50, MILLISECONDS),
-            new SerializedActivity(
-                "foo",
-                Map.of("x", SerializedValue.of(2),
-                       "y", SerializedValue.of("test")))),
-        UUID.randomUUID().toString(), Pair.of(
-            duration(150, MILLISECONDS),
-            new SerializedActivity(
-                "foo",
-                Map.of("x", SerializedValue.of(2),
-                       "y", SerializedValue.of("test_2")))),
-        UUID.randomUUID().toString(), Pair.of(
-            duration(150, MILLISECONDS),
-            new SerializedActivity(
-                "foo",
-                Map.of("x", SerializedValue.of(3),
-                       "y", SerializedValue.of("test_3"))))
-    );
+    final var schedule = loadSchedule();
     final var startTime = Instant.now();
     final var simulationDuration = duration(5, SECONDS);
     final var samplingPeriod = duration(1, SECOND);
 
     final var simulationResults = SimulationDriver.simulate(
-        adaptation,
+        new GeneratedAdaptationFactory().instantiate(),
         schedule,
         startTime,
         simulationDuration,
         samplingPeriod);
 
     simulationResults.timelines.forEach((name, samples) -> System.out.format("%s: %s\n", name, samples));
+  }
+
+  private static Map<String, Pair<Duration, SerializedActivity>> loadSchedule() {
+    final var schedule = new HashMap<String, Pair<Duration, SerializedActivity>>();
+
+    final var planJson = Json.createReader(SimulateMapSchedule.class.getResourceAsStream("plan.json")).readValue();
+    for (final var scheduledActivity : planJson.asJsonArray()) {
+      final var deferInMicroseconds = scheduledActivity.asJsonObject().getJsonNumber("defer").longValueExact();
+      final var activityType = scheduledActivity.asJsonObject().getString("type");
+
+      final var arguments = new HashMap<String, SerializedValue>();
+      for (final var field : scheduledActivity.asJsonObject().getJsonObject("arguments").entrySet()) {
+        arguments.put(field.getKey(), JsonEncoding.decode(field.getValue()));
+      }
+
+      schedule.put(
+          UUID.randomUUID().toString(),
+          Pair.of(
+              duration(deferInMicroseconds, MICROSECONDS),
+              new SerializedActivity(activityType, arguments)));
+    }
+
+    return schedule;
   }
 }
