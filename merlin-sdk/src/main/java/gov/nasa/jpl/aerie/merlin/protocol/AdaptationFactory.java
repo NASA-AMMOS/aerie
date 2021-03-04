e5a5a49da7b4f839b6f7deb8522df94a343538d0
Paul Rosemurgy <paul.c.rosemurgy@jpl.nasa.gov>
2021-03-04 09:49:09 -0800

    Expose `makeBuilder` method within AdaptationFactory

--- merlin-sdk/src/main/java/gov/nasa/jpl/aerie/merlin/protocol/AdaptationFactory.java
+++ merlin-sdk/src/main/java/gov/nasa/jpl/aerie/merlin/protocol/AdaptationFactory.java
@@ -1,5 +1,9 @@
 package gov.nasa.jpl.aerie.merlin.protocol;
 
+import gov.nasa.jpl.aerie.merlin.framework.AdaptationBuilder;
+import gov.nasa.jpl.aerie.merlin.timeline.Schema;
+
 public interface AdaptationFactory {
   Adaptation<?> instantiate();
+  <$Schema> AdaptationBuilder<$Schema> makeBuilder(final Schema.Builder<$Schema> schemaBuilder);
 }
