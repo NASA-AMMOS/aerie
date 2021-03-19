31fd45bab886c08d0cc748c94f0878d655ea5855
roffo <kenneth.r.roffo@jpl.nasa.gov>
2021-03-19 15:58:53 -0400

    Add expression for expressing a discrete profile of reals as a linear profile

---
+++ constraints/src/main/java/gov/nasa/jpl/aerie/constraints/InputMismatchException.java
@@ -0,0 +1,7 @@
+package gov.nasa.jpl.aerie.constraints;
+
+public final class InputMismatchException extends RuntimeException {
+  public InputMismatchException(final String message) {
+    super(message);
+  }
+}
