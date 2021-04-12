d7e020ac881e93c2d94f89bca44d4c736ae4a0a4
Chris Camargo <Christopher.A.Camargo@jpl.nasa.gov>
2021-04-12 16:46:58 -0700

    feat: add change producer activity to banananation
    - Add String value resource we can use to test the
    timeline visualization against

--- banananation/src/main/java/gov/nasa/jpl/aerie/banananation/Mission.java
+++ banananation/src/main/java/gov/nasa/jpl/aerie/banananation/Mission.java
@@ -6,6 +6,7 @@ import gov.nasa.jpl.aerie.contrib.serialization.mappers.BooleanValueMapper;
 import gov.nasa.jpl.aerie.contrib.serialization.mappers.DoubleValueMapper;
 import gov.nasa.jpl.aerie.contrib.serialization.mappers.EnumValueMapper;
 import gov.nasa.jpl.aerie.contrib.serialization.mappers.IntegerValueMapper;
+import gov.nasa.jpl.aerie.contrib.serialization.mappers.StringValueMapper;
 import gov.nasa.jpl.aerie.merlin.framework.Registrar;
 
 public final class Mission {
@@ -13,17 +14,20 @@ public final class Mission {
   public final AdditiveRegister peel;
   public final Register<Flag> flag;
   public final Counter<Integer> plant;
+  public final Register<String> producer;
 
   public Mission(final Registrar registrar) {
     this.flag = Register.create(registrar, Flag.A);
     this.peel = AdditiveRegister.create(registrar, 4.0);
     this.fruit = AdditiveRegister.create(registrar, 4.0);
     this.plant = Counter.ofInteger(registrar, 200);
+    this.producer = Register.create(registrar, "Chiquita");
 
     registrar.resource("/flag", this.flag, new EnumValueMapper<>(Flag.class));
     registrar.resource("/flag/conflicted", this.flag::isConflicted, new BooleanValueMapper());
     registrar.resource("/peel", this.peel, new DoubleValueMapper());
     registrar.resource("/fruit", this.fruit, new DoubleValueMapper());
     registrar.resource("/plant", this.plant, new IntegerValueMapper());
+    registrar.resource("/producer", this.producer, new StringValueMapper());
   }
 }
--- banananation/src/main/java/gov/nasa/jpl/aerie/banananation/package-info.java
+++ banananation/src/main/java/gov/nasa/jpl/aerie/banananation/package-info.java
@@ -6,6 +6,7 @@
 @WithActivityType(PeelBananaActivity.class)
 @WithActivityType(ParameterTestActivity.class)
 @WithActivityType(PickBananaActivity.class)
+@WithActivityType(ChangeProducerActivity.class)
 
 package gov.nasa.jpl.aerie.banananation;
 
@@ -13,6 +14,7 @@ import gov.nasa.jpl.aerie.banananation.activities.BiteBananaActivity;
 import gov.nasa.jpl.aerie.banananation.activities.ParameterTestActivity;
 import gov.nasa.jpl.aerie.banananation.activities.PeelBananaActivity;
 import gov.nasa.jpl.aerie.banananation.activities.PickBananaActivity;
+import gov.nasa.jpl.aerie.banananation.activities.ChangeProducerActivity;
 import gov.nasa.jpl.aerie.contrib.serialization.rulesets.BasicValueMappers;
 import gov.nasa.jpl.aerie.merlin.framework.annotations.Adaptation;
 import gov.nasa.jpl.aerie.merlin.framework.annotations.Adaptation.WithActivityType;
