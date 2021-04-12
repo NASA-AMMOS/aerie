fe03e8ccaa219596554467c02b3f1524286c559d
Chris Camargo <Christopher.A.Camargo@jpl.nasa.gov>
2021-04-12 13:00:23 -0700

    feat: add pick banana activity to banananation
    - This adds an integer resource we can use to test the
    timeline visualization against

--- banananation/src/main/java/gov/nasa/jpl/aerie/banananation/package-info.java
+++ banananation/src/main/java/gov/nasa/jpl/aerie/banananation/package-info.java
@@ -5,12 +5,14 @@
 @WithActivityType(BiteBananaActivity.class)
 @WithActivityType(PeelBananaActivity.class)
 @WithActivityType(ParameterTestActivity.class)
+@WithActivityType(PickBananaActivity.class)
 
 package gov.nasa.jpl.aerie.banananation;
 
 import gov.nasa.jpl.aerie.banananation.activities.BiteBananaActivity;
 import gov.nasa.jpl.aerie.banananation.activities.ParameterTestActivity;
 import gov.nasa.jpl.aerie.banananation.activities.PeelBananaActivity;
+import gov.nasa.jpl.aerie.banananation.activities.PickBananaActivity;
 import gov.nasa.jpl.aerie.contrib.serialization.rulesets.BasicValueMappers;
 import gov.nasa.jpl.aerie.merlin.framework.annotations.Adaptation;
 import gov.nasa.jpl.aerie.merlin.framework.annotations.Adaptation.WithActivityType;
