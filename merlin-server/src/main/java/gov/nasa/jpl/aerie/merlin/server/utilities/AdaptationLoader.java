9640049d3934af7618188b6ffceb75b0b255c4fa
Jonathan Castello <Jonathan.Castello@jpl.caltech.edu>
2021-05-22 04:15:30 -0700

    Load an adaptation factory class by name
    When running the DevAppDriver, the foo-adaptation is on the classpath
    due to some tests that use it.  Unfortunately, ServiceLoader is very
    sensitive to classpath changes, and when we request information about
    a particular uploaded adaptation via the HTTP API, ServiceLoader often
    picks up foo-adaptation instead of the JAR we meant to interrogate.
    
    ServiceLoader makes sense to use when you don't know who is providing
    a service.  In our case, we want to use a specific provider, not just
    any provider we can find.
    
    This commit changes our strategy to load a specific class by name.  We
    identify the adaptation's factory class via the same mechanism as
    ServiceLoader -- a file in the JAR's META-INF/services/ directory --
    and then load that class directly via a classloader pointing to the
    adaptation JAR.

--- merlin-server/src/main/java/gov/nasa/jpl/aerie/merlin/server/utilities/AdaptationLoader.java
+++ merlin-server/src/main/java/gov/nasa/jpl/aerie/merlin/server/utilities/AdaptationLoader.java
@@ -6,37 +6,76 @@ import gov.nasa.jpl.aerie.merlin.protocol.AdaptationFactory;
 import gov.nasa.jpl.aerie.merlin.protocol.SerializedValue;
 import gov.nasa.jpl.aerie.merlin.timeline.Schema;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.InvocationTargetException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
-import java.util.ServiceLoader;
-import java.util.ServiceLoader.Provider;
+import java.util.jar.JarFile;
+import java.util.stream.Collectors;
 
 public final class AdaptationLoader {
     public static Adaptation<?> loadAdaptation(final SerializedValue missionModelConfig, final Path path, final String name, final String version)
         throws AdaptationLoadException
     {
-        final var factory = loadAdaptationProvider(path, name, version).get();
+        final var factory = loadAdaptationProvider(path, name, version);
         final var builder = new AdaptationBuilder<>(Schema.builder());
         factory.instantiate(missionModelConfig, builder);
         return builder.build();
     }
 
-    public static Provider<AdaptationFactory> loadAdaptationProvider(final Path path, final String name, final String version)
+    public static AdaptationFactory loadAdaptationProvider(final Path path, final String name, final String version)
         throws AdaptationLoadException
     {
+        // Look for a MerlinAdaptation implementor in the adaptation. For correctness, we're assuming there's
+        // only one matching MerlinAdaptation in any given adaptation.
+        final var className = getImplementingClassName(path, name, version);
+
         // Construct a ClassLoader with access to classes in the adaptation location.
         final var parentClassLoader = Thread.currentThread().getContextClassLoader();
         final var classLoader = new URLClassLoader(new URL[] {adaptationPathToUrl(path)}, parentClassLoader);
 
-        // Look for MerlinAdaptation implementor in the adaptation. For correctness, we're assuming there's
-        // only one matching MerlinAdaptation in any given adaptation.
-        return ServiceLoader
-            .load(AdaptationFactory.class, classLoader)
-            .stream()
-            .findFirst()
-            .orElseThrow(() -> new AdaptationLoadException(path, name, version));
+        try {
+            final var factoryClass$ = classLoader.loadClass(className);
+            if (!AdaptationFactory.class.isAssignableFrom(factoryClass$)) {
+                throw new AdaptationLoadException(path, name, version);
+            }
+
+            // SAFETY: We checked above that AdaptationFactory is assignable from this type.
+            @SuppressWarnings("unchecked")
+            final var factoryClass = (Class<? extends AdaptationFactory>) factoryClass$;
+
+            return factoryClass.getConstructor().newInstance();
+        } catch (final ClassNotFoundException | NoSuchMethodException | InstantiationException
+            | IllegalAccessException | InvocationTargetException ex)
+        {
+            throw new AdaptationLoadException(path, name, version, ex);
+        }
+    }
+
+    private static String getImplementingClassName(final Path jarPath, final String name, final String version)
+    throws AdaptationLoadException {
+        try {
+            final var jarFile = new JarFile(jarPath.toFile());
+            final var jarEntry = jarFile.getEntry("META-INF/services/" + AdaptationFactory.class.getCanonicalName());
+            final var inputStream = jarFile.getInputStream(jarEntry);
+
+            final var classPathList = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))
+                .lines()
+                .collect(Collectors.toList());
+
+            if (classPathList.size() != 1) {
+                throw new AdaptationLoadException(jarPath, name, version);
+            }
+
+            return classPathList.get(0);
+        } catch (final IOException ex) {
+            throw new AdaptationLoadException(jarPath, name, version, ex);
+        }
     }
 
     private static URL adaptationPathToUrl(final Path path) {
@@ -51,12 +90,18 @@ public final class AdaptationLoader {
 
     public static class AdaptationLoadException extends Exception {
         private AdaptationLoadException(final Path path, final String name, final String version) {
-            super(String.format(
-                "No implementation found for `%s` at path `%s` wih name \"%s\" and version \"%s\"",
-                AdaptationFactory.class.getSimpleName(),
-                path,
-                name,
-                version));
+            this(path, name, version, null);
+        }
+
+        private AdaptationLoadException(final Path path, final String name, final String version, final Throwable cause) {
+            super(
+                String.format(
+                    "No implementation found for `%s` at path `%s` wih name \"%s\" and version \"%s\"",
+                    AdaptationFactory.class.getSimpleName(),
+                    path,
+                    name,
+                    version),
+                cause);
         }
     }
 }
