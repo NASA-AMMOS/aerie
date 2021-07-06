d0c73bb4fb900bc023ee5e78fffcf29622b19d0e
Jonathan Castello <Jonathan.Castello@jpl.caltech.edu>
2021-07-06 13:45:26 -0700

    Fix Register to correctly report conflicts on same-value writes

--- contrib/src/test/java/gov/nasa/jpl/aerie/contrib/cells/register/RegisterTest.java
+++ contrib/src/test/java/gov/nasa/jpl/aerie/contrib/cells/register/RegisterTest.java
@@ -7,7 +7,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
+import java.util.List;
+
 import static gov.nasa.jpl.aerie.merlin.framework.ModelActions.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -16,13 +19,26 @@ public final class RegisterTest {
   private final Register<Integer> register = Register.create(0);
 
   @Test
-  @DisplayName("Conflicting writes should mark the conflict flag and have no other effect")
-  public void testConflict() {
+  @DisplayName("Conflicting writes with different values should mark the conflict flag and have no other effect")
+  public void differentValueConflicts() {
     assertEquals(0, register.get());
     spawn(() -> register.set(1));
     spawn(() -> register.set(2));
     delay(Duration.ZERO);
-    assertTrue(register.isConflicted());
+    assertAll(List.of(
+        () -> assertTrue(register.isConflicted()),
+        () -> assertEquals(0, register.get())));
+  }
+
+  @Test
+  @DisplayName("Conflicting writes with the same value should mark the conflict flag and have no other effect")
+  public void sameValueConflicts() {
     assertEquals(0, register.get());
+    spawn(() -> register.set(1));
+    spawn(() -> register.set(1));
+    delay(Duration.ZERO);
+    assertAll(List.of(
+        () -> assertTrue(register.isConflicted()),
+        () -> assertEquals(0, register.get())));
   }
 }
