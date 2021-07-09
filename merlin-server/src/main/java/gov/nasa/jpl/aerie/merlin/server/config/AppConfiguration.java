f355eb2b8cc19f239cf84626ee5efa7f5ab2ed7c
Jonathan Castello <Jonathan.Castello@jpl.caltech.edu>
2021-07-09 00:39:48 -0700

    Remove AppConfiguration.Builder

--- merlin-server/src/main/java/gov/nasa/jpl/aerie/merlin/server/config/AppConfiguration.java
+++ merlin-server/src/main/java/gov/nasa/jpl/aerie/merlin/server/config/AppConfiguration.java
@@ -4,6 +4,7 @@ import javax.json.JsonObject;
 import java.net.URI;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.function.BiFunction;
 
 public record AppConfiguration (
     int httpPort,
@@ -17,94 +18,41 @@ public record AppConfiguration (
         Objects.requireNonNull(store);
     }
 
-    public static Builder builder() {
-        return new Builder();
-    }
-
     public static AppConfiguration parseProperties(final JsonObject config) {
-        var builder = builder();
+        final var httpPort = config.getInt("HTTP_PORT");
+        final var javalinLogging =
+            (config.getBoolean("enable-javalin-logging", false))
+                ? JavalinLoggingState.Enabled
+                : JavalinLoggingState.Disabled;
 
-        builder.setHttpPort(config.getInt("HTTP_PORT"));
-        builder.setMongoUri(URI.create(config.getString("MONGO_URI")));
-        builder.setMongoDatabase(config.getString("MONGO_DATABASE"));
-        builder.setMongoPlanCollection(config.getString("MONGO_PLAN_COLLECTION"));
-        builder.setMongoActivityCollection(config.getString("MONGO_ACTIVITY_COLLECTION"));
-        builder.setMongoAdaptationCollection(config.getString("MONGO_ADAPTATION_COLLECTION"));
-        builder.setMongoSimulationResultsCollection(config.getString("MONGO_SIMULATION_RESULTS_COLLECTION"));
+        final var missionModelConfigPath = getOptional(config, "MISSION_MODEL_CONFIG_PATH", JsonObject::getString);
 
-        if (config.getBoolean("enable-javalin-logging", false)) {
-            builder.setJavalinLogging(JavalinLoggingState.Enabled);
-        }
+        final var mongoUri = URI.create(config.getString("MONGO_URI"));
+        final var mongoDatabase = config.getString("MONGO_DATABASE");
+        final var mongoPlanCollection = config.getString("MONGO_PLAN_COLLECTION");
+        final var mongoActivityCollection = config.getString("MONGO_ACTIVITY_COLLECTION");
+        final var mongoAdaptationCollection = config.getString("MONGO_ADAPTATION_COLLECTION");
+        final var mongoSimulationResultsCollection = config.getString("MONGO_SIMULATION_RESULTS_COLLECTION");
 
-        if (config.containsKey("MISSION_MODEL_CONFIG_PATH")) {
-            builder.setMissionModelConfigPath(config.getString("MISSION_MODEL_CONFIG_PATH"));
-        }
-
-        return builder.build();
+        return new AppConfiguration(
+            httpPort,
+            javalinLogging,
+            missionModelConfigPath,
+            new MongoStore(
+                mongoUri,
+                mongoDatabase,
+                mongoPlanCollection,
+                mongoActivityCollection,
+                mongoAdaptationCollection,
+                mongoSimulationResultsCollection));
     }
 
-    public static final class Builder {
-        private Optional<Integer> httpPort = Optional.empty();
-        private Optional<URI> mongoUri = Optional.empty();
-        private Optional<String> mongoDatabase = Optional.empty();
-        private Optional<String> mongoPlanCollection = Optional.empty();
-        private Optional<String> mongoActivityCollection = Optional.empty();
-        private Optional<String> mongoAdaptationCollection = Optional.empty();
-        private Optional<String> mongoSimulationResultsCollection = Optional.empty();
-        private Optional<JavalinLoggingState> enableJavalinLogging = Optional.empty();
-        private Optional<String> missionModelConfigPath = Optional.empty();
-
-        private Builder() {}
-
-        public Builder setHttpPort(int httpPort) {
-            this.httpPort = Optional.of(httpPort);
-            return this;
-        }
-        public Builder setMongoUri(URI mongoUri) {
-            this.mongoUri = Optional.of(mongoUri);
-            return this;
-        }
-        public Builder setMongoDatabase(String mongoDatabase) {
-            this.mongoDatabase = Optional.of(mongoDatabase);
-            return this;
-        }
-        public Builder setMongoPlanCollection(String mongoPlanCollection) {
-            this.mongoPlanCollection = Optional.of(mongoPlanCollection);
-            return this;
-        }
-        public Builder setMongoActivityCollection(String mongoActivityCollection) {
-            this.mongoActivityCollection = Optional.of(mongoActivityCollection);
-            return this;
-        }
-        public Builder setMongoAdaptationCollection(String mongoAdaptationCollection) {
-            this.mongoAdaptationCollection = Optional.of(mongoAdaptationCollection);
-            return this;
-        }
-        public Builder setMongoSimulationResultsCollection(String mongoSimulationResultsCollection) {
-          this.mongoSimulationResultsCollection = Optional.of(mongoSimulationResultsCollection);
-          return this;
-        }
-        public Builder setJavalinLogging(JavalinLoggingState enableJavalinLogging) {
-            this.enableJavalinLogging = Optional.of(enableJavalinLogging);
-            return this;
-        }
-        public Builder setMissionModelConfigPath(final String missionModelConfigPath) {
-            this.missionModelConfigPath = Optional.of(missionModelConfigPath);
-            return this;
-        }
-
-        public AppConfiguration build() {
-            return new AppConfiguration(
-                this.httpPort.orElseThrow(),
-                this.enableJavalinLogging.orElse(JavalinLoggingState.Disabled),
-                this.missionModelConfigPath,
-                new MongoStore(
-                    this.mongoUri.orElseThrow(),
-                    this.mongoDatabase.orElseThrow(),
-                    this.mongoPlanCollection.orElseThrow(),
-                    this.mongoActivityCollection.orElseThrow(),
-                    this.mongoAdaptationCollection.orElseThrow(),
-                    this.mongoSimulationResultsCollection.orElseThrow()));
+    private static <T>
+    Optional<T> getOptional(final JsonObject object, final String key, final BiFunction<JsonObject, String, T> getter) {
+        if (object.containsKey(key)) {
+            return Optional.of(getter.apply(object, key));
+        } else {
+            return Optional.empty();
         }
     }
 }
--- merlin-server/src/test/java/gov/nasa/jpl/aerie/merlin/server/config/AppConfigurationTest.java
+++ merlin-server/src/test/java/gov/nasa/jpl/aerie/merlin/server/config/AppConfigurationTest.java
@@ -5,40 +5,33 @@ import org.junit.Test;
 import javax.json.Json;
 import javax.json.JsonObject;
 import java.net.URI;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AppConfigurationTest {
-
     @Test
     public void testParseProperties() {
-        // Create JsonObject with expected properties
-        int http_port = 7654;
-        URI mongo_uri = URI.create("http://localhost.mongo.test");
-        String mongo_database = "mongo_database_test";
-        String mongo_plan_collection = "mongo_plan_collection_test";
-        String mongo_activity_collection = "mongo_activity_collection_test";
-        String mongo_adaptation_collection = "mongo_adaptation_collection_test";
-        String mongo_simulation_results_collection = "mongo_simulation_results_collection_test";
-
-        AppConfiguration expected = AppConfiguration.builder()
-                .setHttpPort(http_port)
-                .setMongoUri(mongo_uri)
-                .setMongoDatabase(mongo_database)
-                .setMongoPlanCollection(mongo_plan_collection)
-                .setMongoActivityCollection(mongo_activity_collection)
-                .setMongoAdaptationCollection(mongo_adaptation_collection)
-                .setMongoSimulationResultsCollection(mongo_simulation_results_collection)
-                .build();
+        AppConfiguration expected = new AppConfiguration(
+            7654,
+            JavalinLoggingState.Disabled,
+            Optional.empty(),
+            new MongoStore(
+                URI.create("http://localhost.mongo.test"),
+                "mongo_database_test",
+                "mongo_plan_collection_test",
+                "mongo_activity_collection_test",
+                "mongo_adaptation_collection_test",
+                "mongo_simulation_results_collection_test"));
 
         JsonObject config = Json.createObjectBuilder()
-                .add("HTTP_PORT", http_port)
-                .add("MONGO_URI", mongo_uri.toString())
-                .add("MONGO_DATABASE", mongo_database)
-                .add("MONGO_PLAN_COLLECTION", mongo_plan_collection)
-                .add("MONGO_ACTIVITY_COLLECTION", mongo_activity_collection)
-                .add("MONGO_ADAPTATION_COLLECTION", mongo_adaptation_collection)
-                .add("MONGO_SIMULATION_RESULTS_COLLECTION", mongo_simulation_results_collection)
+                .add("HTTP_PORT", expected.httpPort())
+                .add("MONGO_URI", expected.store().uri().toString())
+                .add("MONGO_DATABASE", expected.store().database())
+                .add("MONGO_PLAN_COLLECTION", expected.store().planCollection())
+                .add("MONGO_ACTIVITY_COLLECTION", expected.store().activityCollection())
+                .add("MONGO_ADAPTATION_COLLECTION", expected.store().adaptationCollection())
+                .add("MONGO_SIMULATION_RESULTS_COLLECTION", expected.store().simulationResultsCollection())
                 .build();
 
         // Parse the JsonObject with parseProperties
