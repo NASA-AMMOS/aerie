bad35d5c261670a951ae962333e70deba0d4ceea
Jonathan Castello <Jonathan.Castello@jpl.caltech.edu>
2021-07-11 17:46:21 -0700

    Cancel the temporary cell in UncachedSimulationService
    If the UncachedSimulationService is used with a simulation agent that
    operates asynchronously, and hence expects a persistent location where
    the results can be placed when they're ready, the cell will almost
    always be Incomplete when the UncachedSimulationService returns. Since
    the cell is a one-shot throwaway, we should at least cancel the cell
    after getting the results so that the simulation agent knows it can
    safely abort.

--- merlin-server/src/main/java/gov/nasa/jpl/aerie/merlin/server/services/UncachedSimulationService.java
+++ merlin-server/src/main/java/gov/nasa/jpl/aerie/merlin/server/services/UncachedSimulationService.java
@@ -2,19 +2,25 @@ package gov.nasa.jpl.aerie.merlin.server.services;
 
 import gov.nasa.jpl.aerie.merlin.server.ResultsProtocol;
 
-import java.util.Objects;
-
-public final class UncachedSimulationService implements SimulationService {
-  private final SynchronousSimulationAgent action;
-
-  public UncachedSimulationService(final SynchronousSimulationAgent action) {
-    this.action = Objects.requireNonNull(action);
-  }
-
+public record UncachedSimulationService (SimulationAgent action) implements SimulationService {
   @Override
   public ResultsProtocol.State getSimulationResults(final String planId, final long planRevision) {
     final var cell = new ResultsProtocol.InMemoryCell();
-    this.action.simulate(planId, planRevision, cell);
-    return cell.get();
+
+    try {
+      this.action.simulate(planId, planRevision, cell);
+    } catch (final InterruptedException ex) {
+      // Do nothing. We'll get the current result (probably an Incomplete)
+      // and throw away the cell anyway.
+
+      // TODO: Fail back to the common supervisor of this Service and the Agent.
+      //   We don't know if the simulation request was successfully received or not,
+      //   so the interaction between the two is now in an undefined state.
+    }
+
+    final var result = cell.get();
+    cell.cancel();
+
+    return result;
   }
 }
