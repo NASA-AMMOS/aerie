936b7932b39e9858b38ca388f2039adc3f9f5710
Jonathan Castello <Jonathan.Castello@jpl.caltech.edu>
2021-09-25 16:31:01 -0700

    Add a common supertype Approximator
    Ideally, we'll get rid of these Approximator classes soon, but until
    then, it's much easier to work with them polymorphically.

--- merlin-sdk/src/main/java/gov/nasa/jpl/aerie/merlin/protocol/model/RealApproximator.java
+++ merlin-sdk/src/main/java/gov/nasa/jpl/aerie/merlin/protocol/model/RealApproximator.java
@@ -1,10 +1,8 @@
 package gov.nasa.jpl.aerie.merlin.protocol.model;
 
-import gov.nasa.jpl.aerie.merlin.protocol.types.DelimitedDynamics;
 import gov.nasa.jpl.aerie.merlin.protocol.types.RealDynamics;
 
-public interface RealApproximator<Dynamics> {
+public interface RealApproximator<Dynamics> extends Approximator<Dynamics, RealDynamics> {
   // TODO: Allow for finer control over the approximation, e.g. "approximate by steps".
   //   or "approximate by error tolerance".
-  Iterable<DelimitedDynamics<RealDynamics>> approximate(Dynamics dynamics);
 }
