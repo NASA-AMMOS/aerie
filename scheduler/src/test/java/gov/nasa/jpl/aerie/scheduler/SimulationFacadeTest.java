7db880be6785a2c32d4d148b6ef545b9ce812f25
srschaff <steve.schaffer@jpl.nasa.gov>
2021-10-26 02:40:33 -0700

    add new SimulationFacade tests using small simulated plan against all query types

--- scheduler/src/test/java/gov/nasa/jpl/aerie/scheduler/SimulationFacadeTest.java
+++ scheduler/src/test/java/gov/nasa/jpl/aerie/scheduler/SimulationFacadeTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -17,14 +18,15 @@ public class SimulationFacadeTest {
   SimulationFacade facade;
 
   //concrete named time points used to setup tests and validate expectations
-  private static final Time beginHorizon = Time.fromMilli(0);
-  private static final Time endHorizon = Time.fromMilli(5000);
   private static final Time t0 = Time.fromMilli(0);
-  private static final Time t1 = Time.fromMilli(1500);
+  private static final Time t1 = Time.fromMilli(1000);
+  private static final Time t1_5 = Time.fromMilli(1500);
+  private static final Time t2 = Time.fromMilli(2000);
+  private static final Time tEnd = Time.fromMilli(5000);
 
   //hard-coded range of scheduling/simulation operations
-  private static final Range<Time> planningHorizon = new Range<>(beginHorizon, endHorizon);
-  private static final TimeWindows entireHorizon = TimeWindows.of(planningHorizon);
+  private static final Range<Time> horizon = new Range<>(t0, tEnd);
+  private static final TimeWindows entireHorizon = TimeWindows.of(horizon);
 
   //scheduler-side mirrors of test activity types used
   //TODO: should eventually mirror these from the adaptation itself
@@ -55,11 +57,11 @@ public class SimulationFacadeTest {
   public void setUp() {
     adaptation = SimulationUtility.getAdaptation();
 
-    missionModel = new MissionModel(adaptation, planningHorizon);
+    missionModel = new MissionModel(adaptation, horizon);
     missionModel.add(actTypeBite);
     missionModel.add(actTypePeel);
 
-    facade = new SimulationFacade(planningHorizon, adaptation);
+    facade = new SimulationFacade(horizon, adaptation);
   }
 
   @AfterEach
@@ -118,23 +120,24 @@ public class SimulationFacadeTest {
   }
 
   /**
-   * constructs a simple test plan with one peel and bite:
-   * time:-------|t0--------|t1-------->
-   * |peel(fS)  |bite(0.1)
+   * constructs a simple test plan with one peel and bite
    *
-   * should result in resource histories:
-   * time:-------|t0--------|t1-------->
-   * peel:4.0----|3.0------------------>
-   * fruit:4.0----|3.0-------|2.9------->
+   * expected activity/resource histories:
+   * <pre>
+   * time: |t0--------|t1--------|t2-------->
+   * acts:            |peel(fS)  |bite(0.1)
+   * peel: |4.0-------|3.0------------------>
+   * fruit:|4.0-------|3.0-------|2.9------->
+   * </pre>
    **/
   private PlanInMemory makeTestPlanP0B1() {
     final var plan = makeEmptyPlan();
 
-    var act1 = new ActivityInstance("PeelBanana1", actTypePeel, t0);
+    var act1 = new ActivityInstance("PeelBanana1", actTypePeel, t1);
     act1.setParameters(Map.of("peelDirection", "fromStem"));
     plan.add(act1);
 
-    var act2 = new ActivityInstance("BiteBanana1", actTypeBite, t1);
+    var act2 = new ActivityInstance("BiteBanana1", actTypeBite, t2);
     act2.setParameters(Map.of("biteSize", 0.1));
     plan.add(act2);
 
@@ -142,12 +145,57 @@ public class SimulationFacadeTest {
   }
 
   @Test
-  public void doubleWhenValueAboveOnSimplePlan() {
+  public void getValueAtTimeDoubleOnSimplePlanMidpoint() {
+    facade.simulatePlan(makeTestPlanP0B1());
+    var actual = getFruitRes().getValueAtTime(t1_5);
+    assertThat(actual).isEqualTo(3.0);
+  }
+
+  @Test
+  public void getValueAtTimeDoubleOnSimplePlan() {
+    facade.simulatePlan(makeTestPlanP0B1());
+    var actual = getFruitRes().getValueAtTime(t2);
+    assertThat(actual).isEqualTo(2.9);
+  }
+
+  @Test
+  public void whenValueAboveDoubleOnSimplePlan() {
     facade.simulatePlan(makeTestPlanP0B1());
     var actual = getFruitRes().whenValueAbove(2.9, entireHorizon);
-    var expected = TimeWindows.of(t0, t1);
+    var expected = TimeWindows.of(t0, t2);
     assertThat(actual).isEqualTo(expected);
   }
 
+  @Test
+  public void whenValueBelowDoubleOnSimplePlan() {
+    facade.simulatePlan(makeTestPlanP0B1());
+    var actual = getFruitRes().whenValueBelow(3.0, entireHorizon);
+    var expected = TimeWindows.of(t2, tEnd);
+    assertThat(actual).isEqualTo(expected);
+  }
+
+  @Test
+  public void whenValueBetweenDoubleOnSimplePlan() {
+    facade.simulatePlan(makeTestPlanP0B1());
+    var actual = getFruitRes().whenValueBetween(3.00, 3.99, entireHorizon);
+    var expected = TimeWindows.of(t1, t2);
+    assertThat(actual).isEqualTo(expected);
+  }
+
+  @Test
+  public void whenValueEqualDoubleOnSimplePlan() {
+    facade.simulatePlan(makeTestPlanP0B1());
+    var actual = getFruitRes().whenValueEqual(3.00, entireHorizon);
+    var expected = TimeWindows.of(t1, t2);
+    assertThat(actual).isEqualTo(expected);
+  }
+
+  @Test
+  public void whenValueNotEqualDoubleOnSimplePlan() {
+    facade.simulatePlan(makeTestPlanP0B1());
+    var actual = getFruitRes().whenValueNotEqual(3.00, entireHorizon);
+    var expected = TimeWindows.of(List.of(Range.of(t0, t1), Range.of(t2, tEnd)));
+    assertThat(actual).isEqualTo(expected);
+  }
 
 }
