b1d98ff9b24b769d8ef9578cde53aa2231c40eb1
Jonathan Castello <Jonathan.Castello@jpl.caltech.edu>
2021-10-26 16:36:02 -0700

    Derive an iterative implementation of EventGraph#evaluate
    This implementation was automatically derived from the recursive
    implementation via a technique called "defunctionalizing the
    continuation".  It's "automatic" in the sense that no creativity is
    required; but I did have to follow the recipe manually, of course.
    
    It's good to keep the recursive implementation around, both for
    ensuring the correctness of the derived implementation relative to the
    natural one, and so that future changes can be made to the recursive
    implementation, and then the defunctionalized implementation can be
    tossed out and regenerated from scratch. (It's quite hard sometimes to
    make edits directly to an iterative implementation; they can be very
    sensitive and require nonlocal reasoning.)

---
+++ merlin-driver/src/main/java/gov/nasa/jpl/aerie/merlin/timeline/effects/EventGraphEvaluator.java
@@ -0,0 +1,23 @@
+package gov.nasa.jpl.aerie.merlin.timeline.effects;
+
+import gov.nasa.jpl.aerie.merlin.protocol.model.EffectTrait;
+
+import java.util.Optional;
+import java.util.function.Function;
+
+public interface EventGraphEvaluator {
+  <EventType, Effect> Optional<Effect> evaluateOptional(
+      EffectTrait<Effect> trait,
+      Function<EventType, Optional<Effect>> substitution,
+      EventGraph<EventType> graph);
+
+  default <EventType, Effect> Effect evaluate(
+      final EffectTrait<Effect> trait,
+      final Function<EventType, Effect> substitution,
+      final EventGraph<EventType> graph
+  ) {
+    return this
+        .evaluateOptional(trait, substitution.andThen(Optional::of), graph)
+        .orElseGet(trait::empty);
+  }
+}
---
+++ merlin-driver/src/main/java/gov/nasa/jpl/aerie/merlin/timeline/effects/IterativeEventGraphEvaluator.java
@@ -0,0 +1,91 @@
+package gov.nasa.jpl.aerie.merlin.timeline.effects;
+
+import gov.nasa.jpl.aerie.merlin.protocol.model.EffectTrait;
+
+import java.util.Optional;
+import java.util.function.Function;
+
+public final class IterativeEventGraphEvaluator implements EventGraphEvaluator {
+  @Override
+  public <EventType, Effect>
+  Optional<Effect> evaluateOptional(
+      final EffectTrait<Effect> trait,
+      final Function<EventType, Optional<Effect>> substitution,
+      EventGraph<EventType> graph
+  ) {
+    EvaluationContinuation2<EventType, Effect> andThen = new EvaluationContinuation2.Empty<>();
+
+    while (true) {
+      // Drill down the leftmost branches of the par-seq graph until we hit a leaf.
+      Optional<Effect> effect$;
+      while (true) {
+        if (graph instanceof EventGraph.Sequentially<EventType> g) {
+          graph = g.prefix();
+          andThen = new EvaluationContinuation2.Right<>(Combiner.Sequentially, g.suffix(), andThen);
+        } else if (graph instanceof EventGraph.Concurrently<EventType> g) {
+          graph = g.left();
+          andThen = new EvaluationContinuation2.Right<>(Combiner.Concurrently, g.right(), andThen);
+        } else if (graph instanceof EventGraph.Atom<EventType> g) {
+          effect$ = substitution.apply(g.atom());
+          break;
+        } else if (graph instanceof EventGraph.Empty) {
+          effect$ = Optional.empty();
+          break;
+        } else {
+          throw new IllegalArgumentException();
+        }
+      }
+
+      // If this branch didn't produce anything, use the sibling's value instead.
+      Effect effect;
+      if (effect$.isPresent()) {
+        effect = effect$.get();
+      } else {
+        if (andThen instanceof EvaluationContinuation2.Combine<EventType, Effect> f) {
+          andThen = f.andThen();
+          effect = f.left();
+        } else if (andThen instanceof EvaluationContinuation2.Right<EventType, Effect> f) {
+          andThen = f.andThen();
+          graph = f.right();
+          continue;
+        } else if (andThen instanceof EvaluationContinuation2.Empty) {
+          return Optional.of(trait.empty());
+        } else {
+          throw new IllegalArgumentException();
+        }
+      }
+
+      // Retrace our steps, accumulating the result until we need to drill down again.
+      while (true) {
+        if (andThen instanceof EvaluationContinuation2.Combine<EventType, Effect> f) {
+          andThen = f.andThen();
+          effect = switch (f.combiner()) {
+            case Sequentially -> trait.sequentially(f.left(), effect);
+            case Concurrently -> trait.concurrently(f.left(), effect);
+          };
+        } else if (andThen instanceof EvaluationContinuation2.Right<EventType, Effect> f) {
+          andThen = new EvaluationContinuation2.Combine<>(f.combiner(), effect, f.andThen());
+          graph = f.right();
+          break;
+        } else if (andThen instanceof EvaluationContinuation2.Empty) {
+          return Optional.of(effect);
+        } else {
+          throw new IllegalArgumentException();
+        }
+      }
+    }
+  }
+
+  private enum Combiner { Sequentially, Concurrently }
+
+  private sealed interface EvaluationContinuation2<Event, Effect> {
+    record Empty<Event, Effect> ()
+        implements EvaluationContinuation2<Event, Effect> {}
+
+    record Right<Event, Effect> (Combiner combiner, EventGraph<Event> right, EvaluationContinuation2<Event, Effect> andThen)
+        implements EvaluationContinuation2<Event, Effect> {}
+
+    record Combine<Event, Effect> (Combiner combiner, Effect left, EvaluationContinuation2<Event, Effect> andThen)
+        implements EvaluationContinuation2<Event, Effect> {}
+  }
+}
