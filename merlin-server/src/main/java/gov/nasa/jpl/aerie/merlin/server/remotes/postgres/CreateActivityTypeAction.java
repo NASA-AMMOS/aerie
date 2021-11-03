27cafaabf8bd5875039ae6fe06e808ce6a2c908b
Ryan Goetz <Ryan.R.Goetz@jpl.nasa.gov>
2021-11-03 12:34:47 -0700

    CreateActivityTypeAction builds a string to entry and hook up everything.
    Issue: When uploading a new model jar. The models activityType's parameter order isn't preserved anymore when we swapped to Hasura. This is because json isn't designed to preserve any order.
    
    A map of 'String' to entry(ordered, ValueSchema) is created and passed to `setValueSchemaOrderedMap` The server will parse out the order and ValueSchema and send a valid json to postgres and Hasura. The 'ActivityType' entries in the DB now contained an order number for each parameter. The Aerie-UI can now sort with this 'order' entry keeping the order that Activity Implementation builder intended.
    
    AERIE-1541

--- merlin-server/src/main/java/gov/nasa/jpl/aerie/merlin/server/remotes/postgres/CreateActivityTypeAction.java
+++ merlin-server/src/main/java/gov/nasa/jpl/aerie/merlin/server/remotes/postgres/CreateActivityTypeAction.java
@@ -1,12 +1,14 @@
 package gov.nasa.jpl.aerie.merlin.server.remotes.postgres;
 
 import gov.nasa.jpl.aerie.merlin.protocol.types.Parameter;
+import org.apache.commons.lang3.mutable.MutableInt;
 import org.intellij.lang.annotations.Language;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 /*package-local*/ final class CreateActivityTypeAction implements AutoCloseable {
@@ -24,11 +26,16 @@ import java.util.stream.Collectors;
   }
 
   public long apply(final long modelId, final String name, final List<Parameter> parameters) throws SQLException, FailedInsertException {
+
+    MutableInt order = new MutableInt();
+    final var paramMap = parameters.stream().collect(Collectors.toMap(
+        p -> p.name(),
+        p -> Map.entry(order.getAndIncrement(),p.schema())));
+
     this.statement.setLong(1, modelId);
-    final var paramMap = parameters.stream().collect(Collectors.toMap(Parameter::name, Parameter::schema));
     this.statement.setString(2, name);
-    PreparedStatements.setValueSchemaMap(this.statement, 3, paramMap);
-    PreparedStatements.setValueSchemaMap(this.statement, 4, paramMap);
+    PreparedStatements.setValueSchemaOrderedMap(this.statement, 3, paramMap);
+    PreparedStatements.setValueSchemaOrderedMap(this.statement, 4, paramMap);
 
     try (final var results = statement.executeQuery()) {
       if (!results.next()) throw new FailedInsertException("activity_type");
