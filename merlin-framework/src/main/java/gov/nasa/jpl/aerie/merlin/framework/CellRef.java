6b18f2e9d6694a7b687fac78822a8db2cc434829
Jonathan Castello <Jonathan.Castello@jpl.caltech.edu>
2021-11-04 10:16:21 -0700

    Allow CellRef to be created with custom Applicators

--- merlin-framework/src/main/java/gov/nasa/jpl/aerie/merlin/framework/CellRef.java
+++ merlin-framework/src/main/java/gov/nasa/jpl/aerie/merlin/framework/CellRef.java
@@ -1,6 +1,7 @@
 package gov.nasa.jpl.aerie.merlin.framework;
 
 import gov.nasa.jpl.aerie.merlin.protocol.driver.Query;
+import gov.nasa.jpl.aerie.merlin.protocol.model.Applicator;
 import gov.nasa.jpl.aerie.merlin.protocol.model.EffectTrait;
 import gov.nasa.jpl.aerie.merlin.protocol.model.Projection;
 
@@ -13,18 +14,33 @@ public final class CellRef<Event, CellType> {
     this.query = query;
   }
 
+  public static <Event, Effect, CellType>
+  CellRef<Event, CellType> allocate(
+      final CellType initialState,
+      final Applicator<Effect, CellType> applicator,
+      final EffectTrait<Effect> trait,
+      final Function<Event, Effect> eventToEffect
+  ) {
+    final var query = ModelActions.context.get().allocate(
+        initialState,
+        applicator,
+        Projection.from(trait, eventToEffect));
+    return new CellRef<>(query);
+  }
+
   public static <Effect, CellType extends Cell<Effect, CellType>>
   CellRef<Effect, CellType> allocate(final CellType initialState, final EffectTrait<Effect> trait) {
-    return allocate(initialState, trait, $ -> $);
+    return allocate(initialState, new CellApplicator<>(), trait, $ -> $);
   }
 
   public static <Event, Effect, CellType extends Cell<Effect, CellType>>
-  CellRef<Event, CellType> allocate(final CellType initialState, final EffectTrait<Effect> trait, Function<Event, Effect> eventToEffect) {
-    final var query = ModelActions.context.get().allocate(
-        initialState,
-        new CellApplicator<>(),
-        Projection.from(trait, eventToEffect));
-    return new CellRef<>(query);
+  CellRef<Event, CellType> allocate(final CellType initialState, final EffectTrait<Effect> trait, final Function<Event, Effect> eventToEffect) {
+    return allocate(initialState, new CellApplicator<>(), trait, eventToEffect);
+  }
+
+  public static <Effect, CellType>
+  CellRef<Effect, CellType> allocate(final CellType initialState, final Applicator<Effect, CellType> applicator, final EffectTrait<Effect> trait) {
+    return allocate(initialState, applicator, trait, $ -> $);
   }
 
   public CellType get() {
