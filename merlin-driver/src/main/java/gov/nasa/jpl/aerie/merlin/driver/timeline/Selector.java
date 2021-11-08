5bdfd42fc6da210a8c50bc40da13c0b7105341b6
Jonathan Castello <Jonathan.Castello@jpl.caltech.edu>
2021-11-08 06:23:23 -0800

    Handle the most common Selector case first

--- merlin-driver/src/main/java/gov/nasa/jpl/aerie/merlin/driver/timeline/Selector.java
+++ merlin-driver/src/main/java/gov/nasa/jpl/aerie/merlin/driver/timeline/Selector.java
@@ -17,6 +17,9 @@ public record Selector<Effect>(List<SelectorRow<?, Effect>> rows) {
   }
 
   public Optional<Effect> select(final Event event) {
+    // Bail out as fast as possible if we're in a trivial (and incredibly common) case.
+    if (this.rows.size() == 1) return this.rows.get(0).select(event);
+
     for (final var row : this.rows) {
       final var effect = row.select(event);
       if (effect.isPresent()) return effect;
@@ -25,6 +28,9 @@ public record Selector<Effect>(List<SelectorRow<?, Effect>> rows) {
   }
 
   public boolean matchesAny(final Collection<Topic<?>> topics) {
+    // Bail out as fast as possible if we're in a trivial (and incredibly common) case.
+    if (this.rows.size() == 1) return topics.contains(this.rows.get(0).topic());
+
     for (final var row : this.rows) {
       if (topics.contains(row.topic)) return true;
     }
@@ -33,7 +39,7 @@ public record Selector<Effect>(List<SelectorRow<?, Effect>> rows) {
 
   public record SelectorRow<EventType, Effect>(Topic<EventType> topic, Function<EventType, Effect> transform) {
     public Optional<Effect> select(final Event event$) {
-      return event$.extract(this.topic, transform);
+      return event$.extract(this.topic, this.transform);
     }
   }
 }
