e082a55276fc3e93c787c222ff7c7b9a0f188c2c
Jonathan Castello <Jonathan.Castello@jpl.caltech.edu>
2021-11-08 20:34:19 -0800

    Simplify FrameBuilder

--- merlin-driver/src/main/java/gov/nasa/jpl/aerie/merlin/driver/engine/TaskFrame.java
+++ merlin-driver/src/main/java/gov/nasa/jpl/aerie/merlin/driver/engine/TaskFrame.java
@@ -41,9 +41,13 @@ public final class TaskFrame<Signal> {
   private CausalEventSource events;
   private final Deque<Triple<CausalEventSource, LiveCells, Signal>> branches;
 
-  private TaskFrame(final FrameBuilder<Signal> builder) {
-    this.continuation = builder.continuation;
-    this.sibling = builder.sibling;
+  private TaskFrame(
+      final Optional<TaskFrame<Signal>> continuation,
+      final EventGraph<Event> sibling,
+      final FrameBuilder<Signal> builder
+  ) {
+    this.continuation = continuation;
+    this.sibling = sibling;
 
     this.events = builder.events;
     this.branches = builder.branches;
@@ -51,9 +55,9 @@ public final class TaskFrame<Signal> {
 
   public static <Signal>
   TaskFrame<Signal> of(final LiveCells context, final Consumer<FrameBuilder<Signal>> body) {
-    final var builder = new FrameBuilder<Signal>(EventGraph.empty(), context, Optional.empty());
+    final var builder = new FrameBuilder<Signal>(context);
     body.accept(builder);
-    return builder.yield();
+    return new TaskFrame<>(Optional.empty(), EventGraph.empty(), builder);
   }
 
   public static <Signal>
@@ -87,12 +91,13 @@ public final class TaskFrame<Signal> {
       final var branchContext = branch.getMiddle();
       final var signal = branch.getRight();
 
-      final var builder = new FrameBuilder<>(this.events.commit(), branchContext, Optional.of(this));
-      this.events = branchBase;
-
+      final var builder = new FrameBuilder<Signal>(branchContext);
       executor.accept(signal, builder);
 
-      return builder.yield();
+      final var sibling = this.events.commit();
+      this.events = branchBase;
+
+      return new TaskFrame<>(Optional.of(this), sibling, builder);
     } else if (this.continuation.isPresent()) {
       // We're done here, but a parent frame is waiting for us to finish.
       // Commit our tip back up to the parent.
@@ -107,55 +112,28 @@ public final class TaskFrame<Signal> {
   }
 
   public static final class FrameBuilder<Signal> {
-    private final Optional<TaskFrame<Signal>> continuation;
-    private final EventGraph<Event> sibling;
-
     private CausalEventSource events;
     private LiveCells cells;
     private final Deque<Triple<CausalEventSource, LiveCells, Signal>> branches = new ArrayDeque<>();
 
-    private boolean yielded = false;
-
-    private FrameBuilder(
-        final EventGraph<Event> sibling,
-        final LiveCells context,
-        final Optional<TaskFrame<Signal>> continuation
-    ) {
-      this.continuation = continuation;
-      this.sibling = sibling;
-
+    private FrameBuilder(final LiveCells context) {
       this.events = new CausalEventSource();
       this.cells = new LiveCells(this.events, context);
     }
 
-    private TaskFrame<Signal> yield() {
-      if (this.yielded) throw new RuntimeException("Unexpected action from task after yield");
-      this.yielded = true;
-
-      return new TaskFrame<>(this);
-    }
-
     public <State> Optional<State> getState(final Query<State> query) {
-      if (this.yielded) throw new RuntimeException("Unexpected action from task after yield");
-
       return this.cells.getState(query);
     }
 
     public Optional<Duration> getExpiry(final Query<?> query) {
-      if (this.yielded) throw new RuntimeException("Unexpected action from task after yield");
-
       return this.cells.getExpiry(query);
     }
 
     public void emit(final Event event) {
-      if (this.yielded) throw new RuntimeException("Unexpected action from task after yield");
-
       this.events.add(EventGraph.atom(event));
     }
 
     public void signal(final Signal target) {
-      if (this.yielded) throw new RuntimeException("Unexpected action from task after yield");
-
       // If we haven't emitted any events, subscribe the target to the previous branch point instead.
       // This avoids making long chains of LiveCells over segments where no events have actually been accumulated.
       if (this.events.points().isEmpty() && !this.branches.isEmpty()) {
