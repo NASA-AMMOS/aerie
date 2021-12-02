74ac8016d4e7ddf49e96e64d324402c64a4d87a9
srschaff <steve.schaffer@jpl.nasa.gov>
2021-12-02 06:09:01 -0800

    switch to javax.json to match aerie

--- scheduler-server/src/main/java/gov/nasa/jpl/aerie/scheduler/server/services/GraphQLMerlinService.java
+++ scheduler-server/src/main/java/gov/nasa/jpl/aerie/scheduler/server/services/GraphQLMerlinService.java
@@ -2,17 +2,18 @@ package gov.nasa.jpl.aerie.scheduler.server.services;
 
 import com.impossibl.postgres.api.data.Interval;
 import gov.nasa.jpl.aerie.json.BasicParsers;
-import gov.nasa.jpl.aerie.merlin.protocol.types.Duration;
-import gov.nasa.jpl.aerie.merlin.protocol.types.SerializedValue;
 import gov.nasa.jpl.aerie.merlin.server.exceptions.NoSuchPlanException;
+import gov.nasa.jpl.aerie.merlin.server.http.InvalidEntityException;
+import gov.nasa.jpl.aerie.merlin.server.http.InvalidJsonException;
 import gov.nasa.jpl.aerie.merlin.server.http.SerializedValueJsonParser;
 import gov.nasa.jpl.aerie.merlin.server.models.Timestamp;
 import gov.nasa.jpl.aerie.scheduler.PlanningHorizon;
 import gov.nasa.jpl.aerie.scheduler.Time;
 import gov.nasa.jpl.aerie.scheduler.server.models.PlanMetadata;
-import org.json.JSONException;
-import org.json.JSONObject;
 
+import javax.json.Json;
+import javax.json.JsonException;
+import javax.json.JsonObject;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -20,7 +21,7 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.file.Path;
 import java.time.Instant;
-import java.util.Map;
+import java.util.List;
 import java.util.Optional;
 import java.util.zip.GZIPInputStream;
 
@@ -31,10 +32,10 @@ import java.util.zip.GZIPInputStream;
  */
 public record GraphQLMerlinService(URI merlinGraphqlURI) implements MerlinService {
 
+  /**
+   * timeout for http graphql requests issued to aerie
+   */
   private static final java.time.Duration httpTimeout = java.time.Duration.ofSeconds(60);
-
-  //TODO: maybe use fancy aerie typed json parsers/serializers, ala BasicParsers.productP use in MerlinParsers
-  //TODO: or upgrade to gson or similar modern library with registered object mappings
 
   /**
    * dispatch the given graphql request to aerie and collect the results
@@ -45,31 +46,33 @@ public record GraphQLMerlinService(URI merlinGraphqlURI) implements MerlinServic
    * @param gqlStr the graphQL query or mutation to send to aerie
    * @return the json response returned by aerie, or an empty optional in case of io errors
    */
-  private Optional<JSONObject> postRequest(String gqlStr) {
-    //TODO: how severely to error out if aerie cannot be reached or has a 500 error etc?
+  private Optional<JsonObject> postRequest(String gqlStr) {
     try {
-      final var reqBody = new JSONObject().put("query", gqlStr).toString();
+      final var reqBody = Json.createObjectBuilder().add("query", gqlStr).build();
       final var httpReq = HttpRequest
           .newBuilder().uri(merlinGraphqlURI).timeout(httpTimeout)
           .header("Content-Type", "application/json")
           .header("Accept-Encoding", "gzip, deflate, br")
           .header("Accept", "application/json")
           .header("Origin", merlinGraphqlURI.toString())
-          .POST(HttpRequest.BodyPublishers.ofString(reqBody))
+          .POST(HttpRequest.BodyPublishers.ofString(reqBody.toString()))
           .build();
       final var httpResp = HttpClient
           .newHttpClient().send(httpReq, HttpResponse.BodyHandlers.ofInputStream());
       if (httpResp.statusCode() != 200) {
+        //TODO: how severely to error out if aerie cannot be reached or has a 500 error or json is garbled etc etc?
         return Optional.empty();
       }
-      //TODO: allow for streaming to json parser instead of loading entire body into memory (twice!)
-      final var respBody = new String(new GZIPInputStream(httpResp.body()).readAllBytes());
-      return Optional.of(new JSONObject(respBody));
-    } catch (IOException | InterruptedException e) {
+      final var respBody = Json.createReader(new GZIPInputStream(httpResp.body())).readObject();
+      return Optional.of(respBody);
+    } catch (IOException | InterruptedException | JsonException e) { // or also JsonParsingException
       //TODO: maybe retry if interrupted? but depends on semantics (eg don't duplicate mutation if not idempotent)
       return Optional.empty();
     }
   }
+
+  //TODO: maybe use fancy aerie typed json parsers/serializers, ala BasicParsers.productP use in MerlinParsers
+  //TODO: or upgrade to gson or similar modern library with registered object mappings
 
   /**
    * {@inheritDoc}
@@ -80,8 +83,9 @@ public record GraphQLMerlinService(URI merlinGraphqlURI) implements MerlinServic
         .formatted(planId);
     final var response = postRequest(request).orElseThrow(() -> new NoSuchPlanException(planId));
     try {
-      return response.getJSONObject("data").getJSONObject("plan_by_pk").getLong("revision");
-    } catch (JSONException e) {
+      return response.getJsonObject("data").getJsonObject("plan_by_pk").getJsonNumber("revision").longValueExact();
+    } catch (ClassCastException | ArithmeticException e) {
+      //TODO: better error reporting upward to service response (NSPEx doesn't allow passing e as cause)
       throw new NoSuchPlanException(planId);
     }
   }
@@ -103,31 +107,31 @@ public record GraphQLMerlinService(URI merlinGraphqlURI) implements MerlinServic
         + "    parameters { parameters } "
         + "  } "
         + "} }"
-        ).formatted(planId);
+    ).formatted(planId);
     final var response = postRequest(request).orElseThrow(() -> new NoSuchPlanException(planId));
     try {
       //TODO: elevate and then leverage existing MerlinParsers (after updating them to match current db!)
-      final var plan = response.getJSONObject("data").getJSONObject("plan_by_pk");
-      final long planPK = plan.getLong("id");
-      final long planRev = plan.getLong("revision");
+      final var plan = response.getJsonObject("data").getJsonObject("plan_by_pk");
+      final long planPK = plan.getJsonNumber("id").longValue();
+      final long planRev = plan.getJsonNumber("revision").longValue();
       final var startTime = Timestamp.fromString(plan.getString("start_time"));
       final var duration = Interval.parse(plan.getString("duration"));
 
-      final var model = plan.getJSONObject("mission_model");
-      final var modelId = model.getLong("id");
+      final var model = plan.getJsonObject("mission_model");
+      final var modelId = model.getJsonNumber("id").longValue();
       final var modelName = model.getString("name");
       final var modelVersion = model.getString("version");
 
-      final var file = model.getJSONObject("uploaded_file");
+      final var file = model.getJsonObject("uploaded_file");
       final var modelPath = Path.of(file.getString("path"));
 
-      final var params = model.getJSONObject("parameters").getJSONObject("parameters");
-      //TODO: would be nice to recycle map / SerVal parsers from merlin
-      //BasicParsers.mapP(new SerializedValueJsonParser()).parse(new JsonValueparams);
-      final var modelConfiguration = Map.<String, SerializedValue>of();
+      final var params = model.getJsonObject("parameters").getJsonObject("parameters");
+      final var modelConfiguration = BasicParsers
+          .mapP(new SerializedValueJsonParser()).parse(params)
+          .getSuccessOrThrow((reason) -> new InvalidJsonException(new InvalidEntityException(List.of(reason))));
 
       //TODO: unify scheduler/aerie time types to avoid conversions
-      final var endTime = new Timestamp((Instant)duration.addTo(startTime.toInstant()));
+      final var endTime = new Timestamp((Instant) duration.addTo(startTime.toInstant()));
       final var horizon = new PlanningHorizon(
           Time.fromString(startTime.toString()),
           Time.fromString(endTime.toString()));
@@ -137,7 +141,8 @@ public record GraphQLMerlinService(URI merlinGraphqlURI) implements MerlinServic
           horizon,
           modelId, modelPath, modelName, modelVersion,
           modelConfiguration);
-    } catch (JSONException e) {
+    } catch (ClassCastException | ArithmeticException | InvalidJsonException e) {
+      //TODO: better error reporting upward to service response (NSPEx doesn't allow passing e as cause)
       throw new NoSuchPlanException(planId);
     }
   }
