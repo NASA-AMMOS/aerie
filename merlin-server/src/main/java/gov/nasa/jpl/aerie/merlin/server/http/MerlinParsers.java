c150bfb43d3bca67554adf1b7fd62b9884bbc4b0
roffo <kenneth.r.roffo@jpl.nasa.gov>
2021-12-02 16:36:29 -0500

    Add spacing to make MerlinBindings definitions consistent

--- merlin-server/src/main/java/gov/nasa/jpl/aerie/merlin/server/http/MerlinParsers.java
+++ merlin-server/src/main/java/gov/nasa/jpl/aerie/merlin/server/http/MerlinParsers.java
@@ -168,34 +168,34 @@ public abstract class MerlinParsers {
 
   private static final JsonParser<HasuraAction.Session> hasuraActionSessionP
       = productP
-      .field("x-hasura-role", stringP)
-      .optionalField("x-hasura-user-id", stringP)
-      .map(Iso.of(
+      . field("x-hasura-role", stringP)
+      . optionalField("x-hasura-user-id", stringP)
+      . map(Iso.of(
           untuple((role, userId) -> new HasuraAction.Session(role, userId.orElse(""))),
           $ -> tuple($.hasuraRole(), Optional.ofNullable($.hasuraUserId()))));
 
   private static <S> JsonParser<Pair<Pair<String, S>, HasuraAction.Session>> hasuraActionP(final JsonParser<S> inputP) {
     return productP
-        .field("action", productP.field("name", stringP))
-        .field("input", inputP)
-        .field("session_variables", hasuraActionSessionP);
+        .  field("action", productP.field("name", stringP))
+        .  field("input", inputP)
+        .  field("session_variables", hasuraActionSessionP);
   }
 
   public static final JsonParser<HasuraAction<HasuraAction.MissionModelInput>> hasuraMissionModelActionP
       = hasuraActionP(productP.field("missionModelId", stringP))
-      .map(Iso.of(
+      . map(Iso.of(
           untuple((name, missionModelId, session) -> new HasuraAction<>(name, new HasuraAction.MissionModelInput(missionModelId), session)),
           $ -> tuple($.name(), $.input().missionModelId(), $.session())));
 
   public static final JsonParser<HasuraAction<HasuraAction.PlanInput>> hasuraPlanActionP
       = hasuraActionP(productP.field("planId", stringP))
-      .map(Iso.of(
+      . map(Iso.of(
           untuple((name, planId, session) -> new HasuraAction<>(name, new HasuraAction.PlanInput(planId), session)),
           $ -> tuple($.name(), $.input().planId(), $.session())));
 
   public static final JsonParser<HasuraMissionModelEvent> hasuraMissionModelEventTriggerP
       = productP
-      .field("event", productP
+      . field("event", productP
           .field("data", productP
               .field("new", productP
                   .field("id", longP)
@@ -205,26 +205,23 @@ public abstract class MerlinParsers {
               .map(Iso.of(untuple(newDataId -> newDataId), Uncurry::tuple)))
           .rest()
           .map(Iso.of(untuple(dataId -> dataId), Uncurry::tuple)))
-      .rest()
-      .map(Iso.of(
+      . rest()
+      . map(Iso.of(
           untuple(missionModelId -> new HasuraMissionModelEvent(String.valueOf(missionModelId))),
           $ -> tuple(Long.parseLong($.missionModelId()))));
 
   private static final JsonParser<HasuraAction.ActivityInput> hasuraActivityInputP
       = productP
-      .field("missionModelId", stringP)
-      .field("activityTypeName", stringP)
-      .field("activityArguments", mapP(serializedValueP))
-      .map(Iso.of(
-          untuple((missionModelId, activityTypeName, arguments) -> new HasuraAction.ActivityInput(
-              missionModelId,
-              activityTypeName,
-              arguments)),
+      . field("missionModelId", stringP)
+      . field("activityTypeName", stringP)
+      . field("activityArguments", mapP(serializedValueP))
+      . map(Iso.of(
+          untuple(HasuraAction.ActivityInput::new),
           $ -> tuple($.missionModelId(), $.activityTypeName(), $.arguments())));
 
   public static final JsonParser<HasuraAction<HasuraAction.ActivityInput>> hasuraActivityActionP
       = hasuraActionP(hasuraActivityInputP)
-      .map(Iso.of(
-          untuple((name, activityInput, session) -> new HasuraAction<>(name, activityInput, session)),
+      . map(Iso.of(
+          untuple(HasuraAction::new),
           $ -> tuple($.name(), $.input(), $.session())));
 }
