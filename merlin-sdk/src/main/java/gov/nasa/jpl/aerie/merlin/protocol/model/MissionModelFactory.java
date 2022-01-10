c175c815b2efd3ff98108240af9bb7c535501676
Paul Rosemurgy <paul.c.rosemurgy@jpl.nasa.gov>
2022-01-10 12:54:54 -0800

    Add `getConfigurationType()` to mission model factory

--- merlin-sdk/src/main/java/gov/nasa/jpl/aerie/merlin/protocol/model/MissionModelFactory.java
+++ merlin-sdk/src/main/java/gov/nasa/jpl/aerie/merlin/protocol/model/MissionModelFactory.java
@@ -6,8 +6,10 @@ import gov.nasa.jpl.aerie.merlin.protocol.types.SerializedValue;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 public interface MissionModelFactory<Model> {
+  Optional<ConfigurationType<?>> getConfigurationType();
   Map<String, TaskSpecType<Model, ?>> getTaskSpecTypes();
   List<Parameter> getParameters();
   Model instantiate(SerializedValue configuration, Initializer builder) throws MissionModelInstantiationException;
