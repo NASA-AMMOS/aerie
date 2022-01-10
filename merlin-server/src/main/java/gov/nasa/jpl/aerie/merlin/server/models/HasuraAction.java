415e497aa9b9b731a0d3eb2184f644acf9dcc278
Paul Rosemurgy <paul.c.rosemurgy@jpl.nasa.gov>
2022-01-10 15:44:29 -0800

    Add `getModelEffectiveArguments` hasura action endpoint

--- merlin-server/src/main/java/gov/nasa/jpl/aerie/merlin/server/models/HasuraAction.java
+++ merlin-server/src/main/java/gov/nasa/jpl/aerie/merlin/server/models/HasuraAction.java
@@ -15,6 +15,8 @@ public record HasuraAction<I extends HasuraAction.Input>(String name, I input, S
   public record ActivityInput(String missionModelId,
                               String activityTypeName,
                               Map<String, SerializedValue> arguments) implements Input {}
+  public record MissionModelArgumentsInput(String missionModelId,
+                              Map<String, SerializedValue> arguments) implements Input {}
   public record UploadExternalDatasetInput(String planId,
                                            Timestamp datasetStart,
                                            ProfileSet profileSet) implements Input {}
