b2f33de07828ad3af65f07f0f74a8cb72ea37c65
Paul Rosemurgy <paul.c.rosemurgy@jpl.nasa.gov>
2022-02-02 15:45:58 -0800

    Add `@Validation` annotations to banananation

--- examples/banananation/src/main/java/gov/nasa/jpl/aerie/banananation/Configuration.java
+++ examples/banananation/src/main/java/gov/nasa/jpl/aerie/banananation/Configuration.java
@@ -4,11 +4,23 @@ import java.nio.file.Path;
 
 import static gov.nasa.jpl.aerie.merlin.framework.annotations.Export.Template;
 
+import gov.nasa.jpl.aerie.merlin.framework.annotations.Export;
+
 public record Configuration(int initialPlantCount, String initialProducer, Path initialDataPath) {
 
   public static final int DEFAULT_PLANT_COUNT = 200;
   public static final String DEFAULT_PRODUCER = "Chiquita";
   public static final Path DEFAULT_DATA_PATH = Path.of("/etc/os-release");
+
+  @Export.Validation("plant count must be positive")
+  public boolean validateInitialPlantCount() {
+    return this.initialPlantCount > 0;
+  }
+
+  @Export.Validation("data path must exist")
+  public boolean validateInitialDataPath() {
+    return initialDataPath.toFile().exists();
+  }
 
   public static @Template Configuration defaultConfiguration() {
     return new Configuration(DEFAULT_PLANT_COUNT, DEFAULT_PRODUCER, DEFAULT_DATA_PATH);
