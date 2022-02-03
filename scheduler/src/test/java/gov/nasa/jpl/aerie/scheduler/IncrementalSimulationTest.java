1c22815d815250e06fc9b9660288f83506a9a9d6
Adrien Maillard <adrien.maillard@jpl.nasa.gov>
2022-02-03 09:37:40 -0800

    add benchmark and tests for the stepping simulation

---
+++ scheduler/src/test/java/gov/nasa/jpl/aerie/scheduler/IncrementalSimulationTest.java
@@ -0,0 +1,70 @@
+package gov.nasa.jpl.aerie.scheduler;
+
+import gov.nasa.jpl.aerie.merlin.driver.SerializedActivity;
+import gov.nasa.jpl.aerie.merlin.protocol.types.Duration;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Objects;
+
+import static gov.nasa.jpl.aerie.merlin.protocol.types.Duration.SECONDS;
+
+public class IncrementalSimulationTest {
+
+  SimulationDriverFacade incrementalSimulationDriver;
+  Duration endOfLastAct;
+
+  @BeforeEach
+  public void init() {
+    final var acts = getActivities();
+    final var fooMissionModel = SimulationUtility.getFooMissionModel();
+    incrementalSimulationDriver =
+        new SimulationDriverFacade(new IncrementalSimulationDriver(fooMissionModel));
+    for (var act : acts) {
+      final var start = System.nanoTime();
+      incrementalSimulationDriver.simulateActivity(act.activity, act.name, act.start);
+    }
+  }
+  @Test
+  public void simulationResultsTest(){
+    //ensures that simulation results are generated until the end of the last act;
+    var simResults = incrementalSimulationDriver.getSimulationResults();
+    assert(simResults.realProfiles.get("/utcClock").get(0).getLeft().isEqualTo(endOfLastAct));
+    /*ensures that when current simulation results cover more than the asked period and that nothing has happened
+    between two requests, the same results are returned*/
+    var simResults2 = incrementalSimulationDriver.getSimulationResultsUntil(Duration.of(7,SECONDS));
+    assert(Objects.equals(simResults, simResults2));
+  }
+
+  @Test
+  public void durationTest(){
+    var act1Dur = incrementalSimulationDriver.getActivityDuration("first");
+    var act2Dur = incrementalSimulationDriver.getActivityDuration("second");
+    assert(act1Dur.isEqualTo(Duration.of(1, SECONDS)));
+    assert(act2Dur.isEqualTo(Duration.of(1, SECONDS)));
+  }
+
+  private ArrayList<TestSimulatedActivity> getActivities(){
+    final var acts = new ArrayList<TestSimulatedActivity>();
+    var act1 = new TestSimulatedActivity(
+        Duration.of(0, SECONDS),
+        new SerializedActivity("BasicActivity", Map.of()),
+        "first");
+    acts.add(act1);
+    var act2 = new TestSimulatedActivity(
+        Duration.of(14, SECONDS),
+        new SerializedActivity("BasicActivity", Map.of()),
+        "second");
+    acts.add(act2);
+
+    endOfLastAct = Duration.of(15,SECONDS);
+    return acts;
+  }
+
+
+  record TestSimulatedActivity(Duration start, SerializedActivity activity, String name){}
+
+
+}
--- scheduler/src/test/java/gov/nasa/jpl/aerie/scheduler/SimulationUtility.java
+++ scheduler/src/test/java/gov/nasa/jpl/aerie/scheduler/SimulationUtility.java
@@ -2,7 +2,6 @@ package gov.nasa.jpl.aerie.scheduler;
 
 import gov.nasa.jpl.aerie.banananation.Configuration;
 import gov.nasa.jpl.aerie.banananation.generated.ConfigurationMapper;
-import gov.nasa.jpl.aerie.banananation.generated.GeneratedMissionModelFactory;
 import gov.nasa.jpl.aerie.merlin.driver.MissionModel;
 import gov.nasa.jpl.aerie.merlin.driver.MissionModelBuilder;
 import gov.nasa.jpl.aerie.merlin.protocol.types.SerializedValue;
@@ -12,13 +11,23 @@ import java.nio.file.Path;
 public final class SimulationUtility {
 
   private static MissionModel<?> makeMissionModel(final MissionModelBuilder builder, final SerializedValue config) {
-    final var factory = new GeneratedMissionModelFactory();
+    final var factory = new gov.nasa.jpl.aerie.banananation.generated.GeneratedMissionModelFactory();
     final var model = factory.instantiate(config, builder);
     return builder.build(model, factory.getTaskSpecTypes());
   }
 
+  public static MissionModel<?>
+  getFooMissionModel() {
+    final var conf = new gov.nasa.jpl.aerie.foomissionmodel.Configuration();
+    final var serializedConfig = SerializedValue.of(new gov.nasa.jpl.aerie.foomissionmodel.generated.ConfigurationMapper().getArguments(conf));
+    final var factory = new gov.nasa.jpl.aerie.foomissionmodel.generated.GeneratedMissionModelFactory();
+    final var builder = new MissionModelBuilder();
+    final var model = factory.instantiate(serializedConfig, builder);
+    return builder.build(model, factory.getTaskSpecTypes());
+  }
 
-  public static MissionModel<?> getMissionModel(){
+
+  public static MissionModel<?> getBananaMissionModel(){
     final var config = new Configuration(Configuration.DEFAULT_PLANT_COUNT, Configuration.DEFAULT_PRODUCER, Path.of("/etc/hosts"));
     final var serializedConfig = SerializedValue.of(new ConfigurationMapper().getArguments(config));
     return makeMissionModel(new MissionModelBuilder(), serializedConfig);
