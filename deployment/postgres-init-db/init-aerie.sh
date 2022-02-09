10c1cbfc5ccc762ed71c0e72edf813b36bbf3fa2
Paul Rosemurgy <paul.c.rosemurgy@jpl.nasa.gov>
2022-02-09 13:40:40 -0800

    Add initial `aerie_sched` DB to Postgres and Hasura

--- deployment/postgres-init-db/init-aerie.sh
+++ deployment/postgres-init-db/init-aerie.sh
@@ -17,6 +17,11 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
   GRANT ALL PRIVILEGES ON DATABASE aerie_merlin TO aerie;
   \echo 'Done!'
 
+  \echo 'Initializing aerie_sched database...'
+  CREATE DATABASE aerie_sched;
+  GRANT ALL PRIVILEGES ON DATABASE aerie_sched TO aerie;
+  \echo 'Done!'
+
   \echo 'Initializing aerie_ui database...'
   CREATE DATABASE aerie_ui;
   GRANT ALL PRIVILEGES ON DATABASE aerie_ui TO aerie;
@@ -28,6 +33,12 @@ export PGPASSWORD=aerie
 psql -v ON_ERROR_STOP=1 --username "aerie" --dbname "aerie_merlin" <<-EOSQL
   \echo 'Initializing aerie_merlin database objects...'
   \ir /docker-entrypoint-initdb.d/sql/merlin/init.sql
+  \echo 'Done!'
+EOSQL
+
+psql -v ON_ERROR_STOP=1 --username "aerie" --dbname "aerie_sched" <<-EOSQL
+  \echo 'Initializing aerie_sched database objects...'
+  \ir /docker-entrypoint-initdb.d/sql/scheduler/init.sql
   \echo 'Done!'
 EOSQL
 
