5c71fd8ca65bdf813342c0db03816af6effd0db8
Matt Dailis <matthew.l.dailis@jpl.nasa.gov>
2022-02-16 06:00:32 -0800

    Add controllable growingDuration to GrowBananaActivity example

--- examples/banananation/src/main/java/gov/nasa/jpl/aerie/banananation/activities/GrowBananaActivity.java
+++ examples/banananation/src/main/java/gov/nasa/jpl/aerie/banananation/activities/GrowBananaActivity.java
@@ -1,11 +1,14 @@
 package gov.nasa.jpl.aerie.banananation.activities;
 
-import gov.nasa.jpl.aerie.banananation.Flag;
 import gov.nasa.jpl.aerie.banananation.Mission;
 import gov.nasa.jpl.aerie.merlin.framework.annotations.ActivityType;
 import gov.nasa.jpl.aerie.merlin.framework.annotations.ActivityType.EffectModel;
+import gov.nasa.jpl.aerie.merlin.framework.annotations.ActivityType.ControllableDuration;
 import gov.nasa.jpl.aerie.merlin.framework.annotations.Export.Validation;
 import gov.nasa.jpl.aerie.merlin.framework.annotations.Export.Template;
+import gov.nasa.jpl.aerie.merlin.protocol.types.Duration;
+
+import static gov.nasa.jpl.aerie.merlin.framework.ModelActions.delay;
 
 /**
  * Monke has evolve. Monke now make banana. Monke is farmer.
@@ -16,10 +19,10 @@ import gov.nasa.jpl.aerie.merlin.framework.annotations.Export.Template;
  * @contact John Doe
  */
 @ActivityType("GrowBanana")
-public record GrowBananaActivity(int quantity) {
+public record GrowBananaActivity(int quantity, Duration growingDuration) {
 
   public static @Template GrowBananaActivity defaults() {
-    return new GrowBananaActivity(0);
+    return new GrowBananaActivity(0, Duration.ZERO);
   }
 
   @Validation("Quantity must be positive")
@@ -28,7 +31,9 @@ public record GrowBananaActivity(int quantity) {
   }
 
   @EffectModel
+  @ControllableDuration(parameterName = "growingDuration")
   public void run(final Mission mission) {
+    delay(this.growingDuration());
     mission.plant.add(this.quantity());
   }
 }
