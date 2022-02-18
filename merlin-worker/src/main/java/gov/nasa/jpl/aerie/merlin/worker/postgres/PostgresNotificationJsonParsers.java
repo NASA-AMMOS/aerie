7765272a02ed0f74b7dc08f797c04defd217044e
Patrick Kenneally <patrick.kenneally@colorado.edu>
2022-02-18 13:43:56 -0800

    Add listener capability

---
+++ merlin-worker/src/main/java/gov/nasa/jpl/aerie/merlin/worker/postgres/PostgresNotificationJsonParsers.java
@@ -0,0 +1,32 @@
+package gov.nasa.jpl.aerie.merlin.worker.postgres;
+
+import gov.nasa.jpl.aerie.json.Iso;
+import gov.nasa.jpl.aerie.json.JsonParser;
+
+import static gov.nasa.jpl.aerie.json.BasicParsers.longP;
+import static gov.nasa.jpl.aerie.json.BasicParsers.productP;
+import static gov.nasa.jpl.aerie.json.Uncurry.tuple;
+import static gov.nasa.jpl.aerie.json.Uncurry.untuple;
+
+public final class PostgresNotificationJsonParsers {
+
+  public static final JsonParser<PostgresSimulationNotificationPayload> postgresSimulationNotificationP
+      = productP
+      . field("model_revision", longP)
+      . field("plan_revision", longP)
+      . field("simulation_revision", longP)
+      . optionalField("simulation_template_revision", longP)
+      . field("plan_id", longP)
+      . field("dataset_id", longP)
+      . field("simulation_id", longP)
+      . map(Iso.of(
+          untuple(PostgresSimulationNotificationPayload::new),
+          $ -> tuple($.modelRevision(),
+                     $.planRevision(),
+                     $.simulationRevision(),
+                     $.simulationTemplateRevision(),
+                     $.planId(),
+                     $.datasetId(),
+                     $.simulationId())
+  ));
+}
---
+++ merlin-worker/src/main/java/gov/nasa/jpl/aerie/merlin/worker/postgres/PostgresSimulationNotificationPayload.java
@@ -0,0 +1,13 @@
+package gov.nasa.jpl.aerie.merlin.worker.postgres;
+
+import java.util.Optional;
+
+public record PostgresSimulationNotificationPayload(
+    long modelRevision,
+    long planRevision,
+    long simulationRevision,
+    Optional<Long> simulationTemplateRevision,
+    long planId,
+    long datasetId,
+    long simulationId
+) {}
