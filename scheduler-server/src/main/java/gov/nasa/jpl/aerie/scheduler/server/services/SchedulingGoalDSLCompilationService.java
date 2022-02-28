995c95f81ab7d6ba0617a329deb27a23993a7398
Matt Dailis <matthew.l.dailis@jpl.nasa.gov>
2022-02-28 07:50:29 -0800

    Update Java code to follow new protocol

--- scheduler-server/src/main/java/gov/nasa/jpl/aerie/scheduler/server/services/SchedulingGoalDSLCompilationService.java
+++ scheduler-server/src/main/java/gov/nasa/jpl/aerie/scheduler/server/services/SchedulingGoalDSLCompilationService.java
@@ -4,10 +4,12 @@ import gov.nasa.jpl.aerie.json.Iso;
 import gov.nasa.jpl.aerie.json.JsonParser;
 import gov.nasa.jpl.aerie.scheduler.server.http.InvalidEntityException;
 import gov.nasa.jpl.aerie.scheduler.server.http.InvalidJsonException;
+import org.json.JSONObject;
 
 import javax.json.Json;
 import javax.json.stream.JsonParsingException;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -16,29 +18,30 @@ import java.io.OutputStreamWriter;
 import java.io.StringReader;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
+import java.util.Spliterator;
+import java.util.Spliterators;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 import static gov.nasa.jpl.aerie.json.BasicParsers.productP;
 import static gov.nasa.jpl.aerie.json.BasicParsers.stringP;
 
 public class SchedulingGoalDSLCompilationService {
 
-  private final InputStream inputStream;
-  private final OutputStream outputStream;
-  private final InputStream errorStream;
   private final Process nodeProcess;
 
   public SchedulingGoalDSLCompilationService() throws SchedulingGoalDSLCompilationException, IOException {
-    final var schedulingDslCompilerPath = System.getenv("SCHEDULING_DSL_COMPILER_PATH");
-    final var commands = new String[] {"node", schedulingDslCompilerPath};
-    this.nodeProcess = Runtime.getRuntime().exec(commands);
+    final var schedulingDslCompilerRoot = System.getenv("SCHEDULING_DSL_COMPILER_ROOT");
+    final var schedulingDslCompilerCommand = System.getenv("SCHEDULING_DSL_COMPILER_COMMAND");
+    this.nodeProcess = Runtime.getRuntime().exec(new String[]{"node", schedulingDslCompilerCommand}, null, new File(schedulingDslCompilerRoot));
+
     try {
-      this.inputStream = this.nodeProcess.getInputStream();
-      this.outputStream = this.nodeProcess.getOutputStream();
-      this.errorStream = this.nodeProcess.getErrorStream();
+      this.nodeProcess.getInputStream();
+      this.nodeProcess.getOutputStream();
     } catch (Exception e) {
       throw new SchedulingGoalDSLCompilationException("Could not create node subprocess: ", e);
     }
-    System.out.println("SchedulingGoalDSLCompilerService created");
   }
 
   public void close() {
@@ -55,25 +58,37 @@ public class SchedulingGoalDSLCompilationService {
   public GoalDefinition compileSchedulingGoalDSL(final String goalTypescript, final String goalName)
   throws SchedulingGoalDSLCompilationException, IOException
   {
-    var inputWriter = new OutputStreamWriter(this.outputStream, StandardCharsets.UTF_8);
-    var outputReader = new BufferedReader(new InputStreamReader(this.inputStream));
-    var errorReader = new BufferedReader(new InputStreamReader(this.errorStream));
+    /*
+    * PROTOCOL:
+    *   denote this java program as JAVA, and the node subprocess as NODE
+    *
+    *   JAVA -- stdin --> NODE: { "source": "sourcecode", "filename": "goalname" } \n
+    *   NODE -- stdout --> JAVA: one of "success\n" or "error\n"
+    *   NODE -- stdout --> JAVA: payload associated with success or failure, must be exactly one line terminated with \n
+    * */
+    final var inputWriter = this.nodeProcess.outputWriter();
+    final var outputReader = this.nodeProcess.inputReader();
 
-    inputWriter.write("{ \"source\": \"" + goalTypescript + "\", \"filename\": \"" + goalName + "\" }\n");
+    final var quotedGoalTypescript = JSONObject.quote(goalTypescript); // adds extra quotes to start and end
+    inputWriter.write("{ \"source\": " + quotedGoalTypescript + ", \"filename\": \"" + goalName + "\" }\n");
+    inputWriter.flush();
 
-    var error = errorReader.readLine();
-    var output = outputReader.readLine();
-    System.out.println(output);
-
-    if (error != null) {
-      throw new SchedulingGoalDSLCompilationException(error);
+    final var status = outputReader.readLine();
+    if (status.equals("error")) {
+      throw new SchedulingGoalDSLCompilationException(outputReader.readLine());
     }
 
-    try {
-      return parseJson(output, schedulingJsonP);
-    } catch (InvalidJsonException | InvalidEntityException e) {
-      throw new SchedulingGoalDSLCompilationException("Could not parse JSON returned from typescript: ", e);
+    if (status.equals("success")) {
+      final var output = outputReader.readLine();
+      try {
+        return parseJson(output, schedulingJsonP);
+      } catch (InvalidJsonException | InvalidEntityException e) {
+        throw new SchedulingGoalDSLCompilationException("Could not parse JSON returned from typescript: ", e);
+      }
     }
+
+    // Status was neither failure nor success, the protocol has been violated.
+    throw new Error("scheduling dsl compiler returned unexpected status: " + status);
   }
 
   private static <T> T parseJson(final String jsonStr, final JsonParser<T> parser) throws InvalidJsonException, InvalidEntityException {
