3b76cda90b34e2203833fe38835a16b8cd95e379
Ryan Goetz <Ryan.R.Goetz@jpl.nasa.gov>
2022-02-28 12:15:32 -0800

    Added volume to the command expansion server and fixed issue using the wrong DB name
    When the command dictionary is uploaded, there needs to be a location where we save the command type libs on the filesystem. The server has been updated to use this storage
    
    There was a bug where the command expansion service wasn't pointed to the correct database. I also updated the server to use the correct name as a default.
    
    [AERIE-1717]

--- command-expansion-server/src/env.ts
+++ command-expansion-server/src/env.ts
@@ -5,15 +5,17 @@ export type Env = {
   POSTGRES_PASSWORD: string;
   POSTGRES_PORT: string;
   POSTGRES_USER: string;
+  STORAGE: string;
 };
 
 export const defaultEnv: Env = {
   PORT: "3000",
-  POSTGRES_AERIE_EXPANSION_DB: "aerie_expansion",
+  POSTGRES_AERIE_EXPANSION_DB: "aerie_commanding",
   POSTGRES_HOST: "localhost",
   POSTGRES_PASSWORD: "aerie",
   POSTGRES_PORT: "5432",
   POSTGRES_USER: "aerie",
+  STORAGE: "commanding_file_store",
 };
 
 export function getEnv(): Env {
@@ -21,11 +23,11 @@ export function getEnv(): Env {
 
   const PORT = env["COMMANDING_SERVER_PORT"] ?? defaultEnv.PORT;
   const POSTGRES_AERIE_EXPANSION_DB = env["COMMANDING_DB"] ?? defaultEnv.POSTGRES_AERIE_EXPANSION_DB;
-  const POSTGRES_HOST = env["POSTGRES_HOST"] ?? defaultEnv.POSTGRES_HOST;
+  const POSTGRES_HOST = env["COMMANDING_DB_SERVER"] ?? defaultEnv.POSTGRES_HOST;
   const POSTGRES_PASSWORD = env["COMMANDING_DB_PASSWORD"] ?? defaultEnv.POSTGRES_PASSWORD;
   const POSTGRES_PORT = env["COMMANDING_DB_PORT"] ?? defaultEnv.POSTGRES_PORT;
   const POSTGRES_USER = env["COMMANDING_DB_USER"] ?? defaultEnv.POSTGRES_USER;
-
+  const STORAGE = env["COMMANDING_LOCAL_STORE"] ?? defaultEnv.STORAGE;
   return {
     PORT,
     POSTGRES_AERIE_EXPANSION_DB,
@@ -33,5 +35,6 @@ export function getEnv(): Env {
     POSTGRES_PASSWORD,
     POSTGRES_PORT,
     POSTGRES_USER,
+    STORAGE,
   };
 }
