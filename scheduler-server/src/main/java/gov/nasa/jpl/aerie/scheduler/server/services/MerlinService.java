ded64563db69748d1e2a2932739f1624f8e0b044
Matt Dailis <matthew.l.dailis@jpl.nasa.gov>
2022-03-02 13:49:18 -0800

    Add GraphQL query to get activity types for plan id

--- scheduler-server/src/main/java/gov/nasa/jpl/aerie/scheduler/server/services/MerlinService.java
+++ scheduler-server/src/main/java/gov/nasa/jpl/aerie/scheduler/server/services/MerlinService.java
@@ -130,5 +130,7 @@ public interface MerlinService {
    */
   Map<ActivityInstance, ActivityInstanceId> createAllPlanActivities(final PlanId planId, final Plan plan) throws IOException, NoSuchPlanException;
 
+  TypescriptCodeGenerationService.MissionModelTypes getMissionModelTypes(final PlanId missionModelId) throws IOException;
+
 
 }
