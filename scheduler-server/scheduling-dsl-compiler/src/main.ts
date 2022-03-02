2a4a62d7b713c4ebf30be5ba468bfd470c8bd3c3
Matt Dailis <matthew.l.dailis@jpl.nasa.gov>
2022-03-02 08:01:49 -0800

    Add logging to scheduling-dsl-compiler

--- scheduler-server/scheduling-dsl-compiler/src/main.ts
+++ scheduler-server/scheduling-dsl-compiler/src/main.ts
@@ -1,7 +1,20 @@
 import Piscina from 'piscina';
+import * as fs from 'fs'
 import * as AST from './libs/scheduler-ast.js';
 
 async function main() {
+
+  var access = fs.createWriteStream('scheduling-dsl-compiler-log.txt', {flags: 'a'});
+
+  // @ts-ignore
+  process.stderr.write = access.write.bind(access);
+
+  console.error("Initial log message")
+
+  process.on('uncaughtException', function(err) {
+    console.error("uncaughtException")
+    console.error((err && err.stack) ? err.stack : err);
+  });
 
   const workerUrl = new URL('./worker.js', import.meta.url);
 
@@ -10,7 +23,12 @@ async function main() {
   });
 
   process.stdin.on("data", async (data: Buffer) => {
+    console.error("data: " + data.toString().trim())
     try {
+      if (data.toString().trim() === "ping") {
+        process.stdout.write("pong\n");
+        return
+      }
       const { source, filename } = JSON.parse(data.toString()) as { source: string, filename: string };
       const result = await piscina.run({
         source,
@@ -20,8 +38,10 @@ async function main() {
       if (stringified === undefined) {
         throw Error(result + " was not JSON serializable")
       }
+      console.error("success\n" + stringified + "\n")
       process.stdout.write("success\n" + stringified + "\n");
     } catch (error: any) {
+      console.error("error\n" + error.message + '\n')
       process.stdout.write("error\n" + error.message + '\n');
     }
   });
