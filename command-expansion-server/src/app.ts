24d6db210939a10ec09b7ae1000b9c778908675e
Joseph Dylan Stewart <joseph.d.stewart@jpl.nasa.gov>
2022-03-03 15:09:50 -0800

    [FEAT] Stub out command server

--- command-expansion-server/src/app.ts
+++ command-expansion-server/src/app.ts
@@ -1,10 +1,8 @@
-import express, { Application, Request, Response } from "express";
+import express, { Application, Request, Response, NextFunction } from "express";
 import bodyParser from "body-parser";
 
 import { getEnv } from "./env.js";
 import { DbExpansion } from "./packages/db/db.js";
-import multer from "multer";
-import fs from "fs";
 import * as ampcs from "@gov.nasa.jpl.aerie/ampcs";
 import { processDictionary } from "./packages/lib/CommandTypeCodegen.js";
 
@@ -19,55 +17,89 @@ const PORT: number = parseInt(getEnv().PORT, 10) ?? 3000;
 DbExpansion.init();
 const db = DbExpansion.getDb();
 
-const upload = multer({ dest: "uploads/" });
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Aerie Command Service");
 });
 
 app.put("/dictionary", async (req, res) => {
-  try {
-    let dictionary: string = req.body.input.dictionary;
+  let dictionary: string = req.body.input.dictionary;
 
-    // un-stringify the xml
-    dictionary = dictionary.replaceAll("&lt;", "<").replaceAll("&gt;", ">").replaceAll("&quot;", '"');
-    console.log(`Dictionary received`);
-    const parsedDictionary = ampcs.parse(dictionary);
-    console.log(
-      `Dictionary parsed - version: ${parsedDictionary.header.version}, mission: ${parsedDictionary.header.mission_name}`
-    );
-    const path = await Promise.resolve(processDictionary(parsedDictionary));
-    console.log(`command-lib generated - path: ${path}`);
+  // un-stringify the xml
+  dictionary = dictionary.replaceAll("&lt;", "<").replaceAll("&gt;", ">").replaceAll("&quot;", '"');
+  console.log(`Dictionary received`);
+  const parsedDictionary = ampcs.parse(dictionary);
+  console.log(
+    `Dictionary parsed - version: ${parsedDictionary.header.version}, mission: ${parsedDictionary.header.mission_name}`
+  );
+  const path = await Promise.resolve(processDictionary(parsedDictionary));
+  console.log(`command-lib generated - path: ${path}`);
 
-    const sqlExpression = `
-      insert into command_dictionary (command_types, mission, version)
-      values ($1, $2, $3)
-      on conflict (mission, version) do update
-      set command_types = $1
-      returning id;
-    `;
+  const sqlExpression = `
+    insert into command_dictionary (command_types, mission, version)
+    values ($1, $2, $3)
+    on conflict (mission, version) do update
+    set command_types = $1
+    returning id;
+  `;
 
-    const { rows } = await db.query(sqlExpression, [
-      path,
-      parsedDictionary.header.mission_name,
-      parsedDictionary.header.version,
-    ]);
-
+  const { rows } = await db.query(sqlExpression, [
+    path,
+    parsedDictionary.header.mission_name,
+    parsedDictionary.header.version,
+  ]);
 
 
-    if (rows.length < 0) {
-      console.error(`POST /dictionary: No command dictionary was updated in the database`);
-      res.status(500).send(`POST /dictionary: No command dictionary was updated in the database`);
-      return;
-    }
-    const id = rows[0];
-    res.status(200).json({ id });
-    return;
-  } catch (err) {
-    console.error(err);
-    res.status(500).send(`POST /dictionary Command Dictionary upload failed: \n${err}`);
+
+  if (rows.length < 0) {
+    console.error(`POST /dictionary: No command dictionary was updated in the database`);
+    res.status(500).send(`POST /dictionary: No command dictionary was updated in the database`);
     return;
   }
+  const id = rows[0];
+  res.status(200).json({ id });
+  return;
+});
+
+app.put('/expansion/:activityTypeName', async (req, res) => {
+  res.status(501).send('PUT /expansion: Not implemented');
+  return;
+});
+
+app.get('/expansion/:expansionId(\\d+)', async (req, res) => {
+  res.status(501).send('GET /expansion: Not implemented');
+  return;
+});
+
+app.put('/expansion-set', async (req, res) => {
+  res.status(501).send('PUT /expansion-set: Not implemented');
+    return;
+});
+
+app.get('/command-types/:dictionaryId(\\d+)', async (req, res) => {
+  res.status(501).send('GET /command-types: Not implemented');
+  return;
+});
+
+app.get('/activity-types/:missionModelId(\\d+)/:activityTypeName', async (req, res) => {
+  res.status(501).send('GET /activity-types: Not implemented');
+  return;
+});
+
+app.get('/commands/:expansionRunId(\\d+)/:activityInstanceId(\\d+)', async (req, res) => {
+  // Pull existing expanded commands for an activity instance of an expansion run
+  res.status(501).send('GET /commands: Not implemented');
+  return;
+});
+
+app.post('/expand-all-activity-instances/:simulationId(\\d+)/:expansionSetId(\\d+)', async (req, res) => {
+    res.status(501).send('POST /expand-all-activity-instances: Not implemented');
+    return;
+});
+
+app.use((err: any, req: Request, res: Response, _next: NextFunction) => {
+  console.error(err)
+  res.status(err.status ?? err.statusCode ?? 500).send(err.message)
 });
 
 app.listen(PORT, () => {
