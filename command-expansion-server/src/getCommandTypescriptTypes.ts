6ec007d1823f3c1d13c2525facbbc850b205160c
Ryan Goetz <Ryan.R.Goetz@jpl.nasa.gov>
2022-03-07 17:15:49 -0800

    Add getCommandTypescriptType class
    This method will query the database to find the command typescript library path and read it from the file system. This library was created when the user uploads the command dictionary

---
+++ command-expansion-server/src/getCommandTypescriptTypes.ts
@@ -0,0 +1,21 @@
+import type {Pool} from 'pg';
+import fs from 'fs';
+import { ErrorWithStatusCode } from './utils/ErrorWithStatusCode.js';
+
+export async function getCommandTypescriptTypes(db: Pool, dictionaryId: number): Promise<string> {
+
+  const {rowCount, rows} = await db.query(`
+    SELECT command_types
+    FROM command_dictionary
+    WHERE id = $1;
+  `, [
+    dictionaryId
+  ]);
+
+
+  const [row] = rows;
+  if (rowCount < 1) {
+    throw new ErrorWithStatusCode(`No dictionary with id: ${dictionaryId}`, 404);
+  }
+  return fs.promises.readFile(row.command_types, 'utf8');
+}
