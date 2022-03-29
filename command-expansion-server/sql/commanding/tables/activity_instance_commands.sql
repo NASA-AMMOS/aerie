e1b6cdcc2b973ba8e422d043bbc5dfdeff887b14
Paul Rosemurgy <paul.c.rosemurgy@jpl.nasa.gov>
2022-03-29 11:17:31 -0700

    Add `errors` to `activity_instance_commands.sql`

--- command-expansion-server/sql/commanding/tables/activity_instance_commands.sql
+++ command-expansion-server/sql/commanding/tables/activity_instance_commands.sql
@@ -1,10 +1,15 @@
 create table activity_instance_commands (
+  id integer generated always as identity,
+
   activity_instance_id integer not null,
-  commands jsonb not null,
+  commands jsonb,
+  errors jsonb not null,
   expansion_run_id integer not null,
 
-  constraint activity_instance_primary_key
-    primary key (activity_instance_id),
+  constraint activity_instance_commands_synthetic_key
+    primary key (id),
+  constraint activity_instance_commands_natural_key
+    unique (activity_instance_id,expansion_run_id),
 
   foreign key (expansion_run_id)
   references expansion_run (id)
@@ -12,9 +17,13 @@ create table activity_instance_commands (
 
 comment on table activity_instance_commands is e''
   'The commands generated from activities instances in the plan.';
+comment on column activity_instance_commands.id is e''
+  'The synthetic identifier for this activity instance command result.';
 comment on column activity_instance_commands.activity_instance_id is e''
   'The activity_instance in the plan.';
 comment on column activity_instance_commands.commands is e''
   'Commands generated for the activity_instance.';
+comment on column activity_instance_commands.errors is e''
+  'Errors encountered while attempting to expand the activity_instance.';
 comment on column activity_instance_commands.expansion_run_id is e''
   'The configuration used during command generation';
