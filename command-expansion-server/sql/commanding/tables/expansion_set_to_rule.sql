c09897393916ef8fa09159dc4465c609d5943132
Paul Rosemurgy <paul.c.rosemurgy@jpl.nasa.gov>
2022-03-30 13:47:28 -0700

    WIP

--- command-expansion-server/sql/commanding/tables/expansion_set_to_rule.sql
+++ command-expansion-server/sql/commanding/tables/expansion_set_to_rule.sql
@@ -1,15 +1,18 @@
 create table expansion_set_to_rule (
   set_id integer not null,
   rule_id integer not null,
+  activity_type text not null,
 
   constraint expansion_set_to_rule_primary_key
-  primary key (set_id,rule_id),
+  primary key (set_id, rule_id),
 
   foreign key (set_id)
   references expansion_set (id),
 
-  foreign key (rule_id)
-  references expansion_rule (id)
+  foreign key (rule_id, activity_type)
+  references expansion_rule (id, activity_type),
+
+  unique (set_id, activity_type)
 );
 comment on table expansion_set_to_rule is e''
   'The join table between expansion_set and expansion_rule';
@@ -27,3 +30,18 @@ create view rule_expansion_set_view as
   select rule_id, expansion_set.*
   from expansion_set_to_rule left join expansion_set
   on expansion_set_to_rule.set_id = expansion_set.id;
+
+
+create function validate_expansion_rule_activity_type()
+returns trigger
+security definer
+language plpgsql as $$
+begin
+  raise exception 'PAUL' hint = 'PAUL HINT'
+  return new;
+end$$;
+
+create trigger validate_expansion_rule_activity_type_trigger
+before insert on expansion_set_to_rule
+for each row
+execute function validate_expansion_rule_activity_type();
