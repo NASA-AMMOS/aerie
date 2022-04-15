d01a38b56fb26e5123540e03997c2ae88e7bae85
Joseph Dylan Stewart <joseph.d.stewart@jpl.nasa.gov>
2022-04-15 11:33:55 -0700

    Fix command expansion tests for 0.11.0

--- command-expansion-server/test/batchLoaders/simulatedActivityInstanceBatchLoader.spec.ts
+++ command-expansion-server/test/batchLoaders/simulatedActivityInstanceBatchLoader.spec.ts
@@ -3,32 +3,33 @@ import { simulatedActivityInstanceBatchLoader } from '../../src/lib/batchLoaders
 import { removeMissionModel, uploadMissionModel } from '../utils/MissionModel.js';
 import { createPlan, removePlan } from '../utils/Plan';
 import { insertActivity, removeActivity } from '../utils/Activity';
-import { executeSimulation, removeSimulation } from '../utils/Simulation';
+import { executeSimulation, removeSimulationArtifacts } from '../utils/Simulation';
 
 let graphqlClient: GraphQLClient;
 let missionModelId: number;
 let planId: number;
 let activityId: number;
-let simulationDatasetId: number;
+let simulationArtifactIds: { simulationId: number, simulationDatasetId: number };
 
 beforeAll(async () => {
   graphqlClient = new GraphQLClient(process.env.MERLIN_GRAPHQL_URL as string);
   missionModelId = await uploadMissionModel(graphqlClient);
   planId = await createPlan(graphqlClient, missionModelId);
   activityId = await insertActivity(graphqlClient, planId);
-  simulationDatasetId = await executeSimulation(graphqlClient, planId);
+  simulationArtifactIds = await executeSimulation(graphqlClient, planId);
 });
 
 afterAll(async () => {
-  console.log(missionModelId, planId, activityId, simulationDatasetId);
-  await removeSimulation(graphqlClient, simulationDatasetId);
+  await removeSimulationArtifacts(graphqlClient, simulationArtifactIds);
   await removeActivity(graphqlClient, activityId);
   await removePlan(graphqlClient, planId);
   await removeMissionModel(graphqlClient, missionModelId);
 });
 
 it('should load simulated activity instance', async () => {
-  const activityInstances = await simulatedActivityInstanceBatchLoader({ graphqlClient })([{ simulationDatasetId }]);
+  const activityInstances = await simulatedActivityInstanceBatchLoader({graphqlClient})([
+    { simulationDatasetId: simulationArtifactIds.simulationDatasetId },
+  ]);
   if (activityInstances[0] instanceof Error) {
     throw activityInstances[0];
   }
