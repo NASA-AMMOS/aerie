46b1191f8de721c81143491dc46e9e3990ba172a
Ryan Goetz <Ryan.R.Goetz@jpl.nasa.gov>
2022-04-21 09:34:35 -0700

    Added Relative, Absolute, and Epoch timing.
    * Added these time values from the seqJson spec.
    * updated the timing "complete" -> to "command_complete" to match the seqJson spec.
    
    [AERIE-1790]

--- command-expansion-server/src/lib/codegen/CommandEDSLPreface.ts
+++ command-expansion-server/src/lib/codegen/CommandEDSLPreface.ts
@@ -2,33 +2,99 @@
 export class Command<A extends ArgType[] | { [argName: string]: any } = [] | {}> {
   public readonly stem: string;
   public readonly arguments: A;
+  public readonly absoluteTime: Temporal.Instant | null = null;
+  public readonly epochTime: { epochName: string; time: Temporal.Duration } | null = null;
+  public readonly relativeTime: Temporal.Duration | null = null;
 
-  private constructor(opts: { stem: string; arguments: A }) {
+  private constructor(opts: CommandOptions<A>) {
     this.stem = opts.stem;
     this.arguments = opts.arguments;
+    if ('absoluteTime' in opts) {
+      this.absoluteTime = opts.absoluteTime;
+    } else if ('epochTime' in opts) {
+      this.epochTime = opts.epochTime;
+    } else if ('relativeTime' in opts) {
+      this.relativeTime = opts.relativeTime;
+    }
   }
 
-  public static new<A extends any[] | { [argName: string]: any }>(opts: { stem: string; arguments: A }): Command<A> {
-    return new Command({
-      stem: opts.stem,
-      arguments: opts.arguments,
-    });
+  public static new<A extends any[] | { [argName: string]: any }>(opts: CommandOptions<A>): Command<A> {
+    if ('absoluteTime' in opts) {
+      return new Command<A>({
+        ...opts,
+        absoluteTime: opts.absoluteTime,
+      });
+    } else if ('epochTime' in opts) {
+      return new Command<A>({
+        ...opts,
+        epochTime: opts.epochTime,
+      });
+    } else if ('relativeTime' in opts) {
+      return new Command<A>({
+        ...opts,
+        relativeTime: opts.relativeTime,
+      });
+    } else {
+      return new Command<A>(opts);
+    }
   }
 
   public toSeqJson() {
     return {
       id: 'command',
       metadata: {},
+
       steps: [
         {
           stem: this.stem,
-          time: { type: 'COMPLETE' },
+          time: {
+            tag: this.absoluteTime
+              ? this.absoluteTime.toString()
+              : this.relativeTime
+              ? this.relativeTime.toString()
+              : this.epochTime
+              ? this.epochTime.time.toString()
+              : '',
+            type: this.absoluteTime
+              ? 'ABSOLUTE'
+              : this.relativeTime
+              ? 'COMMAND_RELATIVE'
+              : this.epochTime
+              ? 'EPOCH_RELATIVE'
+              : 'COMMAND_COMPLETE',
+          },
           type: 'command',
           metadata: {},
+          // include epoch field if we have an epoch time
+          ...(this.epochTime !== null ? { epoch: this.epochTime?.epochName } : {}),
           args: typeof this.arguments == 'object' ? Object.values(this.arguments) : this.arguments,
         },
       ],
     };
+  }
+
+  public absoluteTiming(absoluteTime: Temporal.Instant): Command<A> {
+    return Command.new({
+      stem: this.stem,
+      arguments: this.arguments,
+      absoluteTime: absoluteTime,
+    });
+  }
+
+  public epochTiming(epochName: string, epochTime: Temporal.Duration): Command<A> {
+    return Command.new({
+      stem: this.stem,
+      arguments: this.arguments,
+      epochTime: { epochName, time: epochTime },
+    });
+  }
+
+  public relativeTiming(relativeTime: Temporal.Duration): Command<A> {
+    return Command.new({
+      stem: this.stem,
+      arguments: this.arguments,
+      relativeTime: relativeTime,
+    });
   }
 }
 
@@ -36,12 +102,21 @@ declare global {
   export class Command<A extends ArgType[] | { [argName: string]: any } = [] | {}> {
     public readonly stem: string;
     public readonly arguments: A;
+    public readonly absoluteTime: Temporal.Instant | null;
+    public readonly epochTime: { epochName: string; time: Temporal.Duration } | null;
+    public readonly relativeTime: Temporal.Duration | null;
 
-    private constructor(opts: { stem: string; arguments: A });
+    private constructor(opts: CommandOptions<A>);
 
-    public static new<A extends any[] | { [argName: string]: any }>(opts: { stem: string; arguments: A }): Command<A>;
+    public static new<A extends any[] | { [argName: string]: any }>(opts: CommandOptions<A>): Command<A>;
 
     public toSeqJson(): any;
+
+    public absoluteTiming(absoluteTime: Temporal.Instant): Command<A>;
+
+    public epochTiming(epochName: string, epochTime: Temporal.Duration): Command<A>;
+
+    public relativeTiming(relativeTime: Temporal.Duration): Command<A>;
   }
   type Context = {};
   type ArgType = boolean | string | number;
@@ -62,5 +137,18 @@ declare global {
   type F<BitLength extends 32 | 64> = number;
   type F32 = F<32>;
   type F64 = F<64>;
+  type CommandOptions<A> = { stem: string; arguments: A } & (
+    | {
+        absoluteTime: Temporal.Instant;
+      }
+    | {
+        epochTime: { epochName: string; time: Temporal.Duration };
+      }
+    | {
+        relativeTime: Temporal.Duration;
+      }
+    // CommandComplete
+    | {}
+  );
 }
 /** END Preface */
