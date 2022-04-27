948f6e0e3dd61ec5e0e5343515a2c37692a95536
Matt Dailis <matthew.l.dailis@jpl.nasa.gov>
2022-04-27 08:42:21 -0700

    Load mission model types from JAR when testing

--- scheduler-server/src/test/java/gov/nasa/jpl/aerie/scheduler/server/services/MockMerlinService.java
+++ scheduler-server/src/test/java/gov/nasa/jpl/aerie/scheduler/server/services/MockMerlinService.java
@@ -27,24 +27,26 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 class MockMerlinService implements MissionModelService, PlanService.OwnerRole {
 
-  private final Path modelPath;
-  private final String modelName;
-  private final MissionModelService.MissionModelTypes missionModelTypes;
+  record MissionModelInfo(Path libPath, Path modelPath, String modelName, MissionModelTypes types, Map<String, SerializedValue> config) {}
+
+  private Optional<MissionModelInfo> missionModelInfo = Optional.empty();
   private List<PlannedActivityInstance> initialPlan;
   Collection<PlannedActivityInstance> updatedPlan;
 
-  MockMerlinService(final Path modelPath, final String modelName, final MissionModelService.MissionModelTypes missionModelTypes) {
-    this.modelPath = modelPath;
-    this.modelName = modelName;
+  MockMerlinService() {
     this.initialPlan = List.of();
-    this.missionModelTypes = missionModelTypes;
   }
 
   void setInitialPlan(final List<PlannedActivityInstance> initialPlan) {
     this.initialPlan = initialPlan;
+  }
+
+  void setMissionModel(final MissionModelInfo value) {
+    this.missionModelInfo = Optional.of(value);
   }
 
   @Override
@@ -56,6 +58,7 @@ class MockMerlinService implements MissionModelService, PlanService.OwnerRole {
   public PlanMetadata getPlanMetadata(final PlanId planId)
   throws IOException, NoSuchPlanException, PlanServiceException
   {
+    if (this.missionModelInfo.isEmpty()) throw new RuntimeException("Make sure to call setMissionModel before running a test");
     // Checked that revision matches
     // Uses model version info to load mission model jar
     // The PlanningHorizon here is not used for scheduling (at least not directly)
@@ -66,10 +69,10 @@ class MockMerlinService implements MissionModelService, PlanService.OwnerRole {
             TimeUtility.fromDOY("2021-001T00:00:00"),
             TimeUtility.fromDOY("2021-005T00:00:00")),
         1L,
-        modelPath,
-        modelName,
+        this.missionModelInfo.get().modelPath(),
+        this.missionModelInfo.get().modelName(),
         "1.0.0",
-        Map.of("initialDataPath", SerializedValue.of("/etc/hosts")));
+        this.missionModelInfo.get().config());
   }
 
   @Override
@@ -143,19 +146,21 @@ class MockMerlinService implements MissionModelService, PlanService.OwnerRole {
   public MissionModelTypes getMissionModelTypes(final PlanId planId)
   throws IOException, MissionModelServiceException
   {
-    return this.missionModelTypes;
+    if (this.missionModelInfo.isEmpty()) throw new RuntimeException("Make sure to call setMissionModel before running a test");
+    return this.missionModelInfo.get().types();
   }
 
   @Override
   public MissionModelTypes getMissionModelTypes(final MissionModelId missionModelId)
   throws IOException, MissionModelServiceException, NoSuchMissionModelException
   {
-    return this.missionModelTypes;
+    if (this.missionModelInfo.isEmpty()) throw new RuntimeException("Make sure to call setMissionModel before running a test");
+    return this.missionModelInfo.get().types();
   }
 
   record PlannedActivityInstance(String type, Map<String, SerializedValue> args, Duration startTime) {}
 
-  private static Collection<PlannedActivityInstance> extractPlannedActivityInstances(Plan plan) {
+  private static Collection<PlannedActivityInstance> extractPlannedActivityInstances(final Plan plan) {
     final var plannedActivityInstances = new ArrayList<PlannedActivityInstance>();
     for (final var activity : plan.getActivities()) {
       plannedActivityInstances.add(new PlannedActivityInstance(
