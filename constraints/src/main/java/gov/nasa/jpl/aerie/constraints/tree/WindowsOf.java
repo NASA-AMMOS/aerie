7e86ca23dccf78ddaa5f28fe855ee72a5b0d9b90
Adrien Maillard <adrien.maillard@jpl.nasa.gov>
2022-05-11 11:51:02 -0700

    Add expression transforming Violation objs into Windows

---
+++ constraints/src/main/java/gov/nasa/jpl/aerie/constraints/tree/WindowsOf.java
@@ -0,0 +1,53 @@
+package gov.nasa.jpl.aerie.constraints.tree;
+
+import gov.nasa.jpl.aerie.constraints.model.ActivityInstance;
+import gov.nasa.jpl.aerie.constraints.model.SimulationResults;
+import gov.nasa.jpl.aerie.constraints.model.Violation;
+import gov.nasa.jpl.aerie.constraints.time.Window;
+import gov.nasa.jpl.aerie.constraints.time.Windows;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+
+public final class WindowsOf implements Expression<Windows> {
+  public final Expression<List<Violation>> expression;
+
+  public WindowsOf(Expression<List<Violation>> expression) {
+    this.expression = expression;
+  }
+
+  @Override
+  public Windows evaluate(SimulationResults results, final Window bounds, Map<String, ActivityInstance> environment) {
+    final var ret = new Windows(bounds);
+    final var unsatisfiedWindows = this.expression.evaluate(results, bounds, environment);
+    for(var unsatisfiedWindow : unsatisfiedWindows){
+      ret.intersectWith(unsatisfiedWindow.violationWindows);
+    }
+    return ret;
+  }
+
+  @Override
+  public void extractResources(final Set<String> names) {
+    this.expression.extractResources(names);
+  }
+
+  @Override
+  public String prettyPrint(final String prefix) {
+    return this.expression.prettyPrint(prefix);
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (!(obj instanceof WindowsOf)) return false;
+    final var o = (WindowsOf)obj;
+
+    return Objects.equals(this.expression, o.expression);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(this.expression);
+  }
+}
