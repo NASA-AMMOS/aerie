96bb07fa4c0d159e7cb8c38083039409f92f0d39
Joseph Dylan Stewart <joseph.d.stewart@jpl.nasa.gov>
2022-05-17 15:35:54 -0700

    Reduced view sufficient for current sequence generation needs

--- merlin-server/sql/merlin/views/simulated_activity.sql
+++ merlin-server/sql/merlin/views/simulated_activity.sql
@@ -1,20 +1,14 @@
 create view simulated_activity as
 (
-  select span.id as simulated_activity_id,
-         span.dataset_id as dataset_id,
-         span.parent_id as parent_simulated_activity_id,
+  select span.id as id,
+         simulation_dataset.id as simulation_dataset_id,
+         span.parent_id as parent_id,
          span.start_offset as start_offset,
          span.duration as duration,
          span.attributes as attributes,
-         plan.id as plan_id,
-         activity.id as activity_id,
-         activity_type.model_id as model_id
+         span.type as activity_type_name
 
    from span
      join dataset on span.dataset_id = dataset.id
      join simulation_dataset on dataset.id = simulation_dataset.dataset_id
-     join simulation on simulation_dataset.simulation_id = simulation.id
-     join plan on simulation.plan_id = plan.id
-     left join activity on (span.attributes #> '{directiveId}')::int = activity.id
-     join activity_type on activity_type.name = span.type and activity_type.model_id = plan.model_id
 );
