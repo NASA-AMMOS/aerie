c17e9b5465950ce550bb08601389c1a6f3143f1b
Matt Dailis <matthew.l.dailis@jpl.nasa.gov>
2022-05-23 11:54:04 -0700

    Add AutoValueMapper annotation

---
+++ merlin-framework/src/main/java/gov/nasa/jpl/aerie/merlin/framework/annotations/AutoValueMapper.java
@@ -0,0 +1,10 @@
+package gov.nasa.jpl.aerie.merlin.framework.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.CLASS)
+@Target(ElementType.TYPE)
+public @interface AutoValueMapper {}
