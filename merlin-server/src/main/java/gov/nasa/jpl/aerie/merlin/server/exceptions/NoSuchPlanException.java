833d3349e82a9711b9d4df08d1c540c7039b7009
Paul Rosemurgy <paul.c.rosemurgy@jpl.nasa.gov>
2022-05-26 12:58:17 -0700

    Simplify `NoSuchPlanException`

--- merlin-server/src/main/java/gov/nasa/jpl/aerie/merlin/server/exceptions/NoSuchPlanException.java
+++ merlin-server/src/main/java/gov/nasa/jpl/aerie/merlin/server/exceptions/NoSuchPlanException.java
@@ -2,15 +2,11 @@ package gov.nasa.jpl.aerie.merlin.server.exceptions;
 
 import gov.nasa.jpl.aerie.merlin.server.models.PlanId;
 
-public class NoSuchPlanException extends Exception {
-  private final PlanId id;
+public final class NoSuchPlanException extends Exception {
+  public final PlanId id;
 
   public NoSuchPlanException(final PlanId id) {
     super("No plan exists with id `" + id + "`");
     this.id = id;
-  }
-
-  public PlanId getInvalidPlanId() {
-    return this.id;
   }
 }
