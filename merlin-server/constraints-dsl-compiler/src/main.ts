64b8fe292fc8107f26bd0d29151c9ac65ce0b44d
JoelCourtney <joel.e.courtney@gmail.com>
2022-05-26 14:29:20 -0600

    Ensure compiler server reads until newline received

--- merlin-server/constraints-dsl-compiler/src/main.ts
+++ merlin-server/constraints-dsl-compiler/src/main.ts
@@ -2,6 +2,7 @@ import fs from 'fs';
 import ts from 'typescript';
 import { UserCodeRunner } from '@nasa-jpl/aerie-ts-user-code-runner';
 import type { Constraint } from './libs/constraints-edsl-fluent-api.js'
+import * as readline from "readline";
 
 const codeRunner = new UserCodeRunner();
 const constraintsEDSL = fs.readFileSync(`${process.env.CONSTRAINTS_DSL_COMPILER_ROOT}/src/libs/constraints-edsl-fluent-api.ts`, 'utf8');
@@ -14,8 +15,19 @@ process.on('uncaughtException', (err) => {
   process.exit(1);
 });
 
+const lineReader = readline.createInterface({
+  input: process.stdin
+});
+lineReader.once('line', handleRequest);
+
 async function handleRequest(data: Buffer) {
   try {
+    // Test the health of the service by responding to "ping" with "pong".
+    if (data.toString() === "ping") {
+      process.stdout.write("pong\n");
+      lineReader.once('line', handleRequest);
+      return;
+    }
     const { constraintCode, missionModelGeneratedCode } = JSON.parse(data.toString()) as { constraintCode: string, missionModelGeneratedCode: string };
 
     const result = await codeRunner.executeUserCode<[], Constraint>(
@@ -33,7 +45,7 @@ async function handleRequest(data: Buffer) {
 
     if (result.isErr()) {
       process.stdout.write('error\n' + JSON.stringify(result.unwrapErr().map(err => err.toJSON())) + '\n');
-      process.stdin.once('data', handleRequest);
+      lineReader.once('line', handleRequest);
       return;
     }
 
@@ -43,16 +55,7 @@ async function handleRequest(data: Buffer) {
     }
     process.stdout.write('success\n' + stringified + '\n');
   } catch (error: any) {
-    process.stdout.write('panic\n' + JSON.stringify(error.stack ?? error.message) + '\n');
+    process.stdout.write('panic\n' + JSON.stringify(error.stack ?? error.message) + " attempted to handle: " + data.toString() + '\n');
   }
-  process.stdin.once('data', handleRequest);
+  lineReader.once('line', handleRequest);
 }
-
-process.stdin.once('data', data => {
-  if (data.toString().trim() === 'ping') {
-    // Enable testing the health of the service by sending 'ping' and expecting 'pong' in return.
-    process.stdout.write('pong\n');
-    process.stdin.once('data', handleRequest);
-  }
-});
-
