f109994a7d79908c450dd307cff4477e579fa12a
Pranav Subramanian <pranavs@jpl.nasa.gov>
2022-06-24 11:39:33 -0700

    add activity to foo-mission-model
    added a new activity that just has a controllable duration for the sake of future testing (it was used in earlier tests but ultimately removed, but was useful regardless, so I am committing it)

---
+++ examples/foo-missionmodel/src/main/java/gov/nasa/jpl/aerie/foomissionmodel/activities/BasicFooActivity.java
@@ -0,0 +1,21 @@
+package gov.nasa.jpl.aerie.foomissionmodel.activities;
+
+import gov.nasa.jpl.aerie.foomissionmodel.Mission;
+import gov.nasa.jpl.aerie.merlin.framework.annotations.ActivityType;
+import gov.nasa.jpl.aerie.merlin.protocol.types.Duration;
+
+import static gov.nasa.jpl.aerie.merlin.framework.ModelActions.*;
+import static gov.nasa.jpl.aerie.merlin.framework.annotations.Export.Parameter;
+
+@ActivityType("BasicFooActivity")
+public final class BasicFooActivity {
+  @Parameter
+  public Duration duration = Duration.of(2, Duration.SECONDS);
+
+  @ActivityType.EffectModel
+  @ActivityType.ControllableDuration(parameterName = "duration")
+  public void run(final Mission mission) {
+    delay(duration);
+    mission.activitiesExecuted.add(1);
+  }
+}
--- examples/foo-missionmodel/src/main/java/gov/nasa/jpl/aerie/foomissionmodel/package-info.java
+++ examples/foo-missionmodel/src/main/java/gov/nasa/jpl/aerie/foomissionmodel/package-info.java
@@ -12,6 +12,7 @@
 @WithActivityType(SolarPanelNonLinearTimeDependent.class)
 @WithActivityType(ControllableDurationActivity.class)
 @WithActivityType(OtherControllableDurationActivity.class)
+@WithActivityType(BasicFooActivity.class)
 
 @WithActivityType(DecompositionTestActivities.ParentActivity.class)
 @WithActivityType(DecompositionTestActivities.ChildActivity.class)
@@ -21,6 +22,7 @@ package gov.nasa.jpl.aerie.foomissionmodel;
 import gov.nasa.jpl.aerie.contrib.serialization.rulesets.BasicValueMappers;
 import gov.nasa.jpl.aerie.foomissionmodel.activities.BarActivity;
 import gov.nasa.jpl.aerie.foomissionmodel.activities.BasicActivity;
+import gov.nasa.jpl.aerie.foomissionmodel.activities.BasicFooActivity;
 import gov.nasa.jpl.aerie.foomissionmodel.activities.ControllableDurationActivity;
 import gov.nasa.jpl.aerie.foomissionmodel.activities.DecompositionTestActivities;
 import gov.nasa.jpl.aerie.foomissionmodel.activities.FooActivity;
