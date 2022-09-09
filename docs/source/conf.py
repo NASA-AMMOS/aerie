d56c8e7ebe4ab2fd6bcc5c8977c965070e6c852c
Patrick Kenneally <patrick.kenneally@gmail.com>
2022-09-08 18:00:32 -0700

    Add ScyllaDB multiversion docs theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -77,8 +77,6 @@ smv_tag_whitelist = multiversion_regex_builder(TAGS)
 smv_branch_whitelist = multiversion_regex_builder(BRANCHES)
 # Defines which version is considered to be the latest stable version.
 smv_latest_version = LATEST_VERSION
-# Defines the new name for the latest version.
-smv_rename_latest_version = "stable"
 # Whitelist pattern for remotes (set to None to use local branches only)
 smv_remote_whitelist = r"^origin$"
 # Pattern for released versions
