f2a12f6b4df83b6ee7d5d2c1ce2167be46ba5a38
Paul Rosemurgy <paul.c.rosemurgy@jpl.nasa.gov>
2022-09-16 17:37:56 -0700

    Add `pgTimestampP` PGSQL timestamp parser

---
+++ merlin-server/src/test/java/gov/nasa/jpl/aerie/merlin/server/remotes/postgres/PostgresParsersTest.java
@@ -0,0 +1,19 @@
+package gov.nasa.jpl.aerie.merlin.server.remotes.postgres;
+
+import javax.json.Json;
+import static gov.nasa.jpl.aerie.merlin.server.remotes.postgres.PostgresParsers.pgTimestampP;
+import static org.assertj.core.api.Assertions.assertThat;
+import gov.nasa.jpl.aerie.merlin.server.models.Timestamp;
+import org.junit.jupiter.api.Test;
+
+public final class PostgresParsersTest {
+
+  @Test
+  public void testTimestampParser() {
+    final var timestamp = "2022-01-01T23:43:59.83237+00:00";
+    final var expected = Timestamp.fromString("2022-001T23:43:59.83237");
+    final var actual = pgTimestampP.parse(Json.createValue(timestamp)).getSuccessOrThrow();
+
+    assertThat(expected).isEqualTo(actual);
+  }
+}
