951224a75ff4d7224dc578aac124d78c1c24275f
JoelCourtney <joel.e.courtney@gmail.com>
2022-10-04 13:07:42 -0700

    Remove unnecessary list reverse

--- scheduler-driver/src/main/java/gov/nasa/jpl/aerie/scheduler/constraints/resources/StateQueryParam.java
+++ scheduler-driver/src/main/java/gov/nasa/jpl/aerie/scheduler/constraints/resources/StateQueryParam.java
@@ -35,28 +35,24 @@ public class StateQueryParam implements VariableArgumentComputer {
     //TODO: unify necessary generic profile operations in Profile interface to avoid special casing
     if (simulationResults.realProfiles.containsKey(this.resourceName)) {
       //TODO: improve the profile data structure to allow fast time-keyed query
-      //TODO: improve Interval to allow querying containment in interval directly
-      //for now we look for the last matching profile segment, if any (last to get latest if any overlaps)
+      //for now we look for the last matching profile segment, if any
       final var profile = simulationResults.realProfiles.get(this.resourceName);
-      final Predicate<Segment<LinearEquation>> containsQueryTimeP = (piece)
-          -> !Interval.intersect(piece.interval(), Interval.at(queryT)).isEmpty();
-      final var piece = Lists.reverse(profile.profilePieces.stream().toList())
-                             .stream()
-                             .filter(containsQueryTimeP)
-                             .findFirst()
-                             .orElseThrow(() -> new Error(
-                                 "Linear profile for %s not have a segment at the desired time %s".formatted(resourceName, queryT))
-                             );
+      final var piece = profile.profilePieces
+          .stream()
+          .filter($ -> $.interval().contains(queryT))
+          .findFirst()
+          .orElseThrow(() -> new Error(
+              "Linear profile for %s not have a segment at the desired time %s".formatted(resourceName, queryT))
+          );
       return SerializedValue.of(piece.value().valueAt(queryT));
     } else if (simulationResults.discreteProfiles.containsKey(this.resourceName)) {
       //TODO: improve the profile data structure to allow fast time-keyed query
-      //TODO: improve Interval to allow querying containment in interval directly
-      //for now we look for the last matching profile segment, if any (last to get latest if any overlaps)
+      //for now we look for the last matching profile segment, if any
       final var profile = simulationResults.discreteProfiles.get(this.resourceName);
-      final Predicate<Segment<SerializedValue>> containsQueryTimeP = (piece)
-          -> !Interval.intersect(piece.interval(), Interval.at(queryT)).isEmpty();
-      final var matchPiece = Lists.reverse(profile.profilePieces.stream().toList()).stream()
-                                  .filter(containsQueryTimeP).findFirst();
+      final var matchPiece = profile.profilePieces
+          .stream()
+          .filter($ -> $.interval().contains(queryT))
+          .findFirst();
       return matchPiece
           .map(Segment::value)
           .orElseThrow( () -> new Error("The resource " + this.resourceName + " has no value at time " + queryT));
