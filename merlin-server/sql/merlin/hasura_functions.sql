85c839fb1eec3f13b71e141698de6acdc6a1efdf
Matt Dailis <matthew.l.dailis@jpl.nasa.gov>
2022-10-24 13:27:50 -0700

    Expose duplicate_plan via hasura

--- merlin-server/sql/merlin/hasura_functions.sql
+++ merlin-server/sql/merlin/hasura_functions.sql
@@ -1,3 +1,13 @@
 create schema hasura_functions;
 
--- This file will contain wrappers for sql functions that will be called via hasura
+create table hasura_functions.duplicate_plan_return_value(new_plan_id integer);
+create or replace function hasura_functions.duplicate_plan(plan_id integer, new_plan_name text)
+  returns hasura_functions.duplicate_plan_return_value -- plan_id of the new plan
+  language plpgsql as $$
+declare
+  res integer;
+begin
+  select duplicate_plan(plan_id, new_plan_name) into res;
+  return row(res)::hasura_functions.duplicate_plan_return_value;
+end;
+$$;
