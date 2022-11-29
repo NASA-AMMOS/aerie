630ef9a6e1bfd536d77ca616b05885c5723736f8
Matt Dailis <matthew.l.dailis@jpl.nasa.gov>
2022-11-29 15:34:17 -0800

    Update SchedulerDatabaseTests to not add goals to multiple specifications

--- db-tests/src/test/java/gov/nasa/jpl/aerie/database/SchedulerDatabaseTests.java
+++ db-tests/src/test/java/gov/nasa/jpl/aerie/database/SchedulerDatabaseTests.java
@@ -88,7 +88,7 @@ class SchedulerDatabaseTests {
       specAndTemplateIds = new HashMap<>();
       specAndTemplateIds.put("specification", new int[]{insertSpecification(0), insertSpecification(1)});
       specAndTemplateIds.put("template", new int[]{insertTemplate(), insertTemplate()});
-      goalIds = new int[]{insertGoal(), insertGoal(), insertGoal()};
+      goalIds = new int[]{insertGoal(), insertGoal(), insertGoal(), insertGoal(), insertGoal(), insertGoal()};
     }
 
     @AfterEach
@@ -100,7 +100,7 @@ class SchedulerDatabaseTests {
       helper.clearTable("scheduling_template_goals");
     }
 
-    void insertGoalPriorities(String tableStem, int specOrTemplateIndex, int[] priorities) throws SQLException {
+    void insertGoalPriorities(String tableStem, int specOrTemplateIndex, final int[] goalIndices, int[] priorities) throws SQLException {
       for (int i = 0; i < priorities.length; i++) {
         connection.createStatement().executeUpdate("""
           insert into scheduling_%s_goals(%s_id, goal_id, priority)
@@ -108,7 +108,7 @@ class SchedulerDatabaseTests {
         """.formatted(
             tableStem, tableStem,
             specAndTemplateIds.get(tableStem)[specOrTemplateIndex],
-            goalIds[i],
+            goalIds[goalIndices[i]],
             priorities[i]
         ));
       }
@@ -136,20 +136,20 @@ class SchedulerDatabaseTests {
     @ValueSource(strings = {"specification", "template"})
     void shouldIncrementPrioritiesOnCollision(String tableStem) throws SQLException {
       // untouched values in table, should be unchanged
-      insertGoalPriorities(tableStem, 1, new int[]{0, 1, 2});
-
-      // should cause increments
-      insertGoalPriorities(tableStem, 0, new int[]{0, 0, 0});
-
-      checkPriorities(
-          tableStem, 0,
-          new int[]{0, 1},
-          new int[]{2, 1}
-      );
+      insertGoalPriorities(tableStem, 1, new int[] {0, 1, 2}, new int[]{0, 1, 2});
       checkPriorities(
           tableStem, 1,
           new int[]{0, 1, 2},
           new int[]{0, 1, 2}
+      );
+
+      helper.clearTable("scheduling_%s_goals".formatted(tableStem));
+      // should cause increments
+      insertGoalPriorities(tableStem, 0, new int[] {0, 1, 2}, new int[]{0, 0, 0});
+      checkPriorities(
+          tableStem, 0,
+          new int[]{0, 1, 2},
+          new int[]{2, 1, 0}
       );
     }
 
@@ -157,24 +157,24 @@ class SchedulerDatabaseTests {
     @ValueSource(strings = {"specification", "template"})
     void shouldErrorWhenInsertingNegativePriority(String tableStem) {
       assertThrows(SQLException.class, () -> insertGoalPriorities(
-          tableStem, 0, new int[]{-1}
+          tableStem, 0, new int[] {0, 1, 2}, new int[]{-1}
       ));
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"specification", "template"})
     void shouldErrorWhenInsertingNonConsecutivePriority(String tableStem) throws SQLException {
-      insertGoalPriorities(tableStem, 1, new int[]{0, 1, 2});
+      insertGoalPriorities(tableStem, 1, new int[] {0, 1, 2}, new int[]{0, 1, 2});
       assertThrows(SQLException.class, () -> insertGoalPriorities(
-          tableStem, 0, new int[]{1}
+          tableStem, 0, new int[] {0, 1, 2}, new int[]{1}
       ));
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"specification", "template"})
     void shouldReorderPrioritiesOnUpdate(String tableStem) throws SQLException {
-      insertGoalPriorities(tableStem, 0, new int[]{0, 1, 2});
-      insertGoalPriorities(tableStem, 1, new int[]{0, 1, 2});
+      insertGoalPriorities(tableStem, 0, new int[] {0, 1, 2}, new int[]{0, 1, 2});
+      insertGoalPriorities(tableStem, 1, new int[] {3, 4, 5}, new int[]{0, 1, 2});
 
       // First test lowering a priority
       connection.createStatement().executeUpdate("""
@@ -188,7 +188,7 @@ class SchedulerDatabaseTests {
       );
       checkPriorities(
           tableStem, 1,
-          new int[]{0, 1, 2},
+          new int[]{3, 4, 5},
           new int[]{0, 1, 2}
       );
 
@@ -197,20 +197,23 @@ class SchedulerDatabaseTests {
         update scheduling_%s_goals
         set priority = 2 where %s_id = %d and goal_id = %d;
       """.formatted(tableStem, tableStem, specAndTemplateIds.get(tableStem)[0], goalIds[2]));
-      for (int i : new int[]{0, 1}) {
-        checkPriorities(
-            tableStem, i,
-            new int[]{0, 1, 2},
-            new int[]{0, 1, 2}
-        );
-      }
+      checkPriorities(
+          tableStem, 0,
+          new int[] {0, 1, 2},
+          new int[] {0, 1, 2}
+      );
+      checkPriorities(
+          tableStem, 1,
+          new int[] {3, 4, 5},
+          new int[] {0, 1, 2}
+      );
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"specification", "template"})
     void shouldDecrementPrioritiesOnDelete(String tableStem) throws SQLException {
-      insertGoalPriorities(tableStem, 0, new int[]{0, 1, 2});
-      insertGoalPriorities(tableStem, 1, new int[]{0, 1, 2});
+      insertGoalPriorities(tableStem, 0, new int[] {0, 1, 2}, new int[]{0, 1, 2});
+      insertGoalPriorities(tableStem, 1, new int[] {3, 4, 5}, new int[]{0, 1, 2});
 
       connection.createStatement().executeUpdate("""
         delete from scheduling_%s_goals
@@ -223,7 +226,7 @@ class SchedulerDatabaseTests {
       );
       checkPriorities(
           tableStem, 1,
-          new int[]{0, 1, 2},
+          new int[]{3, 4, 5},
           new int[]{0, 1, 2}
       );
     }
@@ -231,26 +234,33 @@ class SchedulerDatabaseTests {
     @ParameterizedTest
     @ValueSource(strings = {"specification", "template"})
     void shouldTriggerMultipleReorders(String tableStem) throws SQLException {
-      insertGoalPriorities(tableStem, 0, new int[]{0, 1, 2});
-      insertGoalPriorities(tableStem, 1, new int[]{0, 1, 2});
+      insertGoalPriorities(tableStem, 0, new int[] {0, 1, 2}, new int[]{0, 1, 2});
+      insertGoalPriorities(tableStem, 1, new int[] {3, 4, 5}, new int[]{0, 1, 2});
 
       connection.createStatement().executeUpdate("""
         delete from scheduling_%s_goals
         where goal_id = %d;
       """.formatted(tableStem, goalIds[1]));
-      for (int i : new int[]{0, 1}) {
-        checkPriorities(
-            tableStem, i,
-            new int[]{0, 2},
-            new int[]{0, 1}
-        );
-      }
+      connection.createStatement().executeUpdate("""
+        delete from scheduling_%s_goals
+        where goal_id = %d;
+      """.formatted(tableStem, goalIds[4]));
+      checkPriorities(
+          tableStem, 0,
+          new int[] {0, 2},
+          new int[] {0, 1}
+      );
+      checkPriorities(
+          tableStem, 1,
+          new int[] {3, 5},
+          new int[] {0, 1}
+      );
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"specification", "template"})
     void shouldNotTriggerWhenPriorityIsUnchanged(String tableStem) throws SQLException {
-      insertGoalPriorities(tableStem, 1, new int[]{0, 1, 2});
+      insertGoalPriorities(tableStem, 1, new int[] {0, 1, 2}, new int[]{0, 1, 2});
       connection.createStatement().executeUpdate("""
         update scheduling_%s_goals
         set %s_id = %d
