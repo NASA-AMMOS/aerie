f82ed1c8f0fdd51445b1a2812c778f281a4f9086
Theresa Kamerman <theresa.kamerman@jpl.nasa.gov>
2023-04-17 11:20:52 -0700

    Remove unused exceptions:
    - NoSuchActivityInstanceException
    - UnexpectedMissingPlanException
    - UnexpectedMissingMissionModelException
    - UnexpectedJSONException
    - NewMissionModelValidationException

--- merlin-server/src/main/java/gov/nasa/jpl/aerie/merlin/server/remotes/postgres/UnexpectedJsonException.java
+++
@@ -1,7 +0,0 @@
-package gov.nasa.jpl.aerie.merlin.server.remotes.postgres;
-
-public class UnexpectedJsonException extends IntegrationFailureException {
-  public UnexpectedJsonException(final String message, final Throwable cause) {
-    super(message, cause);
-  }
-}
