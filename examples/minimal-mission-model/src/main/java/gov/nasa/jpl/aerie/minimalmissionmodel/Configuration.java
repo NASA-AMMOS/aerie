18292a7a975886af6e9840c0f78336edde5007bd
Matt Dailis <matthew.l.dailis@jpl.nasa.gov>
2023-04-25 19:24:24 -0700

    Add minimal mission model for testing

---
+++ examples/minimal-mission-model/src/main/java/gov/nasa/jpl/aerie/minimalmissionmodel/Configuration.java
@@ -0,0 +1,3 @@
+package gov.nasa.jpl.aerie.minimalmissionmodel;
+
+public record Configuration() { }
---
+++ examples/minimal-mission-model/src/main/java/gov/nasa/jpl/aerie/minimalmissionmodel/Mission.java
@@ -0,0 +1,17 @@
+package gov.nasa.jpl.aerie.minimalmissionmodel;
+
+import gov.nasa.jpl.aerie.contrib.models.Register;
+import gov.nasa.jpl.aerie.contrib.serialization.mappers.EnumValueMapper;
+import gov.nasa.jpl.aerie.merlin.framework.Registrar;
+
+/** A contrived mission model that has only one discrete, non-numeric resource. */
+public final class Mission {
+  public enum GncControlMode {
+    THRUSTERS,
+    REACTION_WHEELS
+  }
+  public final Register<GncControlMode> gncControlMode = Register.forImmutable(GncControlMode.THRUSTERS);
+  public Mission(final Registrar registrar, final Configuration config) {
+    registrar.discrete("/gncControlMode", this.gncControlMode, new EnumValueMapper<>(GncControlMode.class));
+  }
+}
