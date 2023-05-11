c1fcf50a4e02b34924a2ee21e6ddd7630a55fc62
Ryan Goetz <Ryan.R.Goetz@jpl.nasa.gov>
2023-05-11 10:31:50 -1000

    Add JWT ENV to Sequencing Server
    * Update the server to env
    * Update JEST to use env

--- sequencing-server/jestGlobalSetup.js
+++ sequencing-server/jestGlobalSetup.js
@@ -4,7 +4,8 @@ config();
 
 process.env.MERLIN_GATEWAY_URL = process.env.MERLIN_GATEWAY_URL ?? 'http://localhost:9000';
 process.env.MERLIN_GRAPHQL_URL = process.env.MERLIN_GRAPHQL_URL ?? 'http://localhost:8080/v1/graphql';
-process.env.HASURA_GRAPHQL_ADMIN_SECRET = process.env.HASURA_GRAPHQL_ADMIN_SECRET ?? '';
+process.env.HASURA_GRAPHQL_JWT_SECRET = process.env.HASURA_GRAPHQL_JWT_SECRET ?? '{ "type": "HS256", "key": "oursupersecretsupersecurekey1234567890" }'
+process.env.HASURA_GRAPHQL_ADMIN_SECRET = process.env.HASURA_GRAPHQL_ADMIN_SECRET ?? 'aerie';
 
 export default async () => {
 
