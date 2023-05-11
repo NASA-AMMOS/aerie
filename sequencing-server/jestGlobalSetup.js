1cf8e02d8a6a845bc37cce3c6d1f2aef244a2de9
Ryan Goetz <Ryan.R.Goetz@jpl.nasa.gov>
2023-05-11 10:46:12 -1000

    s

--- sequencing-server/jestGlobalSetup.js
+++ sequencing-server/jestGlobalSetup.js
@@ -4,8 +4,7 @@ config();
 
 process.env.MERLIN_GATEWAY_URL = process.env.MERLIN_GATEWAY_URL ?? 'http://localhost:9000';
 process.env.MERLIN_GRAPHQL_URL = process.env.MERLIN_GRAPHQL_URL ?? 'http://localhost:8080/v1/graphql';
-process.env.HASURA_GRAPHQL_JWT_SECRET = process.env.HASURA_GRAPHQL_JWT_SECRET ?? '{ "type": "HS256", "key": "oursupersecretsupersecurekey1234567890" }'
-process.env.HASURA_GRAPHQL_ADMIN_SECRET = process.env.HASURA_GRAPHQL_ADMIN_SECRET ?? 'aerie';
+process.env.HASURA_GRAPHQL_ADMIN_SECRET = process.env.HASURA_GRAPHQL_ADMIN_SECRET ?? '';
 
 export default async () => {
 
