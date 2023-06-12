0072d42d26af3e7f0553475f793e29dbe3a50a8a
Luke (skovati) <skovati@protonmail.com>
2023-06-12 12:48:04 -0700

    create `SimulationDatasetId` record and add to `PlanDatasetRecord`

--- merlin-server/src/main/java/gov/nasa/jpl/aerie/merlin/server/remotes/postgres/PlanDatasetRecord.java
+++ merlin-server/src/main/java/gov/nasa/jpl/aerie/merlin/server/remotes/postgres/PlanDatasetRecord.java
@@ -1,8 +1,12 @@
 package gov.nasa.jpl.aerie.merlin.server.remotes.postgres;
 
 import gov.nasa.jpl.aerie.merlin.protocol.types.Duration;
+import gov.nasa.jpl.aerie.merlin.server.models.SimulationDatasetId;
+
+import java.util.Optional;
 
 public record PlanDatasetRecord(
     long planId,
     long datasetId,
+    Optional<SimulationDatasetId> simulationDatasetId,
     Duration offsetFromPlanStart) {}
