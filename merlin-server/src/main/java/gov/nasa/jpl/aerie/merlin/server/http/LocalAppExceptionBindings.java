8d8d736a1d87c5703c766f49e378d113a46c46e6
Matt Dailis <matthew.l.dailis@jpl.nasa.gov>
2023-06-28 15:15:06 -0700

    Add Unauthorized exception

--- merlin-server/src/main/java/gov/nasa/jpl/aerie/merlin/server/http/LocalAppExceptionBindings.java
+++ merlin-server/src/main/java/gov/nasa/jpl/aerie/merlin/server/http/LocalAppExceptionBindings.java
@@ -4,12 +4,20 @@ import gov.nasa.jpl.aerie.merlin.server.services.LocalMissionModelService;
 import io.javalin.Javalin;
 import io.javalin.plugin.Plugin;
 
+import javax.json.Json;
+
 public final class LocalAppExceptionBindings implements Plugin {
     @Override
     public void apply(final Javalin javalin) {
         javalin.exception(LocalMissionModelService.MissionModelLoadException.class, (ex, ctx) -> ctx
             .status(500)
             .result(ResponseSerializers.serializeMissionModelLoadException(ex).toString())
+            .contentType("application/json"));
+        javalin.exception(Unauthorized.class, (ex, ctx) -> ctx
+            .status(401)
+            .result(Json.createObjectBuilder()
+                        .add("message", "Unauthorized: " + ex.getMessage())
+                        .build().toString())
             .contentType("application/json"));
     }
 }
