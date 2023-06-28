c05cca0bfb8f29541a2f22774f3f40f52928e464
Theresa Kamerman <theresa.kamerman@jpl.nasa.gov>
2023-06-28 13:49:51 -0700

    Add locking triggers to Tags Association Tables
    activity_directive_tags was always supposed to be responsive to if the plan was locked
    
    Additionally, if a tag is in use on a snapshot involved with an in-progress merge review, that tag cannot be deleted. This means that there is never a question of if a tag has been deleted mid-merge.

--- merlin-server/sql/merlin/tables/metadata/activity_directive_tags.sql
+++ merlin-server/sql/merlin/tables/metadata/activity_directive_tags.sql
@@ -16,3 +16,29 @@ create table metadata.activity_directive_tags(
 
 comment on table metadata.activity_directive_tags is e''
   'The tags associated with an activity directive.';
+
+create function adt_check_locked_new()
+  returns trigger
+  security definer
+  language plpgsql as $$
+  begin
+    call plan_locked_exception(new.plan_id);
+    return new;
+  end $$;
+create function adt_check_locked_old()
+  returns trigger
+  security definer
+  language plpgsql as $$
+  begin
+    call plan_locked_exception(old.plan_id);
+    return old;
+  end $$;
+
+create trigger adt_check_plan_locked_insert_update
+  before insert or update on metadata.activity_directive_tags
+  for each row
+  execute procedure adt_check_locked_new();
+create trigger adt_check_plan_locked_update_delete
+  before update or delete on metadata.activity_directive_tags
+  for each row
+  execute procedure adt_check_locked_old();
