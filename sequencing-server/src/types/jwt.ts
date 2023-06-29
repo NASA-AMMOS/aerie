c772bbe4c6372110f096881d24e95790d6a93477
Cody Hansen <codyhansen92@gmail.com>
2023-06-29 10:01:47 -1000

    Removed seq server changes

--- sequencing-server/src/types/jwt.ts
+++
@@ -1,9 +0,0 @@
-import type { AuthSessionVariables } from './authSessionVariables';
-
-export type Jwt = {
-  camToken: string;
-  'https://hasura.io/jwt/claims': AuthSessionVariables & { 'x-hasura-allowed-roles': string[] };
-  username: string;
-  iat: number;
-  exp: number;
-};
