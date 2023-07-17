64681004002b2de5f865306f0a685c513588dc67
Cody Hansen <codyhansen92@gmail.com>
2023-07-17 11:55:34 -1000

    Setup dotenv to source the env file to fix broken local tests

--- sequencing-server/jestGlobalSetup.js
+++ sequencing-server/jestGlobalSetup.js
@@ -1,5 +1,13 @@
 import { config } from 'dotenv';
 import fetch from 'node-fetch';
+import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({ path: __dirname + '/./../.env' });
 config();
 
 process.env.MERLIN_GATEWAY_URL = process.env.MERLIN_GATEWAY_URL ?? 'http://localhost:9000';
@@ -7,17 +15,16 @@ process.env.MERLIN_GRAPHQL_URL = process.env.MERLIN_GRAPHQL_URL ?? 'http://local
 process.env.HASURA_GRAPHQL_ADMIN_SECRET = process.env.HASURA_GRAPHQL_ADMIN_SECRET ?? '';
 
 export default async () => {
-
   try {
     await fetchWithTimeout(`${process.env.MERLIN_GATEWAY_URL}/health`);
   } catch (e) {
-    throw new Error(`Merlin Gateway is not running at ${process.env.MERLIN_GATEWAY_URL}`, {cause: e});
+    throw new Error(`Merlin Gateway is not running at ${process.env.MERLIN_GATEWAY_URL}`, { cause: e });
   }
 
   try {
     await fetchWithTimeout(`${process.env.MERLIN_GRAPHQL_URL.replace('/v1/graphql', '')}/healthz`);
   } catch (e) {
-    throw new Error(`Merlin GraphQL API is not running at ${process.env.MERLIN_GRAPHQL_URL}`, {cause: e});
+    throw new Error(`Merlin GraphQL API is not running at ${process.env.MERLIN_GRAPHQL_URL}`, { cause: e });
   }
 };
 
@@ -25,10 +32,10 @@ async function fetchWithTimeout(resource, options = {}) {
   const { timeout = 2000 } = options;
 
   const controller = new AbortController();
-  const id = setTimeout(() =>  controller.abort(), timeout);
+  const id = setTimeout(() => controller.abort(), timeout);
   const response = await fetch(resource, {
     ...options,
-    signal: controller.signal
+    signal: controller.signal,
   });
   clearTimeout(id);
   return response;
