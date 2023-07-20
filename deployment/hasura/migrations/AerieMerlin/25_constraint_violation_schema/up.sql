b31e855173e05081507a946f23b8a858904adb36
Joel Courtney <joel.e.courtney@gmail.com>
2023-07-20 09:24:46 -0700

    Update migration comment for clarity
    Co-authored-by: Mythicaeda <theresa.kamerman@jpl.nasa.gov>

--- deployment/hasura/migrations/AerieMerlin/25_constraint_violation_schema/up.sql
+++ deployment/hasura/migrations/AerieMerlin/25_constraint_violation_schema/up.sql
@@ -1,5 +1,5 @@
 -- The schema of the database is unchanged, but the JSON schema of the constraint violations in constraint_run is different.
--- Theoretically we could use SQL to transform the schemas of existing JSON objects, but I'd rather just delete them.
+-- Theoretically we could use SQL to transform the schemas of existing JSON objects, but since the table is just a cache it is safe to clear it
 -- programming is hard, ok
 delete from constraint_run;
 
