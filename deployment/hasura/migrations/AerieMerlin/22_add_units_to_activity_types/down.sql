4958b71edb8bd73b2fcdf2646c0d9b67c6779993
Cody Hansen <codyhansen92@gmail.com>
2023-08-03 05:20:17 -1000

    Fixed a typo in the migration for javadoc parsing

--- deployment/hasura/migrations/AerieMerlin/22_add_units_to_activity_types/down.sql
+++ deployment/hasura/migrations/AerieMerlin/22_add_units_to_activity_types/down.sql
@@ -2,7 +2,7 @@ comment on column activity_type.parameter_units is null;
 comment on column activity_type.computed_attribute_units is null;
 alter table activity_type
   drop column parameter_units,
-  drop column computed_attributes_units;
+  drop column computed_attribute_units;
 
 comment on column resource_type.units is null;
 alter table resource_type
