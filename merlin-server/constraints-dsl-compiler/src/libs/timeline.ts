24d074a550437f92cfac81a4c7b4ff0e844ff575
JoelCourtney <joel.e.courtney@gmail.com>
2023-09-02 20:02:57 -0700

    Implement coalesce

--- merlin-server/constraints-dsl-compiler/src/libs/timeline.ts
+++ merlin-server/constraints-dsl-compiler/src/libs/timeline.ts
@@ -1,4 +1,5 @@
-import type {HasInterval, Interval} from "./interval";
+import {HasInterval, Inclusivity, Interval} from "./interval";
+import type {Segment} from "./segment";
 
 export type Timeline<V extends HasInterval> = (bounds: Interval) => V[];
 
@@ -19,6 +20,34 @@ export function bound<V extends HasInterval>(data: any): Timeline<V> {
 
   // TODO
   return bounds => data;
+}
+
+export function coalesce<V>(segments: Segment<V>[], equals?: (l: V, r: V) => boolean): Segment<V>[] {
+  if (segments.length === 0) return segments;
+  if (equals === undefined) {
+    equals = (l, r) => l === r;
+  }
+  let shortIndex = 0;
+  let buffer = segments[0]!;
+  for (const segment of segments.slice(1)) {
+    let comparison = Interval.compareEndToStart(buffer.interval, segment.interval);
+    if (comparison === -1) {
+      segments[shortIndex++] = buffer;
+      buffer = segment;
+    } else {
+      if (equals(buffer.value, segment.value)) {
+        buffer.interval.end = segment.interval.end;
+        buffer.interval.endInclusivity = segment.interval.endInclusivity;
+      } else {
+        buffer.interval.end = segment.interval.end;
+        buffer.interval.endInclusivity = Inclusivity.opposite(segment.interval.endInclusivity);
+        segments[shortIndex++] = buffer;
+        buffer = segment;
+      }
+    }
+  }
+  segments[shortIndex] = buffer;
+  return segments;
 }
 
 export function makeIterable<V>(iter: Iterator<V>): IterableIterator<V> {
