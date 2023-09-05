153f38ac909ed8d85df2b6f139d340ab778586f8
Cody Hansen <codyhansen92@gmail.com>
2023-09-05 06:21:11 -1000

    Reformatted migration files

--- deployment/hasura/migrations/AerieMerlin/26_add_units_to_activity_types/down.sql
+++ deployment/hasura/migrations/AerieMerlin/26_add_units_to_activity_types/down.sql
@@ -1,14 +1,10 @@
-alter table activity_type
-  rename column parameter_definitions to parameters;
-alter table activity_type
-  rename column computed_attribute_definitions to computed_attribute_value_schema;
+alter table activity_type rename column parameter_definitions to parameters;
+alter table activity_type rename column computed_attribute_definitions to computed_attribute_value_schema;
 
-alter table resource_type
-  rename column definition to schema;
+alter table resource_type rename column definition to schema;
 comment on column resource_type.definition is e''
   'The structure of this resource type.';
 
-alter table mission_model_parameters
-  rename column parameter_definitions to parameters;
+alter table mission_model_parameters rename column parameter_definitions to parameters;
 
 call migrations.mark_migration_rolled_back('26');
--- deployment/hasura/migrations/AerieMerlin/26_add_units_to_activity_types/up.sql
+++ deployment/hasura/migrations/AerieMerlin/26_add_units_to_activity_types/up.sql
@@ -1,14 +1,10 @@
-alter table activity_type
-  rename column parameters to parameter_definitions;
-alter table activity_type
-  rename column computed_attribute_value_schema to computed_attribute_definitions;
+alter table activity_type rename column parameters to parameter_definitions;
+alter table activity_type rename column computed_attribute_value_schema to computed_attribute_definitions;
 
-alter table resource_type
-  rename column schema to definition;
+alter table resource_type rename column schema to definition;
 comment on column resource_type.definition is e''
   'The definition including the structure of this resource type.';
 
-alter table mission_model_parameters
-  rename column parameters to parameter_definitions;
+alter table mission_model_parameters rename column parameters to parameter_definitions;
 
 call migrations.mark_migration_applied('26');
