d669657ca738cce489f2df1bc360543802c12a60
David Legg <david.legg@jpl.nasa.gov>
2023-10-26 12:37:27 -0700

    Add unit-awareness library

---
+++ contrib/src/main/java/gov/nasa/jpl/aerie/contrib/streamline/unit_aware/UnitAware.java
@@ -0,0 +1,97 @@
+package gov.nasa.jpl.aerie.contrib.streamline.unit_aware;
+
+import java.util.Comparator;
+import java.util.Objects;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.function.UnaryOperator;
+
+import static java.util.function.Function.identity;
+
+/**
+ * A value of type T with an attached unit.
+ * This can be rescaled to other units measuring the same dimension.
+ */
+public interface UnitAware<T> {
+
+  default T value(Unit desiredUnit) {
+    return in(desiredUnit).value();
+  }
+
+  T value();
+
+  Unit unit();
+
+  UnitAware<T> in(Unit desiredUnit);
+
+  UnitAware<T> map(UnaryOperator<T> unitInvariantFunction);
+
+  /**
+   * General constructor, used primarily by library code.
+   */
+  static <T> UnitAware<T> unitAware(T value, Unit unit, Function<Unit, UnitAware<T>> rescale) {
+
+    return new UnitAware<>() {
+      @Override
+      public T value() {
+        return value;
+      }
+
+      @Override
+      public Unit unit() {
+        return unit;
+      }
+
+      @Override
+      public UnitAware<T> in(Unit desiredUnit) {
+        if (unit == desiredUnit) {
+          // Short-circuit for performance
+          return this;
+        }
+        if (!unit.dimension.equals(desiredUnit.dimension)) {
+          // TODO: Should this be its own kind of exception? A UnitConversionException or DimensionMismatchException?
+          throw new IllegalArgumentException("Cannot convert %s to desired unit %s due to dimension mismatch (%s vs %s)."
+                .formatted(unit, desiredUnit, unit.dimension, desiredUnit.dimension));
+        }
+        return rescale.apply(desiredUnit);
+      }
+
+      @Override
+      public UnitAware<T> map(final UnaryOperator<T> unitInvariantFunction) {
+        return unitAware(unitInvariantFunction.apply(value), unit, rescale);
+      }
+
+      @Override
+      public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (UnitAware<?>) obj;
+        return Objects.equals(value, that.value()) && Objects.equals(unit, that.unit());
+      }
+
+      @Override
+      public int hashCode() {
+        return Objects.hash(value, unit);
+      }
+
+      @Override
+      public String toString() {
+        return "UnitAware[" + "value=" + value + ", " + "unit=" + unit + ']';
+      }
+    };
+  }
+
+  /**
+   * Scaling-function constructor, used primarily in code outside this library.
+   */
+  static <T> UnitAware<T> unitAware(T value, Unit unit, BiFunction<T, Double, T> scaling) {
+    return unitAware(
+        value,
+        unit,
+        desiredUnit -> unitAware(scaling.apply(value, unit.multiplier / desiredUnit.multiplier), desiredUnit, scaling));
+  }
+
+  static <T extends Comparable<T>> Comparator<UnitAware<T>> comparator() {
+    return Comparator.comparing(identity(), (p, q) -> p.value().compareTo(q.value(p.unit())));
+  }
+}
---
+++ contrib/src/main/java/gov/nasa/jpl/aerie/contrib/streamline/unit_aware/UnitAwareOperations.java
@@ -0,0 +1,59 @@
+package gov.nasa.jpl.aerie.contrib.streamline.unit_aware;
+
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+import static gov.nasa.jpl.aerie.contrib.streamline.unit_aware.UnitAware.unitAware;
+
+/**
+ * Utilities for working with unit-aware objects correctly.
+ * Primarily includes arithmetic and comparison functions.
+ */
+public final class UnitAwareOperations {
+  private UnitAwareOperations() {}
+
+  public static <A> UnitAware<A> add(BiFunction<A, Double, A> scaling, BiFunction<A, A, A> addition, UnitAware<? extends A> a, UnitAware<? extends A> b) {
+    return unitAware(addition.apply(a.value(), b.value(a.unit())), a.unit(), scaling);
+  }
+
+  public static <A> UnitAware<A> subtract(BiFunction<A, Double, A> scaling, BiFunction<A, A, A> subtraction, UnitAware<? extends A> a, UnitAware<? extends A> b) {
+    return add(scaling, subtraction, a, b);
+  }
+
+  public static <A, B, C> UnitAware<C> multiply(BiFunction<C, Double, C> scaling, BiFunction<A, B, C> multiplication, UnitAware<? extends A> a, UnitAware<? extends B> b) {
+    return unitAware(multiplication.apply(a.value(), b.value()), a.unit().multiply(b.unit()), scaling);
+  }
+
+  public static <A, B, C> UnitAware<C> divide(BiFunction<C, Double, C> scaling, BiFunction<A, B, C> division, UnitAware<? extends A> a, UnitAware<? extends B> b) {
+    return unitAware(division.apply(a.value(), b.value()), a.unit().divide(b.unit()), scaling);
+  }
+
+  public static <A, B> UnitAware<A> integrate(BiFunction<A, Double, A> scaling, BiFunction<A, B, A> integration, UnitAware<? extends A> a, UnitAware<? extends B> b) {
+    final Unit newUnit = a.unit().multiply(StandardUnits.SECOND);
+    return unitAware(integration.apply(a.value(), b.value(newUnit)), newUnit, scaling);
+  }
+
+  public static <A> UnitAware<A> differentiate(BiFunction<A, Double, A> scaling, Function<A, A> differentiation, UnitAware<? extends A> a) {
+    return unitAware(differentiation.apply(a.value()), a.unit().divide(StandardUnits.SECOND), scaling);
+  }
+
+  public static <A extends Comparable<A>> int compare(UnitAware<? extends A> a, UnitAware<? extends A> b) {
+    return a.value().compareTo(b.value(a.unit()));
+  }
+
+  public static <A extends Comparable<A>> boolean lessThan(UnitAware<? extends A> a, UnitAware<? extends A> b) {
+    return compare(a, b) < 0;
+  }
+
+  public static <A extends Comparable<A>> boolean lessThanOrEquals(UnitAware<? extends A> a, UnitAware<? extends A> b) {
+    return compare(a, b) <= 0;
+  }
+
+  public static <A extends Comparable<A>> boolean greaterThan(UnitAware<? extends A> a, UnitAware<? extends A> b) {
+    return compare(a, b) > 0;
+  }
+
+  public static <A extends Comparable<A>> boolean greaterThanOrEquals(UnitAware<? extends A> a, UnitAware<? extends A> b) {
+    return compare(a, b) >= 0;
+  }
+}
