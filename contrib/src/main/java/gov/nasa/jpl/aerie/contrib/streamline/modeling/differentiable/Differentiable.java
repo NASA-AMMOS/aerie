9c76027f0bdd7cde530f942461dbdbc1e750f9d6
David Legg <david.legg@jpl.nasa.gov>
2023-10-26 18:49:57 -0700

    Add Differentiable dynamics and initial secant approximations

---
+++ contrib/src/main/java/gov/nasa/jpl/aerie/contrib/streamline/modeling/differentiable/Differentiable.java
@@ -0,0 +1,103 @@
+package gov.nasa.jpl.aerie.contrib.streamline.modeling.differentiable;
+
+import gov.nasa.jpl.aerie.contrib.streamline.core.Dynamics;
+import gov.nasa.jpl.aerie.merlin.protocol.types.Duration;
+
+import java.util.function.DoubleUnaryOperator;
+import java.util.function.UnaryOperator;
+
+public interface Differentiable extends Dynamics<Double, Differentiable> {
+  Differentiable derivative();
+
+  static Differentiable differentiable(Differentiable d, DoubleUnaryOperator f, UnaryOperator<Differentiable> fPrime) {
+    return new Differentiable() {
+      @Override
+      public Differentiable derivative() {
+        return fPrime.apply(d).multiply(d.derivative());
+      }
+
+      @Override
+      public Double extract() {
+        return f.applyAsDouble(d.extract());
+      }
+
+      @Override
+      public Differentiable step(final Duration t) {
+        return differentiable(d.step(t), f, fPrime);
+      }
+    };
+  }
+
+  default Differentiable add(Differentiable d) {
+    return new Differentiable() {
+      @Override
+      public Differentiable derivative() {
+        return Differentiable.this.derivative().add(d.derivative());
+      }
+
+      @Override
+      public Double extract() {
+        return Differentiable.this.extract() + d.extract();
+      }
+
+      @Override
+      public Differentiable step(final Duration t) {
+        return Differentiable.this.step(t).add(d.step(t));
+      }
+    };
+  }
+
+  default Differentiable subtract(Differentiable d) {
+    return Differentiable.this.add(d.multiply(-1));
+  }
+
+  default Differentiable multiply(double scalar) {
+    return new Differentiable() {
+      @Override
+      public Differentiable derivative() {
+        return Differentiable.this.derivative().multiply(scalar);
+      }
+
+      @Override
+      public Double extract() {
+        return Differentiable.this.derivative().extract() * scalar;
+      }
+
+      @Override
+      public Differentiable step(final Duration t) {
+        return Differentiable.this.derivative().step(t).multiply(scalar);
+      }
+    };
+  }
+
+  default Differentiable multiply(Differentiable d) {
+    return new Differentiable() {
+      @Override
+      public Differentiable derivative() {
+        return Differentiable.this.derivative().multiply(d).add(Differentiable.this.multiply(d.derivative()));
+      }
+
+      @Override
+      public Double extract() {
+        return Differentiable.this.extract() * d.extract();
+      }
+
+      @Override
+      public Differentiable step(final Duration t) {
+        return Differentiable.this.step(t).multiply(d.step(t));
+      }
+    };
+  }
+
+  default Differentiable divide(double scalar) {
+    return multiply(1 / scalar);
+  }
+
+  default Differentiable divide(Differentiable d) {
+    return this.multiply(d.power(-1));
+  }
+
+  default Differentiable power(int exponent) {
+    return differentiable(this, x -> Math.pow(x, exponent), d -> d.power(exponent - 1).multiply(exponent));
+  }
+}
---
+++ contrib/src/main/java/gov/nasa/jpl/aerie/contrib/streamline/modeling/differentiable/DifferentiableDynamics.java
@@ -0,0 +1,46 @@
+package gov.nasa.jpl.aerie.contrib.streamline.modeling.differentiable;
+
+import gov.nasa.jpl.aerie.contrib.streamline.modeling.polynomial.Polynomial;
+import gov.nasa.jpl.aerie.merlin.protocol.types.Duration;
+
+import static gov.nasa.jpl.aerie.contrib.streamline.modeling.differentiable.Differentiable.differentiable;
+import static gov.nasa.jpl.aerie.contrib.streamline.modeling.polynomial.Polynomial.polynomial;
+
+public final class DifferentiableDynamics {
+  private DifferentiableDynamics() {}
+
+  public static Differentiable constant(double value) {
+    return asDifferentiable(polynomial(value));
+  }
+
+  public static Differentiable asDifferentiable(Polynomial polynomial) {
+    return new Differentiable() {
+      @Override
+      public Differentiable derivative() {
+        return asDifferentiable(polynomial.derivative());
+      }
+
+      @Override
+      public Double extract() {
+        return polynomial.extract();
+      }
+
+      @Override
+      public Differentiable step(final Duration t) {
+        return asDifferentiable(polynomial.step(t));
+      }
+    };
+  }
+
+  public static Differentiable cos(Differentiable argument) {
+    return differentiable(argument, Math::cos, d -> sin(d).multiply(-1));
+  }
+
+  public static Differentiable sin(Differentiable argument) {
+    return differentiable(argument, Math::sin, DifferentiableDynamics::cos);
+  }
+
+  public static Differentiable exp(Differentiable argument) {
+    return differentiable(argument, Math::exp, DifferentiableDynamics::exp);
+  }
+}
