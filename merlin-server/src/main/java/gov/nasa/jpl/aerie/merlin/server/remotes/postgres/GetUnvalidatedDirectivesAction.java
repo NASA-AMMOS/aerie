d480d93026311e79f176529bf3bdac704c7fa777
(skovati) Luke <skovati@protonmail.com>
2023-11-06 12:45:49 -0800

    add bulk directive validation DB actions

---
+++ merlin-server/src/main/java/gov/nasa/jpl/aerie/merlin/server/remotes/postgres/GetUnvalidatedDirectivesAction.java
@@ -0,0 +1,64 @@
+package gov.nasa.jpl.aerie.merlin.server.remotes.postgres;
+
+import gov.nasa.jpl.aerie.merlin.driver.SerializedActivity;
+import gov.nasa.jpl.aerie.merlin.server.models.ActivityDirectiveForValidation;
+import gov.nasa.jpl.aerie.merlin.server.models.ActivityDirectiveId;
+import gov.nasa.jpl.aerie.merlin.server.models.MissionModelId;
+import gov.nasa.jpl.aerie.merlin.server.models.PlanId;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static gov.nasa.jpl.aerie.merlin.server.remotes.postgres.PostgresParsers.activityArgumentsP;
+import static gov.nasa.jpl.aerie.merlin.server.remotes.postgres.PostgresParsers.getJsonColumn;
+
+public class GetUnvalidatedDirectivesAction implements AutoCloseable {
+  private static final String sql = """
+      select ad.id, ad.plan_id, ad.type, ad.arguments, p.model_id
+        from activity_directive ad
+        join activity_directive_validations adv
+          on ad.id = adv.directive_id and ad.plan_id = adv.plan_id
+        join plan p
+          on ad.plan_id = p.id
+        where adv.validations = '{}';
+        """;
+
+  private final PreparedStatement statement;
+
+  public GetUnvalidatedDirectivesAction(Connection connection) throws SQLException {
+    this.statement = connection.prepareStatement(sql);
+  }
+
+  public Map<MissionModelId, List<ActivityDirectiveForValidation>> get() throws SQLException {
+    final var results = this.statement.executeQuery();
+    final var map = new HashMap<MissionModelId, List<ActivityDirectiveForValidation>>();
+
+    while (results.next()) {
+      final var modelId = new MissionModelId(results.getInt("model_id"));
+      final var directiveId = results.getInt("id");
+      final var planId = results.getInt("plan_id");
+      final var type = results.getString("type");
+      final var arguments = getJsonColumn(results, "arguments", activityArgumentsP)
+          .getSuccessOrThrow($ -> new Error("Corrupt activity arguments cannot be parsed: " + $.reason()));
+
+      map.computeIfAbsent(modelId, $ -> new ArrayList<>())
+         .add(new ActivityDirectiveForValidation(
+             new ActivityDirectiveId(directiveId),
+             new PlanId(planId),
+             new SerializedActivity(type, arguments)
+         ));
+    }
+
+    return map;
+  }
+
+  @Override
+  public void close() throws SQLException {
+    this.statement.close();
+  }
+}
--- merlin-server/src/main/java/gov/nasa/jpl/aerie/merlin/server/remotes/postgres/UpdateActivityDirectiveValidationsAction.java
+++ merlin-server/src/main/java/gov/nasa/jpl/aerie/merlin/server/remotes/postgres/UpdateActivityDirectiveValidationsAction.java
@@ -1,8 +1,9 @@
 package gov.nasa.jpl.aerie.merlin.server.remotes.postgres;
 
-import gov.nasa.jpl.aerie.merlin.protocol.model.InputType.ValidationNotice;
-import gov.nasa.jpl.aerie.merlin.server.models.Timestamp;
+import gov.nasa.jpl.aerie.merlin.server.models.ActivityDirectiveForValidation;
+import org.apache.commons.lang3.tuple.Pair;
 import org.intellij.lang.annotations.Language;
+import gov.nasa.jpl.aerie.merlin.server.services.MissionModelService.BulkArgumentValidationResponse;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -11,14 +12,9 @@ import java.util.List;
 
 /*package-local*/ final class UpdateActivityDirectiveValidationsAction implements AutoCloseable {
   private static final @Language("SQL") String sql = """
-    insert into activity_directive_validations
-        as validation (directive_id, plan_id, last_modified_at, validations)
-    values (?, ?, ?::timestamptz, ?::json)
-    on conflict (directive_id, plan_id) do update
-        set
-            last_modified_at = excluded.last_modified_at,
-            validations = excluded.validations
-        where validation.last_modified_at < excluded.last_modified_at
+    update activity_directive_validations
+    set validations = ?::jsonb
+    where (directive_id, plan_id) = (?, ?)
   """;
 
   private final PreparedStatement statement;
@@ -27,15 +23,18 @@ import java.util.List;
     this.statement = connection.prepareStatement(sql);
   }
 
-  public void apply(final long directiveId, final long planId, final Timestamp argumentsModifiedTime, final List<ValidationNotice> notices)
-  throws SQLException, FailedUpdateException
-  {
-    this.statement.setLong(1, directiveId);
-    this.statement.setLong(2, planId);
-    PreparedStatements.setTimestamp(this.statement, 3, argumentsModifiedTime);
-    PreparedStatements.setValidationNotices(this.statement, 4, notices);
+  public void apply(List<Pair<ActivityDirectiveForValidation, BulkArgumentValidationResponse>> updates)
+  throws SQLException, FailedUpdateException {
+    for (final var update : updates) {
+      final var directive = update.getLeft();
+      final var validation = update.getRight();
 
-    statement.executeUpdate();
+      PreparedStatements.setValidationResponse(this.statement, 1, validation);
+      this.statement.setLong(2, directive.id().id());
+      this.statement.setLong(3, directive.planId().id());
+      statement.addBatch();
+    }
+    statement.executeBatch();
   }
 
   @Override
