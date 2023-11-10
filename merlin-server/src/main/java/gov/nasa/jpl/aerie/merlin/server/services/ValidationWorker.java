2c82e81bdbf109f0194b9b417a67fe203bdbeb78
(skovati) Luke <skovati@protonmail.com>
2023-11-10 14:45:41 -0800

    refactor `ValidationWorker` for clarity

--- merlin-server/src/main/java/gov/nasa/jpl/aerie/merlin/server/services/ValidationWorker.java
+++ merlin-server/src/main/java/gov/nasa/jpl/aerie/merlin/server/services/ValidationWorker.java
@@ -2,6 +2,7 @@ package gov.nasa.jpl.aerie.merlin.server.services;
 
 import gov.nasa.jpl.aerie.merlin.server.models.ActivityDirectiveForValidation;
 import gov.nasa.jpl.aerie.merlin.server.services.MissionModelService.NoSuchMissionModelException;
+import gov.nasa.jpl.aerie.merlin.server.services.MissionModelService.BulkArgumentValidationResponse;
 import org.apache.commons.lang3.tuple.Pair;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,6 +20,7 @@ public record ValidationWorker(LocalMissionModelService missionModelService, int
       try {
         Thread.sleep(pollingPeriod);
 
+        // get unvalidated directives, batched by mission model id
         final var validationRequests = missionModelService.getUnvalidatedDirectives();
         if (!validationRequests.isEmpty()) {
           logger.debug(
@@ -27,24 +29,22 @@ public record ValidationWorker(LocalMissionModelService missionModelService, int
               validationRequests.keySet().size());
         }
 
+        // spin up each mission model once and process all corresponding directive validations
         for (final var entry : validationRequests.entrySet()) {
           final var beginTime = System.nanoTime();
+
           final var modelId = entry.getKey();
           logger.debug("processing batch for mission model: {}", modelId.toString());
+
           final var unvalidatedDirectives = entry.getValue();
           final var responses = missionModelService.validateActivityArgumentsBulk(modelId, unvalidatedDirectives);
 
-          // zip lists together
-          final List<Pair<ActivityDirectiveForValidation, MissionModelService.BulkArgumentValidationResponse>>
-              zippedList = IntStream
-                .range(0, Math.min(unvalidatedDirectives.size(), responses.size()))
-                .mapToObj(i -> Pair.of(
-                    unvalidatedDirectives.get(i),
-                    responses.get(i)))
-                .toList();
+          // zip together directives and validations, since DB action needs to insert validations for a given directive
+          final List<Pair<ActivityDirectiveForValidation, BulkArgumentValidationResponse>> zippedList = zip(unvalidatedDirectives, responses);
 
           // write validations out to DB
           missionModelService.updateDirectiveValidations(zippedList);
+
           final var endTime = System.nanoTime();
           final var duration = (endTime - beginTime) / 1_000_000.0;
           logger.debug("processed model batch of size {} in {} ms", unvalidatedDirectives.size(), duration);
@@ -53,10 +53,18 @@ public record ValidationWorker(LocalMissionModelService missionModelService, int
       } catch (NoSuchMissionModelException ex) {
         logger.error("Validation request failed due to no such mission model: {}", ex.toString());
       } catch (InterruptedException ex) {
+        // we were interrupted, so exit gracefully
         return;
       } catch (Throwable t) {
+        // catch all to keep validation thread from dying, which would require a merlin-server restart
         logger.error("Recovering from unexpected error encountered in validation thread: ", t);
       }
     }
+  }
+
+  private static <L, R> List<Pair<L, R>> zip(List<L> left, List<R> right) {
+    return IntStream.range(0, Math.min(left.size(), right.size()))
+                    .mapToObj(i -> Pair.of(left.get(i), right.get(i)))
+                    .toList();
   }
 }
