7cab9c151e044f2c08661dbc63e0f536920bcd0d
Matthew Dailis <matthew.l.dailis@jpl.nasa.gov>
2023-11-10 21:06:20 -0800

    Add initial e2e test for automatic activity validations

---
+++ e2e-tests/src/test/java/gov/nasa/jpl/aerie/e2e/AutomaticValidationTests.java
@@ -0,0 +1,80 @@
+package gov.nasa.jpl.aerie.e2e;
+
+import com.microsoft.playwright.Playwright;
+import gov.nasa.jpl.aerie.e2e.types.ActivityValidation;
+import gov.nasa.jpl.aerie.e2e.utils.GatewayRequests;
+import gov.nasa.jpl.aerie.e2e.utils.HasuraRequests;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import javax.json.Json;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class AutomaticValidationTests {
+  // Requests
+  private Playwright playwright;
+  private HasuraRequests hasura;
+
+  // Per-Test Data
+  private int modelId;
+  private int planId;
+
+  @BeforeAll
+  void beforeAll() {
+    // Setup Requests
+    playwright = Playwright.create();
+    hasura = new HasuraRequests(playwright);
+  }
+
+  @AfterAll
+  void afterAll() {
+    // Cleanup Requests
+    hasura.close();
+    playwright.close();
+  }
+
+  @BeforeEach
+  void beforeEach() throws IOException, InterruptedException {
+    // Insert the Mission Model
+    try (final var gateway = new GatewayRequests(playwright)) {
+      modelId = hasura.createMissionModel(
+          gateway.uploadJarFile(),
+          "Banananation (e2e tests)",
+          "aerie_e2e_tests",
+          "Constraints Tests");
+    }
+    // Insert the Plan
+    planId = hasura.createPlan(
+        modelId,
+        "Test Plan - Automatic Validation Tests",
+        "1212h",
+        "2021-01-01T00:00:00Z");
+  }
+
+  @AfterEach
+  void afterEach() throws IOException {
+    // Remove Model, Plan, and Constraint
+    hasura.deletePlan(planId);
+    hasura.deleteMissionModel(modelId);
+  }
+
+  @Test
+  void validationSuccess() throws IOException, InterruptedException {
+    final var activityId = hasura.insertActivity(
+        planId,
+        "BiteBanana",
+        "1h",
+        Json.createObjectBuilder().add("biteSize", 1).build());
+    Thread.sleep(1000); // TODO consider a while loop here
+    final var activityValidations = hasura.getActivityValidations(planId);
+    final ActivityValidation activityValidation = activityValidations.get((long) activityId);
+    assertEquals(new ActivityValidation.Success(), activityValidation);
+  }
+}
