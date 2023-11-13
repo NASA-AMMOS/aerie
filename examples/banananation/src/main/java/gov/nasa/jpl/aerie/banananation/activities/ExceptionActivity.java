21de8ff38dbfcd3f0219929157d5e67a23b9394c
(skovati) Luke <skovati@protonmail.com>
2023-11-13 13:14:27 -0800

    add `ExceptionActivity` to banananation

---
+++ examples/banananation/src/main/java/gov/nasa/jpl/aerie/banananation/activities/ExceptionActivity.java
@@ -0,0 +1,30 @@
+package gov.nasa.jpl.aerie.banananation.activities;
+
+import gov.nasa.jpl.aerie.merlin.framework.annotations.ActivityType;
+import gov.nasa.jpl.aerie.merlin.framework.annotations.Export.Parameter;
+import gov.nasa.jpl.aerie.merlin.framework.annotations.Export.Validation;
+
+/**
+ * Conditionally throws a runtime exception at both validation time and runtime
+ */
+@ActivityType("ExceptionActivity")
+public final class ExceptionActivity {
+  @Parameter
+  @Banannotation("Set to true to throw runtime exceptions during validation and runtime")
+  public boolean throwException = false;
+
+  @Validation("Throws an exception if set")
+  @Validation.Subject("throwException")
+  public boolean conditionallyThrowException() {
+    if (this.throwException) {
+      throw new RuntimeException("Throwing runtime exception during validation");
+    }
+    return true;
+  }
+
+  public void run() {
+    if (this.throwException) {
+      throw new RuntimeException("Throwing runtime exception during runtime");
+    }
+  }
+}
