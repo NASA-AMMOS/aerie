18e34429789ba12c67a934a7c12f91bfdbf7b5f7
Theresa Kamerman <theresa.kamerman@jpl.nasa.gov>
2023-12-21 16:13:18 -0800

    Replace temp table in validate_anchors with stable function
    Postgres will take out table-level locks even when working with temp tables, and because `validate_anchors` is called whenever an activity is added or has its anchor definition altered, transactions that created or altered activities had their number of in-use locks be unexpectedly high.

--- merlin-server/sql/merlin/applied_migrations.sql
+++ merlin-server/sql/merlin/applied_migrations.sql
@@ -37,3 +37,4 @@ call migrations.mark_migration_applied('31');
 call migrations.mark_migration_applied('32');
 call migrations.mark_migration_applied('33');
 call migrations.mark_migration_applied('34');
+call migrations.mark_migration_applied('35');
