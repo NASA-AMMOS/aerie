9cc5df23e836956bc71e9819b0f7d9e698754e81
Theresa Kamerman <theresa.kamerman@jpl.nasa.gov>
2024-01-11 08:47:45 -0800

    DB Migration

--- merlin-server/sql/merlin/applied_migrations.sql
+++ merlin-server/sql/merlin/applied_migrations.sql
@@ -40,3 +40,4 @@ call migrations.mark_migration_applied('34');
 call migrations.mark_migration_applied('35');
 call migrations.mark_migration_applied('36');
 call migrations.mark_migration_applied('37');
+call migrations.mark_migration_applied('38');
