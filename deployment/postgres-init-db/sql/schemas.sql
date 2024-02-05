a55ac185e9540093a897e9a8313b59a705d3ce5a
Theresa Kamerman <theresa.kamerman@jpl.nasa.gov>
2024-02-05 14:48:04 -0800

    Move Merlin DB Files

---
+++ deployment/postgres-init-db/sql/schemas.sql
@@ -0,0 +1,8 @@
+-- Services
+create schema merlin;
+
+-- Cross Service
+create schema migrations;
+create schema hasura;
+create schema permissions;
+create schema tags;
---
+++ deployment/postgres-init-db/sql/types/merlin/merlin-activity-directive-metadata.sql
@@ -0,0 +1,7 @@
+create domain activity_directive_metadata_set as jsonb
+  constraint activity_directive_metadata_set_is_object
+    check(jsonb_typeof(value) = 'object');
+
+comment on domain activity_directive_metadata_set is e''
+  'The set of mission defined metadata associated with an activity directive.';
+
