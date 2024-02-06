f6efd196af6d0d9ab4016054a2b8555325faab2a
Theresa Kamerman <theresa.kamerman@jpl.nasa.gov>
2024-02-05 17:11:09 -0800

    Add Schema Qualifiers (Tags)
    - move get_tags function to tags schema
    - reorganize tags files

--- deployment/postgres-init-db/sql/tables/tags/merlin/constraint_definition_tags.sql
+++ deployment/postgres-init-db/sql/tables/tags/merlin/constraint_definition_tags.sql
@@ -1,14 +1,14 @@
-create table metadata.constraint_definition_tags (
+create table tags.constraint_definition_tags (
   constraint_id integer not null,
   constraint_revision integer not null,
-  tag_id integer not null references metadata.tags
+  tag_id integer not null references tags.tags
     on update cascade
     on delete cascade,
   primary key (constraint_id, constraint_revision, tag_id),
-  foreign key (constraint_id, constraint_revision) references constraint_definition
+  foreign key (constraint_id, constraint_revision) references merlin.constraint_definition
     on update cascade
     on delete cascade
 );
 
-comment on table metadata.constraint_definition_tags is e''
+comment on table tags.constraint_definition_tags is e''
   'The tags associated with a specific constraint defintion.';
