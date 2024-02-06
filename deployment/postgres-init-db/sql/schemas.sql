3dce1211e0cdb62b5b43239744d3a12e0646ab95
Theresa Kamerman <theresa.kamerman@jpl.nasa.gov>
2024-02-05 16:54:59 -0800

    Add Shared Utility Functions
    These are non-DB specific functions that are reused across the DB. Ie, incrementing a table's revision counter or setting when it was last updated

--- deployment/postgres-init-db/sql/schemas.sql
+++ deployment/postgres-init-db/sql/schemas.sql
@@ -1,11 +1,21 @@
 -- Services
 create schema merlin;
+comment on schema merlin is 'Merlin Service Schema';
 create schema scheduler;
+comment on schema scheduler is 'Scheduler Service Schema';
 create schema sequencing;
+comment on schema sequencing is 'Sequencing Service Schema';
 create schema ui;
+comment on schema ui is 'UI Service Schema';
 
 -- Cross Service
 create schema migrations;
+comment on schema migrations is 'DB Migrations Schema';
 create schema hasura;
+comment on schema hasura is 'Hasura Helper Function Schema';
 create schema permissions;
+comment on schema permissions is 'Aerie User and User Roles Schema';
 create schema tags;
+comment on schema tags is 'Tags Metadata Schema';
+create schema util_functions;
+comment on schema util_functions is 'Cross-service Helper Function Schema';
