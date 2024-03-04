d61bf09f58c61af2e75d9b88cf4086246040fad8
Theresa Kamerman <theresa.kamerman@jpl.nasa.gov>
2024-03-04 11:09:41 -0800

    Update DB Init script

--- deployment/postgres-init-db/init-aerie.sh
+++ deployment/postgres-init-db/init-aerie.sh
@@ -7,54 +7,23 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname postgres <<-EOSQL
   CREATE USER "$AERIE_USERNAME" WITH PASSWORD '$AERIE_PASSWORD';
   \echo 'Done!'
 
+  \echo 'Initializing aerie database...'
+  CREATE DATABASE aerie OWNER "$AERIE_USERNAME";
+  \connect aerie
+  ALTER SCHEMA public OWNER TO "$AERIE_USERNAME";
+  \connect postgres
+  \echo 'Done!'
+
   \echo 'Initializing aerie_hasura database...'
   CREATE DATABASE aerie_hasura;
   GRANT ALL PRIVILEGES ON DATABASE aerie_hasura TO "$AERIE_USERNAME";
-  \echo 'Done!'
-
-  \echo 'Initializing aerie_merlin database...'
-  CREATE DATABASE aerie_merlin;
-  GRANT ALL PRIVILEGES ON DATABASE aerie_merlin TO "$AERIE_USERNAME";
-  \echo 'Done!'
-
-  \echo 'Initializing aerie_scheduler database...'
-  CREATE DATABASE aerie_scheduler;
-  GRANT ALL PRIVILEGES ON DATABASE aerie_scheduler TO "$AERIE_USERNAME";
-  \echo 'Done!'
-
-  \echo 'Initializing aerie_sequencing database...'
-  CREATE DATABASE aerie_sequencing;
-  GRANT ALL PRIVILEGES ON DATABASE aerie_sequencing TO "$AERIE_USERNAME";
-  \echo 'Done!'
-
-  \echo 'Initializing aerie_ui database...'
-  CREATE DATABASE aerie_ui;
-  GRANT ALL PRIVILEGES ON DATABASE aerie_ui TO "$AERIE_USERNAME";
   \echo 'Done!'
 EOSQL
 
 export PGPASSWORD="$AERIE_PASSWORD"
 
-psql -v ON_ERROR_STOP=1 --username "$AERIE_USERNAME" --dbname "aerie_merlin" <<-EOSQL
-  \echo 'Initializing aerie_merlin database objects...'
-  \ir /docker-entrypoint-initdb.d/sql/merlin/init.sql
-  \echo 'Done!'
-EOSQL
-
-psql -v ON_ERROR_STOP=1 --username "$AERIE_USERNAME" --dbname "aerie_scheduler" <<-EOSQL
-  \echo 'Initializing aerie_scheduler database objects...'
-  \ir /docker-entrypoint-initdb.d/sql/scheduler/init.sql
-  \echo 'Done!'
-EOSQL
-
-psql -v ON_ERROR_STOP=1 --username "$AERIE_USERNAME" --dbname "aerie_ui" <<-EOSQL
-  \echo 'Initializing aerie_ui database objects...'
-  \ir /docker-entrypoint-initdb.d/sql/ui/init.sql
-  \echo 'Done!'
-EOSQL
-
-psql -v ON_ERROR_STOP=1 --username "$AERIE_USERNAME" --dbname "aerie_sequencing" <<-EOSQL
-  \echo 'Initializing aerie_sequencing database objects...'
-  \ir /docker-entrypoint-initdb.d/sql/sequencing/init.sql
+psql -v ON_ERROR_STOP=1 --username "$AERIE_USERNAME" --dbname "aerie" <<-EOSQL
+  \echo 'Initializing aerie database objects...'
+  \ir /docker-entrypoint-initdb.d/sql/init.sql
   \echo 'Done!'
 EOSQL
