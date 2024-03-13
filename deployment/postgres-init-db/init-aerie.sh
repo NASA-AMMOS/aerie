cdaa8921022f0488171e252c49d8df74b1cacbd1
Theresa Kamerman <theresa.kamerman@jpl.nasa.gov>
2024-03-13 15:57:24 -0700

    Support one DB user per Aerie service
    Update DB Envvars

--- deployment/postgres-init-db/init-aerie.sh
+++ deployment/postgres-init-db/init-aerie.sh
@@ -7,6 +7,22 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname postgres <<-EOSQL
   CREATE USER "$AERIE_USERNAME" WITH PASSWORD '$AERIE_PASSWORD';
   \echo 'Done!'
 
+  \echo 'Initializing gateway user...'
+  CREATE USER "$GATEWAY_DB_USER" WITH PASSWORD '$GATEWAY_DB_PASSWORD';
+  \echo 'Done!'
+
+  \echo 'Initializing merlin user...'
+  CREATE USER "$MERLIN_DB_USER" WITH PASSWORD '$MERLIN_DB_PASSWORD';
+  \echo 'Done!'
+
+  \echo 'Initializing scheduler user...'
+  CREATE USER "$SCHEDULER_DB_USER" WITH PASSWORD '$SCHEDULER_DB_PASSWORD';
+  \echo 'Done!'
+
+  \echo 'Initializing sequencing user...'
+  CREATE USER "$SEQUENCING_DB_USER" WITH PASSWORD '$SEQUENCING_DB_PASSWORD';
+  \echo 'Done!'
+
   \echo 'Initializing aerie database...'
   CREATE DATABASE aerie OWNER "$AERIE_USERNAME";
   \connect aerie
@@ -23,6 +39,11 @@ EOSQL
 export PGPASSWORD="$AERIE_PASSWORD"
 
 psql -v ON_ERROR_STOP=1 --username "$AERIE_USERNAME" --dbname "aerie" <<-EOSQL
+  \set aerie_user $AERIE_USERNAME
+  \set gateway_user $GATEWAY_DB_USER
+  \set merlin_user $MERLIN_DB_USER
+  \set scheduler_user $SCHEDULER_DB_USER
+  \set sequencing_user $SEQUENCING_DB_USER
   \echo 'Initializing aerie database objects...'
   \ir /docker-entrypoint-initdb.d/sql/init.sql
   \echo 'Done!'
