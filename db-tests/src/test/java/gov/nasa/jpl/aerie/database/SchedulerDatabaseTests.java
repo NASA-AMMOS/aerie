bdbc42350306b5541b82c7b49cde352cdb9c8894
Theresa Kamerman <theresa.kamerman@jpl.nasa.gov>
2024-03-13 10:08:54 -0700

    Update DB Tests
    - Add schema info and language tags to SQL statements
    - Suppress `SqlSourceToSinkFlow` warnings, which are not significant since this is test code
    - Extract duplicate SQL code into methods
    
    - Close the connection and datasource when stopping DB in `DatabaseTestHelper`. Method renamed to `close` to reflect this.
    - Create helper method `DBTestHelper::clearSchema` to simplify test cleanup code
    
    - Move `PlanCollaborationTests::UpdateActivityName` and `DeleteActivityDirective` to `MerlinDBTestHelper`
    - Wrap created statements in "try-with-resources"
    - Fix Plan Collaboration Test `mergeBaseBetweenSelf`, which wasn't calling `get_merge_base` before

--- db-tests/src/test/java/gov/nasa/jpl/aerie/database/SchedulerDatabaseTests.java
+++ db-tests/src/test/java/gov/nasa/jpl/aerie/database/SchedulerDatabaseTests.java
@@ -2,7 +2,6 @@ package gov.nasa.jpl.aerie.database;
 
 import org.junit.jupiter.api.*;
 
-import java.io.File;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -11,36 +10,32 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class SchedulerDatabaseTests {
-  private static final File initSqlScriptFile = new File("../scheduler-server/sql/scheduler/init.sql");
   private DatabaseTestHelper helper;
+  private MerlinDatabaseTestHelper merlinHelper;
 
   private Connection connection;
 
   @BeforeAll
   void beforeAll() throws SQLException, IOException, InterruptedException {
-    helper = new DatabaseTestHelper(
-        "aerie_scheduler_test",
-        "Scheduler Database Tests",
-        initSqlScriptFile
-    );
-    helper.startDatabase();
+    helper = new DatabaseTestHelper("aerie_scheduler_test", "Scheduler Database Tests");
     connection = helper.connection();
+    merlinHelper = new MerlinDatabaseTestHelper(connection);
+    merlinHelper.insertUser("scheduler db tests");
   }
 
   @AfterAll
   void afterAll() throws SQLException, IOException, InterruptedException {
-    helper.stopDatabase();
-    connection = null;
-    helper = null;
+    helper.close();
   }
 
-  int insertSpecification(final long planId) throws SQLException {
+  int getSpecification(final long planId) throws SQLException {
     try (final var statement = connection.createStatement()) {
-      final var res = statement.executeQuery("""
-        insert into scheduling_specification(
-          revision, plan_id, plan_revision, horizon_start, horizon_end, simulation_arguments, analysis_only
-        ) values (0, %d, 0, now(), now(), '{}', false) returning id;
-      """.formatted(planId));
+      final var res = statement.executeQuery(
+          //language=sql
+          """
+          select id from scheduler.scheduling_specification
+          where plan_id = %d;
+          """.formatted(planId));
       res.next();
       return res.getInt("id");
     }
@@ -48,17 +43,19 @@ class SchedulerDatabaseTests {
 
   int insertGoal() throws SQLException {
     try (final var statement = connection.createStatement()) {
-      final var res = statement.executeQuery("""
-        with metadata(id, owner) as (
-          insert into scheduling_goal_metadata(name, description, owner, updated_by)
-          values ('test goal', 'no-op', 'scheduler db tests', 'scheduler db tests')
-          returning id, owner
-        )
-        insert into scheduling_goal_definition(goal_id, definition, author)
-        select m.id, 'nothing', m.owner
-        from metadata m
-        returning goal_id as id;
-      """);
+      final var res = statement.executeQuery(
+          //language=sql
+          """
+          with metadata(id, owner) as (
+            insert into scheduler.scheduling_goal_metadata(name, description, owner, updated_by)
+            values ('test goal', 'no-op', 'scheduler db tests', 'scheduler db tests')
+            returning id, owner
+          )
+          insert into scheduler.scheduling_goal_definition(goal_id, definition, author)
+          select m.id, 'nothing', m.owner
+          from metadata m
+          returning goal_id as id;
+          """);
       res.next();
       return res.getInt("id");
     }
@@ -71,41 +68,40 @@ class SchedulerDatabaseTests {
 
     @BeforeEach
     void beforeEach() throws SQLException {
-      specificationIds = new int[]{insertSpecification(0), insertSpecification(1)};
+      final int modelId = merlinHelper.insertMissionModel(merlinHelper.insertFileUpload());
+      specificationIds = new int[]{
+          getSpecification(merlinHelper.insertPlan(modelId)),
+          getSpecification(merlinHelper.insertPlan(modelId))
+      };
       goalIds = new int[]{insertGoal(), insertGoal(), insertGoal(), insertGoal(), insertGoal(), insertGoal()};
     }
 
     @AfterEach
     void afterEach() throws SQLException {
-      helper.clearTable("scheduling_specification");
-      helper.clearTable("scheduling_goal_metadata");
-      helper.clearTable("scheduling_goal_definition");
-      helper.clearTable("scheduling_specification_goals");
+      helper.clearSchema("merlin");
+      helper.clearSchema("scheduler");
     }
 
     void insertGoalPriorities(int specOrTemplateIndex, final int[] goalIndices, int[] priorities) throws SQLException {
       for (int i = 0; i < priorities.length; i++) {
-        connection.createStatement().executeUpdate("""
-          insert into scheduling_specification_goals(specification_id, goal_id, priority)
-          values (%d, %d, %d);
-        """.formatted(
-            specificationIds[specOrTemplateIndex],
-            goalIds[goalIndices[i]],
-            priorities[i]
-        ));
+        connection.createStatement().executeUpdate(
+            //language=sql
+            """
+            insert into scheduler.scheduling_specification_goals(specification_id, goal_id, priority)
+            values (%d, %d, %d);
+            """.formatted(specificationIds[specOrTemplateIndex], goalIds[goalIndices[i]], priorities[i]));
       }
     }
 
     void checkPriorities(int specOrTemplateIndex, int[] goalIdIndices, int[] priorities) throws SQLException {
       assertEquals(goalIdIndices.length, priorities.length);
       for (int i = 0; i < priorities.length; i++) {
-        final var res = connection.createStatement().executeQuery("""
-          select priority from scheduling_specification_goals
-          where goal_id = %d and specification_id = %d;
-        """.formatted(
-            goalIds[goalIdIndices[i]],
-            specificationIds[specOrTemplateIndex])
-        );
+        final var res = connection.createStatement().executeQuery(
+            //language=sql
+            """
+            select priority from scheduler.scheduling_specification_goals
+            where goal_id = %d and specification_id = %d;
+            """.formatted(goalIds[goalIdIndices[i]], specificationIds[specOrTemplateIndex]));
         res.next();
         assertEquals(priorities[i], res.getInt("priority"));
         res.close();
@@ -118,7 +114,7 @@ class SchedulerDatabaseTests {
       insertGoalPriorities(1, new int[] {0, 1, 2}, new int[]{0, 1, 2});
       checkPriorities(1, new int[]{0, 1, 2}, new int[]{0, 1, 2});
 
-      helper.clearTable("scheduling_specification_goals");
+      helper.clearTable("scheduler.scheduling_specification_goals");
       // should cause increments
       insertGoalPriorities(0, new int[] {0, 1, 2}, new int[]{0, 0, 0});
       checkPriorities(0, new int[]{0, 1, 2}, new int[]{2, 1, 0});
@@ -140,8 +136,10 @@ class SchedulerDatabaseTests {
     }
 
     private int getSpecificationRevision(int specificationId) throws SQLException {
-      final var res = connection.createStatement().executeQuery("""
-          select revision from scheduling_specification
+      final var res = connection.createStatement().executeQuery(
+          //language=sql
+          """
+          select revision from scheduler.scheduling_specification
           where id = %d;
           """.formatted(specificationId));
       res.next();
@@ -152,11 +150,15 @@ class SchedulerDatabaseTests {
     void shouldIncrementSpecRevisionAfterModifyingGoalSpec() throws SQLException {
       insertGoalPriorities(0, new int[] {0, 1, 2, 3, 4}, new int[]{0, 1, 2, 3, 4});
       final var revisionBefore  = getSpecificationRevision(specificationIds[0]);
-      connection.createStatement().executeUpdate("""
-        update scheduling_specification_goals
-        set goal_revision = 0
-        where goal_id = %d;
-      """.formatted(goalIds[3]));
+      try(final var statement = connection.createStatement()){
+        statement.executeUpdate(
+            //language=sql
+            """
+            update scheduler.scheduling_specification_goals
+            set goal_revision = 0
+            where goal_id = %d;
+            """.formatted(goalIds[3]));
+      }
       final var revisionAfter  = getSpecificationRevision(specificationIds[0]);
       assertEquals(revisionBefore + 1, revisionAfter);
     }
@@ -166,21 +168,31 @@ class SchedulerDatabaseTests {
       insertGoalPriorities(0, new int[] {0, 1, 2}, new int[]{0, 1, 2});
       insertGoalPriorities(1, new int[] {3, 4, 5}, new int[]{0, 1, 2});
 
-      // First test lowering a priority
-      connection.createStatement().executeUpdate("""
-        update scheduling_specification_goals
-        set priority = 0 where specification_id = %d and goal_id = %d;
-      """.formatted(specificationIds[0], goalIds[2]));
-      checkPriorities( 0, new int[]{0, 1, 2}, new int[]{1, 2, 0});
-      checkPriorities( 1, new int[]{3, 4, 5}, new int[]{0, 1, 2});
+      try(final var statement = connection.createStatement()) {
+        // First test lowering a priority
+        statement.executeUpdate(
+            //language=sql
+            """
+            update scheduler.scheduling_specification_goals
+            set priority = 0
+            where specification_id = %d
+            and goal_id = %d;
+            """.formatted(specificationIds[0], goalIds[2]));
+        checkPriorities(0, new int[]{0, 1, 2}, new int[]{1, 2, 0});
+        checkPriorities(1, new int[]{3, 4, 5}, new int[]{0, 1, 2});
 
-      /// Next test raising a priority
-      connection.createStatement().executeUpdate("""
-        update scheduling_specification_goals
-        set priority = 2 where specification_id = %d and goal_id = %d;
-      """.formatted(specificationIds[0], goalIds[2]));
-      checkPriorities( 0, new int[] {0, 1, 2}, new int[] {0, 1, 2});
-      checkPriorities( 1, new int[] {3, 4, 5}, new int[] {0, 1, 2});
+        /// Next test raising a priority
+        statement.executeUpdate(
+            //language=sql
+            """
+            update scheduler.scheduling_specification_goals
+            set priority = 2
+            where specification_id = %d
+            and goal_id = %d;
+            """.formatted(specificationIds[0], goalIds[2]));
+      }
+      checkPriorities(0, new int[] {0, 1, 2}, new int[] {0, 1, 2});
+      checkPriorities(1, new int[] {3, 4, 5}, new int[] {0, 1, 2});
     }
 
     @Test
@@ -188,10 +200,15 @@ class SchedulerDatabaseTests {
       insertGoalPriorities(0, new int[] {0, 1, 2}, new int[]{0, 1, 2});
       insertGoalPriorities(1, new int[] {3, 4, 5}, new int[]{0, 1, 2});
 
-      connection.createStatement().executeUpdate("""
-        delete from scheduling_specification_goals
-        where specification_id = %d and goal_id = %d;
-      """.formatted(specificationIds[0], goalIds[1]));
+      try(final var statement = connection.createStatement()) {
+        statement.executeUpdate(
+            //language=sql
+            """
+            delete from scheduler.scheduling_specification_goals
+            where specification_id = %d
+            and goal_id = %d;
+            """.formatted(specificationIds[0], goalIds[1]));
+      }
       checkPriorities(0, new int[]{0, 2}, new int[]{0, 1});
       checkPriorities(1, new int[]{3, 4, 5}, new int[]{0, 1, 2});
     }
@@ -201,14 +218,20 @@ class SchedulerDatabaseTests {
       insertGoalPriorities(0, new int[] {0, 1, 2}, new int[]{0, 1, 2});
       insertGoalPriorities(1, new int[] {3, 4, 5}, new int[]{0, 1, 2});
 
-      connection.createStatement().executeUpdate("""
-        delete from scheduling_specification_goals
-        where goal_id = %d;
-      """.formatted(goalIds[1]));
-      connection.createStatement().executeUpdate("""
-        delete from scheduling_specification_goals
-        where goal_id = %d;
-      """.formatted(goalIds[4]));
+      try(final var statement=connection.createStatement()) {
+        statement.executeUpdate(
+            //language=sql
+            """
+            delete from scheduler.scheduling_specification_goals
+            where goal_id = %d;
+            """.formatted(goalIds[1]));
+        statement.executeUpdate(
+            //language=sql
+            """
+            delete from scheduler.scheduling_specification_goals
+            where goal_id = %d;
+            """.formatted(goalIds[4]));
+      }
       checkPriorities( 0, new int[] {0, 2}, new int[] {0, 1});
       checkPriorities(1, new int[] {3, 5}, new int[] {0, 1});
     }
@@ -216,44 +239,43 @@ class SchedulerDatabaseTests {
     @Test
     void shouldNotTriggerWhenPriorityIsUnchanged() throws SQLException {
       insertGoalPriorities(1, new int[] {0, 1, 2}, new int[]{0, 1, 2});
-      connection.createStatement().executeUpdate("""
-        update scheduling_specification_goals
-        set specification_id = %d
-        where specification_id = %d;
-      """.formatted(
-          specificationIds[0],
-          specificationIds[1]
-      ));
+      try(final var statement = connection.createStatement()) {
+        statement.executeUpdate(
+            //language=sql
+            """
+            update scheduler.scheduling_specification_goals
+            set specification_id = %d
+            where specification_id = %d;
+            """.formatted(specificationIds[0], specificationIds[1]));
+      }
       checkPriorities(0, new int[]{0, 1, 2}, new int[]{0, 1, 2});
     }
 
     @Test
     void shouldGeneratePriorityWhenNull() throws SQLException {
-      connection.createStatement().executeUpdate("""
-          insert into scheduling_specification_goals(specification_id, goal_id)
-          values (%d, %d);
-        """.formatted(
-          specificationIds[0],
-          goalIds[0]
-      ));
-      checkPriorities(0, new int[]{0}, new int[]{0});
-      connection.createStatement().executeUpdate("""
-          insert into scheduling_specification_goals(specification_id, goal_id, priority)
-          values (%d, %d, null);
-        """.formatted(
-          specificationIds[0],
-          goalIds[2]
-      ));
-      checkPriorities(0, new int[]{0, 2}, new int[]{0, 1});
-      connection.createStatement().executeUpdate("""
-          insert into scheduling_specification_goals(specification_id, goal_id)
-          values (%d, %d);
-        """.formatted(
-          specificationIds[0],
-          goalIds[1]
-      ));
-      checkPriorities(0, new int[]{0, 2, 1}, new int[]{0, 1, 2});
+      try(final var statement = connection.createStatement()) {
+        statement.executeUpdate(
+            //language=sql
+            """
+            insert into scheduler.scheduling_specification_goals(specification_id, goal_id)
+            values (%d, %d);
+            """.formatted(specificationIds[0], goalIds[0]));
+        checkPriorities(0, new int[] {0}, new int[] {0});
+        statement.executeUpdate(
+            //language=sql
+            """
+            insert into scheduler.scheduling_specification_goals(specification_id, goal_id, priority)
+            values (%d, %d, null);
+            """.formatted(specificationIds[0], goalIds[2]));
+        checkPriorities(0, new int[] {0, 2}, new int[] {0, 1});
+        statement.executeUpdate(
+            //language=sql
+            """
+            insert into scheduler.scheduling_specification_goals(specification_id, goal_id)
+            values (%d, %d);
+            """.formatted(specificationIds[0], goalIds[1]));
+        checkPriorities(0, new int[] {0, 2, 1}, new int[] {0, 1, 2});
+      }
     }
   }
-
 }
