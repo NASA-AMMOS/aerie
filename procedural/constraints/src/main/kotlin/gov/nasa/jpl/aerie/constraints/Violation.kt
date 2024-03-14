4d1500f3e26157887776cb0e28cf2a91df496a15
JoelCourtney <joel.e.courtney@gmail.com>
2024-03-14 12:08:23 -0700

    Violations timeline

---
+++ procedural/constraints/src/main/kotlin/gov/nasa/jpl/aerie/constraints/Violation.kt
@@ -0,0 +1,22 @@
+package gov.nasa.jpl.aerie.constraints
+
+import gov.nasa.jpl.aerie.timeline.Interval
+import gov.nasa.jpl.aerie.timeline.payloads.IntervalLike
+
+/** A single violation of a constraint. */
+data class Violation(
+    /** Interval on which the violation occurs. */
+    override val interval: Interval,
+
+    /** List of associated activities (directives or instances) that are related to the violation. */
+    val ids: List<ActivityId> = listOf()
+) : IntervalLike<Violation> {
+
+  override fun withNewInterval(i: Interval) = Violation(i, ids)
+
+  /** Constructs a violation on the same interval with a different list of ids. */
+  fun withNewIds(vararg id: ActivityId) = Violation(interval, id.asList())
+
+  /** Constructs a violation on the same interval with a different list of ids. */
+  fun withNewIds(ids: List<ActivityId>) = Violation(interval, ids)
+}
