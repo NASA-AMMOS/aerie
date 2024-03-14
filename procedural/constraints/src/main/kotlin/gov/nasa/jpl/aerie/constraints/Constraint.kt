cf869b5718549ed1dad8c12627dacf7107932c58
JoelCourtney <joel.e.courtney@gmail.com>
2024-03-14 12:08:30 -0700

    Constraint interface

---
+++ procedural/constraints/src/main/kotlin/gov/nasa/jpl/aerie/constraints/Constraint.kt
@@ -0,0 +1,18 @@
+package gov.nasa.jpl.aerie.constraints
+
+import gov.nasa.jpl.aerie.timeline.CollectOptions
+import gov.nasa.jpl.aerie.timeline.plan.Plan
+
+/** The interface that all constraints must satisfy. */
+interface Constraint {
+  /**
+   * Run the constraint on a plan.
+   *
+   * The provided collect options are the options that the [Violations] result will be collected on after
+   * the constraint is run. The constraint does not need to use the options unless it collects a timeline prematurely.
+   *
+   * @param plan the plan to check the constraint on
+   * @param options the [CollectOptions] that the result will be collected with
+   */
+  fun run(plan: Plan, options: CollectOptions): Violations
+}
