99d9a9b8d59c8cbfdfdc224fef08e419a3c70107
(skovati) Luke <skovati@protonmail.com>
2024-03-26 14:42:35 -0700

    Add small test constraints

---
+++ procedural/examples/foo-procedures/src/main/java/gov/nasa/jpl/aerie/procedural/examples/fooprocedures/constraints/Hello.java
@@ -0,0 +1,9 @@
+package gov.nasa.jpl.aerie.procedural.examples.fooprocedures.constraints;
+
+import gov.nasa.jpl.aerie.procedural.examples.fooprocedures.Helper;
+
+class Hello {
+  public static void main(String[] args) {
+    System.out.println(Helper.greeting());
+  }
+}
