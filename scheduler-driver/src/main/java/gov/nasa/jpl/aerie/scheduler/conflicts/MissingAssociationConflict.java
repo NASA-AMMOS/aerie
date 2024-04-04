fcac23dd44cfcf8eec3b6fd32f0c056ce634e045
jmdelfa <jm_delfa@hotmail.com>
2024-04-04 15:08:45 -0700

    Updated MissingActivityTemplateConflict and MissingAssociationConflict. They incorporate the anchorId (if any) and whether the anchor is to Start as information for PrioritySolver.java to solve the conflict

--- scheduler-driver/src/main/java/gov/nasa/jpl/aerie/scheduler/conflicts/MissingAssociationConflict.java
+++ scheduler-driver/src/main/java/gov/nasa/jpl/aerie/scheduler/conflicts/MissingAssociationConflict.java
@@ -4,25 +4,47 @@ import gov.nasa.jpl.aerie.constraints.model.EvaluationEnvironment;
 import gov.nasa.jpl.aerie.constraints.time.Windows;
 import gov.nasa.jpl.aerie.scheduler.model.SchedulingActivityDirective;
 import gov.nasa.jpl.aerie.scheduler.goals.Goal;
+import gov.nasa.jpl.aerie.scheduler.model.SchedulingActivityDirectiveId;
 
 import java.util.Collection;
+import java.util.Optional;
 
 public class MissingAssociationConflict extends Conflict {
   private final Collection<SchedulingActivityDirective> instances;
 
+  private final Optional<Boolean> anchorToStart;
+  private final Optional<SchedulingActivityDirectiveId> anchorIdTo;
   /**
    * ctor creates a new conflict
    *
    * @param goal IN STORED the dissatisfied goal that issued the conflict
-   * @param instancesToChooseFrom IN the list of instances to choose from to perform the association
+   * @param instancesToChooseFrom IN STORED the list of instances to choose from to perform the association
+   * @param anchorToStart IN STORED boolean indicating whether the anchor is associated to the START or the END
+   * The value is used to support a Solver implementation to calculate the absolute START time offset
    */
-  public MissingAssociationConflict(final Goal goal, final Collection<SchedulingActivityDirective> instancesToChooseFrom) {
+  public MissingAssociationConflict(
+      final Goal goal,
+      final Collection<SchedulingActivityDirective> instancesToChooseFrom,
+      final Optional<SchedulingActivityDirectiveId> anchorIdTo,
+      final Optional<Boolean> anchorToStart) {
+
+
     super(goal, new EvaluationEnvironment());
     this.instances = instancesToChooseFrom;
+    this.anchorIdTo = anchorIdTo;
+    this.anchorToStart = anchorToStart;
   }
 
   public Collection<SchedulingActivityDirective> getActivityInstancesToChooseFrom(){
     return instances;
+  }
+
+  public Optional<SchedulingActivityDirectiveId> getAnchorIdTo() {
+    return anchorIdTo;
+  }
+
+  public Optional<Boolean> getAnchorToStart() {
+    return anchorToStart;
   }
 
   @Override
