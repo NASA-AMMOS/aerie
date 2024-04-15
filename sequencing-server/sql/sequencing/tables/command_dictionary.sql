52bf4d4be1b222635b04e184d0ac6ec4983e8c2c
Cody Hansen <codyhansen92@gmail.com>
2024-04-15 09:44:13 -1000

    Changed the command dictionary table to support channel and parameter dictionaries

--- sequencing-server/sql/sequencing/tables/command_dictionary.sql
+++ sequencing-server/sql/sequencing/tables/command_dictionary.sql
@@ -1,10 +1,10 @@
 create table command_dictionary (
   id integer generated always as identity,
 
-  command_types_typescript_path text not null,
   mission text not null,
   version text not null,
   parsed_json jsonb not null default '{}',
+  type text not null,
 
   created_at timestamptz not null default now(),
 
@@ -18,13 +18,13 @@ comment on table command_dictionary is e''
   'A Command Dictionary for a mission.';
 comment on column command_dictionary.id is e''
   'The synthetic identifier for this command dictionary.';
-comment on column command_dictionary.command_types_typescript_path is e''
-  'The location of command dictionary types (.ts) on the filesystem';
 comment on column command_dictionary.mission is e''
   'A human-meaningful identifier for the mission described by the command dictionary';
 comment on column command_dictionary.version is e''
   'A human-meaningful version qualifier.';
 comment on column command_dictionary.parsed_json is e''
   'The XML that has been parsed and converted to JSON';
+comment on column command_dictionary.type is e''
+  'The type of dictionary uploaded, command, channel, or parameter.';
 comment on constraint command_dictionary_natural_key on command_dictionary is e''
   'There an only be one command dictionary of a given version for a given mission.';
