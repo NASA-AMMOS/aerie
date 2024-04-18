bf5aa11c46daba29509d141d9633e79605c31049
Cody Hansen <codyhansen92@gmail.com>
2024-04-18 06:40:15 -1000

    Changed user sequence command dict field to point to a parcel_id instead

---
+++ sequencing-server/sql/sequencing/tables/user_sequence.sql
@@ -0,0 +1,39 @@
+create table user_sequence (
+  created_at timestamptz not null default now(),
+  definition text not null,
+  id integer generated always as identity,
+  name text not null,
+  owner text,
+  parcel_id integer not null,
+  updated_at timestamptz not null default now(),
+
+  constraint user_sequence_primary_key primary key (id)
+);
+
+comment on column user_sequence.created_at is e''
+  'Time the user sequence was created.';
+comment on column user_sequence.definition is e''
+  'The user sequence definition string.';
+comment on column user_sequence.id is e''
+  'ID of the user sequence.';
+comment on column user_sequence.name is e''
+  'Human-readable name of the user sequence.';
+comment on column user_sequence.owner is e''
+  'The user responsible for this sequence.';
+comment on column user_sequence.parcel_id is e''
+  'Parcel the user sequence was created with.';
+comment on column user_sequence.updated_at is e''
+  'Time the user sequence was last updated.';
+
+create or replace function user_sequence_set_updated_at()
+returns trigger
+security definer
+language plpgsql as $$begin
+  new.updated_at = now();
+  return new;
+end$$;
+
+create trigger set_timestamp
+before update on user_sequence
+for each row
+execute function user_sequence_set_updated_at();
