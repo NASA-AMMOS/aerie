dbd71dec5e0dedb1935e5e60f792222af454b5f7
Matthew Dailis <matthew.l.dailis@jpl.nasa.gov>
2024-04-22 10:14:14 -0700

    Build sample procedure

---
+++ procedural/examples/foo-procedures/src/main/java/gov/nasa/jpl/aerie/procedural/examples/fooprocedures/procedures/SampleProcedure.java
@@ -0,0 +1,17 @@
+package gov.nasa.jpl.aerie.procedural.examples.fooprocedures.procedures;
+
+import gov.nasa.jpl.aerie.scheduling.EditablePlan;
+import gov.nasa.jpl.aerie.scheduling.Procedure;
+import gov.nasa.jpl.aerie.scheduling.plan.NewDirective;
+import gov.nasa.jpl.aerie.timeline.CollectOptions;
+import gov.nasa.jpl.aerie.timeline.Duration;
+import gov.nasa.jpl.aerie.timeline.payloads.activities.AnyDirective;
+
+import java.util.Map;
+
+public class SampleProcedure implements Procedure {
+  @Override
+  public void run(EditablePlan plan, CollectOptions options) {
+    plan.create(new NewDirective(new AnyDirective(Map.of()), "name", "type", Duration.hours(24)));
+  }
+}
