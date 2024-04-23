42c2b0eaf3d73fc0a3b0c9acb874bbc124a9aeeb
Matthew Dailis <matthew.l.dailis@jpl.nasa.gov>
2024-04-23 14:30:35 -0700

    Updates while talking to Joel

--- scheduler-driver/src/main/java/gov/nasa/jpl/aerie/scheduler/goals/Procedure.java
+++ scheduler-driver/src/main/java/gov/nasa/jpl/aerie/scheduler/goals/Procedure.java
@@ -15,7 +15,9 @@ import gov.nasa.jpl.aerie.timeline.Interval;
 
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 
 import static gov.nasa.jpl.aerie.merlin.protocol.types.Duration.MICROSECONDS;
 
@@ -24,7 +26,7 @@ public class Procedure extends Goal {
   private final String jarPath;
 
   public Procedure(final PlanningHorizon planningHorizon, String jarPath) {
-    this.simulateAfter = false;
+    this.simulateAfter = true;
     this.planHorizon = planningHorizon;
     this.jarPath = jarPath;
   }
@@ -40,14 +42,27 @@ public class Procedure extends Goal {
     List<SchedulingActivityDirective> newActivities = new ArrayList<>();
 
     final var editablePlan = EditablePlanImpl.init(missionModel, new Interval($(planHorizon.getStartAerie()), $(planHorizon.getEndAerie())));
+
+    /*
+     TODO
+
+     Comments from Joel:
+     - Part of the intent of editablePlan was to be able to re-use it across procedures.
+       - Could be done by initializing EditablePlanImpl with simulation results
+
+     Duration construction and arithmetic can be less awkward
+     */
+
     final var options = new CollectOptions(
         new Interval(Duration.ZERO,
                      Duration.microseconds(planHorizon.getEndAerie().in(gov.nasa.jpl.aerie.merlin.protocol.types.Duration.MICROSECONDS))));
 
     procedure.run(editablePlan, options);
 
+    var id = plan.getActivitiesById().keySet().stream().map(SchedulingActivityDirectiveId::id).max(Comparator.comparingLong($ -> $)).orElse(-1L);
     for (final var newDirective : editablePlan.committed()) {
-      newActivities.add(new SchedulingActivityDirective(new SchedulingActivityDirectiveId(0L), new ActivityType(newDirective.getType()), $(newDirective.getStartTime()), null, newDirective.getInner().arguments, null, null, true));
+      final var newId = new SchedulingActivityDirectiveId(++id);
+      newActivities.add(new SchedulingActivityDirective(newId, new ActivityType(newDirective.getType()), $(newDirective.getStartTime()), $(Duration.ZERO), newDirective.getInner().arguments, null, null, true));
     }
 
     final var evaluation = eval.forGoal(this);
