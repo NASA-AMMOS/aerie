c18190950b5c3dfa02c62957aa96256705fe850a
Matthew Dailis <matthew.l.dailis@jpl.nasa.gov>
2024-04-23 08:17:39 -0700

    Proof of concept creating an activity

--- scheduler-server/src/main/java/gov/nasa/jpl/aerie/scheduler/server/models/SchedulingDSL.java
+++ scheduler-server/src/main/java/gov/nasa/jpl/aerie/scheduler/server/models/SchedulingDSL.java
@@ -289,6 +289,9 @@ public class SchedulingDSL {
         GoalSpecifier goal,
         Expression<Windows> windows
     ) implements GoalSpecifier {}
+    record Procedure(
+        String jarPath
+    ) implements GoalSpecifier {}
   }
 
   public record LinearResource(String name) {}
