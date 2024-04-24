c69c6887900893f3a2e2c804a0f4ac368cfc73dd
JoelCourtney <joel.e.courtney@gmail.com>
2024-04-23 17:20:27 -0700

    Fixes after rebase

--- scheduler-driver/src/main/java/gov/nasa/jpl/aerie/scheduler/goals/Procedure.java
+++ scheduler-driver/src/main/java/gov/nasa/jpl/aerie/scheduler/goals/Procedure.java
@@ -7,19 +7,15 @@ import gov.nasa.jpl.aerie.scheduler.model.Plan;
 import gov.nasa.jpl.aerie.scheduler.model.PlanningHorizon;
 import gov.nasa.jpl.aerie.scheduler.model.SchedulingActivityDirective;
 import gov.nasa.jpl.aerie.scheduler.model.SchedulingActivityDirectiveId;
-import gov.nasa.jpl.aerie.scheduler.simulation.SimulationFacade;
 import gov.nasa.jpl.aerie.scheduler.solver.Evaluation;
 import gov.nasa.jpl.aerie.timeline.CollectOptions;
-import gov.nasa.jpl.aerie.timeline.Duration;
+import gov.nasa.jpl.aerie.merlin.protocol.types.Duration;
 import gov.nasa.jpl.aerie.timeline.Interval;
 
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Map;
-
-import static gov.nasa.jpl.aerie.merlin.protocol.types.Duration.MICROSECONDS;
 
 public class Procedure extends Goal {
 //  private final gov.nasa.jpl.aerie.scheduling.Procedure procedure;
@@ -41,7 +37,7 @@ public class Procedure extends Goal {
 
     List<SchedulingActivityDirective> newActivities = new ArrayList<>();
 
-    final var editablePlan = EditablePlanImpl.init(missionModel, new Interval($(planHorizon.getStartAerie()), $(planHorizon.getEndAerie())), null);
+    final var editablePlan = EditablePlanImpl.init(missionModel, new Interval(planHorizon.getStartAerie(), planHorizon.getEndAerie()), null);
 
     /*
      TODO
@@ -62,7 +58,7 @@ public class Procedure extends Goal {
     var id = plan.getActivitiesById().keySet().stream().map(SchedulingActivityDirectiveId::id).max(Comparator.comparingLong($ -> $)).orElse(-1L);
     for (final var newDirective : editablePlan.committed()) {
       final var newId = new SchedulingActivityDirectiveId(++id);
-      newActivities.add(new SchedulingActivityDirective(newId, new ActivityType(newDirective.getType()), $(newDirective.getStartTime()), $(Duration.ZERO), newDirective.getInner().arguments, null, null, true));
+      newActivities.add(new SchedulingActivityDirective(newId, new ActivityType(newDirective.getType()), newDirective.getStartTime(), Duration.ZERO, newDirective.getInner().arguments, null, null, true));
     }
 
     final var evaluation = eval.forGoal(this);
@@ -71,13 +67,5 @@ public class Procedure extends Goal {
       evaluation.associate(activity, true);
     }
     evaluation.setScore(0.0);
-  }
-
-  public static gov.nasa.jpl.aerie.timeline.Duration $(gov.nasa.jpl.aerie.merlin.protocol.types.Duration duration) {
-    return new gov.nasa.jpl.aerie.timeline.Duration(duration.in(MICROSECONDS));
-  }
-
-  public static gov.nasa.jpl.aerie.merlin.protocol.types.Duration $(gov.nasa.jpl.aerie.timeline.Duration duration) {
-    return gov.nasa.jpl.aerie.merlin.protocol.types.Duration.of(duration.div(new gov.nasa.jpl.aerie.timeline.Duration(1)), MICROSECONDS);
   }
 }
