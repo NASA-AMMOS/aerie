6989c7a43b1ecfec43b0fd249d8d6490b6f7dafa
Cody Hansen <codyhansen92@gmail.com>
2024-04-25 06:01:24 -1000

    Added command_types_typescript_path back into the command_dictionary table

--- sequencing-server/sql/sequencing/tables/command_dictionary.sql
+++ sequencing-server/sql/sequencing/tables/command_dictionary.sql
@@ -1,6 +1,7 @@
 create table command_dictionary (
   id integer generated always as identity,
 
+  command_types_typescript_path text not null,
   mission text not null,
   version text not null,
   parsed_json jsonb not null default '{}',
@@ -17,6 +18,8 @@ comment on table command_dictionary is e''
   'A Command Dictionary for a mission.';
 comment on column command_dictionary.id is e''
   'The synthetic identifier for this command dictionary.';
+comment on column command_dictionary.command_types_typescript_path is e''
+  'The location of command dictionary types (.ts) on the filesystem';
 comment on column command_dictionary.mission is e''
   'A human-meaningful identifier for the mission described by the command dictionary';
 comment on column command_dictionary.version is e''
