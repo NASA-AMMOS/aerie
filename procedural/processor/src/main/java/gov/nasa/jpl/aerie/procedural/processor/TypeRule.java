d390c4f6ffa4e5c2bd0a71dac34c2aa11aa61463
Matthew Dailis <matthew.l.dailis@jpl.nasa.gov>
2024-04-24 20:15:33 -0700

    Create annotation processor for procedural scheduling

---
+++ procedural/processor/src/main/java/gov/nasa/jpl/aerie/procedural/processor/TypeRule.java
@@ -0,0 +1,37 @@
+package gov.nasa.jpl.aerie.procedural.processor;
+
+import com.squareup.javapoet.ClassName;
+import gov.nasa.jpl.aerie.procedural.processor.TypePattern;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+public record TypeRule(
+    TypePattern head,
+    Set<String> enumBoundedTypeParameters,
+    List<TypePattern> parameters,
+    ClassName factory,
+    String method,
+    Optional<String> name
+) {
+  public TypeRule(
+      final TypePattern head,
+      final Set<String> enumBoundedTypeParameters,
+      final List<TypePattern> parameters,
+      final ClassName factory,
+      final String method)
+  {
+    this(head, enumBoundedTypeParameters, parameters, factory, method, Optional.empty());
+  }
+  public TypeRule(
+      final TypePattern head,
+      final Set<String> enumBoundedTypeParameters,
+      final List<TypePattern> parameters,
+      final ClassName factory,
+      final String method,
+      final String name)
+  {
+    this(head, enumBoundedTypeParameters, parameters, factory, method, Optional.of(name));
+  }
+}
