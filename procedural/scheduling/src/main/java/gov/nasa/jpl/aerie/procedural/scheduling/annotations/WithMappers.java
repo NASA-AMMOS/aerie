797a7c8add074530c7dafd0d678ef94bad4835c7
JoelCourtney <joel.e.courtney@gmail.com>
2024-04-25 20:27:56 -0700

    Move scheduling processor objects to procedural package

--- procedural/scheduling/src/main/java/gov/nasa/jpl/aerie/procedural/scheduling/annotations/WithMappers.java
+++ procedural/scheduling/src/main/java/gov/nasa/jpl/aerie/procedural/scheduling/annotations/WithMappers.java
@@ -1,13 +1,14 @@
-package gov.nasa.jpl.aerie.scheduling.annotations;
+package gov.nasa.jpl.aerie.procedural.scheduling.annotations;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Repeatable;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.CLASS)
 @Target(ElementType.PACKAGE)
-@interface AllMappers {
-  WithMappers[] value();
+@Repeatable(AllMappers.class)
+public @interface WithMappers {
+  Class<?> value();
 }
-
