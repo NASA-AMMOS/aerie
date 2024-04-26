cd782892e000609278d48e35fc8b8f9c1ecb054d
JoelCourtney <joel.e.courtney@gmail.com>
2024-04-25 17:43:59 -0700

    Fix procedural simulation impl

--- scheduler-driver/src/main/java/gov/nasa/jpl/aerie/scheduler/solver/PrioritySolver.java
+++ scheduler-driver/src/main/java/gov/nasa/jpl/aerie/scheduler/solver/PrioritySolver.java
@@ -326,14 +326,7 @@ public class PrioritySolver implements Solver {
       satisfyOptionGoal((OptionGoal) goal);
     } else if (goal instanceof Procedure procedure) {
       if (!analysisOnly) {
-        final var originalActivities = plan.getActivities();
         procedure.run(evaluation, plan, problem.getMissionModel(), this.problem::getActivityType);
-        final var newActivities = plan.getActivities();
-          try {
-              simulationFacade.removeAndInsertActivitiesFromSimulation(originalActivities, newActivities);
-          } catch (SimulationFacade.SimulationException e) {
-              throw new RuntimeException(e);
-          }
       }
     } else {
       satisfyGoalGeneral(goal);
--- scheduler-driver/src/main/kotlin/gov/nasa/jpl/aerie/scheduler/goals/InMemoryPlan.kt
+++ scheduler-driver/src/main/kotlin/gov/nasa/jpl/aerie/scheduler/goals/InMemoryPlan.kt
@@ -2,14 +2,18 @@ package gov.nasa.jpl.aerie.scheduler.goals
 
 import gov.nasa.jpl.aerie.merlin.protocol.types.Duration
 import gov.nasa.jpl.aerie.merlin.protocol.types.SerializedValue
+import gov.nasa.jpl.aerie.scheduler.model.ActivityType
 import gov.nasa.jpl.aerie.scheduler.model.PlanningHorizon
 import gov.nasa.jpl.aerie.timeline.Interval
 import gov.nasa.jpl.aerie.timeline.collections.Directives
 import gov.nasa.jpl.aerie.timeline.durationUtils.minus
 import gov.nasa.jpl.aerie.timeline.durationUtils.plus
+import gov.nasa.jpl.aerie.timeline.payloads.activities.Directive
+import gov.nasa.jpl.aerie.timeline.payloads.activities.DirectiveStart
+import gov.nasa.jpl.aerie.timeline.payloads.activities.DirectiveStart.Anchor.AnchorPoint.Companion.anchorToStart
 import java.time.Instant
+import gov.nasa.jpl.aerie.scheduler.model.Plan as SchedulerPlan
 import gov.nasa.jpl.aerie.timeline.plan.Plan as TimelinePlan
-import gov.nasa.jpl.aerie.scheduler.model.Plan  as SchedulerPlan
 
 data class InMemoryPlan(
     private val schedulerPlan: SchedulerPlan,
@@ -22,7 +26,23 @@ data class InMemoryPlan(
   override fun toAbsolute(rel: Duration) = planningHorizon.startInstant + rel
 
   override fun <A : Any> directives(type: String?, deserializer: (SerializedValue) -> A): Directives<A> {
-    TODO("Not yet implemented")
+    val schedulerActivities = (if (type == null) schedulerPlan.activities else schedulerPlan.activitiesByType[ActivityType(type)])
+        ?: throw Exception("could not find activities by type $type")
+
+    val result = ArrayList<Directive<A>>(schedulerActivities.size)
+    for (activity in schedulerActivities) {
+      result.add(
+          Directive(
+              deserializer(SerializedValue.of(activity.arguments)),
+              "Name unavailable",
+              activity.id.id,
+              activity.type.name,
+              if (activity.anchorId == null) DirectiveStart.Absolute(activity.startOffset)
+              else DirectiveStart.Anchor(activity.anchorId.id, activity.startOffset, anchorToStart(activity.anchoredToStart))
+          )
+      )
+    }
+    return Directives(result)
   }
 
 }
