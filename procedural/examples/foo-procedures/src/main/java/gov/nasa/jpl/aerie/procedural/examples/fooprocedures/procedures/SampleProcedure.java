35e301a1fd43c7fbfa38d76d1c16394492a2a91e
JoelCourtney <joel.e.courtney@gmail.com>
2024-04-25 17:49:34 -0700

    Keep up to date with interface changes

--- procedural/examples/foo-procedures/src/main/java/gov/nasa/jpl/aerie/procedural/examples/fooprocedures/procedures/SampleProcedure.java
+++ procedural/examples/foo-procedures/src/main/java/gov/nasa/jpl/aerie/procedural/examples/fooprocedures/procedures/SampleProcedure.java
@@ -1,23 +1,33 @@
 package gov.nasa.jpl.aerie.procedural.examples.fooprocedures.procedures;
 
-import gov.nasa.jpl.aerie.scheduling.EditablePlan;
-import gov.nasa.jpl.aerie.scheduling.Procedure;
-import gov.nasa.jpl.aerie.scheduling.plan.NewDirective;
-import gov.nasa.jpl.aerie.scheduling.simulation.SimulateOptions;
+import gov.nasa.jpl.aerie.procedural.scheduling.plan.EditablePlan;
+import gov.nasa.jpl.aerie.procedural.scheduling.Procedure;
+import gov.nasa.jpl.aerie.procedural.scheduling.plan.NewDirective;
+import gov.nasa.jpl.aerie.procedural.scheduling.simulation.SimulateOptions;
 import gov.nasa.jpl.aerie.timeline.CollectOptions;
 import gov.nasa.jpl.aerie.merlin.protocol.types.Duration;
 import gov.nasa.jpl.aerie.timeline.payloads.activities.AnyDirective;
+import gov.nasa.jpl.aerie.timeline.payloads.activities.DirectiveStart;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Map;
 
 public class SampleProcedure implements Procedure {
   @Override
-  public void run(EditablePlan plan, CollectOptions options) {
-    plan.create(new NewDirective(
-        new AnyDirective(Map.of()),
-        "name",
-        "BiteBanana",
-        Duration.hours(24)));
+  public void run(EditablePlan plan, @NotNull CollectOptions options) {
+    final var firstTime = Duration.hours(24);
+    final var step = Duration.hours(6);
+
+    var currentTime = firstTime;
+    for (var i = 0; i < quantity; i++) {
+      plan.create(new NewDirective(
+          new AnyDirective(Map.of()),
+          "name",
+          "BiteBanana",
+          new DirectiveStart.Absolute(currentTime)
+          ));
+      currentTime = currentTime.plus(step);
+    }
     plan.commit();
     var results = plan.simulate(new SimulateOptions());
     var size = results.instances().collect().size();
