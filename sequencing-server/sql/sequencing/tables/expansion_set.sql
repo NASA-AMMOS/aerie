e91263a38c47806ab533ad81ce87a4134f8f7d4b
Ryan Goetz <Ryan.R.Goetz@jpl.nasa.gov>
2024-04-30 11:29:21 -1000

    sequence_adaptation should be optional
    * A sequence_adaptaion file should be optional for expansion sets. You don't need one in the current workflow

--- sequencing-server/sql/sequencing/tables/expansion_set.sql
+++ sequencing-server/sql/sequencing/tables/expansion_set.sql
@@ -5,7 +5,7 @@ create table expansion_set (
 
   command_dict_id integer not null,
   mission_model_id integer not null,
-  sequence_adaptation_id integer not null,
+  sequence_adaptation_id integer,
 
   created_at timestamptz not null default now(),
   updated_at timestamptz not null default now(),
