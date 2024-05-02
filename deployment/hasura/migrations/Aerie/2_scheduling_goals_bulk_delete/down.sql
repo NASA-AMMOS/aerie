99dd00fbbcd11b1f06f14ed6a4250bcafd0b7c20
Theresa Kamerman <theresa.kamerman@jpl.nasa.gov>
2024-05-02 13:06:29 -0700

    Update delete function to avoid gaps
    By incrementing through the deleted goals in reverse priority order, we guarantee that all the remaining goals that had a higher priority than the deleted goal before the delete will still be at a higher priority when we process the deleted row.
    
    - Add trigger depth restriction on `increment_revision_on_goal_update`

--- deployment/hasura/migrations/Aerie/2_scheduling_goals_bulk_delete/down.sql
+++ deployment/hasura/migrations/Aerie/2_scheduling_goals_bulk_delete/down.sql
@@ -1,1 +1,43 @@
+-- Restore Scheduling Spec (Model) Delete function to work per-row
+create or replace trigger delete_scheduling_model_specification_goal
+  after delete on scheduler.scheduling_model_specification_goals
+  for each row
+execute function scheduler.delete_scheduling_model_specification_goal_func();
+
+create or replace function scheduler.delete_scheduling_model_specification_goal_func()
+  returns trigger
+  language plpgsql as $$
+begin
+  update scheduler.scheduling_model_specification_goals
+  set priority = priority - 1
+  where model_id = old.model_id
+    and priority > old.priority;
+  return null;
+end;
+$$;
+
+-- Restore Scheduling Spec (Plan) Delete function to work per-row
+create or replace trigger delete_scheduling_specification_goal
+  after delete on scheduler.scheduling_specification_goals
+  for each row
+execute function scheduler.delete_scheduling_specification_goal_func();
+
+create or replace function scheduler.delete_scheduling_specification_goal_func()
+  returns trigger
+  language plpgsql as $$
+begin
+  update scheduler.scheduling_specification_goals
+  set priority = priority - 1
+  where specification_id = old.specification_id
+    and priority > old.priority;
+  return null;
+end;
+$$;
+
+-- Remove the "depth" condition from this trigger
+create or replace trigger increment_revision_on_goal_update
+  before insert or update on scheduler.scheduling_specification_goals
+  for each row
+execute function scheduler.increment_spec_revision_on_goal_spec_update();
+
 call migrations.mark_migration_rolled_back('2');
--- deployment/postgres-init-db/sql/tables/scheduler/scheduling_specification/scheduling_specification_goals.sql
+++ deployment/postgres-init-db/sql/tables/scheduler/scheduling_specification/scheduling_specification_goals.sql
@@ -134,11 +134,16 @@ execute function scheduler.update_scheduling_specification_goal_func();
 create function scheduler.delete_scheduling_specification_goal_func()
   returns trigger
   language plpgsql as $$
+  declare
+    r scheduler.scheduling_specification_goals;
 begin
-  update scheduler.scheduling_specification_goals
-  set priority = priority - 1
-  where specification_id = old.specification_id
-    and priority > old.priority;
+  -- Perform updates in reverse-priority order to ensure that there are no gaps
+  for r in select * from removed_rows order by priority desc loop
+    update scheduler.scheduling_specification_goals
+    set priority = priority - 1
+    where specification_id = r.specification_id
+      and priority > r.priority;
+  end loop;
   return null;
 end;
 $$;
@@ -148,7 +153,8 @@ comment on function scheduler.delete_scheduling_specification_goal_func() is e''
 
 create trigger delete_scheduling_specification_goal
   after delete on scheduler.scheduling_specification_goals
-  for each row
+  referencing old table as removed_rows
+  for each statement
 execute function scheduler.delete_scheduling_specification_goal_func();
 
 create function scheduler.increment_spec_revision_on_goal_spec_update()
@@ -164,6 +170,7 @@ end$$;
 create trigger increment_revision_on_goal_update
   before insert or update on scheduler.scheduling_specification_goals
   for each row
+  when (pg_trigger_depth() < 1)
   execute function scheduler.increment_spec_revision_on_goal_spec_update();
 
 create function scheduler.increment_spec_revision_on_goal_spec_delete()
