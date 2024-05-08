fd5eb60ad2af84f9c9a791da417628689a0652c9
Ryan Goetz <Ryan.R.Goetz@jpl.nasa.gov>
2024-05-08 05:43:44 -1000

    Allow for multiplie dictionary upload
    * backend will parse the correct dictionary

--- sequencing-server/src/types/types.ts
+++ sequencing-server/src/types/types.ts
@@ -2,3 +2,9 @@ export enum FallibleStatus {
   SUCCESS = 'SUCCESS',
   FAILURE = 'FAILURE',
 }
+
+export enum DictionaryType {
+  COMMAND = 'COMMAND',
+  CHANNEL = 'CHANNEL',
+  PARAMETER = 'PARAMETER',
+}
