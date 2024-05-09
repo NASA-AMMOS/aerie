284bd24f5d0a2ff574f0e98675b268abd2b68b1a
JoelCourtney <joel.e.courtney@gmail.com>
2024-05-09 15:11:19 -0700

    Documentation updates

--- procedural/scheduling/src/main/kotlin/gov/nasa/jpl/aerie/procedural/scheduling/plan/Edit.kt
+++ procedural/scheduling/src/main/kotlin/gov/nasa/jpl/aerie/procedural/scheduling/plan/Edit.kt
@@ -3,6 +3,12 @@ package gov.nasa.jpl.aerie.procedural.scheduling.plan
 import gov.nasa.jpl.aerie.timeline.payloads.activities.AnyDirective
 import gov.nasa.jpl.aerie.timeline.payloads.activities.Directive
 
+/**
+ * Edits that can be made to the plan.
+ *
+ * Currently only creating new activities is supported.
+ */
 sealed interface Edit {
-  data class Create(val directive: Directive<AnyDirective>): Edit
+  /** Create a new activity from a given directive. */
+  data class Create(/***/ val directive: Directive<AnyDirective>): Edit
 }
--- procedural/scheduling/src/main/kotlin/gov/nasa/jpl/aerie/procedural/scheduling/simulation/PauseBehavior.kt
+++ procedural/scheduling/src/main/kotlin/gov/nasa/jpl/aerie/procedural/scheduling/simulation/PauseBehavior.kt
@@ -2,14 +2,25 @@ package gov.nasa.jpl.aerie.procedural.scheduling.simulation
 
 import gov.nasa.jpl.aerie.merlin.protocol.types.Duration
 
+/** Behavior for when the simulation should pause. */
 sealed interface PauseBehavior {
-  data class AfterDuration(val dur: Duration): PauseBehavior
-  data class AfterActivity(val directive: Long): PauseBehavior
+  /** Pause after a given amount of time has elapsed. */
+  data class AfterDuration(/***/ val dur: Duration): PauseBehavior
+
+  /** Pause after a specific activity directive has finished. */
+  data class AfterActivity(/** Id of the directive to wait for. */ val directive: Long): PauseBehavior
+
+  /** Pause after all edits that were not included in the previous sim are simulated. */
   data object AfterNewEdits: PauseBehavior
+
+  /** Do not pause; continue to the end of the plan. */
   data object AtEnd: PauseBehavior
 
-  data class EarliestOf(val pausePoints: List<PauseBehavior>): PauseBehavior
-  data class LatestOf(val pausePoints: List<PauseBehavior>): PauseBehavior
+  /** Pause at the earliest of a list of possible pause points. */
+  data class EarliestOf(/***/ val pausePoints: List<PauseBehavior>): PauseBehavior
+
+  /** Pause at the latest of a list of possible pause points. */
+  data class LatestOf(/***/ val pausePoints: List<PauseBehavior>): PauseBehavior
 
   // very hard!
   // data class OnCondition(val condition: ???): PauseBehavior
