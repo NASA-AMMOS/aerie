38a940fee4c753a7aa23c59f2ae09bf1de0ff694
Ryan Goetz <Ryan.R.Goetz@jpl.nasa.gov>
2024-05-13 07:54:49 -1000

    Added Parcels to db-e2e sequencing testing

--- db-tests/src/test/java/gov/nasa/jpl/aerie/database/CommandExpansionDatabaseTests.java
+++ db-tests/src/test/java/gov/nasa/jpl/aerie/database/CommandExpansionDatabaseTests.java
@@ -19,10 +19,35 @@ class CommandExpansionDatabaseTests {
   private DatabaseTestHelper helper;
   private Connection connection;
 
+  private int commandDictionaryId;
+  private int parcelId;
+
   @BeforeAll
   void beforeAll() throws SQLException, IOException, InterruptedException {
     helper = new DatabaseTestHelper("command_expansion_test", "Command Expansion Database Tests");
     connection = helper.connection();
+
+    try (final var statement = connection.createStatement()) {
+      final var insertDictionary = statement.executeQuery(
+          //language=sql
+          """
+        insert into sequencing.command_dictionary (path, mission,version)
+        values ('test-path','test-mission','1')
+        returning id, created_at, updated_at
+        """);
+      insertDictionary.next();
+      commandDictionaryId = insertDictionary.getInt("id");
+
+      final var insertParcel = statement.executeQuery(
+          //language=sql
+          """
+          insert into sequencing.parcel (name, command_dictionary_id)
+          values ('test-parcel-name','%s')
+          returning id, created_at, updated_at
+          """.formatted(Integer.toString(commandDictionaryId)));
+      insertParcel.next();
+      parcelId = insertParcel.getInt("id");
+      }
   }
 
   @AfterAll
@@ -40,13 +65,14 @@ class CommandExpansionDatabaseTests {
     @Test
     void shouldModifyUpdatedAtTimeOnUpdate() throws SQLException {
       try (final var statement = connection.createStatement()) {
+
         final var insertRes = statement.executeQuery(
             //language=sql
             """
-            insert into sequencing.expansion_rule (activity_type, expansion_logic)
-            values ('test-activity-type', 'test-activity-logic')
+            insert into sequencing.expansion_rule (activity_type, expansion_logic,parcel_id)
+            values ('test-activity-type', 'test-activity-logic','%s')
             returning id, created_at, updated_at
-            """);
+            """.formatted(Integer.toString(parcelId)));
         insertRes.next();
         final var id = insertRes.getInt("id");
         final var created_at = insertRes.getTimestamp("created_at");
