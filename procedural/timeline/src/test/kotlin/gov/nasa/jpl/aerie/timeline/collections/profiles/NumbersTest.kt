8deb7144976276c12283e5ed2e74dd7f23c2579a
JoelCourtney <joel.e.courtney@gmail.com>
2024-05-23 14:11:09 -0700

    Implement increases and decreases for numeric profiles

--- procedural/timeline/src/test/kotlin/gov/nasa/jpl/aerie/timeline/collections/profiles/NumbersTest.kt
+++ procedural/timeline/src/test/kotlin/gov/nasa/jpl/aerie/timeline/collections/profiles/NumbersTest.kt
@@ -3,9 +3,12 @@ package gov.nasa.jpl.aerie.timeline.collections.profiles
 import gov.nasa.jpl.aerie.merlin.protocol.types.Duration
 import gov.nasa.jpl.aerie.merlin.protocol.types.Duration.seconds
 import gov.nasa.jpl.aerie.timeline.Interval
+import gov.nasa.jpl.aerie.timeline.Interval.Companion.at
 import gov.nasa.jpl.aerie.timeline.Interval.Companion.between
 import gov.nasa.jpl.aerie.timeline.payloads.Segment
 import org.junit.jupiter.api.Test
+import gov.nasa.jpl.aerie.timeline.util.duration.rangeUntil
+import gov.nasa.jpl.aerie.timeline.util.duration.rangeTo
 
 import org.junit.jupiter.api.Assertions.*
 
@@ -21,6 +24,24 @@ class NumbersTest {
             Segment(between(seconds(1), seconds(2), Interval.Inclusivity.Exclusive, Interval.Inclusivity.Inclusive), 4)
         ),
         (five + 4).collect(between(Duration.ZERO, seconds(2)))
+    )
+  }
+
+  @Test
+  fun increases() {
+    val result = Numbers(
+      Segment(seconds(0) ..< seconds(1), 0),
+      Segment(seconds(1) ..< seconds(2), 2),
+      Segment(seconds(2) .. seconds(3), 1)
+    ).increases()
+
+    assertIterableEquals(
+      listOf(
+        Segment(between(seconds(0), seconds(1), Interval.Inclusivity.Exclusive), false),
+        Segment(at(seconds(1)), true),
+        Segment(between(seconds(1), seconds(3), Interval.Inclusivity.Exclusive), false)
+      ),
+      result.collect()
     )
   }
 }
