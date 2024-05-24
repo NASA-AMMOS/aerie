3c636d2b2af4b0750d053d9ee9180cf0197af3b4
JoelCourtney <joel.e.courtney@gmail.com>
2024-05-24 11:47:32 -0700

    Perform binary search on list truncation

--- procedural/timeline/src/test/kotlin/gov/nasa/jpl/aerie/timeline/ops/SerialConstantTest.kt
+++ procedural/timeline/src/test/kotlin/gov/nasa/jpl/aerie/timeline/ops/SerialConstantTest.kt
@@ -9,6 +9,7 @@ import gov.nasa.jpl.aerie.timeline.collections.profiles.Constants
 import gov.nasa.jpl.aerie.timeline.payloads.Segment
 import gov.nasa.jpl.aerie.timeline.collections.profiles.Numbers
 import gov.nasa.jpl.aerie.timeline.util.duration.rangeTo
+import gov.nasa.jpl.aerie.timeline.util.duration.rangeUntil
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertIterableEquals
 import org.junit.jupiter.api.Test
@@ -44,6 +45,10 @@ class SerialConstantTest {
       Segment(seconds(0) .. seconds(2), "a"),
       Segment(seconds(2) .. seconds(5), "b"),
       Segment(seconds(20) .. seconds(25), "c"),
+
+      Segment(seconds(100) ..< seconds(101), "x"),
+      Segment(at(seconds(101)), "y"),
+      Segment(between(seconds(101), seconds(102), Exclusive), "x")
     )
 
     assertEquals("a", profile.sample(seconds(1)))
