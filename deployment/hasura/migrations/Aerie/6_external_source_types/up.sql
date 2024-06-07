4ece8b1b704df384d58e296dac9d7c6f8c9af78e
Joseph Volosin <joseph.p.volosin@nasa.gov>
2024-06-07 14:28:48 -0400

    Creating migration for external_source_type table

---
+++ deployment/hasura/migrations/Aerie/6_external_source_types/up.sql
@@ -0,0 +1,43 @@
+-- Create table for external source types
+CREATE TABLE merlin.external_source_type (
+    id integer NOT NULL,
+    name text NOT NULL,
+    description text NOT NULL,
+    version integer NOT NULL,
+    origin_info_schema jsonb
+);
+
+COMMENT ON TABLE merlin.external_source_type IS 'A table for externally imported event source types.';
+
+-- Ensure the id is serial.
+CREATE SEQUENCE merlin.external_source_type_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+ALTER SEQUENCE merlin.external_source_type_id_seq OWNED BY merlin.external_source_type.id;
+ALTER TABLE ONLY merlin.external_source_type ALTER COLUMN id SET DEFAULT nextval('merlin.external_source_type_id_seq'::regclass);
+
+-- Set primary key
+ALTER TABLE ONLY merlin.external_source_type
+    ADD CONSTRAINT external_source_type_pkey PRIMARY KEY (id);
+
+-- Add sequence for version... TODO, I think this has to happen outside of SQL..
+
+-- Add uniqueness constraint for name & version
+ALTER TABLE ONLY merlin.external_source_type
+    ADD CONSTRAINT logical_identifiers UNIQUE (name, version);
+
+COMMENT ON CONSTRAINT logical_identifiers ON merlin.external_source_type IS 'The tuple (name, version) must be unique!';
+ 
+-- Create source_type_id field on external_source
+ALTER TABLE merlin.external_source ADD source_type_id integer;
+
+-- Update source_type on external_source to link it to external_source_type
+ALTER TABLE ONLY merlin.external_source
+    ADD CONSTRAINT "(source_type, source_type_id) -> (external_source_type)" FOREIGN KEY (source_type, source_type_id) REFERENCES merlin.external_source_type((name, id));
+
+call migrations.mark_migration_applied('6');
