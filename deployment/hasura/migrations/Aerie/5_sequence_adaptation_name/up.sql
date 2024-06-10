d233e21c179534be4da4cf4ffa165d7494ddedb0
Cody Hansen <codyhansen92@gmail.com>
2024-06-10 09:29:34 -1000

    Fixed an issue with the sequence adaptation name up.sql

--- deployment/hasura/migrations/Aerie/5_sequence_adaptation_name/up.sql
+++ deployment/hasura/migrations/Aerie/5_sequence_adaptation_name/up.sql
@@ -3,7 +3,7 @@ alter table sequencing.sequence_adaptation
   add constraint sequence_adaptation_natural_key
     unique (name);
 
-comment on column sequencing.seq.name is e''
+comment on column sequencing.sequence_adaptation.name is e''
   'The name of the sequence adaptation.';
 
 call migrations.mark_migration_applied('5');
