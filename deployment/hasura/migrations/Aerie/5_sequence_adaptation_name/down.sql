4777eb65c53c714bc1704e682f6747ee555b3f57
Cody Hansen <codyhansen92@gmail.com>
2024-06-10 10:52:54 -1000

    Drop the constraint before the column

--- deployment/hasura/migrations/Aerie/5_sequence_adaptation_name/down.sql
+++ deployment/hasura/migrations/Aerie/5_sequence_adaptation_name/down.sql
@@ -1,5 +1,5 @@
 alter table sequencing.sequence_adaptation
-  drop column name,
-  drop constraint sequence_adaptation_natural_key;
+  drop constraint sequence_adaptation_natural_key,
+  drop column name;
 
 call migrations.mark_migration_rolled_back('5');
