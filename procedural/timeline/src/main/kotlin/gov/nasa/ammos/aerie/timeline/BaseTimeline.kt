a4d01d77cc212b52872364ae9a02e5bfd1afd6eb
JoelCourtney <joel.e.courtney@gmail.com>
2024-06-11 13:01:48 -0700

    Remove accidental full names

--- procedural/timeline/src/main/kotlin/gov/nasa/ammos/aerie/timeline/BaseTimeline.kt
+++ procedural/timeline/src/main/kotlin/gov/nasa/ammos/aerie/timeline/BaseTimeline.kt
@@ -8,24 +8,24 @@ import gov.nasa.ammos.aerie.timeline.util.listCollector
  *
  * Only the most extreme power-users should ever need to construct this manually.
  */
-data class BaseTimeline<V: IntervalLike<V>, TL: gov.nasa.ammos.aerie.timeline.Timeline<V, TL>>(
-    override val ctor: (gov.nasa.ammos.aerie.timeline.Timeline<V, TL>) -> TL,
-    private val collector: (gov.nasa.ammos.aerie.timeline.CollectOptions) -> List<V>
-): gov.nasa.ammos.aerie.timeline.Timeline<V, TL> {
+data class BaseTimeline<V: IntervalLike<V>, TL: Timeline<V, TL>>(
+    override val ctor: (Timeline<V, TL>) -> TL,
+    private val collector: (CollectOptions) -> List<V>
+): Timeline<V, TL> {
   private var cached: List<V>? = null
-  private var cachedOptions: gov.nasa.ammos.aerie.timeline.CollectOptions? = null
+  private var cachedOptions: CollectOptions? = null
 
-  override fun cache(opts: gov.nasa.ammos.aerie.timeline.CollectOptions) {
+  override fun cache(opts: CollectOptions) {
     if (cachedOptions == null || !cachedOptions!!.contains(opts)) {
       cached = collect(opts)
       cachedOptions = opts
     }
   }
 
-  override fun collect(opts: gov.nasa.ammos.aerie.timeline.CollectOptions) =
+  override fun collect(opts: CollectOptions) =
     if (cached == null || !cachedOptions!!.contains(opts)) collector(opts)
-    else ctor(gov.nasa.ammos.aerie.timeline.BaseTimeline(ctor, listCollector(cached!!))).collect(opts)
+    else ctor(BaseTimeline(ctor, listCollector(cached!!))).collect(opts)
 
-  override fun <RESULT : gov.nasa.ammos.aerie.timeline.Timeline<V, RESULT>> unsafeCast(ctor: (gov.nasa.ammos.aerie.timeline.Timeline<V, RESULT>) -> RESULT) =
-      gov.nasa.ammos.aerie.timeline.BaseTimeline(ctor, collector).specialize()
+  override fun <RESULT : Timeline<V, RESULT>> unsafeCast(ctor: (Timeline<V, RESULT>) -> RESULT) =
+      BaseTimeline(ctor, collector).specialize()
 }
