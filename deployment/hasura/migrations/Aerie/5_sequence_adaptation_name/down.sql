295b304b0e193e4deff357ea94dd5bfed3d4130e
Cody Hansen <codyhansen92@gmail.com>
2024-06-11 08:41:27 -1000

    Added a default value to sequence adaptation names so migrations dont break

--- deployment/hasura/migrations/Aerie/5_sequence_adaptation_name/down.sql
+++ deployment/hasura/migrations/Aerie/5_sequence_adaptation_name/down.sql
@@ -1,5 +1,5 @@
 alter table sequencing.sequence_adaptation
-  drop constraint sequence_adaptation_natural_key,
+  drop constraint sequence_adaptation_unique_key,
   drop column name;
 
 call migrations.mark_migration_rolled_back('5');
