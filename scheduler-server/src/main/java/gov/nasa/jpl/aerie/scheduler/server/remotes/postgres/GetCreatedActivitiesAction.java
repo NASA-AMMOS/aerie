5cf5b37489e9a1b3d5a57f41645564b17fef6304
(skovati) Luke <skovati@protonmail.com>
2024-07-02 12:42:54 -0700

    integrate `goal_invocation_id` into scheduler-server

--- scheduler-server/src/main/java/gov/nasa/jpl/aerie/scheduler/server/remotes/postgres/GetCreatedActivitiesAction.java
+++ scheduler-server/src/main/java/gov/nasa/jpl/aerie/scheduler/server/remotes/postgres/GetCreatedActivitiesAction.java
@@ -11,12 +11,14 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 /*package-local*/ final class GetCreatedActivitiesAction implements AutoCloseable {
   private static final @Language("SQL") String sql = """
     select
       c.goal_id,
       c.goal_revision,
+      c.goal_invocation_id,
       c.activity_id
     from scheduler.scheduling_goal_analysis_created_activities as c
     where c.analysis_id = ?
@@ -34,7 +36,11 @@ import java.util.Map;
 
     final var createdActivities = new HashMap<GoalId, List<ActivityDirectiveId>>();
     while (resultSet.next()) {
-      final var goalId = new GoalId(resultSet.getLong("goal_id"), resultSet.getLong("goal_revision"));
+      final var goalId = new GoalId(
+          resultSet.getLong("goal_id"),
+          resultSet.getLong("goal_revision"),
+          Optional.of(resultSet.getLong("goal_invocation_id"))
+      );
       final var activityId = new ActivityDirectiveId(resultSet.getLong("activity_id"));
 
       if (!createdActivities.containsKey(goalId)) createdActivities.put(goalId, new ArrayList<>());
