d02648b870cc46f6058065825c336c86051f1bbc
psubram3 <pranav.subramanian@nasa.gov>
2024-07-02 12:32:16 -0700

    separate out derivation group

--- deployment/hasura/migrations/Aerie/5_external_events/down.sql
+++ deployment/hasura/migrations/Aerie/5_external_events/down.sql
@@ -5,6 +5,7 @@ DROP TABLE merlin.plan_external_source CASCADE;
 DROP TABLE merlin.external_source_type CASCADE;
 DROP TABLE merlin.external_event_type CASCADE;
 DROP TABLE merlin.external_source_event_types CASCADE;
+DROP TABLE merlin.derivation_group CASCADE;
 DROP VIEW merlin.derived_events;
 
 call migrations.mark_migration_rolled_back('5');
