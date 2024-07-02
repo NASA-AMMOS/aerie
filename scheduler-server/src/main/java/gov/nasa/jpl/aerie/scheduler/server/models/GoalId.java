3029e1de075782248c956b0292a52ca015346a53
(skovati) Luke <skovati@protonmail.com>
2024-07-02 12:42:54 -0700

    integrate `goal_invocation_id` into scheduler-server

--- scheduler-server/src/main/java/gov/nasa/jpl/aerie/scheduler/server/models/GoalId.java
+++ scheduler-server/src/main/java/gov/nasa/jpl/aerie/scheduler/server/models/GoalId.java
@@ -1,3 +1,12 @@
 package gov.nasa.jpl.aerie.scheduler.server.models;
 
-public record GoalId(long id, long revision) { }
+import java.util.Optional;
+
+public record GoalId(long id, long revision, Optional<Long> goalInvocationId) {
+  public GoalId(long id, long revision) {
+    this(id, revision, Optional.empty());
+  }
+  public GoalId(long id, long revision, long goalInvocationId) {
+    this(id, revision, Optional.of(goalInvocationId));
+  }
+}
