3575a74975ffe2a5588829a14017585f765bd7f8
psubram3 <pranav.subramanian@nasa.gov>
2024-07-02 11:23:12 -0700

    add derived events view

--- deployment/hasura/migrations/Aerie/5_external_events/down.sql
+++ deployment/hasura/migrations/Aerie/5_external_events/down.sql
@@ -5,5 +5,6 @@ DROP TABLE merlin.plan_external_source CASCADE;
 DROP TABLE merlin.external_source_type CASCADE;
 DROP TABLE merlin.external_event_type CASCADE;
 DROP TABLE merlin.external_source_event_types CASCADE;
+DROP VIEW merlin.derived_events;
 
 call migrations.mark_migration_rolled_back('5');
