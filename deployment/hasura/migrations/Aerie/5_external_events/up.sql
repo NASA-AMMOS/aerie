dc3ddaf5218fe4d989f642957b782562997e640f
psubram3 <pranav.subramanian@nasa.gov>
2024-07-03 10:35:16 -0700

    add visibility of source type to derivation group

--- deployment/hasura/migrations/Aerie/5_external_events/up.sql
+++ deployment/hasura/migrations/Aerie/5_external_events/up.sql
@@ -1,27 +1,3 @@
--- Create a table to represent derivation groups for external sources
-CREATE TABLE merlin.derivation_group (
-    id integer NOT NULL,
-    name text NOT NULL
-);
-
-COMMENT ON TABLE merlin.derivation_group IS 'A table to represent the names of groups of sources to run derivation operations over.';
-
--- Ensure the id is serial.
-CREATE SEQUENCE merlin.derivation_group_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-ALTER SEQUENCE merlin.derivation_group_id_seq OWNED BY merlin.derivation_group.id;
-ALTER TABLE ONLY merlin.derivation_group ALTER COLUMN id SET DEFAULT nextval('merlin.derivation_group_id_seq'::regclass);
-
--- Set primary key
-ALTER TABLE ONLY merlin.derivation_group
-    ADD CONSTRAINT derivation_group_pkey PRIMARY KEY (id);
-
 -- Create a table to represent external event sources.
 CREATE TABLE merlin.external_source (
     id integer NOT NULL,
@@ -56,10 +32,6 @@ ALTER TABLE ONLY merlin.external_source
 -- Add foreign key definition for file_id field, linking to uploaded_file table
 ALTER TABLE ONLY merlin.external_source
     ADD CONSTRAINT "file_id -> uploaded_file" FOREIGN KEY (file_id) REFERENCES merlin.uploaded_file(id);
-
--- Add foreign key definition for file_id field, linking to uploaded_file table
-ALTER TABLE ONLY merlin.external_source
-    ADD CONSTRAINT "derivation_group_id -> derivation_group" FOREIGN KEY (derivation_group_id) REFERENCES merlin.derivation_group(id);
 
 
 
@@ -187,6 +159,42 @@ ALTER TABLE ONLY merlin.external_source_type
 
 -- Update merlin.external_source.source_type_id to link it to merlin.external_source_type.id
 ALTER TABLE ONLY merlin.external_source
+    ADD CONSTRAINT "source_type_id -> external_source_type" FOREIGN KEY (source_type_id) REFERENCES merlin.external_source_type(id);
+
+
+
+
+-- Create a table to represent derivation groups for external sources
+CREATE TABLE merlin.derivation_group (
+    id integer NOT NULL,
+    name text NOT NULL,
+    source_type_id integer NOT NULL
+);
+
+COMMENT ON TABLE merlin.derivation_group IS 'A table to represent the names of groups of sources to run derivation operations over.';
+
+-- Ensure the id is serial.
+CREATE SEQUENCE merlin.derivation_group_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+ALTER SEQUENCE merlin.derivation_group_id_seq OWNED BY merlin.derivation_group.id;
+ALTER TABLE ONLY merlin.derivation_group ALTER COLUMN id SET DEFAULT nextval('merlin.derivation_group_id_seq'::regclass);
+
+-- Set primary key
+ALTER TABLE ONLY merlin.derivation_group
+    ADD CONSTRAINT derivation_group_pkey PRIMARY KEY (id);
+
+-- Add foreign key definition for derivation_group_id field, linking to derivation_group table
+ALTER TABLE ONLY merlin.external_source
+    ADD CONSTRAINT "derivation_group_id -> derivation_group" FOREIGN KEY (derivation_group_id) REFERENCES merlin.derivation_group(id);
+
+-- Add foreign key definition for source_type_id field, linking to external_source_type table
+ALTER TABLE ONLY merlin.derivation_group
     ADD CONSTRAINT "source_type_id -> external_source_type" FOREIGN KEY (source_type_id) REFERENCES merlin.external_source_type(id);
 
 
