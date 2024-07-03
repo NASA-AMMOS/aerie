aa3cdc8b89e5ece43f99d1896cf698e6a7c72445
psubram3 <pranav.subramanian@nasa.gov>
2024-07-03 14:27:12 -0700

    update associations to between plans and derivation groups

--- deployment/hasura/migrations/Aerie/5_external_events/up.sql
+++ deployment/hasura/migrations/Aerie/5_external_events/up.sql
@@ -82,51 +82,6 @@ ALTER TABLE ONLY merlin.external_event
 
 
 
--- Create table for plan/external event links
-CREATE TABLE merlin.plan_external_source (
-    id integer NOT NULL,
-    plan_id integer NOT NULL,
-    external_source_id integer NOT NULL,
-    created_at timestamp with time zone DEFAULT now() NOT NULL,
-    owner text
-);
-
-COMMENT ON TABLE merlin.plan_external_source IS 'A table for linking externally imported event sources & plans.';
-
--- Ensure the id is serial
-CREATE SEQUENCE merlin.plan_external_source_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-ALTER SEQUENCE merlin.plan_external_source_id_seq OWNED BY merlin.plan_external_source.id;
-ALTER TABLE ONLY merlin.plan_external_source ALTER COLUMN id SET DEFAULT nextval('merlin.plan_external_source_id_seq'::regclass);
-
-
--- Set primary key
-ALTER TABLE ONLY merlin.plan_external_source
-    ADD CONSTRAINT plan_external_source_pkey PRIMARY KEY (id);
-
-
--- Add uniqueness constraint for plan_id/external_source_id pair
-ALTER TABLE ONLY merlin.plan_external_source
-    ADD CONSTRAINT unique_plan_external_source UNIQUE (plan_id, external_source_id);
-
-COMMENT ON CONSTRAINT unique_plan_external_source ON merlin.plan_external_source IS 'The tuple (plan_id, external_source_id) must be unique!';
-
--- Add foreign keys
-ALTER TABLE ONLY merlin.plan_external_source
-    ADD CONSTRAINT fk_external_source_id FOREIGN KEY (external_source_id) REFERENCES merlin.external_source(id);
-
-ALTER TABLE ONLY merlin.plan_external_source
-    ADD CONSTRAINT fk_plan_id FOREIGN KEY (plan_id) REFERENCES merlin.plan(id);
-
-
-
-
 -- Create table for external source types
 CREATE TABLE merlin.external_source_type (
     id integer NOT NULL,
@@ -196,6 +151,51 @@ ALTER TABLE ONLY merlin.external_source
 -- Add foreign key definition for source_type_id field, linking to external_source_type table
 ALTER TABLE ONLY merlin.derivation_group
     ADD CONSTRAINT "source_type_id -> external_source_type" FOREIGN KEY (source_type_id) REFERENCES merlin.external_source_type(id);
+
+
+
+
+-- Create table for plan/external event links
+CREATE TABLE merlin.plan_derivation_group (
+    id integer NOT NULL,
+    plan_id integer NOT NULL,
+    derivation_group_id integer NOT NULL,
+    created_at timestamp with time zone DEFAULT now() NOT NULL,
+    owner text
+);
+
+COMMENT ON TABLE merlin.plan_derivation_group IS 'A table for linking externally imported event sources & plans.';
+
+-- Ensure the id is serial
+CREATE SEQUENCE merlin.plan_derivation_group_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+ALTER SEQUENCE merlin.plan_derivation_group_id_seq OWNED BY merlin.plan_derivation_group.id;
+ALTER TABLE ONLY merlin.plan_derivation_group ALTER COLUMN id SET DEFAULT nextval('merlin.plan_derivation_group_id_seq'::regclass);
+
+
+-- Set primary key
+ALTER TABLE ONLY merlin.plan_derivation_group
+    ADD CONSTRAINT plan_derivation_group_pkey PRIMARY KEY (id);
+
+
+-- Add uniqueness constraint for plan_id/external_source_id pair
+ALTER TABLE ONLY merlin.plan_derivation_group
+    ADD CONSTRAINT unique_plan_derivation_group UNIQUE (plan_id, derivation_group_id);
+
+COMMENT ON CONSTRAINT unique_plan_derivation_group ON merlin.plan_derivation_group IS 'The tuple (plan_id, external_source_id) must be unique!';
+
+-- Add foreign keys
+ALTER TABLE ONLY merlin.plan_derivation_group
+    ADD CONSTRAINT fk_derivation_group_id FOREIGN KEY (derivation_group_id) REFERENCES merlin.derivation_group(id);
+
+ALTER TABLE ONLY merlin.plan_derivation_group
+    ADD CONSTRAINT fk_plan_id FOREIGN KEY (plan_id) REFERENCES merlin.plan(id);
 
 
 
