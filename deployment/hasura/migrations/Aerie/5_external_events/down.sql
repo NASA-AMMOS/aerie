d248b4bf9b0bf997fa5dddcf918377cbf6907fc7
psubram3 <pranav.subramanian@nasa.gov>
2024-07-09 12:13:28 -0700

    add rule 4 (key handling) to derivation

--- deployment/hasura/migrations/Aerie/5_external_events/down.sql
+++ deployment/hasura/migrations/Aerie/5_external_events/down.sql
@@ -1,7 +1,7 @@
 -- up.sql creates table and sequence, delete them
 DROP TABLE merlin.external_source CASCADE;
 DROP TABLE merlin.external_event CASCADE;
-DROP TABLE merlin.plan_external_source CASCADE;
+DROP TABLE merlin.plan_derivation_group CASCADE;
 DROP TABLE merlin.external_source_type CASCADE;
 DROP TABLE merlin.external_event_type CASCADE;
 DROP TABLE merlin.external_source_event_types CASCADE;
