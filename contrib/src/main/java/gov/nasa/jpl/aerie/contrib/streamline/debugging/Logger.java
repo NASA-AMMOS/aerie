6f5bfed58243921705b44c63494c4f010ca1c54a
David Legg <david.legg@jpl.nasa.gov>
2024-07-10 13:07:21 -0700

    Add streamline Logger
    Adds a Logger class to the streamline framework, which uses topics to emit log messages directly as events.
    Also builds such a Logger when the Registrar is initialized, and reconfigures errors to go to that logger when the error behavior is set to LOG.
    Before this, errors were collected awkwardly into a discrete string resource.

---
+++ contrib/src/main/java/gov/nasa/jpl/aerie/contrib/streamline/debugging/Logger.java
@@ -0,0 +1,60 @@
+package gov.nasa.jpl.aerie.contrib.streamline.debugging;
+
+import gov.nasa.jpl.aerie.contrib.streamline.core.Resources;
+import gov.nasa.jpl.aerie.merlin.framework.Registrar;
+
+import java.time.Instant;
+import java.util.Arrays;
+import java.util.Map;
+
+import static gov.nasa.jpl.aerie.merlin.protocol.types.Duration.MICROSECONDS;
+import static java.util.stream.Collectors.toMap;
+
+public class Logger {
+    private static final int LEVEL_INDICATOR_SIZE = Arrays.stream(LogLevel.values())
+            .map(v -> v.toString().length())
+            .max(Integer::compareTo)
+            .orElseThrow();
+    private static final String LOG_MESSAGE_FORMAT = "%s [%-" + LEVEL_INDICATOR_SIZE + "s] %s";
+
+    private final Map<LogLevel, SimpleLogger> subLoggers;
+    private final Instant planStart;
+
+    public Logger(Registrar registrar, Instant planStart) {
+        // TODO - refactor the plan start out, by instead building a global sim clock to do the same job.
+        //  To do that "right", the global clock should be a resource with custom dynamics for continuous Instant clocks.
+        subLoggers = Arrays.stream(LogLevel.values()).collect(toMap(
+                level -> level,
+                level -> new SimpleLogger(level.name(), registrar)));
+        this.planStart = planStart;
+    }
+
+    public void log(LogLevel level, String messageFormat, Object... args) {
+        Instant time = planStart.plusNanos(Resources.currentTime().in(MICROSECONDS) * 1_000);
+        String message = messageFormat.formatted(args);
+        subLoggers.get(level).log(LOG_MESSAGE_FORMAT.formatted(time, level, message));
+    }
+
+    public void debug(String messageFormat, Object... args) {
+        log(LogLevel.DEBUG, messageFormat, args);
+    }
+
+    public void info(String messageFormat, Object... args) {
+        log(LogLevel.INFO, messageFormat, args);
+    }
+
+    public void warning(String messageFormat, Object... args) {
+        log(LogLevel.WARNING, messageFormat, args);
+    }
+
+    public void error(String messageFormat, Object... args) {
+        log(LogLevel.ERROR, messageFormat, args);
+    }
+
+    public enum LogLevel {
+        DEBUG,
+        INFO,
+        WARNING,
+        ERROR
+    }
+}
