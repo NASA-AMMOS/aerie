7f6a6bdc56637dd92600ec9d6e980c0788767e5d
(skovati) Luke <skovati@protonmail.com>
2024-07-15 08:29:11 -0700

    remove `goal_id` and `goal_revision` from created / satisfied activities
    these fields already exist on the `goal_analysis` table,
    so instead of duplicating data, we can just FK to `goal_analysis`

--- deployment/postgres-init-db/sql/tables/scheduler/scheduling_run/scheduling_goal_analysis_created_activities.sql
+++ deployment/postgres-init-db/sql/tables/scheduler/scheduling_run/scheduling_goal_analysis_created_activities.sql
@@ -1,7 +1,5 @@
 create table scheduler.scheduling_goal_analysis_created_activities (
   analysis_id integer not null,
-  goal_id integer not null,
-  goal_revision integer not null,
   goal_invocation_id integer not null,
   activity_id integer not null,
 
@@ -9,12 +7,12 @@ create table scheduler.scheduling_goal_analysis_created_activities (
     primary key (analysis_id, goal_invocation_id, activity_id),
   constraint created_activities_references_scheduling_request
     foreign key (analysis_id)
-      references scheduler.scheduling_request (analysis_id)
+      references scheduler.scheduling_request
       on update cascade
       on delete cascade,
-  constraint created_activities_references_scheduling_goal
-    foreign key (goal_id, goal_revision)
-      references scheduler.scheduling_goal_definition
+  constraint created_activities_references_scheduling_goal_analysis
+    foreign key (analysis_id, goal_invocation_id)
+      references scheduler.scheduling_goal_analysis
       on update cascade
       on delete cascade
 );
@@ -23,10 +21,6 @@ comment on table scheduler.scheduling_goal_analysis_created_activities is e''
   'The activity instances created by a scheduling run to satisfy a goal.';
 comment on column scheduler.scheduling_goal_analysis_created_activities.analysis_id is e''
   'The associated analysis ID.';
-comment on column scheduler.scheduling_goal_analysis_created_activities.goal_id is e''
-  'The associated goal ID.';
-comment on column scheduler.scheduling_goal_analysis_created_activities.goal_revision is e''
-  'The associated version of the goal definition used.';
 comment on column scheduler.scheduling_goal_analysis_created_activities.goal_invocation_id is e''
   'The associated goal invocation ID.';
 comment on column scheduler.scheduling_goal_analysis_created_activities.activity_id is e''
--- deployment/postgres-init-db/sql/tables/scheduler/scheduling_run/scheduling_goal_analysis_satisfying_activities.sql
+++ deployment/postgres-init-db/sql/tables/scheduler/scheduling_run/scheduling_goal_analysis_satisfying_activities.sql
@@ -1,7 +1,5 @@
 create table scheduler.scheduling_goal_analysis_satisfying_activities (
   analysis_id integer not null,
-  goal_id integer not null,
-  goal_revision integer not null,
   goal_invocation_id integer not null,
   activity_id integer not null,
 
@@ -9,12 +7,12 @@ create table scheduler.scheduling_goal_analysis_satisfying_activities (
     primary key (analysis_id, goal_invocation_id, activity_id),
   constraint satisfying_activities_references_scheduling_request
     foreign key (analysis_id)
-      references scheduler.scheduling_request (analysis_id)
+      references scheduler.scheduling_request
       on update cascade
       on delete cascade,
-  constraint satisfying_activities_references_scheduling_goal
-    foreign key (goal_id, goal_revision)
-      references scheduler.scheduling_goal_definition
+  constraint satisfying_activities_references_scheduling_goal_analysis
+    foreign key (analysis_id, goal_invocation_id)
+      references scheduler.scheduling_goal_analysis
       on update cascade
       on delete cascade
 );
@@ -23,10 +21,6 @@ comment on table scheduler.scheduling_goal_analysis_satisfying_activities is e''
   'The activity instances satisfying a scheduling goal.';
 comment on column scheduler.scheduling_goal_analysis_satisfying_activities.analysis_id is e''
   'The associated analysis ID.';
-comment on column scheduler.scheduling_goal_analysis_satisfying_activities.goal_id is e''
-  'The associated goal ID.';
-comment on column scheduler.scheduling_goal_analysis_satisfying_activities.goal_revision is e''
-  'The associated version of the goal definition used.';
 comment on column scheduler.scheduling_goal_analysis_satisfying_activities.activity_id is e''
   'The ID of an activity instance satisfying the associated goal.';
 comment on column scheduler.scheduling_goal_analysis_satisfying_activities.goal_invocation_id is e''
