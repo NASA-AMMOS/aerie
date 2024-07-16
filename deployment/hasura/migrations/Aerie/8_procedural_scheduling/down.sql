c12c5542566fe2061968a795a3a1a7a0bb912cb9
(skovati) Luke <skovati@protonmail.com>
2024-07-16 11:32:44 -0700

    update proc sched migrations with new PK changes

--- deployment/hasura/migrations/Aerie/8_procedural_scheduling/down.sql
+++ deployment/hasura/migrations/Aerie/8_procedural_scheduling/down.sql
@@ -1,17 +1,71 @@
+-- restore goal_analysis_* tables
+
+-- drop new PKs for created / satisfying activities
+-- and add old columns back so we can populate and re-PK
+alter table scheduler.scheduling_goal_analysis_satisfying_activities
+  drop constraint satisfying_activities_primary_key,
+
+  -- temp set as nullable so we can insert, made not null by PK constraint below
+  add column goal_id integer null,
+  add column goal_revision integer null;
+
 alter table scheduler.scheduling_goal_analysis_created_activities
   drop constraint created_activities_primary_key,
+
+  add column goal_id integer null,
+  add column goal_revision integer null;
+
+update scheduler.scheduling_goal_analysis_satisfying_activities as sa
+  set goal_id = ga.goal_id, goal_revision = ga.goal_revision
+  from scheduler.scheduling_goal_analysis ga
+  where (sa.analysis_id, sa.goal_invocation_id) = (ga.analysis_id, ga.goal_invocation_id);
+
+update scheduler.scheduling_goal_analysis_created_activities as ca
+  set goal_id = ga.goal_id, goal_revision = ga.goal_revision
+  from scheduler.scheduling_goal_analysis ga
+  where (ca.analysis_id, ca.goal_invocation_id) = (ga.analysis_id, ga.goal_invocation_id);
+
+alter table scheduler.scheduling_goal_analysis_satisfying_activities
+  add constraint satisfying_activities_primary_key
+    primary key (analysis_id, goal_id, goal_revision, activity_id),
+
+  drop column goal_invocation_id;
+
+alter table scheduler.scheduling_goal_analysis_created_activities
   add constraint created_activities_primary_key
     primary key (analysis_id, goal_id, goal_revision, activity_id),
 
   drop column goal_invocation_id;
 
 alter table scheduler.scheduling_goal_analysis
-  drop constraint scheduling_goal_analysis_primary_key,
+  drop constraint scheduling_goal_analysis_primary_key;
+
+-- delete all but the first goal invocation from analysis table if there nonuniqueness
+-- between the composite key that will become the new (old) PK
+delete
+from scheduler.scheduling_goal_analysis
+where goal_invocation_id not in (select min(goal_invocation_id)
+                                 from scheduler.scheduling_goal_analysis
+                                 group by analysis_id, goal_id, goal_revision);
+
+alter table scheduler.scheduling_goal_analysis
   add constraint scheduling_goal_analysis_primary_key
     primary key (analysis_id, goal_id, goal_revision),
 
   drop column goal_invocation_id,
   drop column arguments;
+
+-- restore scheduling_specification_goals
+-- delete data for new PK
+-- i.e. find where (spec_id, goal_id) is not unique
+-- and delete all entries except for the one with the lowest goal_invocation_id
+-- so that (spec_id, goal_id) can be the new PK
+-- delete from scheduler.scheduling_specification_goals a
+delete
+from scheduler.scheduling_specification_goals
+where goal_invocation_id not in (select min(goal_invocation_id)
+                                 from scheduler.scheduling_specification_goals
+                                 group by specification_id, goal_id);
 
 alter table scheduler.scheduling_specification_goals
   drop constraint scheduling_specification_goals_primary_key,
@@ -21,10 +75,21 @@ alter table scheduler.scheduling_specification_goals
   drop column goal_invocation_id,
   drop column arguments;
 
-delete from scheduler.scheduling_goal_definition
-  where definition is null;
+-- delete all scheduling procedures from specs
+-- (on delete is restricted)
+delete from scheduler.scheduling_specification_goals sg
+  using scheduler.scheduling_goal_definition gd
+  where gd.goal_id = sg.goal_id
+  and gd.type = 'JAR'::scheduler.goal_type;
+
+-- delete all scheduling procedure definitions
+delete from scheduler.scheduling_goal_metadata gm
+  using scheduler.scheduling_goal_definition gd
+  where gm.id = gd.goal_id
+  and gd.type = 'JAR'::scheduler.goal_type;
 
 alter table scheduler.scheduling_goal_definition
+  drop constraint check_goal_definition_type_consistency,
   drop constraint scheduling_procedure_has_uploaded_jar,
 
   drop column type,
@@ -32,7 +97,6 @@ alter table scheduler.scheduling_goal_definition
   drop column parameter_schema,
 
   alter column definition set not null;
-
 
 drop type scheduler.goal_type;
 
