06b86d246a9c24ad1cc93aa1f02a958a64d4cd1d
pranav-super <pranav.subramanian@colorado.edu>
2024-07-30 14:41:58 -0700

    (re-)add trigger to cleanup on source delete

--- deployment/hasura/migrations/Aerie/9_external_events/up.sql
+++ deployment/hasura/migrations/Aerie/9_external_events/up.sql
@@ -279,16 +279,77 @@ COMMENT ON VIEW merlin.external_source_event_type
 
 
 
--- Set primary key
-ALTER TABLE ONLY merlin.external_source_event_types
-    ADD CONSTRAINT external_source_event_type_pkey PRIMARY KEY (id);
+-- Add trigger to manually clean up empty plan_derivation_group links, derivation_groups, external_source_types,
+--    and external_event_types (in that order) on source delete. Note that external_event and external_source_event_types
+--    automatically cascade source deletions, so manual intervention is only necessary for these 4.
+-- TODO: SHOULD THIS BE ADDED TO FUNCTIONS.YAML? IT ISN'T A BAD IDEA TO BE ABLE TO CALL THIS FROM HASURA, BUT DOES THAT
+--        VIOLATE THE DESIGN SOMEHOW?
+CREATE OR REPLACE FUNCTION merlin.cleanup_on_external_source_delete()
+	returns trigger
+	language plpgsql
+	as
+	$$
+	begin
+	  -- STEP 0: DELETE ASSOCIATED UPLOADED_FILE
+	  --          Note: as users are now permitted to delete external_sources, deleting uploaded_files from the
+	  --                existing delete query requires that we give users delete permissions on uploaded_file,
+	  --                which currently doesn't exist and is therefore something we aim to avoid. As such, deletion
+	  --                is handled here.
+	  WITH to_delete AS (
+    	SELECT uploaded_file.id, uploaded_file.name FROM merlin.uploaded_file
+    		LEFT JOIN merlin.external_source ON external_source.file_id = uploaded_file.id
+    		LEFT JOIN merlin.mission_model ON mission_model.jar_id = uploaded_file.id
+    		WHERE key IS NULL AND jar_id IS NULL
+    		ORDER BY uploaded_file.id
+    )
+    DELETE FROM merlin.uploaded_file
+    	WHERE id IN (SELECT id FROM to_delete);
 
--- Add key for external_event_type_id
-ALTER TABLE ONLY merlin.external_source_event_types
-    ADD CONSTRAINT external_event_type_id FOREIGN KEY (external_event_type_id) REFERENCES merlin.external_event_type(id);
-
--- Add key for external_source_id
-ALTER TABLE ONLY merlin.external_source_event_types
-    ADD CONSTRAINT external_source_id FOREIGN KEY (external_source_id) REFERENCES merlin.external_source(id);
+		-- STEP 1: DELETE LINGERING plan->dg links:
+		WITH to_delete AS (
+			SELECT plan_derivation_group.id FROM merlin.plan_derivation_group
+				LEFT JOIN merlin.derivation_group ON derivation_group.id = plan_derivation_group.derivation_group_id
+				LEFT JOIN merlin.external_source ON derivation_group.source_type_id = external_source.source_type_id
+				WHERE key IS NULL
+		)
+		DELETE FROM merlin.plan_derivation_group
+			WHERE id IN (SELECT id FROM to_delete);
+		-- STEP 2: DELETE LINGERING DGs:
+		WITH to_delete AS (
+			SELECT derivation_group.id, name FROM merlin.derivation_group
+				LEFT JOIN merlin.external_source ON derivation_group.source_type_id = external_source.source_type_id
+				WHERE key IS NULL
+		)
+		DELETE FROM merlin.derivation_group
+			WHERE id IN (SELECT id FROM to_delete);
+		-- STEP 3: DELETE LINGERING EXTERNAL SOURCE TYPES
+		WITH to_delete AS (
+			SELECT external_source_type.id, name from merlin.external_source_type
+				LEFT JOIN merlin.external_source ON external_source_type.id = external_source.source_type_id
+				WHERE key IS NULL
+		)
+		DELETE FROM merlin.external_source_type
+			WHERE id IN (SELECT id FROM to_delete);
+		-- STEP 4: DELETE LINGERING EXTERNAL_EVENT_TYPES
+		WITH to_delete AS (
+			SELECT external_event_type.id, name FROM merlin.external_event_type
+				LEFT JOIN merlin.external_event ON external_event.event_type_id = external_event_type.id
+				WHERE key IS NULL
+		)
+		DELETE FROM merlin.external_event_type
+			WHERE id IN (SELECT id FROM to_delete);
+		-- STEP 5: RETURN
+		RETURN NULL;
+	end;
+	$$;
+ALTER FUNCTION merlin.cleanup_on_external_source_delete()
+    OWNER TO aerie;
+GRANT EXECUTE ON FUNCTION merlin.cleanup_on_external_source_delete() TO aerie;
+CREATE OR REPLACE TRIGGER cleanup_on_external_source_delete
+AFTER DELETE ON merlin.external_source
+	FOR EACH ROW EXECUTE FUNCTION cleanup_on_external_source_delete();
+--ALTER TRIGGER cleanup_on_external_source_delete
+--    OWNER TO aerie;
+--GRANT EXECUTE ON TRIGGER cleanup_on_external_source_delete TO aerie;
 
 call migrations.mark_migration_applied('9');
