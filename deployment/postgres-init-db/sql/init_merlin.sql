03bf14b2e80c357a1d9f729ad0792ef653a92f58
pranav-super <pranav.subramanian@colorado.edu>
2024-08-01 12:36:01 -0700

    remove external_source_event_type view

--- deployment/postgres-init-db/sql/init_merlin.sql
+++ deployment/postgres-init-db/sql/init_merlin.sql
@@ -104,7 +104,6 @@ begin;
   \ir views/merlin/activity_directive_extended.sql
   \ir views/merlin/simulated_activity.sql
   \ir views/merlin/resource_profile.sql
-  \ir views/merlin/external_source_event_type.sql -- consider putting this in ui part of schema
   \ir views/merlin/derived_events.sql
   \ir views/merlin/derivation_group_comp.sql -- consider putting this in ui part of schema
 end;
