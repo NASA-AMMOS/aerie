df974351b5cba2aaef01bef5c2519eb651f48260
pranav-super <pranav.subramanian@colorado.edu>
2024-08-01 12:36:01 -0700

    remove external_source_event_type view

--- deployment/hasura/migrations/Aerie/9_external_events/up.sql
+++ deployment/hasura/migrations/Aerie/9_external_events/up.sql
@@ -261,24 +261,6 @@ ALTER TABLE ONLY merlin.external_event
 
 
 
--- Create table mapping external sources to their contained event types
-CREATE OR REPLACE VIEW merlin.external_source_event_type
- AS
- SELECT external_source.id AS external_source_id,
-    array_agg(DISTINCT external_event.event_type_id) AS event_type_ids,
-    array_agg(DISTINCT external_event_type.name) AS event_types
-   FROM merlin.external_source
-     JOIN merlin.external_event ON external_event.source_id = external_source.id
-     JOIN merlin.external_event_type ON external_event_type.id = external_event.event_type_id
-  GROUP BY external_source.id;
-
-ALTER VIEW IF EXISTS merlin.external_source_event_type OWNER TO aerie;
-COMMENT ON VIEW merlin.external_source_event_type
-    IS 'Should be deprecated with the introduction of strict external source schemas, dictating allowable event types for given source types. But for now, this will do.';
-
-
-
-
 -- Add trigger to manually clean up empty plan_derivation_group links, derivation_groups, external_source_types,
 --    and external_event_types (in that order) on source delete. Note that external_event and external_source_event_types
 --    automatically cascade source deletions, so manual intervention is only necessary for these 4.
