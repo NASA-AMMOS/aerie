ab0a5f24317a7a10e77298969a575b22d950a45e
pranav-super <pranav.subramanian@colorado.edu>
2024-08-05 13:06:15 -0700

    move seen sources to database
    Previously was in local storage, but wouldn't clear correctly on DB reset.

--- deployment/hasura/migrations/Aerie/9_external_events/down.sql
+++ deployment/hasura/migrations/Aerie/9_external_events/down.sql
@@ -4,5 +4,6 @@ DROP TABLE merlin.external_event CASCADE;
 DROP TABLE merlin.plan_derivation_group CASCADE;
 DROP TABLE merlin.external_source_type CASCADE;
 DROP TABLE merlin.external_event_type CASCADE;
+DROP TABLE ui.seen_sources CASCADE;
 
 call migrations.mark_migration_rolled_back('9');
