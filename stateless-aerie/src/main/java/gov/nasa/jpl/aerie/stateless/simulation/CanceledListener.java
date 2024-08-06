db581b12c99e2f2d99a2132d65bae58b1992e481
Theresa Kamerman <theresa.kamerman@jpl.nasa.gov>
2024-08-05 17:45:19 -0700

    Create Simulation Utilities
    Co-authored-by: Ryan Goetz <ryan.r.goetz@jpl.nasa.gov>

---
+++ stateless-aerie/src/main/java/gov/nasa/jpl/aerie/stateless/simulation/CanceledListener.java
@@ -0,0 +1,15 @@
+package gov.nasa.jpl.aerie.stateless.simulation;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.function.Supplier;
+
+public class CanceledListener implements Supplier<Boolean> {
+  private final AtomicBoolean canceled = new AtomicBoolean(false);
+
+  public void cancel() { canceled.set(true); }
+
+  @Override
+  public Boolean get() {
+    return canceled.get();
+  }
+}
---
+++ stateless-aerie/src/main/java/gov/nasa/jpl/aerie/stateless/simulation/SimulationExtentConsumer.java
@@ -0,0 +1,52 @@
+package gov.nasa.jpl.aerie.stateless.simulation;
+
+import gov.nasa.jpl.aerie.merlin.protocol.types.Duration;
+
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.function.Consumer;
+
+public class SimulationExtentConsumer implements Consumer<Duration>, AutoCloseable {
+  private Duration lastAcceptedDuration = Duration.ZERO;
+  private Duration lastReportedDuration = null;
+  private final Timer printTimer;
+
+  /**
+   * Create a new SimulationExtentConsumer that prints the simulation time with a minimum frequency.
+   * @param periodMillis The minimum gap between simulation extent updates, in milliseconds.
+   */
+  public SimulationExtentConsumer(final long periodMillis) {
+    printTimer = new Timer();
+
+    final var timerTask = new TimerTask() {
+      @Override
+      public void run() {
+        // Only print if simulation time has progressed.
+        if(!lastAcceptedDuration.isEqualTo(lastReportedDuration)) {
+          System.out.println("Current simulation time: " + lastAcceptedDuration);
+          lastReportedDuration = lastAcceptedDuration;
+        }
+      }
+    };
+    printTimer.scheduleAtFixedRate(timerTask, periodMillis, periodMillis);
+  }
+
+  /**
+   * Create a new SimulationExtentConsumer that consumes but does not print the simulation time.
+   */
+  public SimulationExtentConsumer() {
+    printTimer = new Timer();
+  }
+
+  @Override
+  public void accept(final Duration duration) {
+    lastAcceptedDuration = duration;
+  }
+
+  public Duration getLastAcceptedDuration() { return lastAcceptedDuration; }
+
+  @Override
+  public void close() {
+    printTimer.cancel();
+  }
+}
