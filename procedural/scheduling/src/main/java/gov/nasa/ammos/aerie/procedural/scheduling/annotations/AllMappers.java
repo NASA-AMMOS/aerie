b9d9da26e4118e8b48635b8030d6ef8bfb748f79
Matthew Dailis <matthew.l.dailis@jpl.nasa.gov>
2024-08-07 06:27:58 -0700

    Implement annotation processor for scheduling procedures

---
+++ procedural/scheduling/src/main/java/gov/nasa/ammos/aerie/procedural/scheduling/annotations/AllMappers.java
@@ -0,0 +1,12 @@
+package gov.nasa.ammos.aerie.procedural.scheduling.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.CLASS)
+@Target(ElementType.PACKAGE)
+@interface AllMappers {
+  WithMappers[] value();
+}
