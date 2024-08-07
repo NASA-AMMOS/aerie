1d0d14f4789727dcfae0341e8892f4b93e23351e
Matthew Dailis <matthew.l.dailis@jpl.nasa.gov>
2024-08-06 17:29:31 -0700

    Update procedural constraints to use ActivityId from merlin

--- procedural/constraints/src/main/kotlin/gov/nasa/ammos/aerie/procedural/constraints/ActivityId.kt
+++
@@ -1,7 +0,0 @@
-package gov.nasa.ammos.aerie.procedural.constraints
-
-/** An activity ID, referencing either a directive or instance. */
-sealed interface ActivityId {
-  /***/ data class InstanceId(/***/ val id: Long): ActivityId
-  /***/ data class DirectiveId(/***/ val id: Long): ActivityId
-}
--- procedural/constraints/src/main/kotlin/gov/nasa/ammos/aerie/procedural/constraints/Violation.kt
+++ procedural/constraints/src/main/kotlin/gov/nasa/ammos/aerie/procedural/constraints/Violation.kt
@@ -2,6 +2,7 @@ package gov.nasa.ammos.aerie.procedural.constraints
 
 import gov.nasa.ammos.aerie.procedural.timeline.Interval
 import gov.nasa.ammos.aerie.procedural.timeline.payloads.IntervalLike
+import gov.nasa.jpl.aerie.merlin.driver.ActivityId
 
 /** A single violation of a constraint. */
 data class Violation(
