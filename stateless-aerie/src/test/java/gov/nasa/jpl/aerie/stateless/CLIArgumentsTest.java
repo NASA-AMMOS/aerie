b8859f41d452eec7a88cdb66c486521667877b05
Theresa Kamerman <theresa.kamerman@jpl.nasa.gov>
2024-08-14 17:47:01 -0700

    finish tests (and fixes some)

--- stateless-aerie/src/test/java/gov/nasa/jpl/aerie/stateless/CLIArgumentsTest.java
+++ stateless-aerie/src/test/java/gov/nasa/jpl/aerie/stateless/CLIArgumentsTest.java
@@ -10,13 +10,16 @@ import org.junit.jupiter.api.Test;
 import javax.json.Json;
 import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.io.StringReader;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CLIArgumentsTest {
@@ -76,11 +79,8 @@ public class CLIArgumentsTest {
     final var otherArgs = new String[][]{{"simulate", "-h"}, {"simulate", "--help"}};
     BlockExitSecurityManager.install();
     for(final var args : otherArgs) {
-      try {
-        Main.main(args);
-      } catch (SystemExit se) {
-        assertEquals(0, se.getStatusCode());
-      }
+      final var sysExit = assertThrows(SystemExit.class, () -> Main.main(args));
+      assertEquals(0, sysExit.getStatusCode());
 
       outputStream.flush();
       assertFalse(out.toString().contains(helpString));
@@ -121,11 +121,8 @@ public class CLIArgumentsTest {
 
       BlockExitSecurityManager.install();
       for (final var args : helpArgs) {
-        try {
-          Main.main(args);
-        } catch (SystemExit se) {
-          assertEquals(0, se.getStatusCode());
-        }
+        final var sysExit = assertThrows(SystemExit.class, () -> Main.main(args));
+        assertEquals(0, sysExit.getStatusCode());
 
         outputStream.flush();
         assertTrue(out.toString().contains(helpString));
@@ -136,9 +133,69 @@ public class CLIArgumentsTest {
       BlockExitSecurityManager.uninstall();
     }
 
-    /**
-     * When verbose is on, progress is reported prior to simulation results.
-     */
+    /** An exception is thrown if simulation is run for a plan that doesn't exist or cannot be parsed. */
+    @Test
+    void badPlan() {
+      final var missingFileError = assertThrows(RuntimeException.class,
+                                      () -> Main.main(new String[]{
+                                          "simulate",
+                                          "-m", "../examples/foo-missionmodel/build/libs/foo-missionmodel.jar",
+                                          "-p", "src/test/resources/fake_plan.json"}));
+      assertEquals("Specified plan JSON file does not exist: src/test/resources/fake_plan.json",
+                   missingFileError.getMessage());
+
+      final var badFileError = assertThrows(RuntimeException.class,
+                                      () -> Main.main(new String[]{
+                                          "simulate",
+                                          "-m", "../examples/foo-missionmodel/build/libs/foo-missionmodel.jar",
+                                          "-p", "../examples/foo-missionmodel/build/libs/foo-missionmodel.jar"}));
+      assertEquals("Error while reading plan JSON file: ../examples/foo-missionmodel/build/libs/foo-missionmodel.jar",
+                   badFileError.getMessage());
+    }
+
+    /** An exception is thrown if simulation is run using a model that doesn't exist or cannot be parsed. */
+    @Test
+    void badModel() {
+      final var missingFileError = assertThrows(RuntimeException.class,
+                                      () -> Main.main(new String[]{
+                                          "simulate",
+                                          "-m", "fake-mission-model.jar",
+                                          "-p", "src/test/resources/simpleFooPlan.json"}));
+      assertEquals("Error while loading mission model: fake-mission-model.jar",
+                   missingFileError.getMessage());
+
+      final var badFileError = assertThrows(RuntimeException.class,
+                                      () -> Main.main(new String[]{
+                                          "simulate",
+                                          "-m", "src/test/resources/simpleFooPlan.json",
+                                          "-p", "src/test/resources/simpleFooPlan.json"}));
+      assertEquals("Error while loading mission model: src/test/resources/simpleFooPlan.json",
+                   badFileError.getMessage());
+    }
+
+    /** An exception is thrown if simulation is run using a sim config that doesn't exist or cannot be parsed. */
+    @Test
+    void badSimConfig() {
+      final var missingFileError = assertThrows(RuntimeException.class,
+                                      () -> Main.main(new String[]{
+                                          "simulate",
+                                          "-m", "../examples/foo-missionmodel/build/libs/foo-missionmodel.jar",
+                                          "-p", "src/test/resources/simpleFooPlan.json",
+                                          "-s", "src/test/resources/fake_config.json"}));
+      assertEquals("Specified simulation configuration JSON file does not exist: src/test/resources/fake_config.json",
+                   missingFileError.getMessage());
+
+      final var badFileError = assertThrows(RuntimeException.class,
+                                      () -> Main.main(new String[]{
+                                          "simulate",
+                                          "-m", "../examples/foo-missionmodel/build/libs/foo-missionmodel.jar",
+                                          "-p", "src/test/resources/simpleFooPlan.json",
+                                          "-s", "src/test/resources/simpleFooPlan.json"}));
+      assertEquals("Error while reading simulation configuration JSON file: src/test/resources/simpleFooPlan.json",
+                   badFileError.getMessage());
+    }
+
+    /** When verbose is on, progress is reported prior to simulation results. */
     @Test
     void verboseOn() throws IOException {
       Main.main(new String[]{"simulate",
@@ -175,9 +232,7 @@ public class CLIArgumentsTest {
       }
     }
 
-    /**
-     * When verbose is off, only simulation results are output.
-     */
+    /** When verbose is off, only simulation results are output. */
     @Test
     void verboseOff() throws IOException {
       Main.main(new String[]{"simulate",
@@ -190,6 +245,57 @@ public class CLIArgumentsTest {
         final var fileJson = fileReader.readObject();
         final var outputJson = outputReader.readObject();
         assertEquals(fileJson, outputJson);
+      }
+    }
+
+    /** Sim config bounds take precedence over plan bounds */
+    @Test
+    void simConfigTemporalSubset() throws FileNotFoundException {
+      Main.main(new String[] {"simulate",
+                              "-m", "../examples/foo-missionmodel/build/libs/foo-missionmodel.jar",
+                              "-p", "src/test/resources/simpleFooPlan.json",
+                              "-s", "src/test/resources/temporalSubsetFooConfiguration.json"});
+      try(final var fileReader = Json.createReader(new FileReader("src/test/resources/subsetFooPlanResults.json"));
+          final var outputReader = Json.createReader(new StringReader(out.toString()))) {
+        final var fileJson = fileReader.readObject();
+        final var outputJson = outputReader.readObject();
+        assertEquals(fileJson, outputJson);
+      }
+    }
+
+    /**
+     * Sim exceptions are given as a well-formatted JSON.
+     * Also tests that sim config arguments are applied to simulation.
+     */
+    @Test
+    void simException() {
+      BlockExitSecurityManager.install();
+      final var sysExit = assertThrows(SystemExit.class,
+                                       () -> Main.main(new String[]{
+                                          "simulate",
+                                          "-m", "../examples/foo-missionmodel/build/libs/foo-missionmodel.jar",
+                                          "-p", "src/test/resources/simpleFooPlan.json",
+                                          "-s", "src/test/resources/exceptionFooConfiguration.json",}));
+      assertEquals(1, sysExit.getStatusCode());
+      BlockExitSecurityManager.uninstall();
+
+      assertTrue(out.toString().isBlank());
+      assertFalse(err.toString().isBlank());
+
+      try(final var errorReader = Json.createReader(new StringReader(err.toString()))) {
+        final var errorJson = errorReader.readObject();
+        final var dataObject = Json.createObjectBuilder()
+                                   .add("elapsedTime", "01:00:00.000000")
+                                   .add( "utcTimeDoy", "2024-183T01:00:00")
+                                   .build();
+
+        assertEquals(4, errorJson.keySet().size());
+        assertTrue(errorJson.keySet().containsAll(List.of("type", "message", "data", "trace")));
+
+        assertEquals("SIMULATION_EXCEPTION", errorJson.getString("type"));
+        assertEquals("Daemon task exception raised.", errorJson.getString("message"));
+        assertEquals(dataObject, errorJson.getJsonObject("data"));
+        assertTrue(errorJson.getString("trace").startsWith("java.lang.RuntimeException: Daemon task exception raised."));
       }
     }
   }
