f79062f746e220cddfa2c39b7342b1ca54e8db33
jpvolosi <joseph.p.volosin@nasa.gov>
2024-08-16 12:33:58 -0400

    cleanup & format external source-related SQL files to match project style

--- deployment/postgres-init-db/sql/tables/merlin/external_events/derivation_group.sql
+++ deployment/postgres-init-db/sql/tables/merlin/external_events/derivation_group.sql
@@ -1,7 +1,7 @@
 -- Create a table to represent derivation groups for external sources
-CREATE TABLE merlin.derivation_group (
-    name text NOT NULL UNIQUE,
-    source_type_name text NOT NULL,
+create table merlin.derivation_group (
+    name text not null unique,
+    source_type_name text not null,
 
     constraint derivation_group_pkey
       primary key (name, source_type_name),
@@ -9,5 +9,5 @@ CREATE TABLE merlin.derivation_group (
       foreign key (source_type_name)
       references merlin.external_source_type(name)
 );
-
-COMMENT ON TABLE merlin.derivation_group IS 'A table to represent the names of groups of sources to run derivation operations over.';
+comment on table merlin.derivation_group is e''
+  'A table to represent the names of groups of sources to run derivation operations over.';
--- deployment/postgres-init-db/sql/views/merlin/derived_events.sql
+++ deployment/postgres-init-db/sql/views/merlin/derived_events.sql
@@ -1,7 +1,7 @@
 -- Create a view for derivation of events
-CREATE OR REPLACE VIEW merlin.derived_events
-  AS
-  SELECT
+create or replace view merlin.derived_events
+  as
+  select
       source_id,
       event_id,
       event_key,
@@ -10,7 +10,7 @@ CREATE OR REPLACE VIEW merlin.derived_events
       start_time,
       source_range,
       valid_at
-     FROM ( SELECT rule1_3.source_id,
+     from ( select rule1_3.source_id,
               rule1_3.event_id,
               rule1_3.event_key,
               rule1_3.event_type_name,
@@ -18,35 +18,37 @@ CREATE OR REPLACE VIEW merlin.derived_events
               rule1_3.start_time,
               rule1_3.source_range,
               rule1_3.valid_at,
-              row_number() OVER (PARTITION BY rule1_3.event_key, rule1_3.derivation_group_name ORDER BY rule1_3.valid_at DESC) AS rn
-             FROM ( SELECT sub.id AS source_id,
-                      external_event.id AS event_id,
-                      external_event.key AS event_key,
+              row_number() over (partition by rule1_3.event_key, rule1_3.derivation_group_name order by rule1_3.valid_at desc) as rn
+              from ( select sub.id as source_id,
+                      external_event.id as event_id,
+                      external_event.key as event_key,
                       external_event.event_type_name,
                       sub.derivation_group_name,
                       external_event.start_time,
                       sub.source_range,
                       sub.valid_at
-                     FROM merlin.external_event
-                       JOIN (
-  	 					WITH derivation_tb_range AS (
-  							SELECT id, key, derivation_group_name, tstzmultirange(tstzrange(start_time, end_time)) as dr, valid_at
-  								FROM merlin.external_source
-  								ORDER BY valid_at ASC
+                      from merlin.external_event
+                        join (
+  	 					with derivation_tb_range as (
+  							select id, key, derivation_group_name, tstzmultirange(tstzrange(start_time, end_time)) as dr, valid_at
+  								from merlin.external_source
+  								order by valid_at asc
   						),
-  						ranges_with_subs AS (
-  							SELECT tr1.id, tr1.derivation_group_name, tr1.key, tr1.dr as original_range, COALESCE(array_remove(array_agg(tr2.dr ORDER BY tr2.valid_at) FILTER (WHERE tr1.derivation_group_name = tr2.derivation_group_name), NULL), '{}') as subsequent_ranges, tr1.valid_at
-  								FROM derivation_tb_range tr1
-  								LEFT JOIN derivation_tb_range tr2 ON tr1.valid_at < tr2.valid_at
-  								GROUP BY tr1.id, tr1.derivation_group_name, tr1.key, tr1.valid_at, tr1.dr
+  						ranges_with_subs as (
+  							select tr1.id, tr1.derivation_group_name, tr1.key, tr1.dr as original_range, COALESCE(array_remove(array_agg(tr2.dr order by tr2.valid_at) filter (where tr1.derivation_group_name = tr2.derivation_group_name), null), '{}') as subsequent_ranges, tr1.valid_at
+  								from derivation_tb_range tr1
+  								left join derivation_tb_range tr2 on tr1.valid_at < tr2.valid_at
+  								group by tr1.id, tr1.derivation_group_name, tr1.key, tr1.valid_at, tr1.dr
   						)
-  						SELECT id, derivation_group_name, key, original_range, subsequent_ranges, merlin.subtract_later_ranges(original_range, subsequent_ranges) as source_range, valid_at
-  							FROM ranges_with_subs
-  							ORDER BY derivation_group_name DESC, valid_at ASC
-  	 				) sub ON sub.id = external_event.source_id
-                    WHERE sub.source_range @> external_event.start_time
-                    ORDER BY sub.derivation_group_name, external_event.start_time) rule1_3) t
-    WHERE rn = 1
-    ORDER BY start_time;
+  						select id, derivation_group_name, key, original_range, subsequent_ranges, merlin.subtract_later_ranges(original_range, subsequent_ranges) as source_range, valid_at
+  							from ranges_with_subs
+  							order by derivation_group_name desc, valid_at asc
+  	 				) sub on sub.id = external_event.source_id
+                    where sub.source_range @> external_event.start_time
+                    order by sub.derivation_group_name, external_event.start_time) rule1_3) t
+    where rn = 1
+    order by start_time;
 
-ALTER VIEW IF EXISTS merlin.derived_events OWNER TO aerie;
+alter view if exists merlin.derived_events owner to aerie;
+comment on view  merlin.derived_events is e''
+  'A view detailing all derived events from the ';
