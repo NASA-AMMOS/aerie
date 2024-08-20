f979eb61521478bc091e8983b50aaa26b7cc4e78
pranav-super <pranav.subramanian@colorado.edu>
2024-08-20 12:08:00 -0700

    update pkey for source to include source type
    it is possible to have a source with same name as other source in another dg, and with the notion of default dg, is clearly possible to have dgs with same name, so uniqueness obtained via a trio

--- deployment/postgres-init-db/sql/tables/merlin/external_events/external_event.sql
+++ deployment/postgres-init-db/sql/tables/merlin/external_events/external_event.sql
@@ -3,16 +3,17 @@ create table merlin.external_event (
     key text not null,
     event_type_name text not null,
 	  source_key text not null,
+	  source_type_name text not null,
     derivation_group_name text not null,
     start_time timestamp with time zone not null,
     duration interval not null,
     properties jsonb,
 
     constraint external_event_pkey
-      primary key (key, source_key, derivation_group_name, event_type_name),
+      primary key (key, source_type_name, source_key, derivation_group_name, event_type_name),
     constraint external_event_references_source_id
-      foreign key (source_key, derivation_group_name)
-      references merlin.external_source (key, derivation_group_name),
+      foreign key (source_key, source_type_name, derivation_group_name)
+      references merlin.external_source (key, source_type_name, derivation_group_name),
     constraint external_event_references_event_type_name
       foreign key (event_type_name)
       references merlin.external_event_type(name)
