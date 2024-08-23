27e065d9b37b9fd8f1b80ef649b798b3d7a3d51a
pranav-super <pranav.subramanian@colorado.edu>
2024-08-23 14:39:45 -0700

    add event bounds checks
    Not completely confident on placing it in external_event.sql, open to comment there.

--- deployment/hasura/migrations/Aerie/9_external_events/up.sql
+++ deployment/hasura/migrations/Aerie/9_external_events/up.sql
@@ -103,6 +103,38 @@ create table merlin.plan_derivation_group (
 comment on table merlin.plan_derivation_group is e''
   'A table for linking externally imported event sources & plans.';
 
+-- Add a trigger verifying that events fit into their sources
+CREATE OR REPLACE FUNCTION merlin.check_event_times()
+ 	RETURNS TRIGGER
+ 	LANGUAGE plpgsql AS
+$func$
+DECLARE
+	source_start timestamp with time zone;
+	source_end timestamp with time zone;
+	event_start timestamp with time zone;
+	event_end timestamp with time zone;
+BEGIN
+  	SELECT start_time INTO source_start FROM merlin.external_source WHERE NEW.source_key = external_source.key AND NEW.derivation_group_name = external_source.derivation_group_name;
+  	SELECT end_time INTO source_end FROM merlin.external_source WHERE NEW.source_key = external_source.key AND NEW.derivation_group_name = external_source.derivation_group_name;
+    event_start := NEW.start_time;
+	event_end := NEW.start_time + NEW.duration;
+	IF event_start < source_start OR event_end < source_start THEN
+		RAISE EXCEPTION 'Event %s out of bounds of source %s', NEW.key, NEW.source_key;
+	END IF;
+	IF event_start > source_end OR event_end > source_end THEN
+		RAISE EXCEPTION 'Event %s out of bounds of source %s', NEW.key, NEW.source_key;
+	END IF;
+	RETURN NULL;
+END;
+$func$;
+
+ALTER FUNCTION merlin.check_event_times()
+    OWNER TO aerie;
+GRANT EXECUTE ON FUNCTION merlin.check_event_times() TO aerie;
+CREATE OR REPLACE TRIGGER check_event_times
+AFTER INSERT ON merlin.external_event
+	FOR EACH ROW EXECUTE FUNCTION merlin.check_event_times();
+
 -- Create a table to track which sources the user has and has not seen added/removed
 create table ui.seen_sources
 (
--- deployment/postgres-init-db/sql/tables/merlin/external_events/external_event.sql
+++ deployment/postgres-init-db/sql/tables/merlin/external_events/external_event.sql
@@ -20,3 +20,35 @@ create table merlin.external_event (
 
 comment on table merlin.external_event is e''
   'A table for externally imported events.';
+
+-- Add a trigger verifying that events fit into their sources
+CREATE OR REPLACE FUNCTION merlin.check_event_times()
+ 	RETURNS TRIGGER
+ 	LANGUAGE plpgsql AS
+$func$
+DECLARE
+	source_start timestamp with time zone;
+	source_end timestamp with time zone;
+	event_start timestamp with time zone;
+	event_end timestamp with time zone;
+BEGIN
+  	SELECT start_time INTO source_start FROM merlin.external_source WHERE NEW.source_key = external_source.key AND NEW.derivation_group_name = external_source.derivation_group_name;
+  	SELECT end_time INTO source_end FROM merlin.external_source WHERE NEW.source_key = external_source.key AND NEW.derivation_group_name = external_source.derivation_group_name;
+    event_start := NEW.start_time;
+	event_end := NEW.start_time + NEW.duration;
+	IF event_start < source_start OR event_end < source_start THEN
+		RAISE EXCEPTION 'Event %s out of bounds of source %s', NEW.key, NEW.source_key;
+	END IF;
+	IF event_start > source_end OR event_end > source_end THEN
+		RAISE EXCEPTION 'Event %s out of bounds of source %s', NEW.key, NEW.source_key;
+	END IF;
+	RETURN NULL;
+END;
+$func$;
+
+ALTER FUNCTION merlin.check_event_times()
+    OWNER TO aerie;
+GRANT EXECUTE ON FUNCTION merlin.check_event_times() TO aerie;
+CREATE OR REPLACE TRIGGER check_event_times
+AFTER INSERT ON merlin.external_event
+	FOR EACH ROW EXECUTE FUNCTION merlin.check_event_times();
