cd1e4193f4b052cb447de744cb0e426afea223b2
pranav-super <pranav.subramanian@colorado.edu>
2024-08-26 13:06:49 -0700

    finish testing
    add duration to derived_events (helpful quantity to include without need for cross-referencing), change check on end time for source to be > start time, not >=

--- deployment/postgres-init-db/sql/tables/merlin/external_events/external_source.sql
+++ deployment/postgres-init-db/sql/tables/merlin/external_events/external_source.sql
@@ -6,7 +6,7 @@ create table merlin.external_source (
     valid_at timestamp with time zone not null,
     start_time timestamp with time zone not null,
     end_time timestamp with time zone not null,
-    CHECK (end_time >= start_time),
+    CHECK (end_time > start_time),
     created_at timestamp with time zone default now() not null,
     metadata jsonb,
 
