537f26b04f4aaf3f1f6e31449e53ebc187b25d0c
pranav-super <pranav.subramanian@colorado.edu>
2024-08-28 10:29:34 -0700

    allow derivation_group_comp to view empty dg's

--- deployment/postgres-init-db/sql/views/ui/derivation_group_comp.sql
+++ deployment/postgres-init-db/sql/views/ui/derivation_group_comp.sql
@@ -1,47 +1,34 @@
 -- create a view that aggregates additional derivation group information
-create or replace view merlin.derivation_group_comp
- as
+create view merlin.derivation_group_comp
+  as
 select
-	name,
-	source_type_name,
-	sources,
-	array_agg(distinct event_type) as event_types,
-	derived_total
-	from (
-		select
-			name,
-			source_type_name,
-			array_agg(distinct concat(sources.source_key, ', ', sources.derivation_group_name, ', ', sources.contained_events)) as sources,
-			UNNEST(event_types) as event_type,
-			count(distinct c.event_key) as derived_total
-			from merlin.derivation_group
-			join (
-				select
-					source_key,
-					derivation_group_name,
-					count(key) as contained_events
-				from merlin.external_event
-				group by source_key, derivation_group_name
-				order by source_key
-			) sources on sources.derivation_group_name = derivation_group.name
-			join (
-				select
-					source_key,
-					derivation_group_name,
-					array_agg(distinct event_type_name) as event_types
-				from merlin.external_event
-				group by source_key, derivation_group_name
-				order by source_key
-			) types on types.derivation_group_name = derivation_group.name
-			left join (
-				select
-					derived_events.event_key,
-					derived_events.derivation_group_name
-			  	from merlin.derived_events
-			) c on c.derivation_group_name = derivation_group.name
-			group by name, source_type_name, event_types
-	)
-	group by name, source_type_name, sources, derived_total;
+  name,
+  source_type_name,
+  array_agg(distinct concat(sources.source_key, ', ', sources.derivation_group_name, ', ', sources.contained_events)) as sources,
+  array_remove(array_agg(distinct event_type_name), null) as event_types,
+	count(distinct counted.event_key) as derived_total
+from (
+	select derivation_group.name,
+		     derivation_group.source_type_name,
+		     types.event_type_name
+  from merlin.derivation_group
+	left outer join ( select external_event.source_key,
+	                         external_event.derivation_group_name,
+	                         external_event.event_type_name
+			              from merlin.external_event
+			              order by external_event.source_key) types on types.derivation_group_name = derivation_group.name
+	group by derivation_group.name, derivation_group.source_type_name, types.event_type_name
+) with_event_types
+full outer join ( select external_event.source_key,
+                         external_event.derivation_group_name,
+                         count(external_event.key) as contained_events
+                  from merlin.external_event
+                  group by external_event.source_key, external_event.derivation_group_name
+                  order by external_event.source_key) sources on sources.derivation_group_name = with_event_types.name
+full outer join ( select derived_events.event_key,
+				                 derived_events.derivation_group_name
+			            from merlin.derived_events) counted on counted.derivation_group_name = with_event_types.name
+group by with_event_types.name, with_event_types.source_type_name;
 
 comment on view  merlin.derivation_group_comp is e''
-  'A view detailing all relevant information for derivation groups. This was created as we wanted all of this information, but had many heavyweight subscriptions and queries to get this desired result. As such, a new view was created to lighten the load.';
+  'A view detailing all relevant information for derivation groups. This was created as we wanted all of this information, but had many heavyweight subscriptions and queries to get this desired result. as such, a new view was created to lighten the load.';
