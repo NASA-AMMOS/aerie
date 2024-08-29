e95fdd1f208df8ff78035eb2abebe6aa3901c1c0
JoelCourtney <joel.e.courtney@gmail.com>
2024-08-29 16:13:25 -0700

    Rename Rule to Goal

--- procedural/scheduling/src/main/kotlin/gov/nasa/ammos/aerie/procedural/scheduling/Goal.kt
+++ procedural/scheduling/src/main/kotlin/gov/nasa/ammos/aerie/procedural/scheduling/Goal.kt
@@ -3,7 +3,7 @@ package gov.nasa.ammos.aerie.procedural.scheduling
 import gov.nasa.ammos.aerie.procedural.scheduling.plan.EditablePlan
 
 /** The interface that all scheduling rules must satisfy. */
-interface Rule {
+interface Goal {
   /**
    * Run the rule.
    *
