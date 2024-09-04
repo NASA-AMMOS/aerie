6186dbe2ff5c2b9e9f976c33729166eb9b5e4dc1
JoelCourtney <joel.e.courtney@gmail.com>
2024-09-04 14:49:36 -0700

    Better deserializer for constants

--- procedural/timeline/src/main/kotlin/gov/nasa/ammos/aerie/procedural/timeline/collections/profiles/Constants.kt
+++ procedural/timeline/src/main/kotlin/gov/nasa/ammos/aerie/procedural/timeline/collections/profiles/Constants.kt
@@ -6,13 +6,12 @@ import gov.nasa.ammos.aerie.procedural.timeline.BaseTimeline
 import gov.nasa.ammos.aerie.procedural.timeline.Interval
 import gov.nasa.ammos.aerie.procedural.timeline.Timeline
 import gov.nasa.ammos.aerie.procedural.timeline.ops.SerialConstantOps
-import gov.nasa.ammos.aerie.procedural.timeline.ops.coalesce.CoalesceSegmentsOp
 import gov.nasa.ammos.aerie.procedural.timeline.util.preprocessList
 
 /** A profile of piece-wise constant values. */
 data class Constants<V: Any>(private val timeline: Timeline<Segment<V>, Constants<V>>):
-    Timeline<Segment<V>, Constants<V>> by timeline,
-    SerialConstantOps<V, Constants<V>>
+  Timeline<Segment<V>, Constants<V>> by timeline,
+  SerialConstantOps<V, Constants<V>>
 {
   constructor(v: V): this(Segment(Interval.MIN_MAX, v))
   constructor(vararg segments: Segment<V>): this(segments.asList())
@@ -20,11 +19,11 @@ data class Constants<V: Any>(private val timeline: Timeline<Segment<V>, Constant
 
   /***/ companion object {
     /**
-     * Delegates to the constructor, for use with [gov.nasa.ammos.aerie.procedural.timeline.plan.Plan.resource].
-     *
-     * Does not convert the serialized values because it does not know what it should be converted into.
-     * You will need to do that yourself using [mapValues].
+     * Returns a deserializer, for use with [gov.nasa.ammos.aerie.procedural.timeline.plan.Plan.resource].
      */
-    @JvmStatic fun deserialize(list: List<Segment<SerializedValue>>) = Constants(list)
+    @JvmStatic fun <V: Any> deserialize(mapper: (Segment<SerializedValue>) -> V): (List<Segment<SerializedValue>>) -> Constants<V> = {
+        l: List<Segment<SerializedValue>> ->
+      Constants(l.map { segment -> segment.mapValue(mapper) })
+    }
   }
 }
