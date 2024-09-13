667104fb3be0bdd4286ec0fb94eb68baa9f2f2eb
JoelCourtney <joel.e.courtney@gmail.com>
2024-09-13 14:59:37 -0700

    Return SerialSegmentOps instead of CoalesceSegmentsOp
    They are effectively the same requirement, but SerialSegmentOps refers to what the timeline IS and CoalesceSegmentsOp refers to what the timeline DOES. "is" is more appropriate in this case

--- procedural/constraints/src/test/kotlin/gov/nasa/ammos/aerie/procedural/constraints/NotImplementedSimulationResults.kt
+++ procedural/constraints/src/test/kotlin/gov/nasa/ammos/aerie/procedural/constraints/NotImplementedSimulationResults.kt
@@ -2,7 +2,7 @@ package gov.nasa.ammos.aerie.procedural.constraints
 
 import gov.nasa.ammos.aerie.procedural.timeline.Interval
 import gov.nasa.ammos.aerie.procedural.timeline.collections.Instances
-import gov.nasa.ammos.aerie.procedural.timeline.ops.coalesce.CoalesceSegmentsOp
+import gov.nasa.ammos.aerie.procedural.timeline.ops.SerialSegmentOps
 import gov.nasa.ammos.aerie.procedural.timeline.payloads.Segment
 import gov.nasa.ammos.aerie.procedural.timeline.plan.SimulationResults
 import gov.nasa.jpl.aerie.merlin.protocol.types.SerializedValue
@@ -10,7 +10,7 @@ import gov.nasa.jpl.aerie.merlin.protocol.types.SerializedValue
 open class NotImplementedSimulationResults: SimulationResults {
   override fun isStale(): Boolean = TODO()
   override fun simBounds(): Interval = TODO()
-  override fun <V : Any, TL : CoalesceSegmentsOp<V, TL>> resource(
+  override fun <V : Any, TL: SerialSegmentOps<V, TL>> resource(
     name: String,
     deserializer: (List<Segment<SerializedValue>>) -> TL
   ): TL = TODO()
