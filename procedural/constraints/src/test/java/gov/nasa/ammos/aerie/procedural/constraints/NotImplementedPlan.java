7526fe0165ba158ba07b3d6a99c5a3a601a04e2e
JoelCourtney <joel.e.courtney@gmail.com>
2024-09-25 15:11:54 -0700

    Test jvm default methods

---
+++ procedural/constraints/src/test/java/gov/nasa/ammos/aerie/procedural/constraints/NotImplementedPlan.java
@@ -0,0 +1,42 @@
+package gov.nasa.ammos.aerie.procedural.constraints;
+
+import gov.nasa.ammos.aerie.procedural.timeline.Interval;
+import gov.nasa.ammos.aerie.procedural.timeline.collections.Directives;
+import gov.nasa.ammos.aerie.procedural.timeline.plan.Plan;
+import gov.nasa.jpl.aerie.merlin.protocol.types.Duration;
+import gov.nasa.jpl.aerie.merlin.protocol.types.SerializedValue;
+import kotlin.NotImplementedError;
+import kotlin.jvm.functions.Function1;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.time.Instant;
+
+public class NotImplementedPlan implements Plan {
+  @NotNull
+  @Override
+  public Interval totalBounds() {
+    throw new NotImplementedError();
+  }
+
+  @NotNull
+  @Override
+  public Duration toRelative(@NotNull final Instant abs) {
+    throw new NotImplementedError();
+  }
+
+  @NotNull
+  @Override
+  public Instant toAbsolute(@NotNull final Duration rel) {
+    throw new NotImplementedError();
+  }
+
+  @NotNull
+  @Override
+  public <A> Directives<A> directives(
+      @Nullable final String type,
+      @NotNull final Function1<? super SerializedValue, ? extends A> deserializer)
+  {
+    throw new NotImplementedError();
+  }
+}
