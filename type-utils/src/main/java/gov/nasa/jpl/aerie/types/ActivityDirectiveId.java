0fe0a1d68923ba0a5fab7f2b0b485e16141397cf
Brad Clement <bclement@jpl.nasa.gov>
2024-09-26 16:01:57 -0700

    make ActivityDirectiveId Comparable again

--- type-utils/src/main/java/gov/nasa/jpl/aerie/types/ActivityDirectiveId.java
+++ type-utils/src/main/java/gov/nasa/jpl/aerie/types/ActivityDirectiveId.java
@@ -1,3 +1,8 @@
 package gov.nasa.jpl.aerie.types;
 
-public record ActivityDirectiveId(long id) implements ActivityId {}
+public record ActivityDirectiveId(long id) implements Comparable<ActivityDirectiveId> {
+  @Override
+  public int compareTo(final ActivityDirectiveId o) {
+    return Long.compare(this.id, o.id);
+  }
+}
