0f2c537f136ae3273ac1f3d1f60c5fc9cfeaac61
pranav-super <pranav.subramanian@colorado.edu>
2024-10-04 10:37:10 -0700

    correct up.sql with external_event; update seen_sources.yaml
    SQUASH WITH PREVIOUS COMMIT.

--- deployment/hasura/migrations/Aerie/11_external_events/up.sql
+++ deployment/hasura/migrations/Aerie/11_external_events/up.sql
@@ -92,6 +92,27 @@ comment on column merlin.external_source.metadata is e''
   'Any metadata or additional data associated with this version that a data originator may have wanted included.\n'
   'Like the ''created_at'' column, this column is used primarily for documentation purposes, and has no associated functionality.';
 
+create table merlin.external_event (
+    key text not null,
+    event_type_name text not null,
+    source_key text not null,
+    derivation_group_name text not null,
+    start_time timestamp with time zone not null,
+    duration interval not null,
+    properties jsonb,
+
+    constraint external_event_pkey
+      primary key (key, source_key, derivation_group_name, event_type_name),
+    constraint external_event_references_source_key_derivation_group
+      foreign key (source_key, derivation_group_name)
+      references merlin.external_source (key, derivation_group_name),
+    constraint external_event_references_event_type_name
+      foreign key (event_type_name)
+      references merlin.external_event_type(name)
+);
+
+comment on table merlin.external_event is e''
+  'Externally imported events.';
 
 comment on column merlin.external_event.key is e''
   'The key, or name, of the external_event.\n'
