3da99493f56e4d06ce66f0f0bbe78d059bc16d30
pranav-super <pranav.subramanian@colorado.edu>
2024-10-04 07:50:21 -0700

    add foreign key to seen_sources.sql

--- deployment/postgres-init-db/sql/tables/ui/seen_sources.sql
+++ deployment/postgres-init-db/sql/tables/ui/seen_sources.sql
@@ -3,13 +3,17 @@ create table ui.seen_sources
     username text not null,
     external_source_name text not null,
     derivation_group text not null,
-    external_source_type text not null, -- included for ease of filtering
+    external_source_type text not null, -- included for ease of filtering in the UI
 
     constraint seen_sources_pkey
       primary key (username, external_source_name, derivation_group),
     constraint seen_sources_references_user
       foreign key (username)
       references permissions.users (username) match simple
+      on delete cascade,
+    constraint seen_sources_references_external_source
+      foreign key (external_source_name, derivation_group)
+      references merlin.external_source (key, derivation_group_name) match simple
       on delete cascade
 );
 
