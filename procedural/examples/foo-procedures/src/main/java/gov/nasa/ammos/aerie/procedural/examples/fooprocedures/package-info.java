e87dea821bb51e7019497a4d03d2829b93258aaa
(skovati) Luke <skovati@protonmail.com>
2024-10-14 13:52:29 -0700

    remove `procedural/examples`
    examples now live in either the `e2e-tests` directory or external repos (aerie-mission-model-template, etc)

--- procedural/examples/foo-procedures/src/main/java/gov/nasa/ammos/aerie/procedural/examples/fooprocedures/package-info.java
+++
@@ -1,5 +0,0 @@
-@WithMappers(BasicValueMappers.class)
-package gov.nasa.ammos.aerie.procedural.examples.fooprocedures;
-
-import gov.nasa.jpl.aerie.contrib.serialization.rulesets.BasicValueMappers;
-import gov.nasa.ammos.aerie.procedural.scheduling.annotations.WithMappers;
