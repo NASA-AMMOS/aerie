d269e788b0dad764c5b983da7f65c733baf20d68
pranav-super <pranav.subramanian@colorado.edu>
2024-10-15 10:45:06 -0700

    add `acknowledged` boolean field to plan_derivation_group.sql
    See: https://github.com/NASA-AMMOS/aerie/pull/1513#discussion_r1800150650

--- deployment/postgres-init-db/sql/tables/merlin/external_events/external_source.sql
+++ deployment/postgres-init-db/sql/tables/merlin/external_events/external_source.sql
@@ -73,3 +73,17 @@ create trigger external_source_pdg_association_delete
 before delete on merlin.external_source
   for each row execute function merlin.external_source_pdg_association_delete();
 
+-- set acknowledged on merlin.plan_derivation_group false for this derivation group as there are new changes
+create function merlin.external_source_pdg_ack_update()
+  returns trigger
+  language plpgsql as $$
+begin
+  update merlin.plan_derivation_group set "acknowledged" = false
+  where plan_derivation_group.derivation_group_name = NEW.derivation_group_name;
+  return new;
+end;
+$$;
+
+create trigger external_source_pdg_ack_update
+after insert on merlin.external_source
+  for each row execute function merlin.external_source_pdg_ack_update();
--- deployment/postgres-init-db/sql/tables/merlin/external_events/plan_derivation_group.sql
+++ deployment/postgres-init-db/sql/tables/merlin/external_events/plan_derivation_group.sql
@@ -2,6 +2,7 @@ create table merlin.plan_derivation_group (
     plan_id integer not null,
     derivation_group_name text not null,
     last_acknowledged_at timestamp with time zone default now() not null,
+    acknowledged boolean not null default true,
 
     constraint plan_derivation_group_pkey
       primary key (plan_id, derivation_group_name),
@@ -26,3 +27,19 @@ comment on column merlin.plan_derivation_group.derivation_group_name is e''
   'The derivation group being associated with the plan.';
 comment on column merlin.plan_derivation_group.last_acknowledged_at is e''
   'The time at which changes to the derivation group were last acknowledged.';
+
+-- update last_acknowledged whenever acknowledged is set to true
+create function merlin.pdg_update_ack_at()
+  returns trigger
+  language plpgsql as $$
+begin
+  if new.acknowledged = true then
+    new.last_acknowledged_at = now();
+  end if;
+  return new;
+end;
+$$;
+
+create trigger pdg_update_ack_at
+before update on merlin.plan_derivation_group
+  for each row execute function merlin.pdg_update_ack_at();
