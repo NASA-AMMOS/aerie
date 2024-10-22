78f20f01b2a0aa1a50bd76f57605f7138b55ea04
JoelCourtney <joel.e.courtney@gmail.com>
2024-10-22 16:00:29 -0700

    Remove unusefull examples

--- procedural/examples/foo-procedures/src/main/java/gov/nasa/ammos/aerie/procedural/examples/fooprocedures/constraints/Hello.java
+++
@@ -1,9 +0,0 @@
-package gov.nasa.ammos.aerie.procedural.examples.fooprocedures.constraints;
-
-import gov.nasa.ammos.aerie.procedural.examples.fooprocedures.Helper;
-
-class Hello {
-  public static void main(String[] args) {
-    System.out.println(Helper.greeting());
-  }
-}
